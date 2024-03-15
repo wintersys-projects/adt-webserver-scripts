d55309a6ad7e10afb81aecea5f2cf95de304883f
wintersys-projects <160187405+wintersys-projects@users.noreply.github.com>
2024-03-15 13:29:31 +0000

    Update Update.sh

--- installscripts/Update.sh
+++ installscripts/Update.sh
@@ -1,31 +1,4 @@
 #!/bin/sh
-###################################################################################
-# Description: This  will perform a software update
-# Date: 18/11/2016
-# Author : Peter Winter
-###################################################################################
-# License Agreement:
-# This file is part of The Agile Deployment Toolkit.
-# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License
-# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
-####################################################################################
-####################################################################################
-#set -x
-
-if ( [ "${1}" != "" ] )
-then
-    buildos="${1}"
-fi
-
-#!/bin/sh
 ######################################################################################################
 # Description: This script will perform a software update
 # Author: Peter Winter
@@ -52,7 +25,7 @@ then
     buildos="${1}"
 fi
 
-if ( [ "`/bin/grep "^PACKAGEMANAGER:*" ${BUILD_HOME}/builddescriptors/buildstylesscp.dat | /usr/bin/awk -F':' '{print $NF}'`" = "apt" ] )
+if ( [ "`${HOME}/providerscripts/utilities/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt" ] )
 then
     if ( [ "${buildos}" = "ubuntu" ] )
     then
@@ -69,7 +42,7 @@ then
     fi
 fi
 
-if ( [ "`/bin/grep "^PACKAGEMANAGER:*" ${BUILD_HOME}/builddescriptors/buildstylesscp.dat | /usr/bin/awk -F':' '{print $NF}'`" = "apt-fast" ] )
+if ( [ "`${HOME}/providerscripts/utilities/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt-fast" ] )
 then
     if ( [ "${buildos}" = "ubuntu" ] )
     then
