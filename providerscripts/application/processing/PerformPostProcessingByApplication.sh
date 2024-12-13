131cbb3b9b351239ad9918ae8e8f98cb04202a5d
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-13 16:41:42 +0000

    Update PerformPostProcessingByApplication.sh

--- providerscripts/application/processing/PerformPostProcessingByApplication.sh
+++ providerscripts/application/processing/PerformPostProcessingByApplication.sh
@@ -30,7 +30,7 @@ then
 	for applicationdir in `/bin/ls -d /home/${SERVER_USER}/providerscripts/application/processing/*/`
 	do
 		applicationname="`/bin/echo ${applicationdir} | /bin/sed 's/\/$//' | /usr/bin/awk -F'/' '{print $NF}'`"
-		if ( [ "`/home/${SERVER_USER}/providerscripts/utilities/CheckConfigValue.sh APPLICATION:${applicationname}`" = "1" ] )
+		if ( [ "`/home/${SERVER_USER}/providerscripts/utilities/config/CheckConfigValue.sh APPLICATION:${applicationname}`" = "1" ] )
 		then
 			. ${applicationdir}PerformPostProcessing.sh
 		fi
