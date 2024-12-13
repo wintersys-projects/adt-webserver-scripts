77b3af3a6c7b7e96d67f88478fb77bbe9dff524c
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-13 16:30:15 +0000

    Update RemoveApplicationBranding.sh

--- providerscripts/application/branding/RemoveApplicationBranding.sh
+++ providerscripts/application/branding/RemoveApplicationBranding.sh
@@ -24,7 +24,7 @@
 for applicationdir in `/bin/ls -d ${HOME}/providerscripts/application/branding/*/`
 do
 	applicationname="`/bin/echo ${applicationdir} | /bin/sed 's/\/$//' | /usr/bin/awk -F'/' '{print $NF}'`"
-	if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh APPLICATION:${applicationname}`" = "1" ] )
+	if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh APPLICATION:${applicationname}`" = "1" ] )
 	then
 		. ${applicationdir}RemoveApplicationBranding.sh
 	fi
