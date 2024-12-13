637b5e0eee1ad185436d9020fbe7ea6c95a29b17
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-13 10:32:24 +0000

    Update PutToDatastore.sh

--- providerscripts/datastore/PutToDatastore.sh
+++ providerscripts/datastore/PutToDatastore.sh
@@ -23,10 +23,10 @@
 file_to_put="$1"
 datastore_to_put_in="$2"
 
-if ( [ "`${HOME}/providerscripts/utilities/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd'`" = "1" ] )
+if ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd'`" = "1" ] )
 then
         datastore_tool="/usr/bin/s3cmd --force --recursive --multipart-chunk-size-mb=5 put "
-elif ( [ "`${HOME}/providerscripts/utilities/CheckBuildStyle.sh 'DATASTORETOOL:s5cmd'`" = "1" ]  )
+elif ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s5cmd'`" = "1" ]  )
 then
         host_base="`/bin/grep host_base /root/.s5cfg | /bin/grep host_base | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
         datastore_tool="/usr/bin/s5cmd --credentials-file /root/.s5cfg --endpoint-url https://${host_base} cp "
