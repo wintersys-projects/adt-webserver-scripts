4baaff96fe138263f2dbe5d82e0a7cfabf2c19c2
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-15 19:12:05 +0000

    Update InitialiseCron.sh

--- cron/InitialiseCron.sh
+++ cron/InitialiseCron.sh
@@ -30,7 +30,7 @@
 /bin/echo "*/1 * * * * export HOME="${HOMEDIR}" && /bin/sleep 30 && ${HOME}/providerscripts/application/configuration/SetApplicationConfiguration.sh" >> /var/spool/cron/crontabs/root
 /bin/echo "*/1 * * * * export HOME="${HOMEDIR}" && /bin/sleep 45 && ${HOME}/providerscripts/application/configuration/SetApplicationConfiguration.sh" >> /var/spool/cron/crontabs/root
 
-/bin/echo "*/1 * * * * export HOME="${HOMEDIR}" && ${HOME}/providerscripts/utilities/status/AcknowledgeBuildCompletion.sh" >> /var/spool/cron/crontabs/root
+#/bin/echo "*/1 * * * * export HOME="${HOMEDIR}" && ${HOME}/providerscripts/utilities/status/AcknowledgeBuildCompletion.sh" >> /var/spool/cron/crontabs/root
 /bin/echo "*/1 * * * * export HOME="${HOMEDIR}" && /bin/sleep 30 && ${HOME}/providerscripts/utilities/processing/UpdateIPs.sh" >> /var/spool/cron/crontabs/root
 /bin/echo "*/1 * * * * export HOME="${HOMEDIR}" && ${HOME}/security/MonitorForNewSSLCertificate.sh" >> /var/spool/cron/crontabs/root
 /bin/echo "*/1 * * * * export HOME="${HOMEDIR}" && ${HOME}/providerscripts/utilities/status/MonitorForOverload.sh" >> /var/spool/cron/crontabs/root
