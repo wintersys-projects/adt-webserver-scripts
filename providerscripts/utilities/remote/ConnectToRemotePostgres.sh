bc024c0f46310447e9d9ba3bf6a5353f18469e91
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-15 20:26:15 +0000

    Update ConnectToRemotePostgres.sh

--- providerscripts/utilities/remote/ConnectToRemotePostgres.sh
+++ providerscripts/utilities/remote/ConnectToRemotePostgres.sh
@@ -34,9 +34,9 @@ raw="$2"
 #DB_P="`command="${SUDO} ${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh credentials/shit 2" && eval ${command}`"
 #DB_U="`command="${SUDO} ${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh credentials/shit 3" && eval ${command}`"
 
-DB_N="`/bin/sed '1q;d' ${HOME}/credentials/shit`"
-DB_P="`/bin/sed '2q;d' ${HOME}/credentials/shit`"
-DB_U="`/bin/sed '3q;d' ${HOME}/credentials/shit`"
+DB_N="`/bin/sed '1q;d' ${HOME}/credentials/db_cred`"
+DB_P="`/bin/sed '2q;d' ${HOME}/credentials/db_cred`"
+DB_U="`/bin/sed '3q;d' ${HOME}/credentials/db_cred`"
 
 if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:DBaaS`" = "1" ] )
 then
