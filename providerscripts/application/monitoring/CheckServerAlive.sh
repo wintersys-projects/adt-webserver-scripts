65b134da32ba8b226f027ec68ae6275dd74f03cc
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-15 20:25:25 +0000

    Update CheckServerAlive.sh

--- providerscripts/application/monitoring/CheckServerAlive.sh
+++ providerscripts/application/monitoring/CheckServerAlive.sh
@@ -32,9 +32,9 @@ fi
 #DB_P="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 2`"
 #DB_U="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 3`"
 
-DB_N="`/bin/sed '1q;d' ${HOME}/credentials/shit`"
-DB_P="`/bin/sed '2q;d' ${HOME}/credentials/shit`"
-DB_U="`/bin/sed '3q;d' ${HOME}/credentials/shit`"
+DB_N="`/bin/sed '1q;d' ${HOME}/credentials/db_cred`"
+DB_P="`/bin/sed '2q;d' ${HOME}/credentials/db_cred`"
+DB_U="`/bin/sed '3q;d' ${HOME}/credentials/db_cred`"
 
 #if ( [ ! -f ${HOME}/runtime/APPLICATION_CONFIGURATION_PREPARED ] )
 #then
