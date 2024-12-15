d8d250a1a9923441dd7541e4e3ea6d4f3dec2bf0
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-15 19:45:06 +0000

    Update CheckServerAlive.sh

--- providerscripts/utilities/status/CheckServerAlive.sh
+++ providerscripts/utilities/status/CheckServerAlive.sh
@@ -27,9 +27,13 @@ then
 	/bin/echo "ALIVE"
 else
 	
-	DB_N="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 1`"
-	DB_P="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 2`"
-	DB_U="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 3`"
+	#DB_N="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 1`"
+#	DB_P="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 2`"
+#	DB_U="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 3`"
+
+     	DB_N="`/bin/sed '1q;d' ${HOME}/credentials/shit`"
+    	DB_P="`/bin/sed '2q;d' ${HOME}/credentials/shit`"
+    	DB_U="`/bin/sed '3q;d' ${HOME}/credentials/shit`"
 
 	if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:DBaaS`" = "1" ] )
 	then
