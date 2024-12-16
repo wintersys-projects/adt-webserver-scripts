0ee6d172eb6c2f3822de5c15c8ab558220533352
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-16 14:14:34 +0000

    Update ws.sh

--- ws.sh
+++ ws.sh
@@ -220,8 +220,6 @@ fi
 
 ${HOME}/providerscripts/utilities/processing/RunServiceCommand.sh ssh restart
 
-${HOME}/security/SetupFirewall.sh
-
 . ${HOME}/installscripts/InstallCoreSoftware.sh
 
 ${HOME}/security/SetupFirewall.sh
