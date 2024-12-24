f67910146586746f25f51cbe55767e1863deafa1
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-24 13:00:28 +0000

    Update InstallCoreSoftware.sh

--- installscripts/InstallCoreSoftware.sh
+++ installscripts/InstallCoreSoftware.sh
@@ -32,8 +32,6 @@ ${HOME}/installscripts/InstallFirewall.sh ${BUILDOS}
 ${HOME}/installscripts/InstallS3FS.sh ${BUILDOS}
 >&2 /bin/echo "${0} InstallGoofyFS.sh"
 ${HOME}/installscripts/InstallGoofyFS.sh ${BUILDOS}
->&2 /bin/echo "${0} InstallJuiceFS.sh"
-${HOME}/installscripts/InstallJuiceFS.sh ${BUILDOS}
 >&2 /bin/echo "${0} InstallRsync.sh"
 ${HOME}/installscripts/InstallRsync.sh ${BUILDOS}
 >&2 /bin/echo "${0} InstallCron.sh"
