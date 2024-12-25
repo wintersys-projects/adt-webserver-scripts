9194604b35989965286890ae205a1f4c6e1bb680
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-25 16:46:07 +0000

    Update SetApplicationConfiguration.sh

--- providerscripts/application/configuration/drupal/SetApplicationConfiguration.sh
+++ providerscripts/application/configuration/drupal/SetApplicationConfiguration.sh
@@ -20,75 +20,25 @@
 #####################################################################################
 #set -x
 
+
 if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`" = "1" ] )
 then
-	exit
+        exit
 fi
 
-if ( [ ! -f ${HOME}/runtime/DRUPAL_CONFIG_SET ] && [ "`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh drupal_settings.php`" != "" ] )
-then	
-	${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh drupal_settings.php ${HOME}/runtime/drupal_settings.php
- 	if ( [ -f /var/www/html/sites/default/settings.php ] )
-  	then
-   		/bin/rm /var/www/html/sites/default/settings.php
-	fi
- 	/bin/cp ${HOME}/runtime/drupal_settings.php /var/www/html/sites/default/settings.php
-  	/bin/touch ${HOME}/runtime/DRUPAL_CONFIG_SET
-fi
+diff="`/usr/bin/diff /var/www/html/sites/default/settings.php ${HOME}/runtime/drupal_settings.php`"
 
-exit
-
-if ( [ "`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh drupal_settings.php`" != "" ] )
+if ( ( [ ! -f ${HOME}/runtime/INITIAL_CONFIG_SET ] || [ "${diff}" != "" ] ) && [ "`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh drupal_settings.php`" != "" ] )
 then
-	${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh drupal_settings.php ${HOME}/runtime/drupal_settings.php
-fi
-
-if ( [ ! -f ${HOME}/runtime/CONFIG_PRIMED ] && [ "`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh drupal_settings.php`" = "" ] )
-then
-	${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh /var/www/html/sites/default/settings.php
-	if ( [ "$?" = "0" ] )
- 	then
-  		/bin/touch ${HOME}/runtime/CONFIG_PRIMED
-	fi
-fi
-
-if ( [ ! -f ${HOME}/runtime/DRUPAL_CONFIG_SET ] && [ "`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh drupal_settings.php`" != "" ] )
-then	
-	${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh drupal_settings.php ${HOME}/runtime/drupal_settings.php
- 	if ( [ -f /var/www/html/sites/default/settings.php ] )
-  	then
-   		/bin/rm /var/www/html/sites/default/settings.php
-	fi
- 	/bin/cp ${HOME}/runtime/drupal_settings.php /var/www/html/sites/default/settings.php
-  	/bin/touch ${HOME}/runtime/DRUPAL_CONFIG_SET
-elif ( [ "`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh drupal_settings.php`" != "" ] )
-then
-	${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh drupal_settings.php ${HOME}/runtime/drupal_settings.php.$$
-	if ( [ "`/usr/bin/diff ${HOME}/runtime/drupal_settings.php.$$ /var/www/html/sites/default/settings.php`" != "" ] )
-	then
-		/bin/cp ${HOME}/runtime/drupal_settings.php.$$ ${HOME}/runtime/drupal_settings.php
-		/bin/mv ${HOME}/runtime/drupal_settings.php.$$ /var/www/html/sites/default/settings.php
-  	else
-   		/bin/rm ${HOME}/runtime/drupal_settings.php.$$
-	fi
-fi
-
-if ( [ -f /var/www/html/sites/default/settings.php ] )
-then
-	/bin/chown www-data:www-data /var/www/html/sites/default/settings.php
-	/bin/chmod 600 /var/www/html/sites/default/settings.php
-fi
-
-if ( [ ! -f ${HOME}/runtime/DB_PREFIX_SET ] )
-then
-	dbprefix="`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh DBPREFIX:*  | /usr/bin/awk -F':' '{print $NF}'`"
-
-	if ( [ "${dbprefix}" = "" ] )
-	then
-		dbprefix="`/bin/cat /var/www/html/dbp.dat`"
-	fi
- 
-	${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh DBPREFIX:${dbprefix}
- 	/bin/touch ${HOME}/runtime/DB_PREFIX_SET
+        ${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh drupal_settings.php ${HOME}/runtime/drupal_settings.php
+        if ( [ "`/usr/bin/diff /var/www/html/sites/default/settings.php ${HOME}/runtime/drupal_settings.php`" != "" ] )
+        then
+                /usr/bin/php -ln ${HOME}/runtime/drupal_settings.php
+                if ( [ "$?" = "0" ] )
+                then
+                        /bin/cp ${HOME}/runtime/drupal_settings.php  /var/www/html/sites/default/settings.php
+                        /bin/touch ${HOME}/runtime/INITIAL_CONFIG_SET
+                fi
+        fi
 fi
 
