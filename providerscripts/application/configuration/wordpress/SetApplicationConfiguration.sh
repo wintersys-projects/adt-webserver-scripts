2b0ec091c70bf77a569d2c21a0181b16782aadfa
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-25 16:52:41 +0000

    Update SetApplicationConfiguration.sh

--- providerscripts/application/configuration/wordpress/SetApplicationConfiguration.sh
+++ providerscripts/application/configuration/wordpress/SetApplicationConfiguration.sh
@@ -22,68 +22,21 @@
 
 if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`" = "1" ] )
 then
-	exit
-fi
-####test
-if ( [ ! -f ${HOME}/runtime/WP_CONFIG_SET ] && [ "`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh wordpress_config.php`" != "" ] )
-then
-	${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh wordpress_config.php ${HOME}/runtime/wordpress_config.php
- 	if ( [ -f /var/www/html/wp-config.php ] )
-  	then
-   		/bin/rm /var/www/html/wp-config.php
-	fi
-	/bin/cp ${HOME}/runtime/wordpress_config.php /var/www/html/wp-config.php
-	/bin/touch ${HOME}/runtime/WP_CONFIG_SET
- fi
- ####test
-
- exit
-
-if ( [ ! -f ${HOME}/runtime/CONFIG_PRIMED ] && [ "`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh wp-config-sample.php`" = "" ] )
-then
-	${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh /var/www/html/wp-config-sample.php
-	if ( [ "$?" = "0" ] )
- 	then
-  		/bin/touch ${HOME}/runtime/CONFIG_PRIMED
-	fi
+        exit
 fi
 
-if ( [ ! -f ${HOME}/runtime/WP_CONFIG_SET ] && [ "`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh wordpress_config.php`" != "" ] )
-then
-	${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh wordpress_config.php ${HOME}/runtime/wordpress_config.php
- 	if ( [ -f /var/www/html/wp-config.php ] )
-  	then
-   		/bin/rm /var/www/html/wp-config.php
-	fi
-	/bin/cp ${HOME}/runtime/wordpress_config.php /var/www/html/wp-config.php
-	/bin/touch ${HOME}/runtime/WP_CONFIG_SET
-elif ( [ "`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh wordpress_config.php`" != "" ] )
-then
-	${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh wordpress_config.php ${HOME}/runtime/wordpress_config.php.$$
-	if ( [ "`/usr/bin/diff ${HOME}/runtime/wordpress_config.php.$$ /var/www/html/wp-config.php`" != "" ] )
-	then
-		/bin/cp ${HOME}/runtime/wordpress_config.php.$$ ${HOME}/runtime/wordpress_config.php
-		/bin/mv ${HOME}/runtime/wordpress_config.php.$$ /var/www/html/wp-config.php
-  	else
-   		/bin/rm ${HOME}/runtime/wordpress_config.php.$$
-	fi
-fi
+diff="`/usr/bin/diff /var/www/html/wp-config.php ${HOME}/runtime/wordpress_config.php`"
 
-if ( [ -f /var/www/html/wp-config.php ] )
+if ( ( [ ! -f ${HOME}/runtime/INITIAL_CONFIG_SET ] || [ "${diff}" != "" ] ) && [ "`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh wordpress_config.php`" != "" ] )
 then
-	/bin/chown www-data:www-data /var/www/html/wp-config.php
-	/bin/chmod 600 /var/www/html/wp-config.php
-fi
-
-if ( [ ! -f ${HOME}/runtime/DB_PREFIX_SET ] )
-then
-	dbprefix="`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh DBPREFIX:*  | /usr/bin/awk -F':' '{print $NF}'`"
-
-	if ( [ "${dbprefix}" = "" ] )
-	then
-		dbprefix="`/bin/cat /var/www/html/dbp.dat`"
-	fi
- 
-	${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh DBPREFIX:${dbprefix}
- 	/bin/touch ${HOME}/runtime/DB_PREFIX_SET
+        ${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh wordpress_config.php ${HOME}/runtime/wordpress_config.php
+        if ( [ "`/usr/bin/diff /var/www/html/wp-config.php  ${HOME}/runtime/wordpress_config.php`" != "" ] )
+        then
+                /usr/bin/php -ln  ${HOME}/runtime/wordpress_config.php
+                if ( [ "$?" = "0" ] )
+                then
+                        /bin/cp  ${HOME}/runtime/wordpress_config.php  /var/www/html/wp-config.php
+                        /bin/touch ${HOME}/runtime/INITIAL_CONFIG_SET
+                fi
+        fi
 fi
