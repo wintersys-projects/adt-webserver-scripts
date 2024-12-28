87e769c57f03ffc49b597fbd2329540ff7d8996e
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-28 19:42:50 +0000

    Update InstallNetworkManager.sh

--- installscripts/InstallNetworkManager.sh
+++ installscripts/InstallNetworkManager.sh
@@ -38,22 +38,16 @@ if ( [ "${apt}" != "" ] )
 then
     if ( [ "${buildos}" = "ubuntu" ] )
     then
-        if ( [ -f ${HOME}/rutime/APT-SINGLE ] )
-     	then
-		    /bin/echo " network-manager" >> ${HOME}/runtime/apt-install-list.dat
-    	else
+
             DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1  -qq -y install network-manager   
-        fi
+        
     fi
 
     if ( [ "${buildos}" = "debian" ] )
     then
-        if ( [ -f ${HOME}/rutime/APT-SINGLE ] )
-     	then
-		    /bin/echo " network-manager" >> ${HOME}/runtime/apt-install-list.dat
-    	else
+
             DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1  -qq -y install network-manager
-        fi
+        
     fi
     /bin/touch ${HOME}/runtime/installedsoftware/InstallNetworkManager.sh				
 fi
