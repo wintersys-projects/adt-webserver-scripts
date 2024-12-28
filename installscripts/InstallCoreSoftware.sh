32da7e8ef24bd2e639f43f8ca4e64dfc9e0d119d
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-28 14:37:03 +0000

    Update InstallCoreSoftware.sh

--- installscripts/InstallCoreSoftware.sh
+++ installscripts/InstallCoreSoftware.sh
@@ -7,66 +7,45 @@ pids=""
 >&2 /bin/echo "${0} UpdateAndUpgrade.sh"
 ${HOME}/installscripts/UpdateAndUpgrade.sh ${BUILDOS}
 >&2 /bin/echo "${0} InstallGo.sh"
-${HOME}/installscripts/InstallGo.sh ${BUILDOS} &
-pids="${pids} $!"
+${HOME}/installscripts/InstallGo.sh ${BUILDOS} 
 >&2 /bin/echo "${0} InstallCurl.sh"
-${HOME}/installscripts/InstallCurl.sh ${BUILDOS} &
-pids="${pids} $!"
+${HOME}/installscripts/InstallCurl.sh ${BUILDOS} 
 >&2 /bin/echo "${0} InstallLibioSocketSSL.sh"
-${HOME}/installscripts/InstallLibioSocketSSL.sh ${BUILDOS} &
-pids="${pids} $!"
+${HOME}/installscripts/InstallLibioSocketSSL.sh ${BUILDOS} 
 >&2 /bin/echo "${0} InstallLibnetSSLLeay.sh"
-${HOME}/installscripts/InstallLibnetSSLLeay.sh ${BUILDOS} &
-pids="${pids} $!"
+${HOME}/installscripts/InstallLibnetSSLLeay.sh ${BUILDOS} 
 >&2 /bin/echo "${0} InstallSendEmail.sh"
-${HOME}/installscripts/InstallSendEmail.sh ${BUILDOS} &
-pids="${pids} $!"
+${HOME}/installscripts/InstallSendEmail.sh ${BUILDOS} 
 >&2 /bin/echo "${0} InstallNetworkManager.sh"
-${HOME}/installscripts/InstallNetworkManager.sh ${BUILDOS} &
-pids="${pids} $!"
+${HOME}/installscripts/InstallNetworkManager.sh ${BUILDOS} 
 >&2 /bin/echo "${0} InstallJQ.sh"
-${HOME}/installscripts/InstallJQ.sh ${BUILDOS} &
-pids="${pids} $!"
+${HOME}/installscripts/InstallJQ.sh ${BUILDOS} 
 >&2 /bin/echo "${0} InstallUnzip.sh"
-${HOME}/installscripts/InstallUnzip.sh ${BUILDOS} &
-pids="${pids} $!"
+${HOME}/installscripts/InstallUnzip.sh ${BUILDOS} 
 >&2 /bin/echo "${0} InstallSSHPass.sh"
-${HOME}/installscripts/InstallSSHPass.sh ${BUILDOS} &
-pids="${pids} $!"
+${HOME}/installscripts/InstallSSHPass.sh ${BUILDOS} 
 >&2 /bin/echo "${0} InstallSysStat.sh"
-${HOME}/installscripts/InstallSysStat.sh ${BUILDOS} &
-pids="${pids} $!"
+${HOME}/installscripts/InstallSysStat.sh ${BUILDOS} 
 >&2 /bin/echo "${0} InstallFirewall.sh"
-${HOME}/installscripts/InstallFirewall.sh ${BUILDOS} &
-pids="${pids} $!"
+${HOME}/installscripts/InstallFirewall.sh ${BUILDOS} 
 >&2 /bin/echo "${0} InstallDatastoreTools.sh"
-${HOME}/installscripts/InstallDatastoreTools.sh ${BUILDOS} &
-pids="${pids} $!"
+${HOME}/installscripts/InstallDatastoreTools.sh ${BUILDOS} 
 >&2 /bin/echo "${0} InstallDatabaseClient.sh"
-${HOME}/installscripts/InstallDatabaseClient.sh  ${BUILDOS} &
-pids="${pids} $!"
+${HOME}/installscripts/InstallDatabaseClient.sh  ${BUILDOS} 
 >&2 /bin/echo "${0} InstallRsync.sh"
-${HOME}/installscripts/InstallRsync.sh ${BUILDOS} &
-pids="${pids} $!"
+${HOME}/installscripts/InstallRsync.sh ${BUILDOS} 
 >&2 /bin/echo "${0} InstallCron.sh"
-${HOME}/installscripts/InstallCron.sh ${BUILDOS} &
-pids="${pids} $!"
+${HOME}/installscripts/InstallCron.sh ${BUILDOS} 
 
-${HOME}/installscripts/InstallMonitoringGear.sh &
-pids="${pids} $!"
+${HOME}/installscripts/InstallMonitoringGear.sh 
 >&2 /bin/echo "${0} Installing Datastore tools"
-/bin/echo "${0} Installing Datastore tools" >> ${HOME}/logs/initialbuild/BUILD_PROCESS_MONITORING.log
-. ${HOME}/installscripts/InstallDatastoreTools.sh &
-pids="${pids} $!"
+. ${HOME}/installscripts/InstallDatastoreTools.sh 
+
 # Install the language engine for whatever language your application is written in
 >&2 /bin/echo "${0} Installing Application Language"
-/bin/echo "${0} Installing Application Language: ${APPLICATION_LANGUAGE}" >> ${HOME}/logs/initialbuild/BUILD_PROCESS_MONITORING.log
-${HOME}/installscripts/InstallApplicationLanguage.sh "${APPLICATION_LANGUAGE}" &
-#pids="${pids} $!"
+${HOME}/installscripts/InstallApplicationLanguage.sh "${APPLICATION_LANGUAGE}" 
 >&2 /bin/echo "${0} Installing Webserver"
-/bin/echo "${0} Installing Webserver: ${WEBSERVER_CHOICE} for ${WEBSITE_NAME} at: ${WEBSITE_URL}" >> ${HOME}/logs/initialbuild/BUILD_PROCESS_MONITORING.log
-${HOME}/providerscripts/webserver/InstallWebserver.sh  &
-pids="${pids} $!"
+${HOME}/providerscripts/webserver/InstallWebserver.sh  
 
 for pid in ${pids}
 do
