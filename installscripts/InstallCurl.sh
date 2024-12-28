d2aaf0214d82d7313053d286d1dc81587633a7b5
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-28 19:39:10 +0000

    Update InstallCurl.sh

--- installscripts/InstallCurl.sh
+++ installscripts/InstallCurl.sh
@@ -37,22 +37,16 @@ if ( [ "${apt}" != "" ] )
 then
 	if ( [ "${buildos}" = "ubuntu" ] )
 	then
-     		if ( [ -f ${HOME}/rutime/APT-SINGLE ] )
-     		then
-			/bin/echo " curl" >> ${HOME}/runtime/apt-install-list.dat
-    		else
+
 			DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -qq install curl	#####UBUNTU-CURL-REPO#####
-		fi
+		
  	fi
 
 	if ( [ "${buildos}" = "debian" ] )
 	then
-     		if ( [ -f ${HOME}/rutime/APT-SINGLE ] )
-     		then
-			/bin/echo " curl" >> ${HOME}/runtime/apt-install-list.dat
-    		else
+
 			DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -qq install curl	#####DEBIAN-CURL-REPO#####
-		fi
+		
  	fi
  	/bin/touch ${HOME}/runtime/installedsoftware/InstallCurl.sh	
 fi
