7fa7bb038926a88f2096486a179aca93e484a997
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-28 20:34:02 +0000

    Update Update.sh

--- installscripts/Update.sh
+++ installscripts/Update.sh
@@ -67,6 +67,7 @@ then
                 /usr/bin/snap install aria2c  
 		mirrors="`/bin/grep "^deb" /etc/apt/sources.list | /bin/grep -Po 'http.* ' | /usr/bin/awk '{print $1}' | /usr/bin/sort -u | /usr/bin/uniq | /usr/bin/tr '\n' ',' | /bin/sed -e 's/,$//' -e 's#/,#,#' -e 's,/$,,'`" 
         	/bin/echo "MIRRORS=( '${mirrors}' )" >> /etc/apt-fast.conf
+	   	/bin/echo 'DOWNLOADBELOW="aria2c -c -s ${_MAXNUM} -x ${_MAXNUM} -k 1M -q --file-allocation=none"' >> /etc/apt-fast.conf
 	fi
 	
 	if ( [ "${buildos}" = "debian" ] )
@@ -93,6 +94,7 @@ then
                 /usr/bin/snap install aria2c  
 		mirrors="`/bin/grep "^deb" /etc/apt/sources.list | /bin/grep -Po 'http.* ' | /usr/bin/awk '{print $1}' | /usr/bin/sort -u | /usr/bin/uniq | /usr/bin/tr '\n' ',' | /bin/sed 's/,$//'`" 
 		/bin/echo "MIRRORS=( '${mirrors}' )" >> /etc/apt-fast.conf
+  		/bin/echo 'DOWNLOADBELOW="aria2c -c -s ${_MAXNUM} -x ${_MAXNUM} -k 1M -q --file-allocation=none"' >> /etc/apt-fast.conf
 	fi   
 fi
 
