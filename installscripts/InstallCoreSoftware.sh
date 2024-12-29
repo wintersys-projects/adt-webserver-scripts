5d73498d7629b1392ff6944263d5d522ee5fab22
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-29 09:39:18 +0000

    Update InstallCoreSoftware.sh

--- installscripts/InstallCoreSoftware.sh
+++ installscripts/InstallCoreSoftware.sh
@@ -7,42 +7,51 @@ fi
 BUILDOS="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
 APPLICATION_LANGUAGE="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'APPLICATIONLANGUAGE'`"
 
+>&2 /bin/echo "${0} InstallNetworkManager.sh"
+${HOME}/installscripts/InstallNetworkManager.sh ${BUILDOS} 
+
+>&2 /bin/echo "${0} InstallFirewall.sh"
+${HOME}/installscripts/InstallFirewall.sh ${BUILDOS} 
+
+>&2 /bin/echo "${0} InstallDatastoreTools.sh"
+${HOME}/installscripts/InstallDatastoreTools.sh ${BUILDOS} 
+
 >&2 /bin/echo "${0} UpdateAndUpgrade.sh"
 ${HOME}/installscripts/UpdateAndUpgrade.sh ${BUILDOS}
 
-if ( [ "${1}" = "preinstall" ] )
-then
-  scripts="`/bin/cat ${HOME}/installscripts/InstallCoreSoftware.sh | /bin/grep BUILDOS | /bin/grep -v "Up.*" | /usr/bin/awk '{print $1}'`"
-  
-  package_names=""
+#if ( [ "${1}" = "preinstall" ] )
+#then
+ # scripts="`/bin/cat ${HOME}/installscripts/InstallCoreSoftware.sh | /bin/grep BUILDOS | /bin/grep -v "Up.*" | /usr/bin/awk '{print $1}'`"
+ # 
+ # package_names=""
+#
+ # for script in ${scripts}
+ # do
+ #       script="`/bin/echo ${script} | /bin/sed -e 's,\${HOME},'${HOME}',g'`"
+ #       package_names="${package_names} `/bin/cat ${script} | /bin/grep DEBIAN_FRONTEND | /usr/bin/awk '{print $8}' | /usr/bin/sort -u | /usr/bin/uniq | /usr/bin/tr '\n' ' '`"
+ # done
 
-  for script in ${scripts}
-  do
-        script="`/bin/echo ${script} | /bin/sed -e 's,\${HOME},'${HOME}',g'`"
-        package_names="${package_names} `/bin/cat ${script} | /bin/grep DEBIAN_FRONTEND | /usr/bin/awk '{print $8}' | /usr/bin/sort -u | /usr/bin/uniq | /usr/bin/tr '\n' ' '`"
-  done
+#  apt=""
+#  if ( [ "`${HOME}/providerscripts/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt" ] )
+#  then
+#        apt="/usr/bin/apt-get"
+#  elif ( [ "`${HOME}/providerscripts/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt-fast" ] )
+#  then
+#        apt="/usr/sbin/apt-fast"
+#  fi
 
-  apt=""
-  if ( [ "`${HOME}/providerscripts/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt" ] )
-  then
-        apt="/usr/bin/apt-get"
-  elif ( [ "`${HOME}/providerscripts/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt-fast" ] )
-  then
-        apt="/usr/sbin/apt-fast"
-  fi
-
-  if ( [ "${apt}" != "" ] )
-  then
-        if ( [ "${BUILDOS}" = "ubuntu" ] )
-        then
-                DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -qq -y install ${package_names}
-        fi
-        if ( [ "${BUILDOS}" = "debian" ] )
-        then
-                DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -qq -y install ${package_names}
-        fi
-  fi
-fi
+ # if ( [ "${apt}" != "" ] )
+ # then
+ #       if ( [ "${BUILDOS}" = "ubuntu" ] )
+ #       then
+ #               DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -qq -y install ${package_names}
+ #       fi
+ #       if ( [ "${BUILDOS}" = "debian" ] )
+ #       then
+ #               DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -qq -y install ${package_names}
+ #       fi
+ # fi
+#fi
 
 >&2 /bin/echo "${0} InstallGo.sh"
 ${HOME}/installscripts/InstallGo.sh ${BUILDOS} 
@@ -54,8 +63,7 @@ ${HOME}/installscripts/InstallLibioSocketSSL.sh ${BUILDOS}
 ${HOME}/installscripts/InstallLibnetSSLLeay.sh ${BUILDOS} 
 >&2 /bin/echo "${0} InstallSendEmail.sh"
 ${HOME}/installscripts/InstallSendEmail.sh ${BUILDOS} 
->&2 /bin/echo "${0} InstallNetworkManager.sh"
-${HOME}/installscripts/InstallNetworkManager.sh ${BUILDOS} 
+
 >&2 /bin/echo "${0} InstallJQ.sh"
 ${HOME}/installscripts/InstallJQ.sh ${BUILDOS} 
 >&2 /bin/echo "${0} InstallUnzip.sh"
@@ -64,10 +72,8 @@ ${HOME}/installscripts/InstallUnzip.sh ${BUILDOS}
 ${HOME}/installscripts/InstallSSHPass.sh ${BUILDOS} 
 >&2 /bin/echo "${0} InstallSysStat.sh"
 ${HOME}/installscripts/InstallSysStat.sh ${BUILDOS} 
->&2 /bin/echo "${0} InstallFirewall.sh"
-${HOME}/installscripts/InstallFirewall.sh ${BUILDOS} 
->&2 /bin/echo "${0} InstallDatastoreTools.sh"
-${HOME}/installscripts/InstallDatastoreTools.sh ${BUILDOS} 
+
+
 >&2 /bin/echo "${0} InstallDatabaseClient.sh"
 ${HOME}/installscripts/InstallDatabaseClient.sh  ${BUILDOS} 
 >&2 /bin/echo "${0} InstallRsync.sh"
@@ -75,8 +81,7 @@ ${HOME}/installscripts/InstallRsync.sh ${BUILDOS}
 >&2 /bin/echo "${0} InstallCron.sh"
 ${HOME}/installscripts/InstallCron.sh ${BUILDOS} 
 ${HOME}/installscripts/InstallMonitoringGear.sh 
->&2 /bin/echo "${0} Installing Datastore tools"
-. ${HOME}/installscripts/InstallDatastoreTools.sh 
+
 >&2 /bin/echo "${0} Installing Webserver"
 ${HOME}/providerscripts/webserver/InstallWebserver.sh  
 >&2 /bin/echo "${0} Installing Application Language"
