e0d9423d11d3e00b280b15f90eefadd5ab9817f9
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-30 18:42:18 +0000

    Update InstallCoreSoftware.sh

--- installscripts/InstallCoreSoftware.sh
+++ installscripts/InstallCoreSoftware.sh
@@ -19,6 +19,9 @@ ${HOME}/installscripts/InstallFirewall.sh ${BUILDOS}
 >&2 /bin/echo "${0} InstallDatastoreTools.sh"
 ${HOME}/installscripts/InstallDatastoreTools.sh ${BUILDOS} 
 
+>&2 /bin/echo "${0} Installing Application Language"
+${HOME}/installscripts/InstallApplicationLanguage.sh "${APPLICATION_LANGUAGE}"
+
 >&2 /bin/echo "${0} Installing Webserver"
 WEBSERVER_CHOICE="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'WEBSERVERCHOICE'`"
 if ( ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'NGINX:source'`" = "1" ] && [ "${WEBSERVER_CHOICE}" = "NGINX" ] ) || ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'APACHE:source'`" = "1" ] && [ "${WEBSERVER_CHOICE}" = "APACHE" ] ) || ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'LIGHTTPD:source'`" = "1" ] && [ "${WEBSERVER_CHOICE}" = "LIGHTTPD" ] ) )
@@ -27,9 +30,6 @@ then
 else
   ${HOME}/providerscripts/webserver/InstallWebserver.sh
 fi
-
->&2 /bin/echo "${0} Installing Application Language"
-${HOME}/installscripts/InstallApplicationLanguage.sh "${APPLICATION_LANGUAGE}"
 
 #if ( [ "${1}" = "preinstall" ] )
 #then
