5ef8c7680b5f39a5e5afdb26f8b7bf911cc951b5
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-31 16:57:06 +0000

    Update MonitorFirewall.sh

--- security/MonitorFirewall.sh
+++ security/MonitorFirewall.sh
@@ -32,7 +32,7 @@ then
    exit
 fi
 
-if ( [ "`${HOME}/providerscripts/datastore/configwrapper/CheckConfigDatastore.sh "INSTALLEDSUCCESSFULLY"`" = "0" ] )
+if ( [ "`${HOME}/providerscripts/datastore/configwrapper/CheckConfigDatastore.sh "INSTALLED_SUCCESSFULLY"`" = "0" ] )
 then
 	exit
 fi
