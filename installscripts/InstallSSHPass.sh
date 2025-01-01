1db421d33305d813197e5d33ebd500bfe8b8f3c0
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-01 22:20:50 +0000

    Update InstallSSHPass.sh

--- installscripts/InstallSSHPass.sh
+++ installscripts/InstallSSHPass.sh
@@ -33,17 +33,18 @@ then
 	apt="/usr/sbin/apt-fast"
 fi
 
+install_command="DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -o Dpkg::Use-Pty=0 -qq -y install "
+
 if ( [ "${apt}" != "" ] )
 then
 	if ( [ "${buildos}" = "ubuntu" ] )
 	then
-			DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1  -qq -y install sshpass		#####UBUNTU-SHPASS-REPO#####
-		
+			${install_command} sshpass		
  	fi
 
 	if ( [ "${buildos}" = "debian" ] )
 	then
-			DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1  -qq -y install sshpass		#####DEBIAN-SSHPASS-REPO#####
+			${install_command} sshpass		
  	fi
       	/bin/touch ${HOME}/runtime/installedsoftware/InstallSSHPass.sh			
 fi
