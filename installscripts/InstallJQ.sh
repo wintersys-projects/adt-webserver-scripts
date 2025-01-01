eb7cb71abfc0d997e99d4b32152a9e3e852a2c3c
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-01 21:50:43 +0000

    Update InstallJQ.sh

--- installscripts/InstallJQ.sh
+++ installscripts/InstallJQ.sh
@@ -33,20 +33,18 @@ then
 	apt="/usr/sbin/apt-fast"
 fi
 
+install_command="DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -o Dpkg::Use-Pty=0 -qq -y install " 
+
 if ( [ "${apt}" != "" ] )
 then
 	if ( [ "${buildos}" = "ubuntu" ] )
 	then
-
-			DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -qq -y install jq #####UBUNTU-JQ-REPO#####
-		
+		${install_command} jq 	
  	fi
 
 	if ( [ "${buildos}" = "debian" ] )
 	then
-
-			DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -qq -y install jq  #####DEBIAN-JQ-REPO#####
-		
+		${install_command} jq 		
  	fi
       	/bin/touch ${HOME}/runtime/installedsoftware/InstallJQ.sh			
 fi
