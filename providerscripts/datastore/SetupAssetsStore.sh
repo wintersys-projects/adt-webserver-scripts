ab4f9318fb59a9028b0066992dfe4acb6eb7291c
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-01 14:36:55 +0000

    Update SetupAssetsStore.sh

--- providerscripts/datastore/SetupAssetsStore.sh
+++ providerscripts/datastore/SetupAssetsStore.sh
@@ -161,7 +161,7 @@ do
 			/bin/echo "aws_access_key_id = ${AWSACCESSKEYID}" >> ~/.aws/credentials
 			/bin/echo "aws_secret_access_key = ${AWSSECRETACCESSKEY}" >> ~/.aws/credentials
 
-			/usr/bin/geesefs -o allow_other --endpoint="https://${endpoint}" --uid="${s3fs_uid}" --gid="${s3fs_gid}" --file-mode=0750 ${assetbucket} /var/www/html/${asset_directory}    
+			/usr/sbin/geesefs -o allow_other --endpoint="https://${endpoint}" --uid="${s3fs_uid}" --gid="${s3fs_gid}" --file-mode=0750 ${assetbucket} /var/www/html/${asset_directory}    
                 elif ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:rclone'`" = "1" ] )
                 then
                         /usr/bin/rclone mount --allow-other --dir-cache-time 2000h --poll-interval 10s --vfs-cache-max-age 90h --vfs-cache-mode full --vfs-cache-max-size 20G  --vfs-cache-poll-interval 5m --cache-dir ${HOME}/s3mount_cache s3:${assetbucket} /var/www/html/${asset_directory} &
