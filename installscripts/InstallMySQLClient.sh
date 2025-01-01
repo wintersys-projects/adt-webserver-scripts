35170b4f89fccd054b2085f501895d4a7454fd8a
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-01 23:04:34 +0000

    Update InstallMySQLClient.sh

--- installscripts/InstallMySQLClient.sh
+++ installscripts/InstallMySQLClient.sh
@@ -33,9 +33,9 @@ elif ( [ "`${HOME}/providerscripts/utilities/config/ExtractBuildStyleValues.sh "
 then
 	apt="/usr/sbin/apt-fast"
 fi
-
-update_command="DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -o Dpkg::Use-Pty=0 -qq -y update " 
-install_command="DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -o Dpkg::Use-Pty=0 -qq -y install " 
+export DEBIAN_FRONTEND=noninteractive
+update_command="${apt} -o DPkg::Lock::Timeout=-1 -o Dpkg::Use-Pty=0 -qq -y update " 
+install_command="${apt} -o DPkg::Lock::Timeout=-1 -o Dpkg::Use-Pty=0 -qq -y install " 
 
 if ( [ "${apt}" != "" ] )
 then
