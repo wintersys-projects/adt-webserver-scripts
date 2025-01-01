8aa7f814abab0648727e1c8b84f1df195a30461d
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-01 14:38:29 +0000

    Update InstallApplication.sh

--- providerscripts/application/InstallApplication.sh
+++ providerscripts/application/InstallApplication.sh
@@ -19,7 +19,7 @@
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 #######################################################################################################
 #######################################################################################################
-#set -x
+set -x
 WEBSITE_URL="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
 BUILD_ARCHIVE_CHOICE="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'BUILDARCHIVECHOICE'`"
 WEBSITE_SUBDOMAIN="`/bin/echo ${WEBSITE_URL} | /usr/bin/awk -F'.' '{print $1}'`"
