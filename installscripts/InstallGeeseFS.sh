88d8b57fa18a4966567702bfc78ca426a07f76cf
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-01 11:04:00 +0000

    Update InstallGeeseFS.sh

--- installscripts/InstallGeeseFS.sh
+++ installscripts/InstallGeeseFS.sh
@@ -1,10 +1,80 @@
-cwd="`/usr/bin/pwd`"
-/usr/bin/git clone https://github.com/yandex-cloud/geesefs
-cd geesefs
-/usr/bin/go build
-if ( [ -f ./geesefs ] )
+#!/bin/sh 
+###############################################################################################
+# Description: This will install Geesefs
+# Author: Peter Winter
+# Date: 12/01/2017
+###############################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################################
+################################################################################################
+
+if ( [ "${1}" != "" ] )
 then
-        /bin/cp ./geesefs /usr/sbin
+	buildos="${1}"
 fi
-cd ${cwd}
-/bin/rm -r ./geesefs
+
+apt=""
+if ( [ "`${HOME}/providerscripts/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt" ] )
+then
+	apt="/usr/bin/apt-get"
+elif ( [ "`${HOME}/providerscripts/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt-fast" ] )
+then
+	apt="/usr/sbin/apt-fast"
+fi
+
+if ( [ "${apt}" != "" ] )
+then
+	if ( [ "${buildos}" = "ubuntu" ] )
+	then
+		if ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:geesefs:binary'`" = "1" ] )
+		then
+			/usr/bin/wget https://github.com/kahing/goofys/releases/latest/download/goofys -P /usr/bin	#####UBUNTU-GOOFYS-BINARY#####
+			/bin/chmod 755 /usr/bin/goofys									#####UBUNTU-GOOFYS-BINARY#####
+		elif ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:geesefs:source'`" = "1" ] )
+		then
+                        cwd="`/usr/bin/pwd`"
+                        /usr/bin/git clone https://github.com/yandex-cloud/geesefs
+                        cd geesefs
+                        /usr/bin/go build
+                        if ( [ -f ./geesefs ] )
+                        then
+                                /bin/cp ./geesefs /usr/sbin
+                        fi
+                        cd ${cwd}
+                        /bin/rm -r ./geesefs
+		fi
+	fi
+
+	if ( [ "${buildos}" = "debian" ] )
+	then
+		if ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:geesefs:binary'`" = "1" ] )
+		then
+			/usr/bin/wget https://github.com/kahing/goofys/releases/latest/download/goofys -P /usr/bin	#####UBUNTU-GOOFYS-BINARY#####
+			/bin/chmod 755 /usr/bin/goofys									#####UBUNTU-GOOFYS-BINARY#####
+		elif ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:geesefs:source'`" = "1" ] )
+		then
+                        cwd="`/usr/bin/pwd`"
+                        /usr/bin/git clone https://github.com/yandex-cloud/geesefs
+                        cd geesefs
+                        /usr/bin/go build
+                        if ( [ -f ./geesefs ] )
+                        then
+                                /bin/cp ./geesefs /usr/sbin
+                        fi
+                        cd ${cwd}
+                        /bin/rm -r ./geesefs
+		fi	
+        fi
+	/bin/touch ${HOME}/runtime/installedsoftware/InstallGeeseFS.sh			
+fi
