8bbe4b764bba1cad887dddefff3b46b97ef3e547
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-01 09:48:54 +0000

    Update InstallCoreSoftware.sh

--- installscripts/InstallCoreSoftware.sh
+++ installscripts/InstallCoreSoftware.sh
@@ -73,8 +73,8 @@ ${HOME}/installscripts/InstallCurl.sh ${BUILDOS}
 ${HOME}/installscripts/InstallLibioSocketSSL.sh ${BUILDOS} 
 >&2 /bin/echo "${0} InstallLibnetSSLLeay.sh"
 ${HOME}/installscripts/InstallLibnetSSLLeay.sh ${BUILDOS} 
->&2 /bin/echo "${0} InstallSendEmail.sh"
-${HOME}/installscripts/InstallSendEmail.sh ${BUILDOS} 
+>&2 /bin/echo "${0} InstallEmailUtil.sh"
+${HOME}/installscripts/InstallEmailUtil.sh ${BUILDOS} 
 
 >&2 /bin/echo "${0} InstallJQ.sh"
 ${HOME}/installscripts/InstallJQ.sh ${BUILDOS} 
