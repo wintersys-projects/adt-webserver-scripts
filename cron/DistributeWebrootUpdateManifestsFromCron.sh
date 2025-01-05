a51b9425bcef0193409600c7bac61b2aa96e7152
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-05 15:26:21 +0000

    Update DistributeWebrootUpdateManifestsFromCron.sh

--- cron/DistributeWebrootUpdateManifestsFromCron.sh
+++ cron/DistributeWebrootUpdateManifestsFromCron.sh
@@ -6,17 +6,17 @@ fi
 
 if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh SYNCWEBROOTS:1`" = "1" ] )
 then
-        ${HOME}providerscripts/utilities/housekeeping/DistributeWebrootUpdatesManifests.sh "0" &
+        ${HOME}/providerscripts/utilities/housekeeping/DistributeWebrootUpdatesManifests.sh "0" &
         /bin/sleep 10
-        ${HOME}providerscripts/utilities/housekeeping/DistributeWebrootUpdatesManifests.sh "10" &
+        ${HOME}/providerscripts/utilities/housekeeping/DistributeWebrootUpdatesManifests.sh "10" &
         /bin/sleep 10
-        ${HOME}providerscripts/utilities/housekeeping/DistributeWebrootUpdatesManifests.sh "20" &
+        ${HOME}/providerscripts/utilities/housekeeping/DistributeWebrootUpdatesManifests.sh "20" &
         /bin/sleep 10
-        ${HOME}providerscripts/utilities/housekeeping/DistributeWebrootUpdatesManifests.sh "30" &
+        ${HOME}/providerscripts/utilities/housekeeping/DistributeWebrootUpdatesManifests.sh "30" &
         /bin/sleep 10
-        ${HOME}providerscripts/utilities/housekeeping/DistributeWebrootUpdatesManifests.sh "40" &
+        ${HOME}/providerscripts/utilities/housekeeping/DistributeWebrootUpdatesManifests.sh "40" &
         /bin/sleep 10
-        ${HOME}providerscripts/utilities/housekeeping/DistributeWebrootUpdatesManifests.sh "50" &
+        ${HOME}/providerscripts/utilities/housekeeping/DistributeWebrootUpdatesManifests.sh "50" &
 fi
 
 
