15f3df10f48a01f577a89e956cc11554acd1ce69
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-05 17:10:15 +0000

    Update SyncRemoteFilesForManifests.sh

--- providerscripts/utilities/housekeeping/SyncRemoteFilesForManifests.sh
+++ providerscripts/utilities/housekeeping/SyncRemoteFilesForManifests.sh
@@ -13,5 +13,15 @@ then
   ${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh failed_webroot_manifest/*-${machine_ip} ${HOME}/runtime/webroot_manifests
 fi
 
-/bin/cat ${HOME}/runtime/webroot_manifests/*incoming*
+/bin/cat *incoming* > aggregate
+
+file_name="hello"
+
+youngest_epoch="`/bin/grep "${file_name}" ./aggregate | /usr/bin/awk -F':' '{print $NF}' | /usr/bin/sort -n | /usr/bin/tail -1`"
+
+chosen_manifest="`/bin/grep "${file_name}:${youngest_epoch}" *incoming* | /usr/bin/awk -F':' '{print $1}'`"
+
+/bin/sed -i "/^${file_name}:/d" *incoming*
+
+/bin/echo "${file_name}:${youngest_epoch}" >> ${chosen_manifest}
 
