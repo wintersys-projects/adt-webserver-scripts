067927d7ca8a78080fd3d974a92840871d6d17d9
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-05 12:10:09 +0000

    Create DistributeWebrootUpdateManifestsFromCron.sh

---
+++ cron/DistributeWebrootUpdateManifestsFromCron.sh
@@ -0,0 +1,14 @@
+if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh SYNCWEBROOTS:1`" != "1" ] )
+then
+        ${HOME}providerscripts/utilities/housekeeping/DistributeWebrootUpdatesManifests.sh &
+        /bin/sleep 10
+        ${HOME}providerscripts/utilities/housekeeping/DistributeWebrootUpdatesManifests.sh &
+        /bin/sleep 10
+        ${HOME}providerscripts/utilities/housekeeping/DistributeWebrootUpdatesManifests.sh &
+        /bin/sleep 10
+        ${HOME}providerscripts/utilities/housekeeping/DistributeWebrootUpdatesManifests.sh &
+        /bin/sleep 10
+        ${HOME}providerscripts/utilities/housekeeping/DistributeWebrootUpdatesManifests.sh &
+        /bin/sleep 10
+        ${HOME}providerscripts/utilities/housekeeping/DistributeWebrootUpdatesManifests.sh &
+fi
