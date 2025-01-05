e9048f8117ee14fc471e64119cc2b3fe55946f9d
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-05 19:22:06 +0000

    Update SyncRemoteFilesForManifests.sh

--- providerscripts/utilities/housekeeping/SyncRemoteFilesForManifests.sh
+++ providerscripts/utilities/housekeeping/SyncRemoteFilesForManifests.sh
@@ -1,4 +1,4 @@
-
+#set -x
 
 
 machine_ip="`${HOME}/providerscripts/utilities/processing/GetIP.sh`"
@@ -13,15 +13,34 @@ then
   ${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh failed_webroot_manifest/*-${machine_ip} ${HOME}/runtime/webroot_manifests
 fi
 
-/bin/cat *incoming* > aggregate
+if ( [ "`/usr/bin/find /home/XvR5HynuvMeGekkK5LTX/runtime/webroot_manifests/ -name "*incoming*" -print`" != "" ] )
+then
+        /bin/cat ${HOME}/runtime/webroot_manifests/*incoming* > ${HOME}/runtime/webroot_manifests/aggregate_webroot_files
+fi
 
-file_name="hello"
+for file_name in `/bin/cat ${HOME}/runtime/webroot_manifests/aggregate_webroot_files`
+do
+        youngest_epoch="`/bin/grep "${file_name}" ${HOME}/runtime/webroot_manifests/aggregate_webroot_files | /usr/bin/awk -F':' '{print $NF}' | /usr/bin/sort -n | /usr/bin/tail -1`"
+        chosen_manifest="`/bin/grep "${file_name}:${youngest_epoch}" ${HOME}/runtime/webroot_manifests/*incoming* | /usr/bin/awk -F':' '{print $1}'`"
+        for manifest_file in "`/usr/bin/find /home/XvR5HynuvMeGekkK5LTX/runtime/webroot_manifests/ -name "*incoming*" -print`"
+        do
+                /bin/sed -i "/^${file_name}:/d" ${manifest_file}
+        done
+        /bin/echo "${file_name}:${youngest_epoch}" >> ${chosen_manifest}
+done
 
-youngest_epoch="`/bin/grep "${file_name}" ./aggregate | /usr/bin/awk -F':' '{print $NF}' | /usr/bin/sort -n | /usr/bin/tail -1`"
+machine_ip="`${HOME}/providerscripts/utilities/processing/GetIP.sh`"
 
-chosen_manifest="`/bin/grep "${file_name}:${youngest_epoch}" *incoming* | /usr/bin/awk -F':' '{print $1}'`"
+webserver_ips="`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh webserverips/* | /bin/sed "s/${machine_ip}//g" | /bin/sed 's/  / /g'`"
 
-/bin/sed -i "/^${file_name}:/d" *incoming*
-
-/bin/echo "${file_name}:${youngest_epoch}" >> ${chosen_manifest}
+for webserver_ip in ${webserver_ips}
+do
+        for invocation_time in `/bin/echo 0 10 20 30 40 50`
+        do
+                for file in `/bin/cat ${HOME}/runtime/webroot_manifests/webroot_manifest_incoming-${webserver_ip}-${invocation_time} | /usr/bin/awk -F':' '{print $1}'`
+                do
+                        /usr/bin/rsync ${file} ${webserver_ip}:/tmp
+                done
+        done
+done
 
