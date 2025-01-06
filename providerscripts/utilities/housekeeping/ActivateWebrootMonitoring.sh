3b8dd0adfba80c7b4a6c4927184494b16726d1c2
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-06 19:27:18 +0000

    Update ActivateWebrootMonitoring.sh

--- providerscripts/utilities/housekeeping/ActivateWebrootMonitoring.sh
+++ providerscripts/utilities/housekeeping/ActivateWebrootMonitoring.sh
@@ -18,23 +18,25 @@ machine_ip="`${HOME}/providerscripts/utilities/processing/GetIP.sh`"
         do
         #put in a find to see if its of type directory and if yes do rm -r else if it is of type file just do rm
 
-                 file="0"
-                 directory="0"
-                 if ( [ "`/usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT} ${SERVER_USER}@${webserver_ip} "${CUSTOM_USER_SUDO} /usr/bin/find ${1}${2} -type f"`" != "" ] )
-                 then
-                   file="1"
-                 elif ( [ "`/usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT} ${SERVER_USER}@${webserver_ip} "${CUSTOM_USER_SUDO} /usr/bin/find ${1}${2} -type d"`" != "" ] )
-                 then
-                   directory="1"
-                 fi
+                # file="0"
+                # directory="0"
+                # if ( [ "`/usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT} ${SERVER_USER}@${webserver_ip} "${CUSTOM_USER_SUDO} /usr/bin/find ${1}${2} -type f"`" != "" ] )
+                # then
+                #   file="1"
+                # elif ( [ "`/usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT} ${SERVER_USER}@${webserver_ip} "${CUSTOM_USER_SUDO} /usr/bin/find ${1}${2} -type d"`" != "" ] )
+                # then
+                #   directory="1"
+                # fi
 
-                 if ( [ "${file}" = "1" ] && [ "${directory}" = "0" ] )
-                 then
-                  /usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT} ${SERVER_USER}@${webserver_ip} "${CUSTOM_USER_SUDO} /usr/bin/rm ${1}${2}"
-                 elif ( [ "${file}" = "0" ] && [ "${directory}" = "1" ] )
-                 then
-                  /usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT} ${SERVER_USER}@${webserver_ip} "${CUSTOM_USER_SUDO} /usr/bin/rm -r ${1}${2}"
-                 fi   
+                 #if ( [ "${file}" = "1" ] && [ "${directory}" = "0" ] )
+                # then
+                #  /usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT} ${SERVER_USER}@${webserver_ip} "${CUSTOM_USER_SUDO} /usr/bin/rm ${1}${2}"
+                # elif ( [ "${file}" = "0" ] && [ "${directory}" = "1" ] )
+                # then
+                #  /usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT} ${SERVER_USER}@${webserver_ip} "${CUSTOM_USER_SUDO} /usr/bin/rm -r ${1}${2}"
+                # fi   
+                 /usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT} ${SERVER_USER}@${webserver_ip} "${CUSTOM_USER_SUDO} /usr/bin/rm -r ${1}${2}"
+
         done 
 }
 
