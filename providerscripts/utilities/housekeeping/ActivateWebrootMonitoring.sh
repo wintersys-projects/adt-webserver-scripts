9648a643db4763d0d72d03ecb5dbe167c3d98857
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-06 19:00:17 +0000

    Update ActivateWebrootMonitoring.sh

--- providerscripts/utilities/housekeeping/ActivateWebrootMonitoring.sh
+++ providerscripts/utilities/housekeeping/ActivateWebrootMonitoring.sh
@@ -35,7 +35,6 @@ machine_ip="`${HOME}/providerscripts/utilities/processing/GetIP.sh`"
                  then
                   /usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT} ${SERVER_USER}@${webserver_ip} "${CUSTOM_USER_SUDO} /usr/bin/rm -r ${1}${2}"
                  fi   
-                fi
         done 
 }
 
