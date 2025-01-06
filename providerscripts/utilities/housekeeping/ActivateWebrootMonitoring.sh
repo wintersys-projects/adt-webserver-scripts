ecc3f52b4f2d5532a09d1279c20a853d3b5b91d2
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-06 14:37:45 +0000

    Update ActivateWebrootMonitoring.sh

--- providerscripts/utilities/housekeeping/ActivateWebrootMonitoring.sh
+++ providerscripts/utilities/housekeeping/ActivateWebrootMonitoring.sh
@@ -7,7 +7,7 @@ CUSTOM_USER_SUDO="/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E "
 machine_ip="`${HOME}/providerscripts/utilities/processing/GetIP.sh`"
 
  file_removed() {
-        other_webserver_ips="`/usr/bin/find ${HOME}/runtime/otherwebserverips -type f`"
+        other_webserver_ips="`/usr/bin/find ${HOME}/runtime/otherwebserverips -type f | /usr/bin/awk -F'/' '{print $NF}'`"
         for webserver_ip in ${other_webserver_ips}
         do
                   /usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT} ${SERVER_USER}@10.0.0.6 "${CUSTOM_USER_SUDO} /usr/bin/rm ${1}${2}"
@@ -15,7 +15,7 @@ machine_ip="`${HOME}/providerscripts/utilities/processing/GetIP.sh`"
 }
 
 file_modified() {
-        other_webserver_ips="`/usr/bin/find ${HOME}/runtime/otherwebserverips -type f`"
+        other_webserver_ips="`/usr/bin/find ${HOME}/runtime/otherwebserverips -type f | /usr/bin/awk -F'/' '{print $NF}'`"
         for webserver_ip in ${other_webserver_ips}
         do
                  /usr/bin/rsync -az -e "/usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT}" --rsync-path="/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -Sv && /usr/bin/sudo /bin/mkdir -p ${1} 2>dev/null && /usr/bin/sudo /usr/bin/rsync " ${1}${2} ${SERVER_USER}@${webserver_ip}:${1}${2}
@@ -36,7 +36,7 @@ file_modified() {
 }
 
 file_created() {
-        other_webserver_ips="`/usr/bin/find ${HOME}/runtime/otherwebserverips -type f`"
+        other_webserver_ips="`/usr/bin/find ${HOME}/runtime/otherwebserverips -type f | /usr/bin/awk -F'/' '{print $NF}'`"
         for webserver_ip in ${other_webserver_ips}
         do
                  /usr/bin/rsync -az -e "/usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT}" --rsync-path="/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -Sv && /usr/bin/sudo /bin/mkdir -p ${1} 2>/dev/null && /usr/bin/sudo /usr/bin/rsync " ${1}${2} ${SERVER_USER}@${webserver_ip}:${1}${2}
