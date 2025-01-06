f0e0ce76b94f22b8eb15e7056776bdaaebb0cf7b
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-06 12:53:47 +0000

    Update ActivateWebrootMonitoring.sh

--- providerscripts/utilities/housekeeping/ActivateWebrootMonitoring.sh
+++ providerscripts/utilities/housekeeping/ActivateWebrootMonitoring.sh
@@ -22,13 +22,17 @@ CUSTOM_USER_SUDO="/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E "
 file_modified() {
         machine_ip="`${HOME}/providerscripts/utilities/processing/GetIP.sh`"
         webserver_ips="`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh webserverips/* | /bin/sed "s/${machine_ip}//g" | /bin/sed 's/  / /g'`"
-        for webserver_ip in ${webserver_ips}
-        do
-                if ( [ "${webserver_ip}" != "${machine_ip}" ] )
-                then
-                 /usr/bin/rsync -avzu  -e "/usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT}" --rsync-path="/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -Sv && /usr/bin/sudo /bin/mkdir -p ${1} && /usr/bin/sudo /usr/bin/rsync " ${1}${2} ${SERVER_USER}@${webserver_ip}:${1}${2}
-                fi
-        done
+         parent_directory="${1}"
+
+         if ( [ "${parent_directory}" != "" ] )
+         then
+                 while ( [ "${parent_directory}" != "/var/www/html" ] )
+                 do
+                   /bin/chown www-data:www-data ${parent_directory}
+                   /bin/chmod 755 ${parent_directory}
+                   parent_directory="`/bin/echo ${parent_directory} | /bin/sed 's:/[^/]*$::'`"
+                 done
+         fi
 }
 
 file_created() {
@@ -42,13 +46,18 @@ file_created() {
  parent_directory="${1}"
                 fi
         done
-      
+
+         parent_directory="${1}"
+
+         if ( [ "${parent_directory}" != "" ] )
+         then
                  while ( [ "${parent_directory}" != "/var/www/html" ] )
                  do
                    /bin/chown www-data:www-data ${parent_directory}
                    /bin/chmod 755 ${parent_directory}
                    parent_directory="`/bin/echo ${parent_directory} | /bin/sed 's:/[^/]*$::'`"
                  done
+         fi
 }
 
 /usr/bin/inotifywait -q -m -r -e modify,delete,create /var/www/html | while read DIRECTORY EVENT FILE; do
