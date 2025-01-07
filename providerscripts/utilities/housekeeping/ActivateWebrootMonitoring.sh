b203fc81b42ea3b5648782604d798aa119ee8fb7
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-07 10:20:07 +0000

    Update ActivateWebrootMonitoring.sh

--- providerscripts/utilities/housekeeping/ActivateWebrootMonitoring.sh
+++ providerscripts/utilities/housekeeping/ActivateWebrootMonitoring.sh
@@ -1,9 +1,9 @@
-#set -x
+set -x
 
 
 if ( [ "`/usr/bin/ps -ef | /bin/grep 'inotify' | /bin/grep -v grep`" != "" ] )
 then
-exit
+        exit
 else
         ${HOME}/providerscripts/datastore/configwrapper/SyncWebrootConfigDatastore.sh
 
@@ -26,6 +26,7 @@ machine_ip="`${HOME}/providerscripts/utilities/processing/GetIP.sh`"
 file_updated() {
         other_webserver_ips="`/usr/bin/find ${HOME}/runtime/otherwebserverips -type f | /usr/bin/awk -F'/' '{print $NF}'`"
 
+
         for webserver_ip in ${other_webserver_ips}
         do
                 if ( [ -d ${1}${2} ] )
@@ -37,12 +38,14 @@ file_updated() {
                         file="${2}"
                 fi
 
+echo ${directory} > /tmp/file
+
                 if ( [ "${file}" = "" ] )
                 then
                         /usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT} ${SERVER_USER}@${webserver_ip} "${CUSTOM_USER_SUDO} /usr/bin/mkdir -p ${directory}"
                 else
                         /usr/bin/rsync -az --checksum -e "/usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT}" --rsync-path="/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -Sv && /usr/bin/sudo /usr/bin/rsync " ${directory}${file} ${SERVER_USER}@${webserver_ip}:${directory}${file}
-                        /usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT} ${SERVER_USER}@${webserver_ip} "${CUSTOM_USER_SUDO} /bin/chown www-data:www-data /var/www/html/${directory}${file} ; ${CUSTOM_USER_SUDO} /bin/chmod 644 /var/www/html/${directory}${file}"
+                        /usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT} ${SERVER_USER}@${webserver_ip} "${CUSTOM_USER_SUDO} /bin/chown www-data:www-data ${directory}${file} ; ${CUSTOM_USER_SUDO} /bin/chmod 644 ${directory}${file}"
                         cropped_filename="`/bin/echo ${file} | /bin/sed 's,/var/www/html/,,g'`"
                         ${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${directory}${file} webroot-update/${cropped_filename} "no"
                 fi
@@ -67,6 +70,7 @@ file_updated() {
             file_updated "$DIRECTORY" "$FILE"
             ;;
         CREATE*)
+echo ${DIRECTORY} > /tmp/file1
             file_updated "$DIRECTORY" "$FILE" 
             ;;
         DELETE*)
