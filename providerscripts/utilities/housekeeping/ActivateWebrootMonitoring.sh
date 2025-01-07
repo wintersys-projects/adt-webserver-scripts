b9c9c4d260053b5085b0cb8d480a5745762c16ca
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-07 16:35:00 +0000

    Update ActivateWebrootMonitoring.sh

--- providerscripts/utilities/housekeeping/ActivateWebrootMonitoring.sh
+++ providerscripts/utilities/housekeeping/ActivateWebrootMonitoring.sh
@@ -26,32 +26,29 @@ machine_ip="`${HOME}/providerscripts/utilities/processing/GetIP.sh`"
 file_updated() {
         other_webserver_ips="`/usr/bin/find ${HOME}/runtime/otherwebserverips -type f | /usr/bin/awk -F'/' '{print $NF}'`"
 
-        directory="${1}"
-        file="${2}"
+        file="${1}"
+        directory="`/bin/echo ${file} | /bin/sed 's:/[^/]*$::'`"
 
         for webserver_ip in ${other_webserver_ips}
         do
-                if ( [ ! -d ${directory}${file} ] )
-                then
-                        /usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT} ${SERVER_USER}@${webserver_ip} "${CUSTOM_USER_SUDO} /usr/bin/mkdir -p ${directory}" 
-                        /usr/bin/rsync -az --checksum -e "/usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT}" --rsync-path="/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -Sv && /usr/bin/sudo /usr/bin/rsync " ${directory}${file} ${SERVER_USER}@${webserver_ip}:${directory}${file}
-                        /usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT} ${SERVER_USER}@${webserver_ip} "${CUSTOM_USER_SUDO} /bin/chown www-data:www-data ${directory}${file} ; ${CUSTOM_USER_SUDO} /bin/chmod 644 ${directory}${file}"
-                        cropped_filename="`/bin/echo ${directory}${file} | /bin/sed 's,/var/www/html/,,g'`"
-                        ${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${directory}${file} webroot-update/${cropped_filename} "no"
-                fi
+                /usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT} ${SERVER_USER}@${webserver_ip} "${CUSTOM_USER_SUDO} /usr/bin/mkdir -p ${directory}" 
+                /usr/bin/rsync -az --checksum -e "/usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT}" --rsync-path="/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -Sv && /usr/bin/sudo /usr/bin/rsync " ${file} ${SERVER_USER}@${webserver_ip}:${file}
+                /usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT} ${SERVER_USER}@${webserver_ip} "${CUSTOM_USER_SUDO} /bin/chown www-data:www-data ${file} ; ${CUSTOM_USER_SUDO} /bin/chmod 644 ${file}"
+                cropped_filename="`/bin/echo ${file} | /bin/sed 's,/var/www/html/,,g'`"
+                ${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${file} webroot-update/${cropped_filename} "no"
         done
          
-         parent_directory="${1}"
+         parent_directories="${1}"
 
-         if ( [ "${parent_directory}" != "" ] )
+         if ( [ "${parent_directories}" != "" ] )
          then
-                /bin/chown www-data:www-data ${parent_directory}
-                /bin/chmod 755 ${parent_directory}
-                 while ( [ "${parent_directory}" != "/var/www/html" ] )
+                /bin/chown www-data:www-data ${parent_directories}
+                /bin/chmod 755 ${parent_directories}
+                 while ( [ "${parent_directories}" != "/var/www/html" ] )
                  do
-                   /bin/chown www-data:www-data ${parent_directory}
-                   /bin/chmod 755 ${parent_directory}
-                   parent_directory="`/bin/echo ${parent_directory} | /bin/sed 's:/[^/]*$::'`"
+                   /bin/chown www-data:www-data ${parent_directories}
+                   /bin/chmod 755 ${parent_directories}
+                   parent_directory="`/bin/echo ${parent_directories} | /bin/sed 's:/[^/]*$::'`"
                  done
         fi
 }
