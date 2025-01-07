9a9f1f66f206bf5af9a0fa56e942ef5cc1000c5e
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-07 23:52:22 +0000

    Update ActivateRSYNC.sh

--- providerscripts/utilities/housekeeping/ActivateRSYNC.sh
+++ providerscripts/utilities/housekeeping/ActivateRSYNC.sh
@@ -12,6 +12,6 @@ machine_ip="`${HOME}/providerscripts/utilities/processing/GetIP.sh`"
         for webserver_ip in ${other_webserver_ips}
         do
 
-                /usr/bin/rsync -azrpv -e "/usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT}" --rsync-path="/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -Sv && /usr/bin/sudo /usr/bin/rsync " /var/www/html/ ${SERVER_USER}@${webserver_ip}:/var/www/html
+                /usr/bin/rsync -azrp -e "/usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT}" --rsync-path="/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -Sv && /usr/bin/sudo /usr/bin/rsync " /var/www/html/ ${SERVER_USER}@${webserver_ip}:/var/www/html
 
         done
