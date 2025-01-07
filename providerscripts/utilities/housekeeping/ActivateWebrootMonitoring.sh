cbdadcca772a691c1242ce60efd509f7961065d5
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-07 09:47:16 +0000

    Update ActivateWebrootMonitoring.sh

--- providerscripts/utilities/housekeeping/ActivateWebrootMonitoring.sh
+++ providerscripts/utilities/housekeeping/ActivateWebrootMonitoring.sh
@@ -3,7 +3,10 @@
 
 if ( [ "`/usr/bin/ps -ef | /bin/grep 'inotify' | /bin/grep -v grep`" != "" ] )
 then
- exit
+exit
+else
+        ${HOME}/providerscripts/datastore/configwrapper/SyncWebrootConfigDatastore.sh
+
 fi
 SERVER_USER="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'SERVERUSER'`"
 SERVER_USER_PASSWORD="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'SERVERUSERPASSWORD'`"
@@ -16,25 +19,6 @@ machine_ip="`${HOME}/providerscripts/utilities/processing/GetIP.sh`"
         other_webserver_ips="`/usr/bin/find ${HOME}/runtime/otherwebserverips -type f | /usr/bin/awk -F'/' '{print $NF}'`"
         for webserver_ip in ${other_webserver_ips}
         do
-        #put in a find to see if its of type directory and if yes do rm -r else if it is of type file just do rm
-
-                # file="0"
-                # directory="0"
-                # if ( [ "`/usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT} ${SERVER_USER}@${webserver_ip} "${CUSTOM_USER_SUDO} /usr/bin/find ${1}${2} -type f"`" != "" ] )
-                # then
-                #   file="1"
-                # elif ( [ "`/usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT} ${SERVER_USER}@${webserver_ip} "${CUSTOM_USER_SUDO} /usr/bin/find ${1}${2} -type d"`" != "" ] )
-                # then
-                #   directory="1"
-                # fi
-
-                 #if ( [ "${file}" = "1" ] && [ "${directory}" = "0" ] )
-                # then
-                #  /usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT} ${SERVER_USER}@${webserver_ip} "${CUSTOM_USER_SUDO} /usr/bin/rm ${1}${2}"
-                # elif ( [ "${file}" = "0" ] && [ "${directory}" = "1" ] )
-                # then
-                #  /usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT} ${SERVER_USER}@${webserver_ip} "${CUSTOM_USER_SUDO} /usr/bin/rm -r ${1}${2}"
-                # fi   
                  /usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT} ${SERVER_USER}@${webserver_ip} "${CUSTOM_USER_SUDO} /usr/bin/rm ${1}${2} || ${CUSTOM_USER_SUDO} /usr/bin/rmdir ${1}${2}" 2>/dev/null
 
         done 
@@ -59,6 +43,8 @@ file_updated() {
                         /usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT} ${SERVER_USER}@${webserver_ip} "${CUSTOM_USER_SUDO} /usr/bin/mkdir -p ${directory}"
                 else
                         /usr/bin/rsync -az --checksum -e "/usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT}" --rsync-path="/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -Sv && /usr/bin/sudo /usr/bin/rsync " ${directory}${file} ${SERVER_USER}@${webserver_ip}:${directory}${file}
+                        cropped_filename="`/bin/echo ${file} | /bin/sed 's,/var/www/html/,,g'`"
+                        ${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${directory}${file} webroot-update/${cropped_filename}
                 fi
         done
          
