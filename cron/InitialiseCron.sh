a20729acea38bf39938d5b26fae08a2225e8f268
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-07 17:15:40 +0000

    Update InitialiseCron.sh

--- cron/InitialiseCron.sh
+++ cron/InitialiseCron.sh
@@ -39,8 +39,9 @@
 
 if ( [ "`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'PERSISTASSETSTOCLOUD'`" = "1" ] )
 then
-	/bin/echo "*/1 * * * * export HOME="${HOMEDIR}" && ${HOME}/providerscripts/utilities/housekeeping/ActivateWebrootMonitoring.sh" >> /var/spool/cron/crontabs/root
-	/bin/echo "@reboot export HOME="${HOMEDIR}" && ${HOME}/providerscripts/utilities/housekeeping/ActivateWebrootMonitoring.sh" >> /var/spool/cron/crontabs/root
+:
+	#/bin/echo "*/1 * * * * export HOME="${HOMEDIR}" && ${HOME}/providerscripts/utilities/housekeeping/ActivateWebrootMonitoring.sh" >> /var/spool/cron/crontabs/root
+	#/bin/echo "@reboot export HOME="${HOMEDIR}" && ${HOME}/providerscripts/utilities/housekeeping/ActivateWebrootMonitoring.sh" >> /var/spool/cron/crontabs/root
 
 	#/bin/echo "*/1 * * * * export HOME="${HOMEDIR}" && ${HOME}/providerscripts/datastore/SetupAssetsStore.sh" >> /var/spool/cron/crontabs/root
 	#/bin/echo "@reboot export HOME="${HOMEDIR}" && ${HOME}/providerscripts/datastore/SetupAssetsStore.sh" >> /var/spool/cron/crontabs/root
