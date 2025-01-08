7126d4bee6131cb38d2c7c41745c6e49e6fba9f6
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-08 18:41:34 +0000

    Update RsyncWebrootsFromCron.sh

--- cron/RsyncWebrootsFromCron.sh
+++ cron/RsyncWebrootsFromCron.sh
@@ -1,1 +1,7 @@
-${HOME}/providerscripts/utilities/housekeeping/RsyncWebroots.sh
+
+
+for interval in "0 10 20 30 40 50"
+do
+  /bin/sleep ${interval}
+  ${HOME}/providerscripts/utilities/housekeeping/RsyncWebroots.sh
+done
