15c3d57610e9ae0c5fd739dd55b5247609c6584e
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-08 00:02:17 +0000

    Update ActivateRSYNC.sh

--- providerscripts/utilities/housekeeping/ActivateRSYNC.sh
+++ providerscripts/utilities/housekeeping/ActivateRSYNC.sh
@@ -11,7 +11,24 @@ machine_ip="`${HOME}/providerscripts/utilities/processing/GetIP.sh`"
 
         for webserver_ip in ${other_webserver_ips}
         do
-
+#exclude config file for each application from rsync
                 /usr/bin/rsync -azrp -e "/usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT}" --rsync-path="/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -Sv && /usr/bin/sudo /usr/bin/rsync " /var/www/html/ ${SERVER_USER}@${webserver_ip}:/var/www/html
 
         done
+
+        #exclude images directory
+
+for node in `/usr/bin/find /var/www/html ! -user www-data -o ! -group www-data`
+do
+        /bin/chown www-data:www-data ${node}
+        if ( [ -d ${node} ] )
+        then
+                /bin/chmod 755 ${node}
+        fi
+        if ( [ -f ${node} ] )
+        then
+                /bin/chmod 644 ${node}
+        fi
+done
+
+
