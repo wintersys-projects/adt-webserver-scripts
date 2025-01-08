d7c295da4f847d9f02863b939d1312e4e8695629
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-08 22:43:01 +0000

    Update RsyncEntireMachine.sh

--- providerscripts/utilities/housekeeping/RsyncEntireMachine.sh
+++ providerscripts/utilities/housekeeping/RsyncEntireMachine.sh
@@ -1,1 +1,4 @@
+
+
+webserver_ip="${1}"
 /usr/bin/rsync -azrpu --exclude=/proc --exclude=/mnt --exclude=/tmp --exclude=/dev --exclude=/sys --exclude=/home -e "/usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT}" --rsync-path="/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -Sv && /usr/bin/sudo /usr/bin/rsync " / ${SERVER_USER}@${webserver_ip}:/
