f56f435fa0585cb606ba2b7308dab6bc5d47ca13
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-08 17:10:55 +0000

    Update ActivateRSYNC.sh

--- providerscripts/utilities/housekeeping/ActivateRSYNC.sh
+++ providerscripts/utilities/housekeeping/ActivateRSYNC.sh
@@ -7,16 +7,30 @@ SERVER_USER_PASSWORD="`${HOME}/providerscripts/utilities/config/ExtractConfigVal
 SSH_PORT="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'SSHPORT'`"
 ALGORITHM="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'ALGORITHM'`"
 machine_ip="`${HOME}/providerscripts/utilities/processing/GetIP.sh`"
+
+directories_to_miss=""
+if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh PERSISTASSETSTOCLOUD:1`" = "1" ] )
+then
+        directories_to_miss="`${HOME}/providerscripts/utilities/config/ExtractConfigValues.sh 'DIRECTORIESTOMOUNT' 'stripped' | /bin/sed 's/\./\//g' | /usr/bin/tr '\n' ' ' | /bin/sed 's/  / /g'`"
+fi
+
+exclude_command=""
+
+if ( [ "${directories_to_miss}" != "" ] )
+then
+        for directory in ${directories_to_miss}
+        do
+                exclude_command="${exclude_command} --exclude=${directory} "
+        done
+fi
         
 other_webserver_ips="`/usr/bin/find ${HOME}/runtime/otherwebserverips -type f | /usr/bin/awk -F'/' '{print $NF}'`"
 
 for webserver_ip in ${other_webserver_ips}
 do
 #exclude config file for each application from rsync
-        /usr/bin/rsync -azrpu -e "/usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT}" --rsync-path="/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -Sv && /usr/bin/sudo /usr/bin/rsync " /var/www/html/ ${SERVER_USER}@${webserver_ip}:/var/www/html
+        /usr/bin/rsync -azrpu ${exclude_command} -e "/usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT}" --rsync-path="/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -Sv && /usr/bin/sudo /usr/bin/rsync " /var/www/html/ ${SERVER_USER}@${webserver_ip}:/var/www/html
 done
-
-        #exclude images directory
 
 for node in `/usr/bin/find /var/www/html ! -user www-data -o ! -group www-data`
 do
