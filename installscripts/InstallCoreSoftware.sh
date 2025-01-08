e6657eee05c25c68691e59e5a3643ff53e8f4b30
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-08 22:17:59 +0000

    Update InstallCoreSoftware.sh

--- installscripts/InstallCoreSoftware.sh
+++ installscripts/InstallCoreSoftware.sh
@@ -19,9 +19,6 @@ ${HOME}/installscripts/InstallFirewall.sh ${BUILDOS}
 >&2 /bin/echo "${0} InstallDatastoreTools.sh"
 ${HOME}/installscripts/InstallDatastoreTools.sh ${BUILDOS} 
 
->&2 /bin/echo "${0} Installing Application Language"
-${HOME}/installscripts/InstallApplicationLanguage.sh "${APPLICATION_LANGUAGE}" 
-
 >&2 /bin/echo "${0} Installing Webserver"
 WEBSERVER_CHOICE="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'WEBSERVERCHOICE'`" 
 if ( ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'NGINX:source'`" = "1" ] && [ "${WEBSERVER_CHOICE}" = "NGINX" ] ) || ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'APACHE:source'`" = "1" ] && [ "${WEBSERVER_CHOICE}" = "APACHE" ] ) || ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'LIGHTTPD:source'`" = "1" ] && [ "${WEBSERVER_CHOICE}" = "LIGHTTPD" ] ) )
@@ -35,39 +32,8 @@ else
   ${HOME}/providerscripts/webserver/InstallWebserver.sh 
 fi
 
-#if ( [ "${1}" = "preinstall" ] )
-#then
- # scripts="`/bin/cat ${HOME}/installscripts/InstallCoreSoftware.sh | /bin/grep BUILDOS | /bin/grep -v "Up.*" | /usr/bin/awk '{print $1}'`"
- # 
- # package_names=""
-#
- # for script in ${scripts}
- # do
- #       script="`/bin/echo ${script} | /bin/sed -e 's,\${HOME},'${HOME}',g'`"
- #       package_names="${package_names} `/bin/cat ${script} | /bin/grep DEBIAN_FRONTEND | /usr/bin/awk '{print $8}' | /usr/bin/sort -u | /usr/bin/uniq | /usr/bin/tr '\n' ' '`"
- # done
-
-#  apt=""
-#  if ( [ "`${HOME}/providerscripts/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt" ] )
-#  then
-#        apt="/usr/bin/apt-get"
-#  elif ( [ "`${HOME}/providerscripts/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt-fast" ] )
-#  then
-#        apt="/usr/sbin/apt-fast"
-#  fi
-
- # if ( [ "${apt}" != "" ] )
- # then
- #       if ( [ "${BUILDOS}" = "ubuntu" ] )
- #       then
- #               DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -qq -y install ${package_names}
- #       fi
- #       if ( [ "${BUILDOS}" = "debian" ] )
- #       then
- #               DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -qq -y install ${package_names}
- #       fi
- # fi
-#fi
+>&2 /bin/echo "${0} Installing Application Language"
+${HOME}/installscripts/InstallApplicationLanguage.sh "${APPLICATION_LANGUAGE}" 
 
 >&2 /bin/echo "${0} InstallGo.sh"
 ${HOME}/installscripts/InstallGo.sh ${BUILDOS} 
@@ -88,7 +54,6 @@ ${HOME}/installscripts/InstallUnzip.sh ${BUILDOS}
 ${HOME}/installscripts/InstallSSHPass.sh ${BUILDOS} 
 >&2 /bin/echo "${0} InstallSysStat.sh"
 ${HOME}/installscripts/InstallSysStat.sh ${BUILDOS} 
-
 
 >&2 /bin/echo "${0} InstallDatabaseClient.sh"
 ${HOME}/installscripts/InstallDatabaseClient.sh  ${BUILDOS} 
