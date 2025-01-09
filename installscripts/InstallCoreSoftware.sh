a51b027cdfee54ba631972afba5fef502e248c6a
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-09 10:31:11 +0000

    Update InstallCoreSoftware.sh

--- installscripts/InstallCoreSoftware.sh
+++ installscripts/InstallCoreSoftware.sh
@@ -10,15 +10,6 @@ APPLICATION_LANGUAGE="`${HOME}/providerscripts/utilities/config/ExtractConfigVal
 >&2 /bin/echo "${0} UpdateAndUpgrade.sh"
 ${HOME}/installscripts/UpdateAndUpgrade.sh ${BUILDOS}
 
->&2 /bin/echo "${0} InstallNetworkManager.sh"
-${HOME}/installscripts/InstallNetworkManager.sh ${BUILDOS} 
-
->&2 /bin/echo "${0} InstallFirewall.sh"
-${HOME}/installscripts/InstallFirewall.sh ${BUILDOS} 
-
->&2 /bin/echo "${0} InstallDatastoreTools.sh"
-${HOME}/installscripts/InstallDatastoreTools.sh ${BUILDOS} 
-
 >&2 /bin/echo "${0} Installing Webserver"
 WEBSERVER_CHOICE="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'WEBSERVERCHOICE'`" 
 if ( ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'NGINX:source'`" = "1" ] && [ "${WEBSERVER_CHOICE}" = "NGINX" ] ) || ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'APACHE:source'`" = "1" ] && [ "${WEBSERVER_CHOICE}" = "APACHE" ] ) || ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'LIGHTTPD:source'`" = "1" ] && [ "${WEBSERVER_CHOICE}" = "LIGHTTPD" ] ) )
@@ -32,9 +23,16 @@ else
   ${HOME}/providerscripts/webserver/InstallWebserver.sh 
 fi
 
+>&2 /bin/echo "${0} InstallNetworkManager.sh"
+${HOME}/installscripts/InstallNetworkManager.sh ${BUILDOS} 
+>&2 /bin/echo "${0} InstallFirewall.sh"
+${HOME}/installscripts/InstallFirewall.sh ${BUILDOS} 
+>&2 /bin/echo "${0} InstallDatastoreTools.sh"
+${HOME}/installscripts/InstallDatastoreTools.sh ${BUILDOS} 
 >&2 /bin/echo "${0} Installing Application Language"
 ${HOME}/installscripts/InstallApplicationLanguage.sh "${APPLICATION_LANGUAGE}" 
-
+>&2 /bin/echo "${0} InstallJQ.sh"
+${HOME}/installscripts/InstallJQ.sh ${BUILDOS} 
 >&2 /bin/echo "${0} InstallGo.sh"
 ${HOME}/installscripts/InstallGo.sh ${BUILDOS} &
 >&2 /bin/echo "${0} InstallCurl.sh"
@@ -45,16 +43,12 @@ ${HOME}/installscripts/InstallLibioSocketSSL.sh ${BUILDOS}
 ${HOME}/installscripts/InstallLibnetSSLLeay.sh ${BUILDOS} 
 >&2 /bin/echo "${0} InstallEmailUtil.sh"
 ${HOME}/installscripts/InstallEmailUtil.sh ${BUILDOS} 
-
->&2 /bin/echo "${0} InstallJQ.sh"
-${HOME}/installscripts/InstallJQ.sh ${BUILDOS} 
 >&2 /bin/echo "${0} InstallUnzip.sh"
 ${HOME}/installscripts/InstallUnzip.sh ${BUILDOS} 
 >&2 /bin/echo "${0} InstallSSHPass.sh"
 ${HOME}/installscripts/InstallSSHPass.sh ${BUILDOS} 
 >&2 /bin/echo "${0} InstallSysStat.sh"
 ${HOME}/installscripts/InstallSysStat.sh ${BUILDOS} 
-
 >&2 /bin/echo "${0} InstallDatabaseClient.sh"
 ${HOME}/installscripts/InstallDatabaseClient.sh  ${BUILDOS} 
 >&2 /bin/echo "${0} InstallRsync.sh"
