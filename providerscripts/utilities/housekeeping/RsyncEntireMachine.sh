f8e6648cf756786ac4fde2c7bb7f4fd5422f6516
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-09 10:41:49 +0000

    Update RsyncEntireMachine.sh

--- providerscripts/utilities/housekeeping/RsyncEntireMachine.sh
+++ providerscripts/utilities/housekeeping/RsyncEntireMachine.sh
@@ -22,4 +22,4 @@ then
 fi
 
 webserver_ip="${1}"
-/usr/bin/rsync -azrpu --exclude=/proc --exclude=/mnt --exclude=/tmp --exclude=/dev --exclude=/sys --exclude=/home ${exclude_command} -e "/usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT}" --rsync-path="/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -Sv && /usr/bin/sudo /usr/bin/rsync " / ${SERVER_USER}@${webserver_ip}:/
+/usr/bin/rsync -azrpu --exclude=/proc --exclude=/mnt --exclude=/tmp --exclude=/dev --exclude=/sys --exclude=/home ${exclude_command} -e "/usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT}" --rsync-path="/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -Sv && /usr/bin/sudo /usr/bin/rsync " ${SERVER_USER}@${webserver_ip}:/ /
