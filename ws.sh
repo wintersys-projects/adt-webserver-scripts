9644ee8ea6dffe8d92058662688c876cdea4c7f0
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-09 11:35:22 +0000

    Update ws.sh

--- ws.sh
+++ ws.sh
@@ -244,10 +244,10 @@ ${HOME}/providerscripts/utilities/processing/RunServiceCommand.sh ssh restart
 
 #################################
 
-${HOME}/providerscripts/utilities/housekeeping/RsyncEntireMachine.sh 10.0.1.5
-/bin/touch /home/${SERVER_USER}/runtime/SUCCESSFULLY_RSYNC_BUILT
+#${HOME}/providerscripts/utilities/housekeeping/RsyncEntireMachine.sh 10.0.1.5
+#/bin/touch ${HOME}/runtime/SUCCESSFULLY_RSYNC_BUILT
 
-exit
+#exit
 
 
 #######################
