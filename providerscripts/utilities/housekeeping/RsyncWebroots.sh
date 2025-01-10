0d1af0d814e02bcee0659c8a22d2e16007b11af8
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-10 12:00:26 +0000

    Update RsyncWebroots.sh

--- providerscripts/utilities/housekeeping/RsyncWebroots.sh
+++ providerscripts/utilities/housekeeping/RsyncWebroots.sh
@@ -41,6 +41,6 @@ done
 
 for webserver_ip in ${other_webserver_ips}
 do
-        /usr/bin/rsync -azrpu ${exclude_command} -e "/usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT}" --rsync-path="/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -Sv && /usr/bin/sudo /usr/bin/rsync " ${SERVER_USER}@${webserver_ip}:/var/www/html /var/www/html
+        /usr/bin/rsync -azrpu ${exclude_command} -e "/usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT}" --rsync-path="/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -Sv && /usr/bin/sudo /usr/bin/rsync " ${SERVER_USER}@${webserver_ip}:/var/www/html/ /var/www/html
         /usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT} ${SERVER_USER}@${webserver_ip} "${CUSTOM_USER_SUDO} ${HOME}/providerscripts/utilities/security/EnforcePermissions.sh"
 done
