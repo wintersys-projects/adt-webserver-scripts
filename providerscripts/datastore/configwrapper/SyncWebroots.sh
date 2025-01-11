0b91cc3f2ab396551ced755cb93b39080206e3e7
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-11 16:35:51 +0000

    Update SyncWebroots.sh

--- providerscripts/datastore/configwrapper/SyncWebroots.sh
+++ providerscripts/datastore/configwrapper/SyncWebroots.sh
@@ -1,2 +1,3 @@
 ${HOME}/providerscripts/utilities/housekeeping/AuditAndUpdateWebrootDeletes.sh
 ${HOME}/providerscripts/datastore/configwrapper/SyncWebrootToDatastore.sh
+${HOME}/providerscripts/datastore/configwrapper/SyncDatastoreToWebroot.sh
