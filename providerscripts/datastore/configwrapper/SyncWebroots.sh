58fa00ab02c8ace79dfb715db5ac277fa874684c
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-11 23:53:27 +0000

    Update SyncWebroots.sh

--- providerscripts/datastore/configwrapper/SyncWebroots.sh
+++ providerscripts/datastore/configwrapper/SyncWebroots.sh
@@ -10,7 +10,7 @@ then
 
         for file in `/bin/cat ${HOME}/runtime/webroot_audit/webroot_file_list.dat.deleted | /bin/sed 's,/var/www/html/,,g'`
         do
-               ${HOME}/providerscripts/datastore/configwrapper/MoveFileConfigDatastore.sh webroot/${file} webroot/${file}-${SERVER_USER}
+               ${HOME}/providerscripts/datastore/configwrapper/CopyFileConfigDatastore.sh webroot/${file} webroot/${file}-${SERVER_USER}
                # ${HOME}/providerscripts/datastore/configwrapper/DeleteFromConfigDatastore.sh webroot/${file}
         done
 fi
@@ -27,5 +27,14 @@ then
 fi
 
 ${HOME}/providerscripts/datastore/configwrapper/SyncDatastoreToWebroot.sh
+
+if ( [ ! -d ${HOME}/runtime/webroot_audit/${SERVER_USER} ] )
+then
+        /bin/mkdir ${HOME}/runtime/webroot_audit/${SERVER_USER}
+else
+        /bin/rm -r ${HOME}/runtime/webroot_audit/${SERVER_USER}/*
+fi
+
+s3cmd sync --dry-run s3://crew-nuocial-uk-config-xn33/webroot/ ${HOME}/runtime/webroot_audit/${SERVER_USER}/ | /bin/grep ${SERVER_USER}
 
 
