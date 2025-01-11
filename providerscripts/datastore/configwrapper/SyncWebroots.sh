4e23b2ee327416af7977af99a675161f1f83935a
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-11 18:34:14 +0000

    Update SyncWebroots.sh

--- providerscripts/datastore/configwrapper/SyncWebroots.sh
+++ providerscripts/datastore/configwrapper/SyncWebroots.sh
@@ -5,5 +5,16 @@ ALGORITHM="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'ALGO
 CUSTOM_USER_SUDO="/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E "
 
 ${HOME}/providerscripts/utilities/housekeeping/AuditAndUpdateWebrootDeletes.sh
+
+ ${HOME}/runtime/webroot_audit/webroot_file_list.dat.deleted
+
+#deletion_command="/bin/rm "
+#if ( [ -f ${HOME}/runtime/webroot_audit/webroot_file_list.dat.deleted ] )
+#then
+#        files_to_delete=`/bin/cat ${HOME}/runtime/webroot_audit/webroot_file_list.dat.deleted | /bin/grep "^<" | /usr/bin/awk '{print $NF}' | /usr/bin/tr '\n' ' '`
+#        deletion_command="${deletion_command} ${files_to_delete}" 
+#fi
+
+#machine_ip="`${HOME}/providerscripts/utilities/processing/GetIP.sh`"
 ${HOME}/providerscripts/datastore/configwrapper/SyncWebrootToDatastore.sh
 ${HOME}/providerscripts/datastore/configwrapper/SyncDatastoreToWebroot.sh
