6a0abb31d060b3ab67b70ddfc03e58d183593eff
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-11 20:46:36 +0000

    Update SyncWebroots.sh

--- providerscripts/datastore/configwrapper/SyncWebroots.sh
+++ providerscripts/datastore/configwrapper/SyncWebroots.sh
@@ -6,11 +6,12 @@ do
  /bin/rm ${file}
 done
 ${HOME}/providerscripts/datastore/configwrapper/SyncDatastoreToWebroot.sh "skip"
-${HOME}/providerscripts/datastore/configwrapper/SyncWebrootToDatastore.sh
-for file in `/bin/cat ${HOME}/runtime/webroot_audit/webroot_file_list.dat.deleted`
-do
- ${HOME}/providerscripts/datastore/configwrapper/DeleteFromConfigDatastore.sh webroot/${file}
- /bin/rm ${file}
-done
 ${HOME}/providerscripts/datastore/configwrapper/SyncDatastoreToWebroot.sh
+${HOME}/providerscripts/datastore/configwrapper/SyncWebrootToDatastore.sh
+#for file in `/bin/cat ${HOME}/runtime/webroot_audit/webroot_file_list.dat.deleted`
+#do
+# ${HOME}/providerscripts/datastore/configwrapper/DeleteFromConfigDatastore.sh webroot/${file}
+# /bin/rm ${file}
+#done
+#${HOME}/providerscripts/datastore/configwrapper/SyncDatastoreToWebroot.sh
 
