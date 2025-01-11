b803de161b83f4b9f1ac7ffc7363e19105e9c3a2
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-11 09:58:03 +0000

    Create UploadWebrootToDatastore.sh

---
+++ providerscripts/datastore/configwrapper/UploadWebrootToDatastore.sh
@@ -0,0 +1,12 @@
+for file in `/usr/bin/find /var/www/html -maxdepth 1 -mindepth 1 -type d -not -path "/var/www/html/images/*" `
+do
+        /usr/bin/s3cmd sync ${file} s3://crew-nuocial-uk-config-xp79/webroot1`/bin/echo ${file} | /bin/sed 's,/var/www/html,,g'`/ &
+        pids="${pids} $!"
+done
+
+for pid in ${pids}
+do
+        wait ${pid}
+done
+
+/bin/echo "Processing complete"
