4d9917303b8ec6dbf6c7023d61682882590b7835
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-11 21:31:58 +0000

    Update SyncWebroots.sh

--- providerscripts/datastore/configwrapper/SyncWebroots.sh
+++ providerscripts/datastore/configwrapper/SyncWebroots.sh
@@ -1,11 +1,17 @@
 ${HOME}/providerscripts/utilities/housekeeping/AuditWebroot.sh
 
-for file in `/bin/cat ${HOME}/runtime/webroot_audit/webroot_file_list.dat.deleted`
-do
- ${HOME}/providerscripts/datastore/configwrapper/DeleteFromConfigDatastore.sh webroot/${file}
-done
+if ( [ -f ${HOME}/runtime/webroot_audit/webroot_file_list.dat.deleted ] )
+then
+        echo "found deleted"
+        /bin/cat ${HOME}/runtime/webroot_audit/webroot_file_list.dat.deleted
 
-for file in `/bin/cat ${HOME}/runtime/webroot_audit/webroot_file_list.dat.deleted`
+        for file in `/bin/cat ${HOME}/runtime/webroot_audit/webroot_file_list.dat.deleted | /bin/sed 's,/var/www/html/,,g'`
+        do
+                ${HOME}/providerscripts/datastore/configwrapper/DeleteFromConfigDatastore.sh webroot/${file}
+        done
+fi
+
+for file in `/bin/cat ${HOME}/runtime/webroot_audit/webroot_file_list.dat.added | /bin/sed 's,/var/www/html/,,g'`
 do
  ${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh webroot/${file}
 done
