1f30e94450ba47f672d9fbead32c8203d5d6d2ff
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-11 20:04:43 +0000

    Update SyncWebroots.sh

--- providerscripts/datastore/configwrapper/SyncWebroots.sh
+++ providerscripts/datastore/configwrapper/SyncWebroots.sh
@@ -1,41 +1,13 @@
-#SERVER_USER="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'SERVERUSER'`"
-#SERVER_USER_PASSWORD="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'SERVERUSERPASSWORD'`"
-#SSH_PORT="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'SSHPORT'`"
-#ALGORITHM="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'ALGORITHM'`"
-#CUSTOM_USER_SUDO="/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E "
-
 ${HOME}/providerscripts/utilities/housekeeping/AuditAndUpdateWebrootDeletes.sh
 
-machine_ip="`${HOME}/providerscripts/utilities/processing/GetIP.sh`"
-
-if ( [ -f ${HOME}/runtime/webroot_audit/webroot_file_list.dat.deleted ] )
-then
- ${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${HOME}/runtime/webroot_audit/webroot_file_list.dat.deleted webroot-deletes/webroot_file_list.dat.deleted.${machine_ip}
-fi
-
-if ( [ ! -d ${HOME}/runtime/webroot_audit/deletes_aggregate ] )
-then
- /bin/mkdir -p ${HOME}/runtime/webroot_audit/deletes_aggregate
-fi
-
-/bin/sleep 10
-
-other_webserver_ips="`/usr/bin/find ${HOME}/runtime/otherwebserverips -type f | /usr/bin/awk -F'/' '{print $NF}'`"
-
-for webserver_ip in ${other_webserver_ips}
+for file in `/bin/cat ${HOME}/runtime/webroot_audit/webroot_file_list.dat.deleted`
 do
-  ${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh webroot-deletes/webroot_file_list.dat.deleted.${webserver_ip} ${HOME}/runtime/webroot_audit/deletes_aggregate/webroot_file_list.dat.deleted.${webserver_ip}
-done
-
-if ( [ -f ${HOME}/runtime/webroot_audit/deletes_aggregate/webroot_file_list.dat.deleted.${machine_ip} ] )
-then
- /bin/rm ${HOME}/runtime/webroot_audit/deletes_aggregate/webroot_file_list.dat.deleted.${machine_ip}
-fi
-
-for file in `/bin/cat ${HOME}/runtime/webroot_audit/deletes_aggregate/*`
-do
+ ${HOME}/providerscripts/datastore/configwrapper/DeleteFromConfigDatastore.sh webroot/${file}
  /bin/rm ${file}
 done
+/bin/sleep 10
 
 ${HOME}/providerscripts/datastore/configwrapper/SyncWebrootToDatastore.sh
+/bin/sleep 10
 ${HOME}/providerscripts/datastore/configwrapper/SyncDatastoreToWebroot.sh
+
