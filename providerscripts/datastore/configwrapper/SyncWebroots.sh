820c330bb254c98630729c47abd1b776d87fcf01
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-11 20:22:12 +0000

    Update SyncWebroots.sh

--- providerscripts/datastore/configwrapper/SyncWebroots.sh
+++ providerscripts/datastore/configwrapper/SyncWebroots.sh
@@ -1,13 +1,14 @@
 ${HOME}/providerscripts/utilities/housekeeping/AuditAndUpdateWebrootDeletes.sh
 
+/bin/sleep 10
+${HOME}/providerscripts/datastore/configwrapper/SyncWebrootToDatastore.sh
+
 for file in `/bin/cat ${HOME}/runtime/webroot_audit/webroot_file_list.dat.deleted`
 do
  ${HOME}/providerscripts/datastore/configwrapper/DeleteFromConfigDatastore.sh webroot/${file}
  /bin/rm ${file}
 done
-/bin/sleep 10
 
-${HOME}/providerscripts/datastore/configwrapper/SyncWebrootToDatastore.sh
 /bin/sleep 10
 ${HOME}/providerscripts/datastore/configwrapper/SyncDatastoreToWebroot.sh
 
