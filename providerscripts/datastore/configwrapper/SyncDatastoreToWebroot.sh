c3c82aa0fc8db63c8e169116f5199b3208b8353c
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-11 22:43:21 +0000

    Update SyncDatastoreToWebroot.sh

--- providerscripts/datastore/configwrapper/SyncDatastoreToWebroot.sh
+++ providerscripts/datastore/configwrapper/SyncDatastoreToWebroot.sh
@@ -34,7 +34,7 @@ do
 done
  
 /usr/bin/s3cmd sync  --exclude-from="${HOME}/runtime/webroot_audit/directories_to_miss" s3://${config_bucket}/webroot/ /var/www/html/
-/usr/bin/s3cmd sync  --delete-removed --exclude-from="${HOME}/runtime/webroot_audit/directories_to_miss" s3://${config_bucket}/webroot/ /var/www/html/
+/usr/bin/s3cmd sync  --delete-removed --skip-existing --exclude-from="${HOME}/runtime/webroot_audit/directories_to_miss" s3://${config_bucket}/webroot/ /var/www/html/
 
 
 
