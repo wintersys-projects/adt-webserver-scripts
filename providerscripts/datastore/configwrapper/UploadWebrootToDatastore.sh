345d7360496cb887558c919c70fb3e854f486625
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-11 10:52:31 +0000

    Update UploadWebrootToDatastore.sh

--- providerscripts/datastore/configwrapper/UploadWebrootToDatastore.sh
+++ providerscripts/datastore/configwrapper/UploadWebrootToDatastore.sh
@@ -1,12 +1,8 @@
 for file in `/usr/bin/find /var/www/html -maxdepth 1 -mindepth 1 -type d -not -path "/var/www/html/images/*" `
 do
         /usr/bin/s3cmd sync "${file}/" s3://crew-nuocial-uk-config-xp79/webroot1`/bin/echo ${file} | /bin/sed 's,/var/www/html,,g'`/ &
-        pids="${pids} $!"
+     
 done
 
-for pid in ${pids}
-do
-        wait ${pid}
-done
+s3cmd sync /var/www/html/* s3://crew-nuocial-uk-config-xrtr/webroot/
 
-#s3cmd sync /var/www/html/* s3://crew-nuocial-uk-config-xant/webroot/
