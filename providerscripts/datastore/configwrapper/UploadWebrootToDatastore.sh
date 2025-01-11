365da9082847f2dbbf40f8695008b9d708b9e496
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-11 09:22:51 +0000

    Update UploadWebrootToDatastore.sh

--- providerscripts/datastore/configwrapper/UploadWebrootToDatastore.sh
+++ providerscripts/datastore/configwrapper/UploadWebrootToDatastore.sh
@@ -3,13 +3,16 @@ then
         /bin/mkdir ${HOME}/runtime/webroot_processing
 fi
 
-/usr/bin/find -type d /var/www/html > ${HOME}/runtime/webroot_processing/complete_webroot_filelist.dat
-
-/usr/bin/gawk -i inplace '{print "/usr/bin/s3cmd --recursive put " $0 " s3://crew-nuocial-uk-config-xp79/webroot"}' ${HOME}/runtime/webroot_processing/complete_webroot_filelist.dat
+/usr/bin/find /var/www/html -mindepth 1 -type d -not -path "/var/www/html/images/*"  | /bin/sed 's,/var/www/html,,' > ${HOME}/runtime/webroot_processing/complete_webroot_filelist.dat
+/bin/sed -i "s,$,/," ${HOME}/runtime/webroot_processing/complete_webroot_filelist.dat
+/usr/bin/find /var/www/html -maxdepth 1 -type f >> ${HOME}/runtime/webroot_processing/complete_webroot_filelist.dat
+/usr/bin/gawk -i inplace '{print "/usr/bin/s3cmd --recursive put /var/www/html"$0 " s3://crew-nuocial-uk-config-xp79/webroot"$0}' ${HOME}/runtime/webroot_processing/complete_webroot_filelist.dat
 
 /bin/rm ${HOME}/runtime/webroot_processing/complete_webroot_filelist_chunk*
 
 /usr/bin/split -n l/12 ${HOME}/runtime/webroot_processing/complete_webroot_filelist.dat  ${HOME}/runtime/webroot_processing/complete_webroot_filelist_chunk.dat_
+
+exit
 
 pids=""
 
