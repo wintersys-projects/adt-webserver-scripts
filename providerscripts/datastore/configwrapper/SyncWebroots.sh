3cfcccaa6b1da40bb1a879430d05f8448f7e6668
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-12 00:03:13 +0000

    Update SyncWebroots.sh

--- providerscripts/datastore/configwrapper/SyncWebroots.sh
+++ providerscripts/datastore/configwrapper/SyncWebroots.sh
@@ -1,5 +1,9 @@
 export HOME=`/bin/cat /home/homedir.dat`
+
+WEBSITE_URL="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
 SERVER_USER="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'SERVERUSER'`"
+TOKEN="`/bin/echo ${SERVER_USER} | /usr/bin/fold -w 4 | /usr/bin/head -n 1 | /usr/bin/tr '[:upper:]' '[:lower:]'`"
+configbucket="`/bin/echo "${WEBSITE_URL}"-config | /bin/sed 's/\./-/g'`-${TOKEN}"
 
 ${HOME}/providerscripts/utilities/housekeeping/AuditWebroot.sh
 
@@ -35,6 +39,24 @@ else
         /bin/rm -r ${HOME}/runtime/webroot_audit/${SERVER_USER}/*
 fi
 
-s3cmd sync --dry-run s3://crew-nuocial-uk-config-xn33/webroot/ ${HOME}/runtime/webroot_audit/${SERVER_USER}/ | /bin/grep ${SERVER_USER}
+for s3_marker_file in `s3cmd sync --dry-run s3://${configbucket}/webroot/ ${HOME}/runtime/webroot_audit/${SERVER_USER}/ | /bin/grep ${SERVER_USER} | /bin/grep download | /usr/bin/awk '{print $2}'`
+do
+#download: 's3://crew-nuocial-uk-config-xn33/webroot/111-Xn33UKDAxQwBSKhG6VdX' -> '/tmp/1/111-Xn33UKDAxQwBSKhG6VdX'
+
+        s3cmd del ${s3_marker_file}
+        real_file="`/bin/echo ${s3_marker_file} | /bin/sed "s/-${SERVER_USER}*//g"`"
+        s3cmd del ${real_file}
+        local_marker_file="`/bin/echo ${s3_marker_file} | /bin/sed 's,.*webroot/,,g'`"
+        real_local_file="`${local_marker_file} | /bin/sed "s/-${SERVER_USER}*//g"`"
+        if ( [ -f ${local_marker_file} ] )
+        then
+                /bin/rm ${local_marker_file}
+        fi
+        if ( [ -f ${real_local_file} ] )
+        then
+                /bin/rm ${real_local_file}
+        fi
+fi
+
 
 
