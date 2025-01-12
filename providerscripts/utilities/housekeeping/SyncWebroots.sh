7b1254e9bc9ddfd5f2443c281ec74283ab57001e
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-12 19:28:09 +0000

    Update SyncWebroots.sh

--- providerscripts/utilities/housekeeping/SyncWebroots.sh
+++ providerscripts/utilities/housekeeping/SyncWebroots.sh
@@ -37,7 +37,23 @@ do
         /bin/rm ${delete_list}
 done
 
-/usr/bin/find /var/www/html -type d -empty -delete
+directories_to_miss=""
+if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh PERSISTASSETSTOCLOUD:1`" = "1" ] )
+then
+        directories_to_miss="`${HOME}/providerscripts/utilities/config/ExtractConfigValues.sh 'DIRECTORIESTOMOUNT' 'stripped' | /bin/sed 's/\./\//g' | /usr/bin/tr '\n' ' ' | /bin/sed 's/  / /g'`"
+fi
+
+exclude_command=""
+
+if ( [ "${directories_to_miss}" != "" ] )
+then
+        for directory in ${directories_to_miss}
+        do
+                exclude_command="${exclude_command} ! -path '/var/www/html/${directory}/*' "
+        done
+fi
+
+/usr/bin/find /var/www/html -type d -empty -delete 
 
 ${HOME}/providerscripts/utilities/security/EnforcePermissions.sh
 
