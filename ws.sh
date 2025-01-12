7b50a34f520aaefb17805477a1ecaf6ca53e4cb7
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-12 18:50:21 +0000

    Update ws.sh

--- ws.sh
+++ ws.sh
@@ -254,7 +254,10 @@ then
  	${HOME}/providerscripts/utilities/config/StoreConfigValue.sh "AUTOSCALED" "1"
 	${HOME}/providerscripts/utilities/config/StoreConfigValue.sh "MYPUBLICIP" "${my_ip}"
 	${HOME}/providerscripts/utilities/config/StoreConfigValue.sh "MYIP" "${my_private_ip}"
+	${HOME}/providerscripts/utilities/status/CheckNetworkManagerStatus.sh
+ 
 	/bin/touch ${HOME}/runtime/SUCCESSFULLY_RSYNC_BUILT
+ 
   	if ( [ -z `/bin/ls ${HOME}/runtime/otherwebserverips` ] )
 	then
  		/bin/rm ${HOME}/runtime/otherwebserverips/*
