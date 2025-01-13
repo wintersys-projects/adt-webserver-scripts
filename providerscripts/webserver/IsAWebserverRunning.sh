154d71a50ffd7d48bdc1c4d870cdacfe17ad692c
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-13 18:51:45 +0000

    Create IsAWebserverRunning.sh

---
+++ providerscripts/webserver/IsAWebserverRunning.sh
@@ -0,0 +1,7 @@
+  if ( [ "`/usr/bin/ps -ef | /bin/grep apache2 | /bin/grep -v grep`" = "" ] && [ "`/usr/bin/ps -ef | /bin/grep php | /bin/grep -v grep`" = "" ] && [ "`/usr/bin/ps -ef | /bin/grep lighttpd | /bin/grep -v grep`" = "" ] )
+  then
+    echo "0"
+  else
+    echo "1"
+  fi
+	
