4aa4b6bf0d40e73096dbbb197a6e155259e3c4b6
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-13 16:31:53 +0000

    Create InstallGoofysFromSource.sh

---
+++ installscripts/goofys/InstallGoofysFromSource.sh
@@ -0,0 +1,24 @@
+if ( [ -d /root/scratch ] )                                             
+then                                                                  
+  /bin/rm -r /root/scratch                                        
+else                                                                   
+  /bin/mkdir /root/scratch                                        
+fi                                                                      
+
+cwd="`/usr/bin/pwd`"                                                    
+${install_command} make   
+/usr/bin/git clone https://github.com/kahing/goofys.git /root/scratch                                          
+cd /root/scratch                                                                                        
+/usr/bin/make install                                                                                  
+
+if ( [ -f ${HOME}/go/bin/goofys ] )                                                                      
+then                                                                                                    
+  /bin/mv ${HOME}/go/bin/goofys /usr/bin                                                                  
+  /bin/chmod 755 /usr/bin/goofys                                                                  
+fi                                                                                                      
+
+if ( [ -d /root/scratch ] )                                                                             
+then                                                                                                   
+  /bin/rm -r /root/scratch                                                                        
+fi                                                                                                      
+cd ${cwd}
