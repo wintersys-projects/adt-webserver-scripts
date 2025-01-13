96350dd87302372e8e6b7af34e1b6b57cc1885d7
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-13 23:14:09 +0000

    Update InstallNGINX.sh

--- installscripts/InstallNGINX.sh
+++ installscripts/InstallNGINX.sh
@@ -52,8 +52,6 @@ then
     			if ( [ ! -f /etc/nginx/BUILT_FROM_SOURCE ] )
      			then
 				${install_command} build-essential libpcre3 libpcre3-dev zlib1g zlib1g-dev libssl-dev libgd-dev libxml2 libxml2-dev uuid-dev
-			 	/bin/touch ${HOME}/runtime/${APPLICATION_LANGUAGE}_LANGUAGE_INSTALLING_IN_BACKGROUND
-     				${HOME}/installscripts/InstallApplicationLanguage.sh "${APPLICATION_LANGUAGE}" &
      				${HOME}/installscripts/nginx/BuildNginxFromSource.sh "Ubuntu"  			
      			fi
 	      		#Make sure nginx avaiable as a service and enable and start it
@@ -80,8 +78,6 @@ then
   			if ( [ ! -f /etc/nginx/BUILT_FROM_SOURCE ] )
      			then
 				${install_command} build-essential libpcre3 libpcre3-dev zlib1g zlib1g-dev libssl-dev libgd-dev libxml2 libxml2-dev uuid-dev
-				/bin/touch ${HOME}/runtime/${APPLICATION_LANGUAGE}_LANGUAGE_INSTALLING_IN_BACKGROUND
-     				${HOME}/installscripts/InstallApplicationLanguage.sh "${APPLICATION_LANGUAGE}" &
     				${HOME}/installscripts/nginx/BuildNginxFromSource.sh "Debian"        		
     			fi
       			 #Make sure nginx avaiable as a service and enable and start it
