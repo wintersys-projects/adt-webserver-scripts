3b372e8e4044db54e574bb521474b9f3919a38c6
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-14 18:59:47 +0000

    Update InstallNGINX.sh

--- installscripts/InstallNGINX.sh
+++ installscripts/InstallNGINX.sh
@@ -38,7 +38,7 @@ fi
 export DEBIAN_FRONTEND=noninteractive
 update_command="${apt} -o DPkg::Lock::Timeout=-1 -o Dpkg::Use-Pty=0 -qq -y update " 
 install_command="${apt} -o DPkg::Lock::Timeout=-1 -o Dpkg::Use-Pty=0 -qq -y install " 
-autoremove_command="${apt} -o DPkg::Lock::Timeout=-1 -o Dpkg::Use-Pty=0 -qq -y autoremove " 
+autoremove_command="${apt} -o DPkg::Lock::Timeout=-1 -o Dpkg::Use-Pty=0 -qq -y autoremove --purge" 
 remove_command="${apt} -o DPkg::Lock::Timeout=-1 -o Dpkg::Use-Pty=0 -qq -y remove --purge " 
 
 if ( [ "${apt}" != "" ] )
