36916dab3794a47f0e75fc45648c5af0fc62f8bf
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-14 14:57:26 +0000

    Update BuildNginxFromSource.sh

--- installscripts/nginx/BuildNginxFromSource.sh
+++ installscripts/nginx/BuildNginxFromSource.sh
@@ -43,7 +43,7 @@ nginx_latest_version="`/usr/bin/curl 'http://nginx.org/download/' |   /bin/egrep
 cd nginx-${nginx_latest_version}
 
 #Get the list of any custom modules that we want to compile with, if there are none, perform a default build
-nginx_modules="`${HOME}/providerscripts/utilities/config/ExtractBuildStyleValues.sh "NGINX" "stripped" | /bin/sed 's/:/ /g' | /bin/sed 's/source//g'`" #####SOURCE_BUILD_VAR#####
+nginx_modules="`${HOME}/providerscripts/utilities/config/ExtractBuildStyleValues.sh "NGINX:source" "stripped" | /bin/sed 's/:/ /g' | /bin/sed 's/source//g'`" #####SOURCE_BUILD_VAR#####
 
 if ( [ "${nginx_modules}" != "" ] )
 then
