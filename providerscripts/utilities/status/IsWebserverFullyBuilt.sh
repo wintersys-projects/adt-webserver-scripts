151320a33481d51550a1e323ea7545c530b799b1
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-15 19:20:45 +0000

    Update and rename IsWebserverFullyBuild.sh to IsWebserverFullyBuilt.sh

--- providerscripts/utilities/status/IsWebserverFullyBuilt.sh
+++ providerscripts/utilities/status/IsWebserverFullyBuilt.sh
@@ -1,7 +1,7 @@
 
 if ( [ -f ${HOME}/runtime/INITIAL_BUILD_WEBSERVER_ONLINE ] || [ -f ${HOME}/runtime/AUTOSCALED_WEBSERVER_ONLINE ] )
 then
-  echo "1" 
+  /bin/echo "1" 
 else
-  echo "0"
+  /bin/echo "0"
 fi
