ae0242cb7e60d9365707dc4e76fe4b3733ad242c
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-15 09:09:43 +0000

    Update PurgeApache.sh

--- installscripts/PurgeApache.sh
+++ installscripts/PurgeApache.sh
@@ -1,3 +1,5 @@
+set -x
+
 
 /usr/bin/apt-get -o DPkg::Lock::Timeout=-1 -o Dpkg::Use-Pty=0 -qq -y remove --purge apache2
 /usr/bin/apt-get -o DPkg::Lock::Timeout=-1 -o Dpkg::Use-Pty=0 -qq -y remove --purge apache2-utils
