954e9e5a424e490b34454a26d8f215feef63b4c5
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-19 18:31:50 +0000

    Update PutBackupToDatastore.sh

--- providerscripts/datastore/PutBackupToDatastore.sh
+++ providerscripts/datastore/PutBackupToDatastore.sh
@@ -23,20 +23,24 @@
 file_to_put="$1"
 datastore_to_put_in="$2"
 
-if ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd'`" = "1" ] )
-then
-        datastore_tool="/usr/bin/s3cmd --force --recursive --multipart-chunk-size-mb=5 put "
-elif ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s5cmd'`" = "1" ]  )
-then
-        host_base="`/bin/grep host_base /root/.s5cfg | /bin/grep host_base | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
-        datastore_tool="/usr/bin/s5cmd --credentials-file /root/.s5cfg --endpoint-url https://${host_base} cp "
-fi
+datastore_regions="`${HOME}/providerscripts/utilities/config/ExtractConfigValues.sh 'S3HOSTBASE' 'stripped' | /usr/bin/tr '\n' ' ' | /bin/sed 's/  / /g' | /bin/sed 's/config//g'`"
 
-count="0"
-while ( [ "`${datastore_tool} ${file_to_put} s3://${datastore_to_put_in} 2>&1 >/dev/null | /bin/grep "ERROR"`" != "" ] && [ "${count}" -lt "5" ] )
+for hostname in ${datastore_regions}
 do
-        /bin/echo "An error has occured `/usr/bin/expr ${count} + 1` times in script ${0}"
-        /bin/sleep 5
-        count="`/usr/bin/expr ${count} + 1`"
-done 
-file="`/bin/echo ${file_to_put} | /usr/bin/awk -F'/' '{print $NF}'`"
+        if ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd'`" = "1" ] )
+        then
+                datastore_tool="/usr/bin/s3cmd --force --recursive --multipart-chunk-size-mb=5 --host=${hostname} put "
+        elif ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s5cmd'`" = "1" ]  )
+        then
+                host_base="`/bin/grep host_base /root/.s5cfg | /bin/grep host_base | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
+                datastore_tool="/usr/bin/s5cmd --credentials-file /root/.s5cfg --endpoint-url https://${hostname} cp "
+        fi
+
+        count="0"
+        while ( [ "`${datastore_tool} ${file_to_put} s3://${datastore_to_put_in} 2>&1 >/dev/null | /bin/grep "ERROR"`" != "" ] && [ "${count}" -lt "5" ] )
+        do
+                /bin/echo "An error has occured `/usr/bin/expr ${count} + 1` times in script ${0}"
+                /bin/sleep 5
+                count="`/usr/bin/expr ${count} + 1`"
+        done 
+done
