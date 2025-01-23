17ca85648ea110be219ddb79688ebf3eab395dfd
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-23 19:14:14 +0000

    Update CheckServerAlive.sh

--- providerscripts/application/monitoring/CheckServerAlive.sh
+++ providerscripts/application/monitoring/CheckServerAlive.sh
@@ -32,9 +32,14 @@ fi
 #DB_P="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 2`"
 #DB_U="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 3`"
 
-DB_N="`/bin/sed '1q;d' ${HOME}/credentials/db_cred`"
-DB_P="`/bin/sed '2q;d' ${HOME}/credentials/db_cred`"
-DB_U="`/bin/sed '3q;d' ${HOME}/credentials/db_cred`"
+#DB_N="`/bin/sed '1q;d' ${HOME}/credentials/db_cred`"
+#DB_P="`/bin/sed '2q;d' ${HOME}/credentials/db_cred`"
+#DB_U="`/bin/sed '3q;d' ${HOME}/credentials/db_cred`"
+
+DB_U="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'DBaaSUSERNAME'`"
+DB_P="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'DBaaSPASSWORD'`"
+DB_N="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'DBaaSNAME'`"
+
 
 #if ( [ ! -f ${HOME}/runtime/APPLICATION_CONFIGURATION_PREPARED ] )
 #then
