bd4f9bb78e45a5f105557151789be1a4865fe4b9
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-29 19:56:50 +0000

    Update Update.sh

--- installscripts/Update.sh
+++ installscripts/Update.sh
@@ -87,6 +87,8 @@ then
                 done
                                  		${HOME}/installscripts/InstallAria2.sh "ubuntu"
                                 /bin/touch /tmp/apt-fast.list
+                        /bin/sed -i 's/^#DOWNLOADBEFORE/DOWNLOADBEFORE/g' /etc/apt-fast.conf
+
 
 
         fi
@@ -125,6 +127,8 @@ then
                                       		${HOME}/installscripts/InstallAria2.sh "debian"
 
                                 /bin/touch /tmp/apt-fast.list
+                                /bin/sed -i 's/^#DOWNLOADBEFORE/DOWNLOADBEFORE/g' /etc/apt-fast.conf
+
 
 
 
