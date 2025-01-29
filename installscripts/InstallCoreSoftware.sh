59e94162e5083dc102620a9e082dc273411bb445
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-29 12:14:00 +0000

    Update InstallCoreSoftware.sh

--- installscripts/InstallCoreSoftware.sh
+++ installscripts/InstallCoreSoftware.sh
@@ -23,6 +23,12 @@ ${HOME}/installscripts/InstallNetworkManager.sh ${BUILDOS}
 ${HOME}/installscripts/InstallFirewall.sh ${BUILDOS} 
 >&2 /bin/echo "${0} InstallDatastoreTools.sh"
 ${HOME}/installscripts/InstallDatastoreTools.sh ${BUILDOS} 
+
+if ( [ ! -f /usr/bin/s3cmd ] && [ ! -f /usr/bin/s5cmd ] )
+then
+>&2 /bin/echo "${0} Failed to install essential datastore tooling, I have to exit"
+  exit
+fi
 #>&2 /bin/echo "${0} InstallUnzip.sh"
 #${HOME}/installscripts/InstallUnzip.sh ${BUILDOS} 
 
