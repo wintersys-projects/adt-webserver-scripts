811b902e9526103adbdab62c58405b599410faab
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-02-01 16:20:02 +0000

    Update InstallSendEmail.sh

--- installscripts/InstallSendEmail.sh
+++ installscripts/InstallSendEmail.sh
@@ -20,11 +20,17 @@
 #######################################################################################################
 #######################################################################################################
 
-#if ( [ "${1}" != "" ] )
-#then
-#	buildos="${1}"
-#fi
-BUILDOS="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
+if ( [ "${1}" != "" ] )
+then
+    buildos="${1}"
+fi
+
+if ( [ "${buildos}" = "" ] )
+then
+    BUILDOS="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
+else 
+    BUILDOS="${buildos}"
+fi
 
 
 apt=""
