bec6d2759ac9d08a526c3d883ea0f93d5a884c6c
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-02-01 16:19:27 +0000

    Update InstallS3FS.sh

--- installscripts/InstallS3FS.sh
+++ installscripts/InstallS3FS.sh
@@ -45,7 +45,7 @@ install_command="${apt} -o DPkg::Lock::Timeout=-1 -o Dpkg::Use-Pty=0 -qq -y inst
 
 if ( [ "${apt}" != "" ] )
 then
-	if ( [ "${buildos}" = "ubuntu" ] )
+	if ( [ "${BUILDOS}" = "ubuntu" ] )
 	then
 		if ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:s3fs:repo'`" = "1" ] )
 		then
@@ -53,7 +53,7 @@ then
 		fi
 	fi
 
-	if ( [ "${buildos}" = "debian" ] )
+	if ( [ "${BUILDOS}" = "debian" ] )
 	then
 		if ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:s3fs:repo'`" = "1" ] )
 		then
