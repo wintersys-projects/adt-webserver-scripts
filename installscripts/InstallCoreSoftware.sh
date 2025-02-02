21f0412121298010c4c5930d6e52fa4761cbe81b
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-02-02 12:35:18 +0000

    Update InstallCoreSoftware.sh

--- installscripts/InstallCoreSoftware.sh
+++ installscripts/InstallCoreSoftware.sh
@@ -1,8 +1,9 @@
 
-while ( [ ! -f /home/SOFTWARE_FOUNDATION_INSTALLED ] )
-do
-  /bin/sleep 1
-done
+#while ( [ ! -f /home/SOFTWARE_FOUNDATION_INSTALLED ] )
+#do
+#  /bin/sleep 1
+#done
+
 if ( [ ! -d ${HOME}/runtime/installedsoftware ] )
 then
   /bin/mkdir -p ${HOME}/runtime/installedsoftware
