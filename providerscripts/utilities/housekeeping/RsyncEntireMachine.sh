2f66a23a7df125980d9ca58cf698b2afb1724359
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-02-04 22:10:55 +0000

    Update RsyncEntireMachine.sh

--- providerscripts/utilities/housekeeping/RsyncEntireMachine.sh
+++ providerscripts/utilities/housekeeping/RsyncEntireMachine.sh
@@ -24,6 +24,15 @@ then
                 exclude_command="${exclude_command} --exclude=${directory} "
         done
 fi
+####NEW WAY
+#Webservers generate tar archives every 10 minutes and then when a new webserver is being built the tar achive gets topped up with the lastest webroot
+#the new webserver then copies the latest tar archive from the webserver of its choice and extracts it to make its own file system
+
+# tar the root directory with the necessary excludes
+#tar cvzf - . | /usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /home/XeHPSWgl1jFkNuE7I70X/.ssh/id_rsa_AGILE_DEPLOYMENT_BUILD_KEY -p 1035 XeHPSWgl1jFkNuE7I70X@10.0.1.3 "cat > /tmp/file.tar.gz"
+#on remote webserver trigger that a new webserver is building tar the whole machine passing it over ssh to the new machine once it is online and creating a tar archive out of it
+#the tar archive can then be extracted by the new webserver
+
 
 #/usr/bin/rsync -azrpu --exclude=/proc --exclude=/mnt --exclude=/tmp --exclude=/dev --exclude=/sys ${exclude_command} -e "/usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT}" --rsync-path="/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -Sv && /usr/bin/sudo /usr/bin/rsync " ${SERVER_USER}@${webserver_ip}:/ /
 #/usr/bin/rsync -azrpu --exclude=/proc --exclude=/mnt --exclude=/tmp --exclude=/dev --exclude=/sys ${exclude_command} -e "/usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${HOME}/.ssh/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY -p ${SSH_PORT}" --rsync-path="/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -Sv /usr/bin/rsync " ${SERVER_USER}@${webserver_ip}:/ /
