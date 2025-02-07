8bba2e5663143ed7d91498e52405081201057b8f
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-02-07 20:00:26 +0000

    Update InstallPHPBase.sh

--- installscripts/InstallPHPBase.sh
+++ installscripts/InstallPHPBase.sh
@@ -115,9 +115,9 @@ then
 fi
 
 /usr/bin/find /etc/php -mindepth 1 ! -regex "^/etc/php/${PHP_VERSION}\(/.*\)?" -delete
-if ( [ "`/usr/bin/php -v | /bin/grep ${PHP_VERSION}`" != "" ] )
-then
+#if ( [ "`/usr/bin/php -v | /bin/grep ${PHP_VERSION}`" != "" ] )
+#then
 	/bin/touch ${HOME}/runtime/installedsoftware/InstallPHPBase.sh				
-else
-	: #Send email
-fi
+#else
+#	: #Send email
+#fi
