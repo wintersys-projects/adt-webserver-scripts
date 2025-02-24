050f8ce65d369eb92db5decd5e0517fdfe74ba07
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-02-24 16:23:29 +0000

    Update BuildNginxFromSource.sh

--- installscripts/nginx/BuildNginxFromSource.sh
+++ installscripts/nginx/BuildNginxFromSource.sh
@@ -61,7 +61,7 @@ else
 	#options=" --prefix=/var/www/html --sbin-path=/usr/sbin/nginx --conf-path=/etc/nginx/nginx.conf --http-log-path=/var/log/nginx/access.log --error-log-path=/var/log/nginx/error.log --with-pcre  --lock-path=/var/lock/nginx.lock --pid-path=/var/run/nginx.pid --with-http_ssl_module --with-http_image_filter_module=dynamic --modules-path=/etc/nginx/modules --with-http_v2_module --with-stream=dynamic --with-http_addition_module --with-http_mp4_module --with-http_realip_module"
 fi
 
-nginx_modules="`${HOME}/providerscripts/utilities/config/ExtractBuildStyleValues.sh "NGINX:modules-list" "stripped" | /bin/sed 's/:/ /g' | /bin/sed 's/source//g'  | /bin/sed 's/^ //'`" 
+nginx_modules="`${HOME}/providerscripts/utilities/config/ExtractBuildStyleValues.sh "NGINX:modules-list" "stripped" | /bin/sed 's/:/ /g' | /bin/sed 's/source//g'  | /bin/sed 's/^ //' | /bin/sed 's/modules-list//g'`" 
 
 if ( [ ! -f /etc/nginx/modules.conf ] )
 then
