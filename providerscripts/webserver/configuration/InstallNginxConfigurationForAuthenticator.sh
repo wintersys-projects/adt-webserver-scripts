f68a81eda9de61467f484bf95acb89bb89863e2e
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-03 15:22:09 +0000

    Update InstallNginxConfigurationForAuthenticator.sh

--- providerscripts/webserver/configuration/InstallNginxConfigurationForAuthenticator.sh
+++ providerscripts/webserver/configuration/InstallNginxConfigurationForAuthenticator.sh
@@ -2,7 +2,16 @@
 HOME="`/bin/cat /home/homedir.dat`"
 WEBSITE_URL="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
 
-/bin/sed -i "s/XXXXWEBSITEURLXXXX/${WEBSITE_URL}/g"
-/bin/sed -i "s/XXXXHOMEXXXX/${HOME}/g"
+/bin/sed -i "s/XXXXWEBSITEURLXXXX/${WEBSITE_URL}/g" ${HOME}/providerscripts/webserver/configuration/authenticator/nginx/sites-available.conf
+/bin/sed -i "s/XXXXHOMEXXXX/${HOME}/g" ${HOME}/providerscripts/webserver/configuration/authenticator/nginx/sites-available.conf
+
+if ( [ ! -d /etc/nginx/sites-available ] )
+then
+  /bin/mkdir -p /etc/nginx/sites-available
+fi
+
+/bin/cp ${HOME}/providerscripts/webserver/configuration/authenticator/nginx/sites-available.conf /etc/nginx/sites-available
+/bin/chown www-data:www-data /etc/nginx/sites-available/authenticator.conf
+/bin/chmod 644 /etc/nginx/sites-available/authenticator.conf
 
 
