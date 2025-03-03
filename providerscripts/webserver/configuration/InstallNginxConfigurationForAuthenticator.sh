60b6cb381ec1a8fb4e8ee7ddc1b43db7eee7ebac
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-03 15:19:05 +0000

    Update InstallNginxConfigurationForAuthenticator.sh

--- providerscripts/webserver/configuration/InstallNginxConfigurationForAuthenticator.sh
+++ providerscripts/webserver/configuration/InstallNginxConfigurationForAuthenticator.sh
@@ -1,4 +1,8 @@
 
+HOME="`/bin/cat /home/homedir.dat`"
+WEBSITE_URL="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
 
-XXXXWEBSITEURLXXXX;
- XXXXHOMEXXXX
+/bin/sed -i "s/XXXXWEBSITEURLXXXX/${WEBSITE_URL}/g"
+/bin/sed -i "s/XXXXHOMEXXXX/${HOME}/g"
+
+
