edc1c50b45325d1fd3876db25eb264489aa68e99
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-04 15:01:29 +0000

    Update InstallNginxConfigurationForAuthenticator.sh

--- providerscripts/webserver/configuration/InstallNginxConfigurationForAuthenticator.sh
+++ providerscripts/webserver/configuration/InstallNginxConfigurationForAuthenticator.sh
@@ -1,6 +1,7 @@
 
 HOME="`/bin/cat /home/homedir.dat`"
 WEBSITE_URL="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
+ROOT_DOMAIN="`/bin/echo ${WEBSITE_URL} | /usr/bin/awk -F'.' '{$1=""}1' | /bin/sed 's/^.//' | /bin/sed 's/ /\./g'`"
 WEBSITE_NAME="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'WEBSITEDISPLAYNAME'`"
 DNS_CHOICE="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'DNSCHOICE'`"
 
@@ -60,7 +61,7 @@ fi
 /bin/chown www-data:www-data /var/www/html/index.php
 /bin/chmod 644 /var/www/html/index.php
 /bin/sed -i "s/XXXXWEBSITEURLXXXX/${WEBSITE_URL}/g" /var/www/html/index.php
-/bin/sed -i "s/XXXXROOTDOMAINXXXX/${WEBSITE_URL}/g" /var/www/html/index.php
+/bin/sed -i "s/XXXXROOTDOMAINXXXX/${ROOT_DOMAIN}/g" /var/www/html/index.php
 
 
 
