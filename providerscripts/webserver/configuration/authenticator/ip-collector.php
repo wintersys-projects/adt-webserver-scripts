2fed428a619eb4a0754bd0c548ca2881230aa1b0
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-04 12:50:13 +0000

    Create ip-collector.php

---
+++ providerscripts/webserver/configuration/authenticator/ip-collector.php
@@ -0,0 +1,31 @@
+<?php
+if(isset($_POST['submit'])){
+$IPAddress = "IP Address:".$_POST['ipaddress']."
+";
+$file=fopen("ipaddresses.dat", "a");
+fwrite($file, $IPAddress);
+fclose($file);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="style.css">
+  <title>Enter your auth.nuocial.uk here and we will send you an authentication link if we know you</title>
+</head>
+<body class="ipcollector-body">
+  <div class="ipcollector-wrapper">
+    <div class="ipcollector-heading">
+      Your IP Address (https://whatsmyip.com)
+    </div>
+    <form class="ipcollector-form" method="post">
+      <input type="text" name="ipaddress" placeholder="IP Address" required autocomplete="off"> <br>
+      <input type="submit" name="submit" value="SAVE" class="ipcollector-submit">
+    </form>
+  </div>
+</body>
+</html>
