88723e2575714493509913092160464e2b5a5b16
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-04 13:31:27 +0000

    Update GenerateAuthenticationEmails.sh

--- providerscripts/webserver/configuration/authenticator/GenerateAuthenticationEmails.sh
+++ providerscripts/webserver/configuration/authenticator/GenerateAuthenticationEmails.sh
@@ -3,7 +3,7 @@ email_list="`/bin/cat /var/www/html/emails.dat | /usr/bin/awk -F':' '{print $NF}
 for email_address in ${email_list}
 do
         file_name="`/usr/bin/openssl rand -base64 32 | /usr/bin/tr -cd 'a-zA-Z0-9' | /usr/bin/cut -b 1-16 | /usr/bin/tr '[:upper:]' '[:lower:]'`"
-        full_file_name="/var/www/html/${file_name}.php"
+        full_file_name="/var/www/html/ip-address-${file_name}.php"
         /bin/cp ${HOME}/providerscripts/webserver/configuration/authenticator/ip-collector.php ${full_file_name}
         /bin/chown www-data:www-data ${full_file_name}
         /bin/chmod 644 ${full_file_name}
