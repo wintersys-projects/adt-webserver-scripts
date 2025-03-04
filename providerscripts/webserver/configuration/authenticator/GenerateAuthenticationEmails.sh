d89a8b7996f131606d35319fb787e18d73dc9214
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-04 12:53:42 +0000

    Update GenerateAuthenticationEmails.sh

--- providerscripts/webserver/configuration/authenticator/GenerateAuthenticationEmails.sh
+++ providerscripts/webserver/configuration/authenticator/GenerateAuthenticationEmails.sh
@@ -4,7 +4,7 @@ for email_address in ${email_list}
 do
         file_name="`/usr/bin/openssl rand -base64 32 | /usr/bin/tr -cd 'a-zA-Z0-9' | /usr/bin/cut -b 1-16 | /usr/bin/tr '[:upper:]' '[:lower:]'`"
         full_file_name="/var/www/html/${file_name}.php"
-        /bin/cp /var/www/html/hello.php ${full_file_name}
+        /bin/cp ${HOME}/providerscripts/webserver/configuration/authenticator/ip-collector.php ${full_file_name}
         /bin/chown www-data:www-data ${full_file_name}
         /bin/chmod 644 ${full_file_name}
         message="<!DOCTYPE html> <html> <body> <h1>My First Heading</h1> <p>My first paragraph.</p> <a href='https://auth.nuocial.uk/${file_name}.php'>Enable Your IP Address</a> </body> </html>"
