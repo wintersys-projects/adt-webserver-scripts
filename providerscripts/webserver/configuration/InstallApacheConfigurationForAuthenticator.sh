4d81e212c261fb187f3708bb9ac28218b8838bf9
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-06 09:00:52 +0000

    Update InstallApacheConfigurationForAuthenticator.sh

--- providerscripts/webserver/configuration/InstallApacheConfigurationForAuthenticator.sh
+++ providerscripts/webserver/configuration/InstallApacheConfigurationForAuthenticator.sh
@@ -4,56 +4,45 @@ ROOT_DOMAIN="`/bin/echo ${WEBSITE_URL} | /usr/bin/awk -F'.' '{$1=""}1' | /bin/se
 WEBSITE_NAME="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'WEBSITEDISPLAYNAME'`"
 DNS_CHOICE="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'DNSCHOICE'`"
 
-/bin/cp ${HOME}/providerscripts/webserver/configuration/authenticator/nginx/nginx.conf /etc/nginx
-/bin/chown www-data:www-data /etc/nginx/nginx.conf
-/bin/chmod 644 /etc/nginx/nginx.conf
+/bin/cp ${HOME}/providerscripts/webserver/configuration/authenticator/apache/apache2.conf /etc/apache2
+/bin/chown www-data:www-data /etc/apache2/apache2.conf
+/bin/chmod 644 /etc/apache2/apache2.conf
 
-if ( [ "${DNS_CHOICE}" = "cloudflare" ] )
+if ( [ ! -d /etc/apache2/sites-available ] )
 then
-  /bin/sed -i "s,XXXXCLOUDFLAREXXXX,include /etc/nginx/cloudflare;,g" /etc/nginx/nginx.conf
-else
-  /bin/sed -i "s/XXXXCLOUDFLAREXXXX//g" /etc/nginx/nginx.conf
+  /bin/mkdir -p /etc/apache2/sites-available
 fi
 
-if ( [ ! -d /etc/nginx/sites-available ] )
-then
-  /bin/mkdir -p /etc/nginx/sites-available
-fi
+/bin/cp ${HOME}/providerscripts/webserver/configuration/authenticator/apache2/site-available.conf /etc/apache2/sites-available/${WEBSITE_NAME}
+/bin/chown www-data:www-data /etc/apache2/sites-available/${WEBSITE_NAME}
+/bin/chmod 644 /etc/apache2/sites-available/${WEBSITE_NAME}
 
-/bin/cp ${HOME}/providerscripts/webserver/configuration/authenticator/nginx/site-available.conf /etc/nginx/sites-available/${WEBSITE_NAME}
-/bin/chown www-data:www-data /etc/nginx/sites-available/${WEBSITE_NAME}
-/bin/chmod 644 /etc/nginx/sites-available/${WEBSITE_NAME}
-
-/bin/sed -i "s/XXXXWEBSITEURLXXXX/${WEBSITE_URL}/g" /etc/nginx/sites-available/${WEBSITE_NAME}
-/bin/sed -i "s,XXXXHOMEXXXX,${HOME},g" /etc/nginx/sites-available/${WEBSITE_NAME}
-
-/bin/cp ${HOME}/providerscripts/webserver/configuration/authenticator/nginx/blockuseragents.rules /etc/nginx/
-/bin/chown www-data:www-data /etc/nginx/blockuseragents.rules
-/bin/chmod 644 /etc/nginx/blockuseragents.rules
+/bin/sed -i "s/XXXXWEBSITEURLXXXX/${WEBSITE_URL}/g" /etc/apache2/sites-available/${WEBSITE_NAME}
+/bin/sed -i "s,XXXXHOMEXXXX,${HOME},g" /etc/apache2/sites-available/${WEBSITE_NAME}
 
 port="`${HOME}/providerscripts/utilities/config/ExtractBuildStyleValues.sh "PHP" "stripped" | /usr/bin/awk -F'|' '{print $NF}'`"
 
 if ( [ "`/bin/echo ${port} | /bin/grep -o "^[0-9]*$"`" = "" ] )
 then
-	if ( [ -f ${HOME}/providerscripts/webserver/configuration/authenticator/nginx/fastcgi_socket.conf ] )
-	then
-		/bin/sed -i -e "/XXXXFASTCGIXXXX/{r ${HOME}/providerscripts/webserver/configuration/authenticator/nginx/fastcgi_socket.conf" -e "d}" /etc/nginx/sites-available/${WEBSITE_NAME}
-		/bin/sed -i "s/XXXXPHPVERSIONXXXX/${PHP_VERSION}/" /etc/nginx/sites-available/${WEBSITE_NAME}
-	fi
+        if ( [ -f ${HOME}/providerscripts/webserver/configuration/authenticator/apache2/fastcgi_socket.conf ] )
+        then
+                /bin/sed -i -e "/XXXXFASTCGIXXXX/{r ${HOME}/providerscripts/webserver/configuration/authenticator/apache2/fastcgi_socket.conf" -e "d}" /etc/apache2/sites-available/${WEBSITE_NAME}
+                /bin/sed -i "s/XXXXPHPVERSIONXXXX/${PHP_VERSION}/" /etc/apache2/sites-available/${WEBSITE_NAME}
+        fi
 else
-	if ( [ -f ${HOME}/providerscripts/webserver/configuration/authenticator/nginx/fastcgi_port.conf ] )
-	then
-		/bin/sed -i -e "/XXXXFASTCGIXXXX/{r ${HOME}/providerscripts/webserver/configuration/authenticator/nginx/fastcgi_port.conf" -e "d}" /etc/nginx/sites-available/${WEBSITE_NAME}
-		/bin/sed -i "s/XXXXPORTXXXX/${port}/" /etc/nginx/sites-available/${WEBSITE_NAME}
-	fi
+        if ( [ -f ${HOME}/providerscripts/webserver/configuration/authenticator/apache2/fastcgi_port.conf ] )
+        then
+                /bin/sed -i -e "/XXXXFASTCGIXXXX/{r ${HOME}/providerscripts/webserver/configuration/authenticator/apache2/fastcgi_port.conf" -e "d}" /etc/nginx/sites-available/${WEBSITE_NAME}
+                /bin/sed -i "s/XXXXPORTXXXX/${port}/" /etc/apache2/sites-available/${WEBSITE_NAME}
+        fi
 fi
 
-if ( [ ! -d /etc/nginx/sites-enabled ] )
+if ( [ ! -d /etc/apache2/sites-enabled ] )
 then
-  /bin/mkdir -p /etc/nginx/sites-enabled
+  /bin/mkdir -p /etc/apache2/sites-enabled
 fi
 
-/bin/ln -s /etc/nginx/sites-available/${WEBSITE_NAME} /etc/nginx/sites-enabled/${WEBSITE_NAME}
+/bin/ln -s /etc/apache2/sites-available/${WEBSITE_NAME} /etc/apache2/sites-enabled/${WEBSITE_NAME}
 
 /bin/rm -r /var/www/html/*
 /bin/cp ${HOME}/providerscripts/webserver/configuration/authenticator/index.php /var/www/html/index.php
