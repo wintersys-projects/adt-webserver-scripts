0a05b71ef75b5d0caf592f1aac6751cf1053fbf3
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-11 10:51:32 +0000

    Update InstallLighttpdConfigurationForAuthenticator.sh

--- providerscripts/webserver/configuration/InstallLighttpdConfigurationForAuthenticator.sh
+++ providerscripts/webserver/configuration/InstallLighttpdConfigurationForAuthenticator.sh
@@ -82,21 +82,23 @@ then
         /bin/chmod 600 /etc/lighttpd/modules.conf
 fi
 
-lighttpd_modules="`${HOME}/providerscripts/utilities/config/ExtractBuildStyleValues.sh "LIGHTTPD:modules-list" "stripped" | /bin/sed 's/|.*//g' | /bin/sed 's/:/ /g' | /bin/sed 's/modules-list//'`"
+/bin/cp ${HOME}/providerscripts/webserver/configuration/authenticator/lighttpd/modules.conf /etc/lighttpd/modules.conf
 
-if ( [ "${lighttpd_modules}" != "" ] )
-then
-        /bin/echo "server.modules = (" > /etc/lighttpd/modules.conf
+#lighttpd_modules="`${HOME}/providerscripts/utilities/config/ExtractBuildStyleValues.sh "LIGHTTPD:modules-list" "stripped" | /bin/sed 's/|.*//g' | /bin/sed 's/:/ /g' | /bin/sed 's/modules-list//'`"
 
-        for module in ${lighttpd_modules}
-        do
-                /bin/echo '"'${module}'",' >> /etc/lighttpd/modules.conf
-        done
-
-        /usr/bin/truncate -s -2 /etc/lighttpd/modules.conf
-        /bin/echo "" >> /etc/lighttpd/modules.conf
-        /bin/echo ")" >> /etc/lighttpd/modules.conf
-fi
+#if ( [ "${lighttpd_modules}" != "" ] )
+#then
+#        /bin/echo "server.modules = (" > /etc/lighttpd/modules.conf##
+#
+#        for module in ${lighttpd_modules}
+#        do
+#                /bin/echo '"'${module}'",' >> /etc/lighttpd/modules.conf
+#        done#
+#
+#        /usr/bin/truncate -s -2 /etc/lighttpd/modules.conf
+#        /bin/echo "" >> /etc/lighttpd/modules.conf
+#        /bin/echo ")" >> /etc/lighttpd/modules.conf
+#fi
 
 #config_settings="`${HOME}/providerscripts/utilities/config/ExtractBuildStyleValues.sh "LIGHTTPD:settings" "stripped" | /bin/sed 's/|.*//g' | /bin/sed 's/:/ /g'`"
 
