62ea53be1d4536145528b47ab6132b271830efde
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-11 13:01:57 +0000

    Update SetApplicationConfiguration.sh

--- providerscripts/application/configuration/joomla/SetApplicationConfiguration.sh
+++ providerscripts/application/configuration/joomla/SetApplicationConfiguration.sh
@@ -50,7 +50,7 @@ then
     	fi
 fi
 
-if ( [ "`${HOME}/providerscripts/datastore/configwrapper/AgeOfConfigFile.sh joomla_configuration.php`" -lt "130" ] || [ ! -f ${HOME}/runtime/INITIAL_CONFIG_SET ] )
+if ( [ "`${HOME}/providerscripts/datastore/configwrapper/AgeOfConfigFile.sh joomla_configuration.php`" -lt "130" ] || [ ! -f ${HOME}/runtime/INITIAL_CONFIG_SET ] || [ "`/usr/bin/diff /var/www/html/configuration.php ${HOME}/runtime/joomla_configuration.php`" != "" ] )
 then
 	${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh joomla_configuration.php ${HOME}/runtime/joomla_configuration.php
         if ( [ ! -f /var/www/html/configuration.php ] || [ "`/usr/bin/diff /var/www/html/configuration.php ${HOME}/runtime/joomla_configuration.php`" != "" ] )
