c816349779fd993c25a405efe87e27eba2667c60
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-11 13:03:23 +0000

    Update SetApplicationConfiguration.sh

--- providerscripts/application/configuration/moodle/SetApplicationConfiguration.sh
+++ providerscripts/application/configuration/moodle/SetApplicationConfiguration.sh
@@ -45,7 +45,7 @@ then
     	fi
 fi
 
-if ( [ "`${HOME}/providerscripts/datastore/configwrapper/AgeOfConfigFile.sh moodle_config.php`" -lt "130" ] || [ ! -f ${HOME}/runtime/INITIAL_CONFIG_SET ]  )
+if ( [ "`${HOME}/providerscripts/datastore/configwrapper/AgeOfConfigFile.sh moodle_config.php`" -lt "130" ] || [ ! -f ${HOME}/runtime/INITIAL_CONFIG_SET ] || [ "`/usr/bin/diff /var/www/html/moodle/config.php ${HOME}/runtime/moodle_config.php`" != "" ] )
 then
 	${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh moodle_config.php ${HOME}/runtime/moodle_config.php
         if ( [ ! -f /var/www/html/moodle/config.php ] || [ "`/usr/bin/diff /var/www/html/moodle/config.php ${HOME}/runtime/moodle_config.php`" != "" ] )
