4b6a9c4b27420c266e06c7d15372c192a2e820af
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-23 11:47:53 +0000

    Update InitialiseVirginInstall.sh

--- providerscripts/application/configuration/joomla/InitialiseVirginInstall.sh
+++ providerscripts/application/configuration/joomla/InitialiseVirginInstall.sh
@@ -24,41 +24,40 @@
 ##########################################################################################
 ##########################################################################################
 #set -x
-
+ 
 if ( ( [ ! -f /var/www/html/dbp.dat ] || [ "`/bin/cat /var/www/html/dbp.dat`" = "" ] ) && [ -f /var/www/html/configuration.php ] )
 then
-        dbprefix="`/bin/grep "dbprefix" /var/www/html/configuration.php | /usr/bin/awk -F"'" '{print $2}'`"
+	dbprefix="`/bin/grep "dbprefix" /var/www/html/configuration.php | /usr/bin/awk -F"'" '{print $2}'`"
 
-        if ( [ "${dbprefix}" = "" ] )
-        then
-                dbprefix="`/bin/grep "dbprefix" /var/www/html/configuration.php | /usr/bin/awk -F'"' '{print $2}'`"
-        fi
-
-        /bin/echo ${dbprefix} > /var/www/html/dbp.dat
-        /bin/chown www-data:www-data /var/www/html/dbp.dat
-        /bin/chmod 600 /var/www/html/dbp.dat
+	if ( [ "${dbprefix}" = "" ] )
+	then
+		dbprefix="`/bin/grep "dbprefix" /var/www/html/configuration.php | /usr/bin/awk -F'"' '{print $2}'`"
+	fi
+	/bin/echo ${dbprefix} > /var/www/html/dbp.dat
+	/bin/chown www-data:www-data /var/www/html/dbp.dat
+	/bin/chmod 600 /var/www/html/dbp.dat
 fi
 
 if ( ( [ ! -f /var/www/html/dbe.dat ] || [ "`/bin/cat /var/www/html/dbe.dat`" = "" ] ) && [ -f /var/www/html/configuration.php ] )
 then
-        if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh DATABASEDBaaSINSTALLATIONTYPE:Maria`" = "1" ] || [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:Maria`" = "1" ] )
-        then
-                /bin/echo "For your information this application requires Maria DB as its database" > /var/www/html/dbe.dat
-        fi
+	if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh DATABASEDBaaSINSTALLATIONTYPE:Maria`" = "1" ] || [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:Maria`" = "1" ] )
+	then
+		/bin/echo "For your information this application requires Maria DB as its database" > /var/www/html/dbe.dat
+	fi
 
-        if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh DATABASEDBaaSINSTALLATIONTYPE:MySQL`" = "1" ] || [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:MySQL`" = "1" ] )
-        then
-                /bin/echo "For your information this application requires MySQL as its database" > /var/www/html/dbe.dat
-        fi
+	if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh DATABASEDBaaSINSTALLATIONTYPE:MySQL`" = "1" ] || [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:MySQL`" = "1" ] )
+	then
+		/bin/echo "For your information this application requires MySQL as its database" > /var/www/html/dbe.dat
+	fi
 
-        if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh DATABASEDBaaSINSTALLATIONTYPE:Postgres`" = "1" ] || [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:Postgres`" = "1" ] )
-        then
-                /bin/echo "For your information this application requires Postgres as its database" > /var/www/html/dbe.dat
-        fi
-        
-        if ( [ -f /var/www/html/dbe.dat ] )
+	if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh DATABASEDBaaSINSTALLATIONTYPE:Postgres`" = "1" ] || [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:Postgres`" = "1" ] )
  	then
-        	/bin/chown www-data:www-data /var/www/html/dbe.dat
-        	/bin/chmod 600 /var/www/html/dbe.dat
+		/bin/echo "For your information this application requires Postgres as its database" > /var/www/html/dbe.dat
+	fi
+
+	if ( [ -f /var/www/html/dbe.dat ] )
+ 	then
+		/bin/chown www-data:www-data /var/www/html/dbe.dat
+		/bin/chmod 600 /var/www/html/dbe.dat
 	fi
 fi
