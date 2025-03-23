6181215980f99435edfdbf9072fbf878b2f7f4b5
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-23 18:52:25 +0000

    Update AgeOfDatastoreFile.sh

--- providerscripts/datastore/AgeOfDatastoreFile.sh
+++ providerscripts/datastore/AgeOfDatastoreFile.sh
@@ -21,7 +21,7 @@
 #set -x
 
 inspected_file="${1}"
-
+ 
 if ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd'`" = "1" ] )
 then
   time_file_written="`/usr/bin/s3cmd info s3://${inspected_file}| /bin/grep "Last mod" | /usr/bin/awk -F',' '{print $2}'`"
