72c748cb7eaa63f474c376c597283922ea2a2738
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-23 19:40:39 +0000

    Update MonitorCron.sh

--- providerscripts/utilities/status/MonitorCron.sh
+++ providerscripts/utilities/status/MonitorCron.sh
@@ -21,7 +21,7 @@
 #set -x
 
 /usr/bin/crontab -l >/dev/null
-
+ 
 if ( [ "$?" != "0" ] )
 then
 	${HOME}/providerscripts/email/SendEmail.sh "CRON COULD NOT BE LOADED" "Something must br wrong, cron is not loading which is a big problem I will reboot" "ERROR"
