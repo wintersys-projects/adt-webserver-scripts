a84535ff0cfb000bf1eeec37a438d633fdf32426
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-25 18:11:57 +0000

    Update RestartWebserver.sh

--- providerscripts/webserver/RestartWebserver.sh
+++ providerscripts/webserver/RestartWebserver.sh
@@ -18,7 +18,7 @@
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 #######################################################################################
 #######################################################################################
-set -x
+#set -x
 
 WEBSITE_URL="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
 
