989c4f6394ab15c45fd7faab5a459b75f1ab39b5
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-26 10:25:39 +0000

    Update DetermineApplicationType.sh

--- providerscripts/application/processing/moodle/DetermineApplicationType.sh
+++ providerscripts/application/processing/moodle/DetermineApplicationType.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-#################################################################################
-# Description: This script will interrogate the filesystem of the application to
-# determine which application type it is
+############################################################################################
+# Description: This script will determine what application type we are
 # Author: Peter Winter
 # Date: 10/01/2017
-##################################################################################
+############################################################################################
 # License Agreement:
 # This file is part of The Agile Deployment Toolkit.
 # The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
@@ -17,11 +16,16 @@
 # GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
-#################################################################################
-#################################################################################
+#############################################################################################
+#############################################################################################
 #set -x
 
-if ( [ -d /var/www/html/moodle/admin ] && [ -d /var/www/html/moodle/portfolio ] &&  [ -d /var/www/html/moodle/repository ] )
+while ( [ ! -f /var/www/html/dba.dat ] )
+do
+	/bin/sleep 5
+done
+
+if ( [ "`/bin/cat /var/www/html/dba.dat`" = "MOODLE" ] )
 then
 	${HOME}/providerscripts/utilities/config/StoreConfigValue.sh "APPLICATION" "moodle"
 fi
