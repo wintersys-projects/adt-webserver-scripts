f5ab2c39230691384ac644e285bd94409b884f43
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-27 11:43:04 +0000

    Update ApplicationConfigurationUpdate.sh

--- providerscripts/application/configuration/wordpress/ApplicationConfigurationUpdate.sh
+++ providerscripts/application/configuration/wordpress/ApplicationConfigurationUpdate.sh
@@ -28,4 +28,25 @@ then
 	/bin/echo "Syntax error detected in your configuration file"
 	exit
 fi
-/usr/bin/run ${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${HOME}/runtime/wordpress_config.php wordpress_config.php "no"
+
+if ( [ -f /var/www/html/configuration.php ] )
+then
+        /bin/cp /var/www/html/configuration.php ${HOME}/runtime/wp_config.php.hold.$$
+fi
+
+/bin/cp ${HOME}/runtime/wordpress_config.php /var/www/html/configuration.php
+/bin/chown www-data:www-data /var/www/html/configuration.php
+/bin/chmod 644 /var/www/html/configuration.php
+
+if ( [ "`/usr/bin/curl -m 2 --insecure -I "https://localhost:443/index.php" 2>&1 | /bin/grep \"HTTP\" | /bin/grep -w \"200\|301\|302\|303\"`" != "" ] ) 
+then
+        /bin/echo "I am distributing your suggested configuration file as I verified it suitable"
+        /usr/bin/run ${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${HOME}/runtime/joomla_configuration.php joomla_configuration.php "no"
+        /bin/rm ${HOME}/runtime/wp_config.php.hold.$$
+else
+        /bin/echo "I am not distributing the configuration file you suggested, I found it to have a problem"
+        /bin/echo "Your configuration remains as it originally was"
+        /bin/mv ${HOME}/runtime/wp_config.php.hold.$$ /var/www/html/configuration.php
+        /bin/chown www-data:www-data /var/www/html/configuration.php
+        /bin/chmod 644 /var/www/html/configuration.php
+fi
