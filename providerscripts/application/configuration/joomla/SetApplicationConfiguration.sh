22eae099050ee08fed27d714f9f6d00b89d7aa16
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-27 10:28:52 +0000

    Update SetApplicationConfiguration.sh

--- providerscripts/application/configuration/joomla/SetApplicationConfiguration.sh
+++ providerscripts/application/configuration/joomla/SetApplicationConfiguration.sh
@@ -33,12 +33,12 @@ fi
 
 if ( [ -f ${HOME}/runtime/joomla_configuration.php ] )
 then
-	/bin/chown www-data:www-data ${HOME}/runtime/joomla_configuration.php
-	/bin/chmod 400 ${HOME}/runtime/joomla_configuration.php
+	/bin/chown www-data:root ${HOME}/runtime/joomla_configuration.php
+	/bin/chmod 440 ${HOME}/runtime/joomla_configuration.php
 else
 	${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh joomla_configuration.php ${HOME}/runtime/joomla_configuration.php
-	/bin/chown www-data:www-data ${HOME}/runtime/joomla_configuration.php
-	/bin/chmod 400 ${HOME}/runtime/joomla_configuration.php
+	/bin/chown www-data:root ${HOME}/runtime/joomla_configuration.php
+	/bin/chmod 440 ${HOME}/runtime/joomla_configuration.php
 fi
 
 if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`" = "1" ] )
