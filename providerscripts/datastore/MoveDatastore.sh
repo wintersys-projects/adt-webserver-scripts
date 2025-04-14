bfd3f3549a89caff445437483c588758f5235416
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-04-15 00:45:25 +0100

    Update MoveDatastore.sh

--- providerscripts/datastore/MoveDatastore.sh
+++ providerscripts/datastore/MoveDatastore.sh
@@ -32,13 +32,16 @@ then
         datastore_tool="/usr/bin/s5cmd --credentials-file /root/.s5cfg --endpoint-url https://${host_base} "
 fi
 
-count="0"
-while ( [ "`${datastore_tool} mv s3://${original_object} s3://${new_object} 2>&1 >/dev/null | /bin/grep "ERROR"`" != "" ] && [ "${count}" -lt "5" ] )
-do
-        /bin/echo "An error has occured `/usr/bin/expr ${count} + 1` times in script ${0}"
-        /bin/sleep 5
-        count="`/usr/bin/expr ${count} + 1`"
-done 
+if ( [ "`${datastore_tool} ls s3://${original_object}`" != "" ] )
+then
+        count="0"
+        while ( [ "`${datastore_tool} mv s3://${original_object} s3://${new_object} 2>&1 >/dev/null | /bin/grep "ERROR"`" != "" ] && [ "${count}" -lt "5" ] )
+        do
+                /bin/echo "An error has occured `/usr/bin/expr ${count} + 1` times in script ${0}"
+                /bin/sleep 5
+                count="`/usr/bin/expr ${count} + 1`"
+        done
+fi
 
 
 
