6fd52450664209899ed03e1d2100c86e47ad5604
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-04-15 00:42:34 +0100

    Update AgeOfDatastoreFile.sh

--- providerscripts/datastore/AgeOfDatastoreFile.sh
+++ providerscripts/datastore/AgeOfDatastoreFile.sh
@@ -24,10 +24,11 @@ inspected_file="${1}"
  
 if ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd'`" = "1" ] )
 then
-  time_file_written="`/usr/bin/s3cmd info s3://${inspected_file}| /bin/grep "Last mod" | /usr/bin/awk -F',' '{print $2}'`"
+	time_file_written="`/usr/bin/s3cmd info s3://${inspected_file}| /bin/grep "Last mod" | /usr/bin/awk -F',' '{print $2}'`"
 elif ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s5cmd'`" = "1" ]  )
 then
-  time_file_written="`/usr/bin/s5cmd ls s3://${inspected_file} | /usr/bin/awk '{print $1,$2}'`"
+	host_base="`/bin/grep host_base /root/.s5cfg | /bin/grep host_base | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
+	time_file_written="`/usr/bin/s5cmd --credentials-file /root/.s5cfg --endpoint-url https://${host_base} ls s3://${inspected_file} | /usr/bin/awk '{print $1,$2}'`"
 fi
 
 time_file_written="`/usr/bin/date -d "${time_file_written}" +%s`"
