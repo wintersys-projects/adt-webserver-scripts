7b38ab345feb183f3e32544dab336d961cb7182d
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-04-17 11:25:05 +0100

    Update ws.sh

--- ws.sh
+++ ws.sh
@@ -189,3 +189,6 @@ ${HOME}/providerscripts/webserver/RestartWebserver.sh
 
 /bin/echo "${0} Updating Software"
 ${HOME}/installscripts/UpdateAndUpgrade.sh ${BUILDOS} &
+
+/bin/echo "${0} Restarting Webserver"
+${HOME}/providerscripts/webserver/RestartWebserver.sh
