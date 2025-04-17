73b51c34b1e184d10b9da400bfe0997cb8cd1cbb
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-04-17 21:36:32 +0100

    Update UpdateAndUpgrade.sh

--- installscripts/UpdateAndUpgrade.sh
+++ installscripts/UpdateAndUpgrade.sh
@@ -21,12 +21,12 @@
 
 HOME="`/bin/cat /home/homedir.dat`"
 
+${HOME}/installscripts/Update.sh ${1}
+${HOME}/installscripts/Upgrade.sh ${1}
+
 /bin/rm -r /var/www/html/client_body_temp 2>/dev/null
 /bin/rm -r /var/www/html/fastcgi_temp 2>/dev/null
 /bin/rm -r /var/www/html/proxy_temp 2>/dev/null
 /bin/rm -r /var/www/html/scgi_temp 2>/dev/null
 /bin/rm -r /var/www/html/uwsgi_temp 2>/dev/null
 /bin/rm -r /var/www/html/html 2>/dev/null
-
-${HOME}/installscripts/Update.sh ${1}
-${HOME}/installscripts/Upgrade.sh ${1}
