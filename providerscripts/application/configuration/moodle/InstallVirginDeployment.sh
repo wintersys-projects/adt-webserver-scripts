242f46f355577477e319b6118577ac0ff68eb8f8
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-04-17 18:34:42 +0100

    Update InstallVirginDeployment.sh

--- providerscripts/application/configuration/moodle/InstallVirginDeployment.sh
+++ providerscripts/application/configuration/moodle/InstallVirginDeployment.sh
@@ -21,17 +21,11 @@
 #set -x
 
 /usr/bin/git clone https://github.com/moodle/moodle.git /var/www/html/moodle
-
-#version="`/bin/echo ${application} | /usr/bin/awk -F':' '{print $NF}'`"
-#cd /var/www/html
-#/usr/bin/git clone git://git.moodle.org/moodle.git
-#cd moodle
-#branch=`/usr/bin/git branch -r | /bin/grep STABLE | /usr/bin/sort --version-sort | /bin/grep -v master | /usr/bin/tail -n 1 | /bin/sed 's/.*\///'`
-#cd ..
-#/bin/rm -r moodle
-#/usr/bin/git clone -b ${branch} git://git.moodle.org/moodle.git
-#/bin/chown -R www-data:www-data /var/www
-#/bin/rm -r /var/www/html/moodle/.git
-#/bin/echo "success"
+cd moodle
+branch=`/usr/bin/git branch -r | /bin/grep STABLE | /usr/bin/sort --version-sort | /bin/grep -v master | /usr/bin/tail -n 1 | /bin/sed 's/.*\///'`
+cd ..
+/bin/rm -r moodle
+/usr/bin/git clone -b ${branch} git://git.moodle.org/moodle.git /var/www/html/moodle
+/bin/rm -r /var/www/html/moodle/.git
 
 
