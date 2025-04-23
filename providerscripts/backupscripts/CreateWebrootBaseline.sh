b22314d742ecd026f1fe90d9919474657260cdcb
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-04-23 10:38:28 +0100

    Update CreateWebrootBaseline.sh

--- providerscripts/backupscripts/CreateWebrootBaseline.sh
+++ providerscripts/backupscripts/CreateWebrootBaseline.sh
@@ -115,11 +115,6 @@ ${HOME}/providerscripts/application/customise/UnCustomiseBackupByApplication.sh
 
 exec 1>>/dev/tty
 
-/bin/echo "==============DO YOU NEED ASSETS TO BE SETUP?????========================================================================="
-/bin/echo "Depending on your deployment plans, you might want the assets of your new baseline to be mounted from your S3 object store"
-/bin/echo "If that is the case, you should run ${HOME}/providerscripts/datastore/assets/SetupAssetsStore.sh on this machine"
-/bin/echo "NOTE: this will use the setting DIRECTORIES_TO_MOUNT to determine which assets need to be stored in your S3 datastore"
-/bin/echo "==============================================================================================================================="
 
 /bin/echo ""
 /bin/echo "==================================================================================================================================="
