034df4a3baaa2e4398be4ecab7f3717519cf2ab7
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-04-25 09:31:49 +0100

    Update PutBackupToDatastore.sh

--- providerscripts/datastore/PutBackupToDatastore.sh
+++ providerscripts/datastore/PutBackupToDatastore.sh
@@ -29,33 +29,39 @@ datastore_to_put_in="$2"
 
 datastore_regions="`${HOME}/providerscripts/utilities/config/ExtractConfigValues.sh 'S3HOSTBASE' 'stripped' | /usr/bin/tr '\n' ' ' | /bin/sed 's/  / /g' | /bin/sed 's/config//g'`"
 count="0"
+suffix=""
 for datastore_region in ${datastore_regions}
 do
-	if ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd'`" = "1" ] )
-	then
-		if ( [ "${count}" = "0" ] )
-		then
-			config_file="${HOME}/.s3cfg" 
-		else
-			config_file="${HOME}/.s3cfg-${count}"
-		fi
-		/usr/bin/s3cmd --config=${config_file}  mb s3://${datastore_to_put_in}
-		datastore_tool="/usr/bin/s3cmd --force --recursive --multipart-chunk-size-mb=5 --config=${config_file} put "
-	elif ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s5cmd'`" = "1" ]  )
-	then
-		host_base="`/bin/grep host_base /root/.s5cfg | /bin/grep host_base | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
-		/usr/bin/s5cmd --credentials-file /root/.s5cfg --endpoint-url https://${datastore_region} mb s3://${datastore_to_put_in}
-		datastore_tool="/usr/bin/s5cmd --credentials-file /root/.s5cfg --endpoint-url https://${datastore_region} cp "
-	fi
+        if ( [ "${count}" != "0" ] )
+        then
+                suffix="-${count}"
+        fi
 
-	count="`/usr/bin/expr ${count} + 1`"
-	${HOME}/providerscripts/datastore/MountDatastore.sh ${datastore_to_put_in}
+        if ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd'`" = "1" ] )
+        then
+                if ( [ "${count}" = "0" ] )
+                then
+                        config_file="${HOME}/.s3cfg" 
+                else
+                        config_file="${HOME}/.s3cfg-${count}"
+                fi
+                /usr/bin/s3cmd --config=${config_file}  mb s3://${datastore_to_put_in}${suffix}
+                datastore_tool="/usr/bin/s3cmd --force --recursive --multipart-chunk-size-mb=5 --config=${config_file} put "
+        elif ( [ "`${HOME}/providerscripts/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s5cmd'`" = "1" ]  )
+        then
+                host_base="`/bin/grep host_base /root/.s5cfg | /bin/grep host_base | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
+                /usr/bin/s5cmd --credentials-file /root/.s5cfg --endpoint-url https://${datastore_region} mb s3://${datastore_to_put_in}${suffix}
+                datastore_tool="/usr/bin/s5cmd --credentials-file /root/.s5cfg --endpoint-url https://${datastore_region} cp "
+        fi
 
-	count1="0"
-	while ( [ "`${datastore_tool} ${file_to_put} s3://${datastore_to_put_in} 2>&1 >/dev/null | /bin/grep "ERROR"`" != "" ] && [ "${count1}" -lt "5" ] )
-	do
-		/bin/echo "An error has occured `/usr/bin/expr ${count1} + 1` times in script ${0}"
-		/bin/sleep 5
-		count1="`/usr/bin/expr ${count1} + 1`"
-	done 
+        count="`/usr/bin/expr ${count} + 1`"
+        ${HOME}/providerscripts/datastore/MountDatastore.sh ${datastore_to_put_in}
+
+        count1="0"
+        while ( [ "`${datastore_tool} ${file_to_put} s3://${datastore_to_put_in}${suffix} 2>&1 >/dev/null | /bin/grep "ERROR"`" != "" ] && [ "${count1}" -lt "5" ] )
+        do
+                /bin/echo "An error has occured `/usr/bin/expr ${count1} + 1` times in script ${0}"
+                /bin/sleep 5
+                count1="`/usr/bin/expr ${count1} + 1`"
+        done 
 done
