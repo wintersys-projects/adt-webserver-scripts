cd5c5a397aa3aa97ee7955ab1fe7288f51aa0406
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-05-01 15:47:28 +0100

    Update ws.sh

--- ws.sh
+++ ws.sh
@@ -111,14 +111,14 @@ then
 	/bin/echo "${0} Installing the bespoke application"
 	${HOME}/providerscripts/application/InstallApplication.sh
 
-	if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh BUILDARCHIVECHOICE:baseline`" = "1" ] )
-	then
-		/bin/echo "${0} Applying application specific customisations as this is a baseline build"
-		${HOME}/providerscripts/application/branding/ApplyApplicationBranding.sh
-		${HOME}/providerscripts/application/customise/CustomiseApplication.sh
-	fi
-	${HOME}/providerscripts/datastore/assets/SetupAssetsStore.sh
-else
+	#if ( [ "`${HOME}/providerscripts/utilities/config/CheckConfigValue.sh BUILDARCHIVECHOICE:baseline`" = "1" ] )
+	#then
+#		/bin/echo "${0} Applying application specific customisations as this is a baseline build"#
+#		${HOME}/providerscripts/application/branding/ApplyApplicationBranding.sh
+#		${HOME}/providerscripts/application/customise/CustomiseApplication.sh
+#	fi
+#	${HOME}/providerscripts/datastore/assets/SetupAssetsStore.sh
+#else
 	${HOME}/providerscripts/datastore/assets/SetupAssetsStore.sh
 	/bin/touch ${HOME}/runtime/BESPOKE_APPLICATION_INSTALLED
 fi
