3c0532a2c91e2ca01399cb7c0ecb8916fe50081e
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-05-11 17:10:47 +0100

    Update CheckWebserverIsUp.sh

--- providerscripts/webserver/CheckWebserverIsUp.sh
+++ providerscripts/webserver/CheckWebserverIsUp.sh
@@ -74,7 +74,7 @@ then
 	then
     		${HOME}/providerscripts/utilities/processing/RunServiceCommand.sh php${PHP_VERSION}-fpm restart && . /etc/apache2/conf/envvars && /usr/local/apache2/bin/apachectl -k restart 
 	fi
-	if ( [ "`/usr/bin/ps -ef | /bin/grep 'apache2 ' | /bin/grep -v grep`" = "" ] )
+	if ( [ "`/usr/bin/ps -ef | /bin/grep 'apache2' | /bin/grep -v grep`" = "" ] )
 	then
 		${HOME}/providerscripts/utilities/processing/RunServiceCommand.sh apache2 restart
   
