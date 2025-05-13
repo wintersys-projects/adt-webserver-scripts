398135d0805a3e9108045324a146fce8a22c13d6
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-05-13 22:35:44 +0100

    Update SyncWebroots.sh

--- providerscripts/utilities/housekeeping/SyncWebroots.sh
+++ providerscripts/utilities/housekeeping/SyncWebroots.sh
@@ -107,6 +107,7 @@ eval ${find_new_command} > ${HOME}/runtime/webroot_audit/webroot_file_list.dat.m
 if ( [ -s ${HOME}/runtime/webroot_audit/webroot_file_list.dat.updates ] )
 then
     /usr/bin/tar cfzp ${HOME}/runtime/webroot_audit/webroot_updates.${machine_ip}.tar.gz -T ${HOME}/runtime/webroot_audit/webroot_file_list.dat.updates --owner=www-data --group=www-data
+    ######do this but append to all timers
 fi
 
 if ( [ -s ${HOME}/runtime/webroot_audit/webroot_file_list.dat.updates ] || [ -s ${HOME}/runtime/webroot_audit/webroot_file_list.dat.modified ] || [ -s ${HOME}/runtime/webroot_audit/webroot_file_list.dat.deleted ] )
