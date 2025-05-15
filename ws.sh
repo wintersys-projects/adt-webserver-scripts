ddee9407973db491ec9de2e560e41f3b1e2c3935
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-05-15 19:38:13 +0100

    Update ws.sh

--- ws.sh
+++ ws.sh
@@ -21,7 +21,9 @@
 
 #Get ourselves orientated so we know where our home is
 USER_HOME="`/usr/bin/awk -F: '{ print $1}' /etc/passwd | /bin/grep "X*X"`"
-export HOME="/home/${USER_HOME}" | /usr/bin/tee -a ~/.bashrc
+/bin/echo 'export HOME="/home/'${USER_HOME}'"' >> /home/${USER_HOME}/.bashrc
+/bin/chmod 644 /home/${USER_HOME}/.bashrc
+/bin/chown ${USER_HOME}:root /home/${USER_HOME}/.bashrc
 
 /bin/echo "set mouse=r" > /root/.vimrc
 
