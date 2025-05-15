46ebb65e21d8d8c6991b46e1c81f89c5abb738eb
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-05-15 09:40:48 +0100

    Update InitialiseDatastoreConfig.sh

--- providerscripts/datastore/InitialiseDatastoreConfig.sh
+++ providerscripts/datastore/InitialiseDatastoreConfig.sh
@@ -21,7 +21,7 @@
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 ######################################################################################
 ######################################################################################
-#set -x
+set -x
 
 export HOME="`/bin/cat /home/homedir.dat`"
 SERVER_USER="`${HOME}/providerscripts/utilities/config/ExtractConfigValue.sh 'SERVERUSER'`"
