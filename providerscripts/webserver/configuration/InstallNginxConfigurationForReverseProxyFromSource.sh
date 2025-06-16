7d496bd30f60ed6113c06fb555edea54ec6dcf20
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-16 22:11:47 +0100

    Update InstallNginxConfigurationForReverseProxyFromSource.sh

--- providerscripts/webserver/configuration/InstallNginxConfigurationForReverseProxyFromSource.sh
+++ providerscripts/webserver/configuration/InstallNginxConfigurationForReverseProxyFromSource.sh
@@ -68,22 +68,22 @@ fi
 
 /usr/bin/openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
 
-port="`${HOME}/utilities/config/ExtractBuildStyleValues.sh "PHP" "stripped" | /usr/bin/awk -F'|' '{print $NF}'`"
+#port="`${HOME}/utilities/config/ExtractBuildStyleValues.sh "PHP" "stripped" | /usr/bin/awk -F'|' '{print $NF}'`"
 
-if ( [ "`/bin/echo ${port} | /bin/grep -o "^[0-9]*$"`" = "" ] )
-then
-        if ( [ -f ${HOME}/providerscripts/webserver/configuration/reverseproxy/nginx/online/source/fastcgi_socket.conf ] )
-        then
-                /bin/sed -i -e "/XXXXFASTCGIXXXX/{r ${HOME}/providerscripts/webserver/configuration/reverseproxy/nginx/online/source/fastcgi_socket.conf" -e "d}" /etc/nginx/sites-available/${WEBSITE_NAME}
-                /bin/sed -i "s/XXXXPHPVERSIONXXXX/${PHP_VERSION}/" /etc/nginx/sites-available/${WEBSITE_NAME}
-        fi
-else
-        if ( [ -f ${HOME}/providerscripts/webserver/configuration/reverseproxy/nginx/online/source/fastcgi_port.conf ] )
-        then
-                /bin/sed -i -e "/XXXXFASTCGIXXXX/{r ${HOME}/providerscripts/webserver/configuration/reverseproxy/nginx/online/source/fastcgi_port.conf" -e "d}" /etc/nginx/sites-available/${WEBSITE_NAME}
-                /bin/sed -i "s/XXXXPORTXXXX/${port}/" /etc/nginx/sites-available/${WEBSITE_NAME}
-        fi
-fi
+#if ( [ "`/bin/echo ${port} | /bin/grep -o "^[0-9]*$"`" = "" ] )
+#then
+#        if ( [ -f ${HOME}/providerscripts/webserver/configuration/reverseproxy/nginx/online/source/fastcgi_socket.conf ] )
+#        then
+#                /bin/sed -i -e "/XXXXFASTCGIXXXX/{r ${HOME}/providerscripts/webserver/configuration/reverseproxy/nginx/online/source/fastcgi_socket.conf" -e "d}" /etc/nginx/sites-available/${WEBSITE_NAME}
+#                /bin/sed -i "s/XXXXPHPVERSIONXXXX/${PHP_VERSION}/" /etc/nginx/sites-available/${WEBSITE_NAME}
+#        fi
+#else
+#        if ( [ -f ${HOME}/providerscripts/webserver/configuration/reverseproxy/nginx/online/source/fastcgi_port.conf ] )
+#        then
+#                /bin/sed -i -e "/XXXXFASTCGIXXXX/{r ${HOME}/providerscripts/webserver/configuration/reverseproxy/nginx/online/source/fastcgi_port.conf" -e "d}" /etc/nginx/sites-available/${WEBSITE_NAME}
+#                /bin/sed -i "s/XXXXPORTXXXX/${port}/" /etc/nginx/sites-available/${WEBSITE_NAME}
+#        fi
+#fi
 
 if ( [ -f /etc/nginx/sites-available/${WEBSITE_NAME} ] )
 then
