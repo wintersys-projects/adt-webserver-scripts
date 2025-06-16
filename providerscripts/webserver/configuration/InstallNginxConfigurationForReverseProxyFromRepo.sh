2e287bf79dc68458cc91401cef0f84c961830447
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-16 22:51:35 +0100

    Update InstallNginxConfigurationForReverseProxyFromRepo.sh

--- providerscripts/webserver/configuration/InstallNginxConfigurationForReverseProxyFromRepo.sh
+++ providerscripts/webserver/configuration/InstallNginxConfigurationForReverseProxyFromRepo.sh
@@ -62,23 +62,6 @@ fi
 
 /usr/bin/openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
 
-#port="`${HOME}/utilities/config/ExtractBuildStyleValues.sh "PHP" "stripped" | /usr/bin/awk -F'|' '{print $NF}'`"
-
-#if ( [ "`/bin/echo ${port} | /bin/grep -o "^[0-9]*$"`" = "" ] )
-#then
-#        if ( [ -f ${HOME}/providerscripts/webserver/configuration/reverseproxy/nginx/online/repo/fastcgi_socket.conf ] )
-#        then
-#                /bin/sed -i -e "/XXXXFASTCGIXXXX/{r ${HOME}/providerscripts/webserver/configuration/reverseproxy/nginx/online/repo/fastcgi_socket.conf" -e "d}" /etc/nginx/sites-available/${WEBSITE_NAME}
-#                /bin/sed -i "s/XXXXPHPVERSIONXXXX/${PHP_VERSION}/" /etc/nginx/sites-available/${WEBSITE_NAME}
-#        fi
-#else
-#        if ( [ -f ${HOME}/providerscripts/webserver/configuration/reverseproxy/nginx/online/repo/fastcgi_port.conf ] )
-#        then
-#                /bin/sed -i -e "/XXXXFASTCGIXXXX/{r ${HOME}/providerscripts/webserver/configuration/reverseproxy/nginx/online/repo/fastcgi_port.conf" -e "d}" /etc/nginx/sites-available/${WEBSITE_NAME}
-#                /bin/sed -i "s/XXXXPORTXXXX/${port}/" /etc/nginx/sites-available/${WEBSITE_NAME}
-#        fi
-#fi
-
 if ( [ -f /etc/nginx/sites-available/${WEBSITE_NAME} ] )
 then
         /bin/chmod 600 /etc/nginx/sites-available/${WEBSITE_NAME}
