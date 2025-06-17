15e73be7cc21b4fb27bccb836244de95dd9beaa2
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-17 10:25:24 +0100

    Update InstallApacheConfigurationForAuthenticatorFromSource.sh

--- providerscripts/webserver/configuration/InstallApacheConfigurationForAuthenticatorFromSource.sh
+++ providerscripts/webserver/configuration/InstallApacheConfigurationForAuthenticatorFromSource.sh
@@ -27,6 +27,8 @@ WEBSITE_NAME="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEDISPLAYNAM
 DNS_CHOICE="`${HOME}/utilities/config/ExtractConfigValue.sh 'DNSCHOICE'`"
 PHP_VERSION="`${HOME}/utilities/config/ExtractConfigValue.sh 'PHPVERSION'`"
 USER_EMAIL_DOMAIN="`${HOME}/utilities/config/ExtractConfigValue.sh 'USEREMAILDOMAIN'`"
+MOD_SECURITY="`${HOME}/utilities/config/ExtractConfigValue.sh 'MODSECURITY'`"
+
 
 /usr/sbin/a2dismod mpm_prefork
 
@@ -79,6 +81,11 @@ fi
 
 /bin/ln -s /etc/apache2/sites-available/${WEBSITE_NAME}.conf /etc/apache2/sites-enabled/${WEBSITE_NAME}
 /bin/chown -R www-data:www-data /etc/apache2
+
+if ( [ "${MOD_SECURITY}" = "1" ] )
+then
+        /bin/sed -i -e "/#XXXXMODSECURITYXXXX/{r ${HOME}/providerscripts/webserver/configuration/authenticator/apache/online/source/modsecurity.conf" -e "d}" /etc/apache2/sites-available/${WEBSITE_NAME}
+fi
 
 /bin/rm -r /var/www/html/*
 /bin/cp ${HOME}/providerscripts/webserver/configuration/authenticator/index.php /var/www/html/index.php
