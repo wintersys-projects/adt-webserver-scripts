9ef84ddd6599339f7b56b70c0b3149fc7fa80f22
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-17 07:11:19 +0100

    Create InstallModSecurityFromRepo.sh

---
+++ installscripts/modsecurity/InstallModSecurityFromRepo.sh
@@ -0,0 +1,22 @@
+apt -qq -y install libapache2-mod-security2
+if ( [ ! -d /etc/apache2/mods-enabled ] )
+then
+        /bin/mkdir /etc/apache2/mods-enabled
+fi
+a2enmod security2
+cp /etc/modsecurity/modsecurity.conf-recommended /etc/modsecurity/modsecurity.conf
+
+/bin/sed -i 's/^SecRuleEngine.*/SecRuleEngine On/' /etc/modsecurity/modsecurity.conf
+/bin/sed -i 's/^SecResponseBodyAccess.*/SecResponseBodyAccess Off/' /etc/modsecurity/modsecurity.conf
+/bin/sed -i 's/^SecRequestBodyLimit.*/SecRequestBodyLimit 74448896/' /etc/modsecurity/modsecurity.conf
+
+git clone https://github.com/coreruleset/coreruleset.git
+
+cd coreruleset/
+
+mv crs-setup.conf.example /etc/modsecurity/crs-setup.conf
+
+mv rules/ /etc/modsecurity/
+
+/bin/echo "IncludeOptional /etc/modsecurity/*.conf" >> /etc/apache2/mods-enabled/security2.conf
+/bin/echo "Include /etc/modsecurity/rules/*.conf" >> /etc/apache2/mods-enabled/security2.conf
