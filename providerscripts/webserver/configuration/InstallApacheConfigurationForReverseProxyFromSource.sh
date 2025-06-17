b36337ced2e8068439357edb3d24ab343d5686d5
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-17 14:12:20 +0100

    Update InstallApacheConfigurationForReverseProxyFromSource.sh

--- providerscripts/webserver/configuration/InstallApacheConfigurationForReverseProxyFromSource.sh
+++ providerscripts/webserver/configuration/InstallApacheConfigurationForReverseProxyFromSource.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-###########################################################################################################
-# Description: This will install the apache configuration for an authentication machine from source
+#################################################################################
 # Author : Peter Winter
-# Date: 17/05/2017
-######################################################################################################
+# Date   : 13/07/2016
+# Description : This script will provide a base installation of Apache from source. You are
+# welcome to modify it to your needs.
+#################################################################################
 # License Agreement:
 # This file is part of The Agile Deployment Toolkit.
 # The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
@@ -16,96 +17,80 @@
 # GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
-#######################################################################################################
-#######################################################################################################
+################################################################################
+################################################################################
+#set -x
 
-HOME="`/bin/cat /home/homedir.dat`"
+BUILDOS="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
+PHP_VERSION="`${HOME}/utilities/config/ExtractConfigValue.sh 'PHPVERSION'`"
+WEBSITE_NAME="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEDISPLAYNAME'`"
 WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
 ROOT_DOMAIN="`/bin/echo ${WEBSITE_URL} | /usr/bin/awk -F'.' '{$1=""}1' | /bin/sed 's/^.//' | /bin/sed 's/ /\./g'`"
-WEBSITE_NAME="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEDISPLAYNAME'`"
-DNS_CHOICE="`${HOME}/utilities/config/ExtractConfigValue.sh 'DNSCHOICE'`"
-PHP_VERSION="`${HOME}/utilities/config/ExtractConfigValue.sh 'PHPVERSION'`"
-USER_EMAIL_DOMAIN="`${HOME}/utilities/config/ExtractConfigValue.sh 'USEREMAILDOMAIN'`"
 MOD_SECURITY="`${HOME}/utilities/config/ExtractConfigValue.sh 'MODSECURITY'`"
+NO_REVERSE_PROXY="`${HOME}/utilities/config/ExtractConfigValue.sh 'NOREVERSEPROXY'`"
 
-/usr/sbin/a2dismod mpm_prefork
 
-apache_modules="`${HOME}/utilities/config/ExtractBuildStyleValues.sh "APACHE:modules-list" "stripped" | /bin/sed 's/|.*//g' | /bin/sed 's/:/ /g' | /bin/sed 's/modules-list//g'`"
-for module in ${apache_modules}
-do
-    /usr/sbin/a2enmod ${module}
-    /usr/sbin/a2enconf ${module}
-done
 
-if ( [ -f /etc/apache2/ports.conf ] )
-then
-    /bin/sed -i 's/^Listen 80/#Listen 80/g' /etc/apache2/ports.conf
-fi
+#Install configuration values for apache
+/bin/cp ${HOME}/providerscripts/webserver/configuration/reverseproxy/apache/online/source/httpd.conf /etc/apache2/httpd.conf
+/bin/cp ${HOME}/providerscripts/webserver/configuration/reverseproxy/apache/online/source/envvars.conf /etc/apache2/envvars
+/bin/cp ${HOME}/providerscripts/webserver/configuration/reverseproxy/apache/online/source/ports.conf /etc/apache2/ports.conf
+/bin/cp ${HOME}/providerscripts/webserver/configuration/reverseproxy/apache/online/source/init.d.conf /etc/init.d/apache2
 
-if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh APPLICATIONLANGUAGE:PHP`" = "1" ] )
-then
-    /usr/sbin/a2enmod php${PHP_VERSION}-fpm
-    /usr/sbin/a2enconf php${PHP_VERSION}-fpm
-fi
-
-/usr/bin/openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
-
-/bin/cp ${HOME}/providerscripts/webserver/configuration/reverseproxy/apache/online/source/apache2.conf /etc/apache2
-/bin/chown www-data:www-data /etc/apache2/apache2.conf
-/bin/chmod 644 /etc/apache2/apache2.conf
-
-/bin/rm /etc/apache2/sites-enabled/* 2>/dev/null
-
-if ( [ ! -d /etc/apache2/sites-available ] )
-then
-    /bin/mkdir -p /etc/apache2/sites-available
-fi
-
-/bin/cp ${HOME}/providerscripts/webserver/configuration/reverseproxy/apache/online/source/site-available.conf /etc/apache2/sites-available/${WEBSITE_NAME}.conf
-/bin/chown www-data:www-data /etc/apache2/sites-available/${WEBSITE_NAME}.conf
-/bin/chmod 644 /etc/apache2/sites-available/${WEBSITE_NAME}.conf
-
-/bin/echo "/etc/apache2/sites-available/${WEBSITE_NAME}.conf" > ${HOME}/runtime/WEBSERVER_CONFIG_LOCATION.dat
-
-/bin/sed -i "s/XXXXWEBSITEURLXXXX/${WEBSITE_URL}/g" /etc/apache2/sites-available/${WEBSITE_NAME}.conf
-/bin/sed -i "s,XXXXHOMEXXXX,${HOME},g" /etc/apache2/sites-available/${WEBSITE_NAME}.conf
-
-port="`${HOME}/utilities/config/ExtractBuildStyleValues.sh "PHP" "stripped" | /usr/bin/awk -F'|' '{print $NF}'`"
-
-if ( [ "`/bin/echo ${port} | /bin/grep -o "^[0-9]*$"`" = "" ] )
-then
-    if ( [ -f ${HOME}/providerscripts/webserver/configuration/reverseproxy/apache/online/source/fastcgi_socket.conf ] )
-    then
-        /bin/sed -i -e "/XXXXFASTCGIXXXX/{r ${HOME}/providerscripts/webserver/configuration/reverseproxy/apache/online/source/fastcgi_socket.conf" -e "d}" /etc/apache2/sites-available/${WEBSITE_NAME}.conf
-        /bin/sed -i "s/XXXXPHPVERSIONXXXX/${PHP_VERSION}/" /etc/apache2/sites-available/${WEBSITE_NAME}.conf
-    fi
-else
-    if ( [ -f ${HOME}/providerscripts/webserver/configuration/reverseproxy/apache/online/source/fastcgi_port.conf ] )
-    then
-        /bin/sed -i -e "/XXXXFASTCGIXXXX/{r ${HOME}/providerscripts/webserver/configuration/reverseproxy/apache/online/source/fastcgi_port.conf" -e "d}" /etc/apache2/sites-available/${WEBSITE_NAME}.conf
-        /bin/sed -i "s/XXXXPORTXXXX/${port}/" /etc/apache2/sites-available/${WEBSITE_NAME}.conf
-    fi
-fi
-
-/bin/sed -i "s/XXXXROOTDOMAINXXXX/${ROOT_DOMAIN}/" /etc/apache2/sites-available/${WEBSITE_NAME}.conf
+/bin/sed -i "s/^#ServerRoot.*/ServerRoot \"\/etc\/apache2\"/g" /etc/apache2/httpd.conf
 
 if ( [ ! -d /etc/apache2/sites-enabled ] )
 then
-    /bin/mkdir -p /etc/apache2/sites-enabled
+	/bin/mkdir -p /etc/apache2/sites-enabled
 fi
 
-/bin/ln -s /etc/apache2/sites-available/${WEBSITE_NAME}.conf /etc/apache2/sites-enabled/${WEBSITE_NAME}
-/bin/chown -R www-data:www-data /etc/apache2
+if ( [ ! -d /etc/apache2/sites-available ] )
+then
+	/bin/mkdir -p /etc/apache2/sites-available
+fi
+
+if ( [ ! -d /var/log/apache2 ] )
+then
+	/bin/mkdir -p /var/log/apache2
+fi
+
+/bin/chown www-data:www-data /var/log/apache2
+
+/usr/bin/openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
+
+if ( [ -f ${HOME}/providerscripts/webserver/configuration/reverseproxy/apache/online/source/site-available.conf ] )
+then
+	/bin/cp ${HOME}/providerscripts/webserver/configuration/reverseproxy/apache/online/source/site-available.conf /etc/apache2/sites-available/${WEBSITE_NAME}.conf
+	/bin/sed -i "s/XXXXWEBSITEURLXXXX/${WEBSITE_URL}/g" /etc/apache2/sites-available/${WEBSITE_NAME}.conf
+	export HOME="`/bin/cat /home/homedir.dat`"
+	/bin/sed -i "s,XXXXHOMEXXXX,${HOME},g" /etc/apache2/sites-available/${WEBSITE_NAME}.conf
+	/bin/sed -i "s/XXXXROOTDOMAINXXXX/${ROOT_DOMAIN}/g" /etc/apache2/sites-available/${WEBSITE_NAME}.conf
+	/bin/chmod 600 /etc/apache2/sites-available/${WEBSITE_NAME}.conf
+	/bin/chown root:root /etc/apache2/sites-available/${WEBSITE_NAME}.conf
+	/bin/ln -s /etc/apache2/sites-available/${WEBSITE_NAME}.conf /etc/apache2/sites-enabled/${WEBSITE_NAME}.conf
+  	/bin/echo "/etc/apache2/sites-available/${WEBSITE_NAME}.conf" > ${HOME}/runtime/WEBSERVER_CONFIG_LOCATION.dat
+fi
+
+if ( [ -f /etc/apache2/httpd.conf ] )
+then
+	/bin/sed -i "s/XXXXWEBSITEURLXXXX/ServerName ${WEBSITE_URL}/g" /etc/apache2/httpd.conf
+	/bin/sed -i "s/XXXXAPPLICATIONNAMEXXXX/${WEBSITE_NAME}/g" /etc/apache2/httpd.conf
+fi
 
 if ( [ "${MOD_SECURITY}" = "1" ] )
 then
         /bin/sed -i -e "/#XXXXMODSECURITYXXXX/{r ${HOME}/providerscripts/webserver/configuration/reverseproxy/apache/online/source/modsecurity.conf" -e "d}" /etc/apache2/sites-available/${WEBSITE_NAME}.conf
 fi
 
-/bin/rm -r /var/www/html/*
-/bin/cp ${HOME}/providerscripts/webserver/configuration/authenticator/index.php /var/www/html/index.php
-/bin/chown www-data:www-data /var/www/html/index.php
-/bin/chmod 644 /var/www/html/index.php
 
-/bin/sed -i "s/XXXXUSEREMAILDOMAINXXXX/${USER_EMAIL_DOMAIN}/g" /var/www/html/index.php
-/bin/sed -i "s/XXXXWEBSITEURLXXXX/${WEBSITE_URL}/g" /var/www/html/index.php
+config_settings="`${HOME}/utilities/config/ExtractBuildStyleValues.sh "APACHE:settings" "stripped" | /bin/sed 's/|.*//g' | /bin/sed 's/:/ /g'`"
+
+for setting in ${config_settings}
+do
+	setting_name="`/bin/echo ${setting} | /usr/bin/awk -F'=' '{print $1}'`"
+	setting_value="`/bin/echo ${setting} | /usr/bin/awk -F'=' '{print $2}'`"
+	/usr/bin/find /etc/apache2 -name '*' -type f -exec sed -i "s/^${setting_name}.*/${setting_name} ${setting_value}/" {} +
+done
+
+/usr/bin/systemctl enable apache2.service
+/usr/bin/systemctl start apache2.service &
