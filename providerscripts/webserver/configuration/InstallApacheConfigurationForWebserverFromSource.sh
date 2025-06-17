c217662c895ff5b714f41a3da5741f27a0a1f19c
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-17 13:25:38 +0100

    Update InstallApacheConfigurationForWebserverFromSource.sh

--- providerscripts/webserver/configuration/InstallApacheConfigurationForWebserverFromSource.sh
+++ providerscripts/webserver/configuration/InstallApacheConfigurationForWebserverFromSource.sh
@@ -28,6 +28,8 @@ WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
 ROOT_DOMAIN="`/bin/echo ${WEBSITE_URL} | /usr/bin/awk -F'.' '{$1=""}1' | /bin/sed 's/^.//' | /bin/sed 's/ /\./g'`"
 APPLICATION="`${HOME}/utilities/config/ExtractConfigValue.sh 'APPLICATION'`"
 MOD_SECURITY="`${HOME}/utilities/config/ExtractConfigValue.sh 'MODSECURITY'`"
+NO_REVERSE_PROXY="`${HOME}/utilities/config/ExtractConfigValue.sh 'NOREVERSEPROXY'`"
+
 
 
 #Install configuration values for apache
@@ -76,7 +78,7 @@ then
 	/bin/sed -i "s/XXXXAPPLICATIONNAMEXXXX/${WEBSITE_NAME}/g" /etc/apache2/httpd.conf
 fi
 
-if ( [ "${MOD_SECURITY}" = "1" ] )
+if ( [ "${MOD_SECURITY}" = "1" ] && [ "${NO_REVERSE_PROXY}" = "0" ] )
 then
         /bin/sed -i -e "/#XXXXMODSECURITYXXXX/{r ${HOME}/providerscripts/webserver/configuration/${APPLICATION}/apache/online/source/modsecurity.conf" -e "d}" /etc/apache2/sites-available/${WEBSITE_NAME}.conf
 fi
