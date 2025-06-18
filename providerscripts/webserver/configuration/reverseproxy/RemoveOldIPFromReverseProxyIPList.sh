5421185f7a557ec7feaa0a212683aa18884ff9ca
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-18 16:06:24 +0100

    Update RemoveOldIPFromReverseProxyIPList.sh

--- providerscripts/webserver/configuration/reverseproxy/RemoveOldIPFromReverseProxyIPList.sh
+++ providerscripts/webserver/configuration/reverseproxy/RemoveOldIPFromReverseProxyIPList.sh
@@ -37,8 +37,4 @@ then
   ${HOME}/providerscripts/webserver/ReloadWebserver.sh
 fi
 
-if ( [ "`/bin/grep ${webserver_ip} /etc/lighttpd/lighttpd.conf`" != "" ] )
-then
-  /bin/sed -i "/${webserver_ip}/d" /etc/lighttpd/lighttpd.conf
-  ${HOME}/providerscripts/webserver/ReloadWebserver.sh
-fi
+
