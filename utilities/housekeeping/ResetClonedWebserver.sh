8a5246fdf7c0c3e3fc05cae34ac5f4d2817cb815
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-19 17:10:02 +0100

    Update ResetClonedWebserver.sh

--- utilities/housekeeping/ResetClonedWebserver.sh
+++ utilities/housekeeping/ResetClonedWebserver.sh
@@ -4,9 +4,9 @@ BUILDOS="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
 
 /bin/rm ${HOME}/runtime/FIREWALL-ACTIVE
 
-if ( [ "`/usr/bin/hostname | /bin/grep '-rp-'`" != "" ] )
+if ( [ "`/usr/bin/hostname | /bin/grep '\-rp-'`" != "" ] )
 then
-  ${HOME}/utilities/housekeeping/ResetReverseProxyIPs.sh
+  ${HOME}/providerscripts/webserver/configuration/reverseproxy/ResetReverseProxyIPs.sh
 fi
 
 ${HOME}/installscripts/UpdateAndUpgrade.sh ${BUILDOS} &
