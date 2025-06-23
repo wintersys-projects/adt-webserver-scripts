a1217e36ca1959a96834acade6a1f1732d37b433
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-23 21:17:51 +0100

    Update CustomiseApacheByApplication.sh

--- providerscripts/webserver/configuration/joomla/CustomiseApacheByApplication.sh
+++ providerscripts/webserver/configuration/joomla/CustomiseApacheByApplication.sh
@@ -30,11 +30,11 @@ then
   		/bin/chown www-data:www-data /var/www/html/.htaccess
 	fi
  	
-  	if ( [ -f ${HOME}/providerscripts/webserver/configuration/joomla/apache/online/source/htaccess-main.conf ] )	
+  	if ( [ -f ${HOME}/providerscripts/webserver/configuration/joomla/apache/online/source/htaccess-uploads.conf ] )	
 	then
-		/bin/cp ${HOME}/providerscripts/webserver/configuration/joomla/apache/online/source/htaccess-main.conf /var/www/html/.htaccess
-		/bin/chmod 444 /var/www/html/.htaccess
-  		/bin/chown www-data:www-data /var/www/html/.htaccess
+		/bin/cp ${HOME}/providerscripts/webserver/configuration/joomla/apache/online/source/htaccess-uploads.conf /var/www/html/images/.htaccess
+		/bin/chmod 444 /var/www/html/images/.htaccess
+  		/bin/chown www-data:www-data /var/www/html/images/.htaccess
 	fi
 else
 	if ( [ -f ${HOME}/providerscripts/webserver/configuration/joomla/apache/online/repo/htaccess-main.conf ] )	
@@ -42,6 +42,13 @@ else
 		/bin/cp ${HOME}/providerscripts/webserver/configuration/joomla/apache/online/repo/htaccess-main.conf /var/www/html/.htaccess
 		/bin/chmod 444 /var/www/html/.htaccess
   		/bin/chown www-data:www-data /var/www/html/.htaccess
+	fi
+
+  	if ( [ -f ${HOME}/providerscripts/webserver/configuration/joomla/apache/online/repo/htaccess-uploads.conf ] )	
+	then
+		/bin/cp ${HOME}/providerscripts/webserver/configuration/joomla/apache/online/repo/htaccess-uploads.conf /var/www/html/images/.htaccess
+		/bin/chmod 444 /var/www/html/images/.htaccess
+  		/bin/chown www-data:www-data /var/www/html/images/.htaccess
 	fi
 fi
 
