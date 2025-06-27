72f61c50cbc85b04d64d4d3ab5ab49de33c9be39
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-27 23:01:39 +0100

    Update InstallNginxConfigurationForAuthenticatorFromSource.sh

--- providerscripts/webserver/configuration/InstallNginxConfigurationForAuthenticatorFromSource.sh
+++ providerscripts/webserver/configuration/InstallNginxConfigurationForAuthenticatorFromSource.sh
@@ -77,7 +77,7 @@ then
 		/bin/sed -i "s/XXXXPHPVERSIONXXXX/${PHP_VERSION}/" /etc/nginx/sites-available/${WEBSITE_NAME}
 	fi
 else
-	if ( [ -f ${HOME}/providerscripts/webserver/configuration/${APPLICATION}/nginx/online/repo/php_port.conf ] )
+	if ( [ -f ${HOME}/providerscripts/webserver/configuration/authenticator/nginx/online/repo/php_port.conf ] )
 	then
 		/bin/sed -i -e "/#XXXXPHPPORTXXXX/{r ${HOME}/providerscripts/webserver/configuration/authenticator/nginx/online/repo/php_port.conf" -e "d}" /etc/nginx/sites-available/${WEBSITE_NAME}
   		/bin/sed -i "s/#XXXXPORTMODEONXXXX//" /etc/nginx/sites-available/${WEBSITE_NAME}
