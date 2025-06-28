3030c532af4d4e3f4b0485cfdc68d1b41f64e3de
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-28 05:05:20 +0100

    Update StoreNewAssets.sh

--- providerscripts/datastore/assets/StoreNewAssets.sh
+++ providerscripts/datastore/assets/StoreNewAssets.sh
@@ -13,9 +13,9 @@ applicationasset_buckets="`/bin/echo ${applicationassetdirs} | /bin/sed 's/\//\-
 
 for asset_bucket in ${applicationasset_buckets}
 do
-        asset_buckets="${asset_buckets} `/bin/echo "${WEBSITE_URL}"-assets | /bin/sed 's/\./-/g'`-${asset_bucket}"
+        asset_bucket="`/bin/echo "${WEBSITE_URL}"-assets | /bin/sed 's/\./-/g'`-${asset_bucket}"
         asset_directory="`/bin/echo ${asset_bucket} | /bin/sed 's/-/\./g'`"
-        ${HOME}/providerscripts/datastore/SyncDatastore.sh /
+        ${HOME}/providerscripts/datastore/SyncDatastore.sh /var/www/html/${asset_directory} s3://${asset_bucket}
 done
 
 
