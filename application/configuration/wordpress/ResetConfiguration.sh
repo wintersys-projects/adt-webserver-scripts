0c60adfaeeeb42a769998b400306a346949bd58d
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-03 17:44:21 +0100

    Update ResetConfiguration.sh

--- application/configuration/wordpress/ResetConfiguration.sh
+++ application/configuration/wordpress/ResetConfiguration.sh
@@ -3,9 +3,9 @@ then
   /bin/rm ${HOME}/runtime/INITIAL_CONFIG_SET 
 fi
 
-if ( [ -f /var/www/html/configuration.php ] )
+if ( [ -f /var/www/html/wp-config.php ] )
 then
-  /bin/rm /var/www/html/configuration.php
+  /bin/rm /var/www/html/wp-config.php
 fi
 
 /bin/touch ${HOME}/runtime/CONFIGURATION_RESET_ACTIONED
