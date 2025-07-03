0cfb2ae7c70be32894ead69bbbfe5d32c8cd1bea
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-03 17:45:27 +0100

    Update ResetConfiguration.sh

--- application/configuration/drupal/ResetConfiguration.sh
+++ application/configuration/drupal/ResetConfiguration.sh
@@ -3,9 +3,9 @@ then
   /bin/rm ${HOME}/runtime/INITIAL_CONFIG_SET 
 fi
 
-if ( [ -f /var/www/html/configuration.php ] )
+if ( [ -f /var/www/html/sites/default/settings.php ] )
 then
-  /bin/rm /var/www/html/configuration.php
+  /bin/rm /var/www/html/sites/default/settings.php
 fi
 
 /bin/touch ${HOME}/runtime/CONFIGURATION_RESET_ACTIONED
