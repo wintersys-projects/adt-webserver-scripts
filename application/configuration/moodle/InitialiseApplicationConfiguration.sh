80f4d6e600b5dcb0778932b9665a8891b3eabc58
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-12 10:02:37 +0100

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/moodle/InitialiseApplicationConfiguration.sh
+++ application/configuration/moodle/InitialiseApplicationConfiguration.sh
@@ -7,7 +7,7 @@ ${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh moodle
 
 if ( [ ! -f ${HOME}/runtime/moodle_config.php] )
 then
-  ${HOME}/providerscripts/email/SendEmail.sh "CONFIGURATION FILE ABSENT" "Unable to obtain the joomla configuration from the datastore during application initiation" "ERROR"
+  ${HOME}/providerscripts/email/SendEmail.sh "CONFIGURATION FILE ABSENT" "Unable to obtain the moodle configuration from the datastore during application initiation" "ERROR"
 fi
 
 /usr/bin/php -ln ${HOME}/runtime/moodle_config.php
@@ -20,11 +20,11 @@ then
   
   if ( [ ! -f /var/www/html/config.php ] )
   then
-    ${HOME}/providerscripts/email/SendEmail.sh "CONFIGURATION FILE ABSENT" "Failed to copy joomla configuration file to the live location during application initiation" "ERROR"
+    ${HOME}/providerscripts/email/SendEmail.sh "CONFIGURATION FILE ABSENT" "Failed to copy moodle configuration file to the live location during application initiation" "ERROR"
   else
     /bin/touch ${HOME}/runtime/INITIAL_CONFIG_SET
   fi
   
 else
-  ${HOME}/providerscripts/email/SendEmail.sh "CONFIGURATION FILE MALFORMED" "The joomla configuration file appears to be malformed during application initiation" "ERROR"
+  ${HOME}/providerscripts/email/SendEmail.sh "CONFIGURATION FILE MALFORMED" "The moodle configuration file appears to be malformed during application initiation" "ERROR"
 fi 
