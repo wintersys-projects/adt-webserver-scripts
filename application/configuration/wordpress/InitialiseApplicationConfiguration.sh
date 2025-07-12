e164b8c84e72d10e5c07a273e7ed52ceb1b97253
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-12 09:55:08 +0100

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/wordpress/InitialiseApplicationConfiguration.sh
+++ application/configuration/wordpress/InitialiseApplicationConfiguration.sh
@@ -9,7 +9,7 @@ ${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh wordpr
 
 if ( [ ! -f ${HOME}/runtime/wordpress_config.php ] )
 then
-  ${HOME}/providerscripts/email/SendEmail.sh "CONFIGURATION FILE ABSENT" "Unable to obtain the joomla configuration from the datastore during application initiation" "ERROR"
+  ${HOME}/providerscripts/email/SendEmail.sh "CONFIGURATION FILE ABSENT" "Unable to obtain the wordpress configuration from the datastore during application initiation" "ERROR"
 fi
 
 /usr/bin/php -ln ${HOME}/runtime/wordpress_config.php
@@ -22,11 +22,11 @@ then
   
   if ( [ ! -f /var/www/html/wp-config.php ] )
   then
-    ${HOME}/providerscripts/email/SendEmail.sh "CONFIGURATION FILE ABSENT" "Failed to copy joomla configuration file to the live location during application initiation" "ERROR"
+    ${HOME}/providerscripts/email/SendEmail.sh "CONFIGURATION FILE ABSENT" "Failed to copy wordpress configuration file to the live location during application initiation" "ERROR"
   else
     /bin/touch ${HOME}/runtime/INITIAL_CONFIG_SET
   fi
   
 else
-  ${HOME}/providerscripts/email/SendEmail.sh "CONFIGURATION FILE MALFORMED" "The joomla configuration file appears to be malformed during application initiation" "ERROR"
+  ${HOME}/providerscripts/email/SendEmail.sh "CONFIGURATION FILE MALFORMED" "The wordpress configuration file appears to be malformed during application initiation" "ERROR"
 fi 
