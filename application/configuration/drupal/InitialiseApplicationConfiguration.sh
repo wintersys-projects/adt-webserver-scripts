ec4044cd2b9c71f79532f8968639f4df30f55fdc
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-12 10:05:42 +0100

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/drupal/InitialiseApplicationConfiguration.sh
+++ application/configuration/drupal/InitialiseApplicationConfiguration.sh
@@ -5,18 +5,18 @@ then
 	exit
 fi
 
-${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh joomla_configuration.php ${HOME}/runtime/joomla_configuration.php
+${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh joomla_configuration.php ${HOME}/runtime/drupal_settings.php
 
-if ( [ ! -f ${HOME}/runtime/joomla_configuration.php ] )
+if ( [ ! -f ${HOME}/runtime/drupal_settings.php ] )
 then
   ${HOME}/providerscripts/email/SendEmail.sh "CONFIGURATION FILE ABSENT" "Unable to obtain the joomla configuration from the datastore during application initiation" "ERROR"
 fi
 
-/usr/bin/php -ln ${HOME}/runtime/joomla_configuration.php
+/usr/bin/php -ln ${HOME}/runtime/drupal_settings.php
 
 if ( [ "$?" = "0" ] )
 then
-  /bin/cp ${HOME}/runtime/joomla_configuration.php /var/www/html/sites/default/settings.php
+  /bin/cp ${HOME}/runtime/drupal_settings.php /var/www/html/sites/default/settings.php
   /bin/chmod 600 /var/www/html/sites/default/settings.php
   /bin/chown www-data:www-data /var/www/html/sites/default/settings.php
   
