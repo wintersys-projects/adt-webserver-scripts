704336e354721f166b42ea21a4bc243100e84182
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-12 11:47:11 +0100

    Update UpdateApplicationConfiguration.sh

--- application/configuration/wordpress/UpdateApplicationConfiguration.sh
+++ application/configuration/wordpress/UpdateApplicationConfiguration.sh
@@ -1,4 +1,4 @@
-if ( [ "`${HOME}/providerscripts/datastore/configwrapper/AgeOfConfigFile.sh wordpress_config.php`" -lt "130" ] && [ "`/usr/bin/find /var/www/html/configuration.php -cmin -1`" = "" ] )
+if ( [ "`${HOME}/providerscripts/datastore/configwrapper/AgeOfConfigFile.sh wordpress_config.php`" -lt "130" ] && [ "`/usr/bin/find /var/www/html/wp-config.php -cmin -1`" = "" ] )
 then
        if ( [ -f ${HOME}/runtime/wordpress_config.php ] )
        then
@@ -8,13 +8,13 @@ then
         /usr/bin/php -ln ${HOME}/runtime/wordpress_config.php
         if ( [ "$?" = "0" ] )
         then
-                /bin/cp ${HOME}/runtime/wordpress_config.php /var/www/html/configuration.php
-                /bin/chmod 600 /var/www/html/configuration.php
-                /bin/chown www-data:www-data /var/www/html/configuration.php
+                /bin/cp ${HOME}/runtime/wordpress_config.php /var/www/html/wp-config.php
+                /bin/chmod 600 /var/www/html/wp-config.php
+                /bin/chown www-data:www-data /var/www/html/wp-config.php
 
                 if ( [ "`/usr/bin/curl -m 2 --insecure -I "https://localhost:443/index.php" 2>&1 | /bin/grep \"HTTP\" | /bin/grep -w \"200\|301\|302\|303\"`" = "" ] )
                 then
-                        /bin/cp ${HOME}/runtime/wordpress_config.php.$$ /var/www/html/configuration.php
+                        /bin/cp ${HOME}/runtime/wordpress_config.php.$$ /var/www/html/wp-config.php
                 fi
         fi
 fi
@@ -29,7 +29,7 @@ then
                         /bin/mv ${HOME}/runtime/wordpress_config.php ${HOME}/runtime/wordpress_config.php.$$
                 fi
 
-                /bin/cp /var/www/html/configuration.php ${HOME}/runtime/wordpress_config.php
+                /bin/cp /var/www/html/wp-config.php ${HOME}/runtime/wordpress_config.php
                 /usr/bin/php -ln ${HOME}/runtime/wordpress_config.php
                 if ( [ "$?" = "0" ] )
                 then
