30582f2519c48f317d279c3b20a888648bab2ebe
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-12 11:47:41 +0100

    Update UpdateApplicationConfiguration.sh

--- application/configuration/wordpress/UpdateApplicationConfiguration.sh
+++ application/configuration/wordpress/UpdateApplicationConfiguration.sh
@@ -20,7 +20,7 @@ then
 fi
 
 
-if ( [ "`/usr/bin/find /var/www/html/configuration.php -cmin -1`" != "" ] )
+if ( [ "`/usr/bin/find /var/www/html/wp-config.php -cmin -1`" != "" ] )
 then
         if ( [ "`/usr/bin/curl -m 2 --insecure -I "https://localhost:443/index.php" 2>&1 | /bin/grep \"HTTP\" | /bin/grep -w \"200\|301\|302\|303\"`" != "" ] )
         then
