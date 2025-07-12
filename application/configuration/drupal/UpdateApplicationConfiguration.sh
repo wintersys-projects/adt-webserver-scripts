6311c7a683b3a2ebfbe2f661b0441f6c6c10c7f0
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-12 11:35:02 +0100

    Update UpdateApplicationConfiguration.sh

--- application/configuration/drupal/UpdateApplicationConfiguration.sh
+++ application/configuration/drupal/UpdateApplicationConfiguration.sh
@@ -1,20 +1,20 @@
 if ( [ "`${HOME}/providerscripts/datastore/configwrapper/AgeOfConfigFile.sh drupal_settings.php`" -lt "130" ] && [ "`/usr/bin/find /var/www/html/configuration.php -cmin -1`" = "" ] )
 then
-       if ( [ -f ${HOME}/runtime/joomla_configuration.php ] )
+       if ( [ -f ${HOME}/runtime/drupal_settings.php ] )
        then
-        /bin/mv ${HOME}/runtime/joomla_configuration.php ${HOME}/runtime/joomla_configuration.php.$$
+        /bin/mv ${HOME}/runtime/drupal_settings.php ${HOME}/runtime/drupal_settings.php.$$
        fi
-        ${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh drupal_settings.php ${HOME}/runtime/joomla_configuration.php
-        /usr/bin/php -ln ${HOME}/runtime/joomla_configuration.php
+        ${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh drupal_settings.php ${HOME}/runtime/drupal_settings.php
+        /usr/bin/php -ln ${HOME}/runtime/drupal_settings.php
         if ( [ "$?" = "0" ] )
         then
-                /bin/cp ${HOME}/runtime/joomla_configuration.php /var/www/html/configuration.php
+                /bin/cp ${HOME}/runtime/drupal_settings.php /var/www/html/configuration.php
                 /bin/chmod 600 /var/www/html/configuration.php
                 /bin/chown www-data:www-data /var/www/html/configuration.php
 
                 if ( [ "`/usr/bin/curl -m 2 --insecure -I "https://localhost:443/index.php" 2>&1 | /bin/grep \"HTTP\" | /bin/grep -w \"200\|301\|302\|303\"`" = "" ] )
                 then
-                        /bin/cp ${HOME}/runtime/joomla_configuration.php.$$ /var/www/html/configuration.php
+                        /bin/cp ${HOME}/runtime/drupal_settings.php.$$ /var/www/html/configuration.php
                 fi
         fi
 fi
@@ -24,16 +24,16 @@ if ( [ "`/usr/bin/find /var/www/html/configuration.php -cmin -1`" != "" ] )
 then
         if ( [ "`/usr/bin/curl -m 2 --insecure -I "https://localhost:443/index.php" 2>&1 | /bin/grep \"HTTP\" | /bin/grep -w \"200\|301\|302\|303\"`" != "" ] )
         then
-                if ( [ -f ${HOME}/runtime/joomla_configuration.php ] )
+                if ( [ -f ${HOME}/runtime/drupal_settings.php ] )
                 then
-                        /bin/mv ${HOME}/runtime/joomla_configuration.php ${HOME}/runtime/joomla_configuration.php.$$
+                        /bin/mv ${HOME}/runtime/drupal_settings.php ${HOME}/runtime/drupal_settings.php.$$
                 fi
 
-                /bin/cp /var/www/html/configuration.php ${HOME}/runtime/joomla_configuration.php
-                /usr/bin/php -ln ${HOME}/runtime/joomla_configuration.php
+                /bin/cp /var/www/html/configuration.php ${HOME}/runtime/drupal_settings.php
+                /usr/bin/php -ln ${HOME}/runtime/drupal_settings.php
                 if ( [ "$?" = "0" ] )
                 then
-                        ${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${HOME}/runtime/joomla_configuration.php drupal_settings.php "no"
+                        ${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${HOME}/runtime/drupal_settings.php drupal_settings.php "no"
                 fi
         fi
 fi
