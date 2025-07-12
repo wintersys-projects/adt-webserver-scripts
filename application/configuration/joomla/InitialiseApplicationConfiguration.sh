9907e10ebd64f8b97493a4bf4da140c3e6940c9a
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-12 09:27:05 +0100

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/joomla/InitialiseApplicationConfiguration.sh
+++ application/configuration/joomla/InitialiseApplicationConfiguration.sh
@@ -23,10 +23,14 @@ then
   /bin/cp ${HOME}/runtime/joomla_configuration.php /var/www/html/configuration.php
   /bin/chmod 600 /var/www/html/configuration.php
   /bin/chown www-data:www-data /var/www/html/configuration.php
-  if ( [ ! -f ${HOME}/runtime/joomla_configuration.php ] )
+  
+  if ( [ ! -f /var/www/html/joomla_configuration.php ] )
   then
     ${HOME}/providerscripts/email/SendEmail.sh "CONFIGURATION FILE ABSENT" "Failed to copy joomla configuration file to the live location during application initiation" "ERROR"
+  else
+    /bin/touch ${HOME}/runtime/INITIAL_CONFIG_SET
   fi
+  
 else
   ${HOME}/providerscripts/email/SendEmail.sh "CONFIGURATION FILE MALFORMED" "The joomla configuration file appears to be malformed during application initiation" "ERROR"
 fi 
