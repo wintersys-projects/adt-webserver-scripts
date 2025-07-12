3c508b222833d3000310015675edc1c14c56d974
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-12 15:18:32 +0100

    Update UpdateApplicationConfiguration.sh

--- application/configuration/joomla/UpdateApplicationConfiguration.sh
+++ application/configuration/joomla/UpdateApplicationConfiguration.sh
@@ -1,3 +1,23 @@
+if ( [ "`/usr/bin/find /var/www/html/configuration.php -cmin -1`" != "" ] )
+then
+        if ( [ "`/usr/bin/curl -m 2 --insecure -I "https://localhost:443/index.php" 2>&1 | /bin/grep \"HTTP\" | /bin/grep -w \"200\|301\|302\|303\"`" != "" ] )
+        then
+                if ( [ -f ${HOME}/runtime/joomla_configuration.php ] )
+                then
+                        /bin/mv ${HOME}/runtime/joomla_configuration.php ${HOME}/runtime/joomla_configuration.php.$$
+                fi
+
+                /bin/cp /var/www/html/configuration.php ${HOME}/runtime/joomla_configuration.php
+                /usr/bin/php -ln ${HOME}/runtime/joomla_configuration.php
+                if ( [ "$?" = "0" ] )
+                then
+                        ${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${HOME}/runtime/joomla_configuration.php joomla_configuration.php "no"
+                fi
+        fi
+fi
+
+/bin/sleep 20
+
 if ( [ "`${HOME}/providerscripts/datastore/configwrapper/AgeOfConfigFile.sh joomla_configuration.php`" -lt "130" ] && [ "`/usr/bin/find /var/www/html/configuration.php -cmin -1`" = "" ] )
 then
        if ( [ -f ${HOME}/runtime/joomla_configuration.php ] )
@@ -29,21 +49,3 @@ then
         fi
 fi
 
-
-if ( [ "`/usr/bin/find /var/www/html/configuration.php -cmin -1`" != "" ] )
-then
-        if ( [ "`/usr/bin/curl -m 2 --insecure -I "https://localhost:443/index.php" 2>&1 | /bin/grep \"HTTP\" | /bin/grep -w \"200\|301\|302\|303\"`" != "" ] )
-        then
-                if ( [ -f ${HOME}/runtime/joomla_configuration.php ] )
-                then
-                        /bin/mv ${HOME}/runtime/joomla_configuration.php ${HOME}/runtime/joomla_configuration.php.$$
-                fi
-
-                /bin/cp /var/www/html/configuration.php ${HOME}/runtime/joomla_configuration.php
-                /usr/bin/php -ln ${HOME}/runtime/joomla_configuration.php
-                if ( [ "$?" = "0" ] )
-                then
-                        ${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${HOME}/runtime/joomla_configuration.php joomla_configuration.php "no"
-                fi
-        fi
-fi
