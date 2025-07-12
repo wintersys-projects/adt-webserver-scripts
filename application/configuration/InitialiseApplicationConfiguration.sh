a76477d87cb8eb9a0bbfdcf1e48dfd9dbdd08380
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-12 08:30:06 +0100

    Create InitialiseApplicationConfiguration.sh

---
+++ application/configuration/InitialiseApplicationConfiguration.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+#########################################################################################
+# Description: This script will initialise the application configuration
+# Date: 16/11/2016
+# Author: Peter Winter
+######################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+####################################################################################
+####################################################################################
+#set -x
+
+#if ( [ "`/bin/ls /var/www/html/*temp`" != "" ] )
+#then
+#	/bin/rm -r /var/www/html/*temp
+#fi
+ 
+if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`" = "0" ] )
+then
+	for applicationdir in `/bin/ls -d ${HOME}/application/configuration/*/`
+	do
+			. ${applicationdir}InitialiseApplicationConfiguration.sh
+	done
+fi
