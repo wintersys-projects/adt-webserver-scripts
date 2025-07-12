1a1a007d75513e7514c6316c7d32ee46fa25b91e
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-12 15:50:47 +0100

    Update UpdateApplicationConfiguration.sh

--- application/configuration/drupal/UpdateApplicationConfiguration.sh
+++ application/configuration/drupal/UpdateApplicationConfiguration.sh
@@ -1,25 +1,3 @@
-if ( [ "`${HOME}/providerscripts/datastore/configwrapper/AgeOfConfigFile.sh drupal_settings.php`" -lt "130" ] && [ "`/usr/bin/find /var/www/html/sites/default/settings.php -cmin -1`" = "" ] )
-then
-       if ( [ -f ${HOME}/runtime/drupal_settings.php ] )
-       then
-        /bin/mv ${HOME}/runtime/drupal_settings.php ${HOME}/runtime/drupal_settings.php.$$
-       fi
-        ${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh drupal_settings.php ${HOME}/runtime/drupal_settings.php
-        /usr/bin/php -ln ${HOME}/runtime/drupal_settings.php
-        if ( [ "$?" = "0" ] )
-        then
-                /bin/cp ${HOME}/runtime/drupal_settings.php /var/www/html/sites/default/settings.php
-                /bin/chmod 600 /var/www/html/sites/default/settings.php
-                /bin/chown www-data:www-data /var/www/html/sites/default/settings.php
-
-                if ( [ "`/usr/bin/curl -m 2 --insecure -I "https://localhost:443/index.php" 2>&1 | /bin/grep \"HTTP\" | /bin/grep -w \"200\|301\|302\|303\"`" = "" ] )
-                then
-                        /bin/cp ${HOME}/runtime/drupal_settings.php.$$ /var/www/html/sites/default/settings.php
-                fi
-        fi
-fi
-
-
 if ( [ "`/usr/bin/find /var/www/html/sites/default/settings.php -cmin -1`" != "" ] )
 then
         if ( [ "`/usr/bin/curl -m 2 --insecure -I "https://localhost:443/index.php" 2>&1 | /bin/grep \"HTTP\" | /bin/grep -w \"200\|301\|302\|303\"`" != "" ] )
@@ -37,3 +15,36 @@ then
                 fi
         fi
 fi
+
+/bin/sleep 20
+
+if ( [ "`${HOME}/providerscripts/datastore/configwrapper/AgeOfConfigFile.sh drupal_settings.php`" -lt "130" ] && [ "`/usr/bin/find /var/www/html/sites/default/settings.php -cmin -1`" = "" ] )
+then
+       if ( [ -f ${HOME}/runtime/drupal_settings.php ] )
+       then
+        /bin/mv ${HOME}/runtime/drupal_settings.php ${HOME}/runtime/drupal_settings.php.$$
+       fi
+        if ( [ -f ${HOME}/runtime/drupal_settings.php ] )
+        then
+               ${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh drupal_settings.php ${HOME}/runtime/drupal_settings.php
+               /usr/bin/php -ln ${HOME}/runtime/drupal_settings.php
+               if ( [ "$?" = "0" ] )
+               then
+                     if ( [ "`/usr/bin/diff ${HOME}/runtime/joomla_configuration.php /var/www/html/configuration.php`" = "" ] )
+                     then
+                            exit
+                     fi
+                       /bin/cp ${HOME}/runtime/drupal_settings.php /var/www/html/sites/default/settings.php
+                       /bin/chmod 600 /var/www/html/sites/default/settings.php
+                       /bin/chown www-data:www-data /var/www/html/sites/default/settings.php
+
+                       if ( [ "`/usr/bin/curl -m 2 --insecure -I "https://localhost:443/index.php" 2>&1 | /bin/grep \"HTTP\" | /bin/grep -w \"200\|301\|302\|303\"`" = "" ] )
+                       then
+                               /bin/cp ${HOME}/runtime/drupal_settings.php.$$ /var/www/html/sites/default/settings.php
+                       fi
+               fi
+        fi
+fi
+
+
+
