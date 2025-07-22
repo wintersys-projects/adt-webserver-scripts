507afb72fd713c73d7d08ee8660c90fd0844647c
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-22 14:01:10 +0100

    Update InitialiseVirginInstall.sh

--- application/configuration/joomla/InitialiseVirginInstall.sh
+++ application/configuration/joomla/InitialiseVirginInstall.sh
@@ -43,7 +43,7 @@ then
 fi
 
 #For ease of use we tell ourselves what database engine this webroot is associated with
-if ( ( [ ! -f /var/www/html/dbe.dat ] || [ "`/bin/cat /var/www/html/dbe.dat`" = "" ] ) && [ -f /var/www/html/configuration.php ] )
+if ( [ ! -f /var/www/html/dbe.dat ] || [ "`/bin/cat /var/www/html/dbe.dat`" = "" ] )
 then
 	if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh DATABASEDBaaSINSTALLATIONTYPE:Maria`" = "1" ] || [ "`${HOME}/utilities/config/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:Maria`" = "1" ] )
 	then
