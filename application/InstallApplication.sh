d44f2c8e19ac47373c5ca19454b5a2061c65d015
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-22 17:19:50 +0100

    Update InstallApplication.sh

--- application/InstallApplication.sh
+++ application/InstallApplication.sh
@@ -19,12 +19,11 @@
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 #######################################################################################################
 #######################################################################################################
-set -x
+#set -x
 
 HOME="`/bin/cat /home/homedir.dat`"
 WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
 BUILD_ARCHIVE_CHOICE="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILDARCHIVECHOICE'`"
-BUILD_CHOICE="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILDCHOICE'`"
 WEBSITE_SUBDOMAIN="`/bin/echo ${WEBSITE_URL} | /usr/bin/awk -F'.' '{print $1}'`"
 APPLICATION_IDENTIFIER="`${HOME}/utilities/config/ExtractConfigValue.sh 'APPLICATIONIDENTIFIER'`"
 APPLICATION_REPOSITORY_PROVIDER="`${HOME}/utilities/config/ExtractConfigValue.sh 'APPLICATIONREPOSITORYPROVIDER'`"
@@ -35,52 +34,52 @@ APPLICATION_BASELINE_SOURCECODE_REPOSITORY="`${HOME}/utilities/config/ExtractCon
 
 if ( [ -d /var/www/html ] )
 then
-	#/bin/rm -r /var/www/html/* 2>/dev/null
- 	/usr/bin/find /var/www/html -type f -name "*" -delete 2>/dev/null
-	/bin/rm -r /var/www/html/.git 2>/dev/null
+        #/bin/rm -r /var/www/html/* 2>/dev/null
+        /usr/bin/find /var/www/html -type f -name "*" -delete 2>/dev/null
+        /bin/rm -r /var/www/html/.git 2>/dev/null
 else
-	/bin/mkdir -p /var/www/html
+        /bin/mkdir -p /var/www/html
 fi
 
 cd /var/www/html
 /usr/bin/git init
 
-if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`" = "1" ] && [ "${BUILD_CHOICE}" = "0" ] && [ "${APPLICATION_IDENTIFIER}" != "0" ] )
+if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`" = "1" ] && [ "${BUILD_ARCHIVE_CHOICE}" = "virgin" ] && [ "${APPLICATION_IDENTIFIER}" != "0" ] )
 then
-	${HOME}/application/configuration/InstallVirginDeploymentByApplication.sh ${APPLICATION_BASELINE_SOURCECODE_REPOSITORY}
-	${HOME}/application/configuration/InstallDirectoryConfigurationByApplication.sh
-	${HOME}/application/configuration/InitialiseVirginInstallByApplication.sh &
-elif ( [ "`${HOME}/utilities/config/CheckConfigValue.sh BUILDARCHIVECHOICE:baseline`" = "1" ] && [ "${BUILD_CHOICE}" = "1" ] )
+        ${HOME}/application/configuration/InstallVirginDeploymentByApplication.sh ${APPLICATION_BASELINE_SOURCECODE_REPOSITORY}
+        ${HOME}/application/configuration/InstallDirectoryConfigurationByApplication.sh
+        ${HOME}/application/configuration/InitialiseVirginInstallByApplication.sh &
+elif ( [ "`${HOME}/utilities/config/CheckConfigValue.sh BUILDARCHIVECHOICE:baseline`" = "1" ] && [ "${BUILD_ARCHIVE_CHOICE}" = "baseline" ] )
 then
-    ${HOME}/providerscripts/git/GitPull.sh ${APPLICATION_REPOSITORY_PROVIDER} ${APPLICATION_REPOSITORY_USERNAME} ${APPLICATION_REPOSITORY_PASSWORD} ${APPLICATION_REPOSITORY_OWNER} ${APPLICATION_BASELINE_SOURCECODE_REPOSITORY}
-    ${HOME}/application/configuration/InstallDirectoryConfigurationByApplication.sh
-    ${HOME}/application/branding/ApplyApplicationBranding.sh
-elif ( [ "${BUILD_CHOICE}" = "2" ] || [ "${BUILD_CHOICE}" = "3" ] || [ "${BUILD_CHOICE}" = "4" ] || [ "${BUILD_CHOICE}" = "5" ] || [ "${BUILD_CHOICE}" = "6" ] )
+        ${HOME}/providerscripts/git/GitPull.sh ${APPLICATION_REPOSITORY_PROVIDER} ${APPLICATION_REPOSITORY_USERNAME} ${APPLICATION_REPOSITORY_PASSWORD} ${APPLICATION_REPOSITORY_OWNER} ${APPLICATION_BASELINE_SOURCECODE_REPOSITORY}
+        ${HOME}/application/configuration/InstallDirectoryConfigurationByApplication.sh
+        ${HOME}/application/branding/ApplyApplicationBranding.sh
+elif ( [ "`/bin/echo 'hourly daily weekly monthly bimonthly' | /bin/grep ${BUILD_ARCHIVE_CHOICE}`" != "" ] )
 then
-    cd ${HOME}
-    application_datastore="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`-${BUILD_ARCHIVE_CHOICE}/applicationsourcecode.tar.gz"
-    ${HOME}/providerscripts/datastore/GetFromDatastore.sh ${application_datastore}
-	if ( [ ! -d ${HOME}/application_sourcecode ] )
-	then
-		/bin/mkdir ${HOME}/application_sourcecode
-	fi
-    /bin/tar xvfz ${HOME}/applicationsourcecode.tar.gz -C ${HOME}/application_sourcecode
-    /bin/rm ${HOME}/applicationsourcecode.tar.gz
-    /bin/rm -r /var/www/html/* 2>/dev/null
-    /bin/mv ${HOME}/application_sourcecode/* /var/www/html
-    /bin/mv ${HOME}/application_sourcecode/.* /var/www/html
-    /bin/rm -rf ${HOME}/application_sourcecode
-    ${HOME}/application/configuration/InstallDirectoryConfigurationByApplication.sh
+        cd ${HOME}
+        application_datastore="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`-${BUILD_ARCHIVE_CHOICE}/applicationsourcecode.tar.gz"
+        ${HOME}/providerscripts/datastore/GetFromDatastore.sh ${application_datastore}
+        if ( [ ! -d ${HOME}/application_sourcecode ] )
+        then
+                /bin/mkdir ${HOME}/application_sourcecode
+        fi
+        /bin/tar xvfz ${HOME}/applicationsourcecode.tar.gz -C ${HOME}/application_sourcecode
+        /bin/rm ${HOME}/applicationsourcecode.tar.gz
+        /bin/rm -r /var/www/html/* 2>/dev/null
+        /bin/mv ${HOME}/application_sourcecode/* /var/www/html
+        /bin/mv ${HOME}/application_sourcecode/.* /var/www/html
+        /bin/rm -rf ${HOME}/application_sourcecode
+        ${HOME}/application/configuration/InstallDirectoryConfigurationByApplication.sh
 fi
 
 ${HOME}/application/customise/CustomiseApplication.sh
 
 if ( [ "`${HOME}/application/configuration/CheckIfApplicationIsInstalled.sh`" = "1" ] )
 then
-    ${HOME}/providerscripts/email/SendEmail.sh "I BELIEVE STRONGLY AN APPLICATION HAS BEEN INSTALLED" "The application sourcecode from the datastore: ${BUILD_ARCHIVE_CHOICE} has been installed" "INFO"
-    /bin/touch ${HOME}/runtime/BESPOKE_APPLICATION_INSTALLED
+        ${HOME}/providerscripts/email/SendEmail.sh "I BELIEVE STRONGLY AN APPLICATION HAS BEEN INSTALLED" "The application sourcecode from the datastore: ${BUILD_ARCHIVE_CHOICE} has been installed" "INFO"
+        /bin/touch ${HOME}/runtime/BESPOKE_APPLICATION_INSTALLED
 else
-    ${HOME}/providerscripts/email/SendEmail.sh "I BELIEVE STRONGLY AN APPLICATION HAS BEEN INSTALLED" "The application sourcecode from the datastore: ${BUILD_ARCHIVE_CHOICE} has been installed" "INFO"
+        ${HOME}/providerscripts/email/SendEmail.sh "I BELIEVE STRONGLY AN APPLICATION HAS BEEN INSTALLED" "The application sourcecode from the datastore: ${BUILD_ARCHIVE_CHOICE} has been installed" "INFO"
 fi
 
 ${HOME}/utilities/security/EnforcePermissions.sh &
