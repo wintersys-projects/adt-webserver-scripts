251d898b9c8c9c96920442d6170f00fc1d3d895a
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-23 14:28:42 +0100

    Update UpdateApplicationConfiguration.sh

--- application/configuration/drupal/UpdateApplicationConfiguration.sh
+++ application/configuration/drupal/UpdateApplicationConfiguration.sh
@@ -20,7 +20,7 @@
 #######################################################################################################
 #######################################################################################################
 #set -x
-
+installed="0"
 if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh APPLICATION:drupal`" = "1" ] )
 then
 	if ( [ -f /var/www/html/core/misc/drupal.js ] && [ -d /var/www/html/themes ] && [ -d /var/www/html/modules ] && [ -d /var/www/html//profiles ] )
@@ -28,62 +28,63 @@ then
 		if ( [ "`/usr/bin/find /var/www/html -type d | /usr/bin/wc -l`" -gt "5" ] && [ "`/usr/bin/find /var/www/html -type f | /usr/bin/wc -l`" -gt "5" ] )
 		then
 			installed="1"
+   		else
+     			installed="0"
 		fi
 	fi
-fi
 
-
-
-if ( [ "`/usr/bin/find /var/www/html/sites/default/settings.php -cmin -1`" != "" ] )
+if ( [ "${installed}" = "1" ] )
 then
-	if ( [ "`/usr/bin/curl -m 2 --insecure -I "https://localhost:443/index.php" 2>&1 | /bin/grep \"HTTP\" | /bin/grep -w \"200\|301\|302\|303\"`" != "" ] )
+	if ( [ "`/usr/bin/find /var/www/html/sites/default/settings.php -cmin -1`" != "" ] )
+	then
+		if ( [ "`/usr/bin/curl -m 2 --insecure -I "https://localhost:443/index.php" 2>&1 | /bin/grep \"HTTP\" | /bin/grep -w \"200\|301\|302\|303\"`" != "" ] )
+		then
+			if ( [ -f ${HOME}/runtime/drupal_settings.php ] )
+			then
+				/bin/mv ${HOME}/runtime/drupal_settings.php ${HOME}/runtime/drupal_settings.php.$$
+			fi
+
+			/bin/cp /var/www/html/sites/default/settings.php ${HOME}/runtime/drupal_settings.php
+			/usr/bin/php -ln ${HOME}/runtime/drupal_settings.php
+			if ( [ "$?" = "0" ] )
+			then
+				${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${HOME}/runtime/drupal_settings.php drupal_settings.php "no"
+			fi
+		fi
+	fi
+
+	/bin/sleep 20
+
+	if ( [ "`${HOME}/providerscripts/datastore/configwrapper/AgeOfConfigFile.sh drupal_settings.php`" -lt "130" ] && [ "`/usr/bin/find /var/www/html/sites/default/settings.php -cmin -1`" = "" ] )
 	then
 		if ( [ -f ${HOME}/runtime/drupal_settings.php ] )
 		then
 			/bin/mv ${HOME}/runtime/drupal_settings.php ${HOME}/runtime/drupal_settings.php.$$
 		fi
-
-		/bin/cp /var/www/html/sites/default/settings.php ${HOME}/runtime/drupal_settings.php
-		/usr/bin/php -ln ${HOME}/runtime/drupal_settings.php
-		if ( [ "$?" = "0" ] )
+	
+ 		${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh drupal_settings.php ${HOME}/runtime/drupal_settings.php
+		if ( [ -f ${HOME}/runtime/drupal_settings.php ] )
 		then
-			${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${HOME}/runtime/drupal_settings.php drupal_settings.php "no"
+			/usr/bin/php -ln ${HOME}/runtime/drupal_settings.php
+			if ( [ "$?" = "0" ] )
+			then
+				if ( [ "`/usr/bin/diff ${HOME}/runtime/drupal_settings.php /var/www/html/sites/default/settings.php`" = "" ] )
+				then
+					exit
+				fi
+				/bin/cp ${HOME}/runtime/drupal_settings.php /var/www/html/sites/default/settings.php
+				/bin/chmod 600 /var/www/html/sites/default/settings.php
+				/bin/chown www-data:www-data /var/www/html/sites/default/settings.php
+
+				if ( [ "`/usr/bin/curl -m 2 --insecure -I "https://localhost:443/index.php" 2>&1 | /bin/grep \"HTTP\" | /bin/grep -w \"200\|301\|302\|303\"`" = "" ] )
+				then
+					/bin/cp ${HOME}/runtime/drupal_settings.php.$$ /var/www/html/sites/default/settings.php
+				fi
+			fi
+		else
+			${HOME}/providerscripts/email/SendEmail.sh "UNABLE TO OBTAIN APPLICATION CONFIGURATION FROM DATASTORE" "The drupal configuration file could not be obtained from the config datastore" "ERROR"
 		fi
 	fi
 fi
-
-/bin/sleep 20
-
-if ( [ "`${HOME}/providerscripts/datastore/configwrapper/AgeOfConfigFile.sh drupal_settings.php`" -lt "130" ] && [ "`/usr/bin/find /var/www/html/sites/default/settings.php -cmin -1`" = "" ] )
-then
-	if ( [ -f ${HOME}/runtime/drupal_settings.php ] )
-	then
-		/bin/mv ${HOME}/runtime/drupal_settings.php ${HOME}/runtime/drupal_settings.php.$$
-	fi
-	${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh drupal_settings.php ${HOME}/runtime/drupal_settings.php
-	if ( [ -f ${HOME}/runtime/drupal_settings.php ] )
-	then
-		/usr/bin/php -ln ${HOME}/runtime/drupal_settings.php
-		if ( [ "$?" = "0" ] )
-		then
-			if ( [ "`/usr/bin/diff ${HOME}/runtime/drupal_settings.php /var/www/html/sites/default/settings.php`" = "" ] )
-			then
-				exit
-			fi
-			/bin/cp ${HOME}/runtime/drupal_settings.php /var/www/html/sites/default/settings.php
-			/bin/chmod 600 /var/www/html/sites/default/settings.php
-			/bin/chown www-data:www-data /var/www/html/sites/default/settings.php
-
-			if ( [ "`/usr/bin/curl -m 2 --insecure -I "https://localhost:443/index.php" 2>&1 | /bin/grep \"HTTP\" | /bin/grep -w \"200\|301\|302\|303\"`" = "" ] )
-			then
-				/bin/cp ${HOME}/runtime/drupal_settings.php.$$ /var/www/html/sites/default/settings.php
-			fi
-		fi
-	else
-		${HOME}/providerscripts/email/SendEmail.sh "UNABLE TO OBTAIN APPLICATION CONFIGURATION FROM DATASTORE" "The drupal configuration file could not be obtained from the config datastore" "ERROR"
-
-	fi
-fi
-
 
 
