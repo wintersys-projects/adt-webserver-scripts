ba90333bef0704858aec268d13970ab12d6b5d26
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-24 10:54:27 +0100

    Update ConnectToRemoteMySQL.sh

--- utilities/remote/ConnectToRemoteMySQL.sh
+++ utilities/remote/ConnectToRemoteMySQL.sh
@@ -19,7 +19,7 @@
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 #######################################################################################
 #######################################################################################
-set -x
+#set -x
 
 if ( [ "`/usr/bin/hostname | /bin/grep "\-rp-"`" != "" ] || [ "`/usr/bin/hostname | /bin/grep "^auth-"`" != "" ] )
 then
