253d2fd6932e1fd6ee4ff7a014f84d57af46fe5f
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-08-19 10:18:26 +0100

    Update UpdateApplicationConfiguration.sh

--- application/configuration/moodle/UpdateApplicationConfiguration.sh
+++ application/configuration/moodle/UpdateApplicationConfiguration.sh
@@ -57,34 +57,37 @@ then
 
 	/bin/sleep 20
 
-	if ( [ "`${HOME}/providerscripts/datastore/configwrapper/AgeOfConfigFile.sh moodle_config.php`" -lt "130" ] && [ "`/usr/bin/find /var/www/html/config.php -cmin -1`" = "" ] )
-	then
-		if ( [ -f ${HOME}/runtime/moodle_config.php ] )
+	if ( [ "`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh moodle_config.php`" != "" ] )
+ 	then
+		if ( [ "`${HOME}/providerscripts/datastore/configwrapper/AgeOfConfigFile.sh moodle_config.php`" -lt "130" ] && [ "`/usr/bin/find /var/www/html/config.php -cmin -1`" = "" ] )
 		then
-			/bin/mv ${HOME}/runtime/moodle_config.php ${HOME}/runtime/moodle_config.php.$$
-		fi
-		${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh moodle_config.php ${HOME}/runtime/moodle_config.php
-		if ( [ -f ${HOME}/runtime/moodle_config.php ] )
-		then
-			/usr/bin/php -ln ${HOME}/runtime/moodle_config.php
-			if ( [ "$?" = "0" ] )
+			if ( [ -f ${HOME}/runtime/moodle_config.php ] )
 			then
-				if ( [ "`/usr/bin/diff ${HOME}/runtime/moodle_config.php /var/www/html/config.php`" = "" ] )
-				then
-					exit
-				fi
-				/bin/cp ${HOME}/runtime/moodle_config.php /var/www/html/config.php
-				/bin/chmod 600 /var/www/html/config.php
-				/bin/chown www-data:www-data /var/www/html/config.php
-
-				if ( [ "`/usr/bin/curl -m 2 --insecure -I "https://localhost:443/index.php" 2>&1 | /bin/grep \"HTTP\" | /bin/grep -w \"200\|301\|302\|303\"`" = "" ] )
-				then
-					/bin/cp ${HOME}/runtime/moodle_config.php.$$ /var/www/html/config.php
-				fi
+				/bin/mv ${HOME}/runtime/moodle_config.php ${HOME}/runtime/moodle_config.php.$$
 			fi
-		else
-			${HOME}/providerscripts/email/SendEmail.sh "UNABLE TO OBTAIN APPLICATION CONFIGURATION FROM DATASTORE" "The moodle configuration file could not be obtained from the config datastore" "ERROR"
-		fi
+			${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh moodle_config.php ${HOME}/runtime/moodle_config.php
+			if ( [ -f ${HOME}/runtime/moodle_config.php ] )
+			then
+				/usr/bin/php -ln ${HOME}/runtime/moodle_config.php
+				if ( [ "$?" = "0" ] )
+				then
+					if ( [ "`/usr/bin/diff ${HOME}/runtime/moodle_config.php /var/www/html/config.php`" = "" ] )
+					then
+						exit
+					fi
+					/bin/cp ${HOME}/runtime/moodle_config.php /var/www/html/config.php
+					/bin/chmod 600 /var/www/html/config.php
+					/bin/chown www-data:www-data /var/www/html/config.php
+
+					if ( [ "`/usr/bin/curl -m 2 --insecure -I "https://localhost:443/index.php" 2>&1 | /bin/grep \"HTTP\" | /bin/grep -w \"200\|301\|302\|303\"`" = "" ] )
+					then
+						/bin/cp ${HOME}/runtime/moodle_config.php.$$ /var/www/html/config.php
+					fi
+				fi
+			else
+				${HOME}/providerscripts/email/SendEmail.sh "UNABLE TO OBTAIN APPLICATION CONFIGURATION FROM DATASTORE" "The moodle configuration file could not be obtained from the config datastore" "ERROR"
+			fi
+   		fi
 	fi
 fi
 
