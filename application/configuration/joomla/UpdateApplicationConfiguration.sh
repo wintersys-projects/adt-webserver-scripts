293d1813377d1f5209f72db1f9f7d896a03a55c7
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-08-19 10:17:16 +0100

    Update UpdateApplicationConfiguration.sh

--- application/configuration/joomla/UpdateApplicationConfiguration.sh
+++ application/configuration/joomla/UpdateApplicationConfiguration.sh
@@ -56,35 +56,39 @@ then
 
 	/bin/sleep 20
 
-	if ( [ "`${HOME}/providerscripts/datastore/configwrapper/AgeOfConfigFile.sh joomla_configuration.php`" -lt "130" ] && [ "`/usr/bin/find /var/www/html/configuration.php -cmin -1`" = "" ] )
-	then
-		if ( [ -f ${HOME}/runtime/joomla_configuration.php ] )
+	if ( [ "`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh joomla_configuration.php`" != "" ] )
+ 	then
+		if ( [ "`${HOME}/providerscripts/datastore/configwrapper/AgeOfConfigFile.sh joomla_configuration.php`" -lt "130" ] && [ "`/usr/bin/find /var/www/html/configuration.php -cmin -1`" = "" ] )
 		then
-			/bin/mv ${HOME}/runtime/joomla_configuration.php ${HOME}/runtime/joomla_configuration.php.$$
-		fi
-		${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh joomla_configuration.php ${HOME}/runtime/joomla_configuration.php
-		if ( [ -f ${HOME}/runtime/joomla_configuration.php ] )
-		then
-			/usr/bin/php -ln ${HOME}/runtime/joomla_configuration.php
-			if ( [ "$?" = "0" ] )
+			if ( [ -f ${HOME}/runtime/joomla_configuration.php ] )
 			then
-				if ( [ "`/usr/bin/diff ${HOME}/runtime/joomla_configuration.php /var/www/html/configuration.php`" = "" ] )
-				then
-					exit
-				fi
-				/bin/cp ${HOME}/runtime/joomla_configuration.php /var/www/html/configuration.php
-				/bin/chmod 600 /var/www/html/configuration.php
-				/bin/chown www-data:www-data /var/www/html/configuration.php
-
-				if ( [ "`/usr/bin/curl -m 2 --insecure -I "https://localhost:443/index.php" 2>&1 | /bin/grep \"HTTP\" | /bin/grep -w \"200\|301\|302\|303\"`" = "" ] )
-				then
-					/bin/cp ${HOME}/runtime/joomla_configuration.php.$$ /var/www/html/configuration.php
-					exit
-				fi
+				/bin/mv ${HOME}/runtime/joomla_configuration.php ${HOME}/runtime/joomla_configuration.php.$$
 			fi
-		else
-			${HOME}/providerscripts/email/SendEmail.sh "UNABLE TO OBTAIN APPLICATION CONFIGURATION FROM DATASTORE" "The joomla configuration file could not be obtained from the config datastore" "ERROR"
-		fi
+		
+  			${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh joomla_configuration.php ${HOME}/runtime/joomla_configuration.php
+			if ( [ -f ${HOME}/runtime/joomla_configuration.php ] )
+			then
+				/usr/bin/php -ln ${HOME}/runtime/joomla_configuration.php
+				if ( [ "$?" = "0" ] )
+				then
+					if ( [ "`/usr/bin/diff ${HOME}/runtime/joomla_configuration.php /var/www/html/configuration.php`" = "" ] )
+					then
+						exit
+					fi
+					/bin/cp ${HOME}/runtime/joomla_configuration.php /var/www/html/configuration.php
+					/bin/chmod 600 /var/www/html/configuration.php
+					/bin/chown www-data:www-data /var/www/html/configuration.php
+
+					if ( [ "`/usr/bin/curl -m 2 --insecure -I "https://localhost:443/index.php" 2>&1 | /bin/grep \"HTTP\" | /bin/grep -w \"200\|301\|302\|303\"`" = "" ] )
+					then
+						/bin/cp ${HOME}/runtime/joomla_configuration.php.$$ /var/www/html/configuration.php
+						exit
+					fi
+				fi
+			else
+				${HOME}/providerscripts/email/SendEmail.sh "UNABLE TO OBTAIN APPLICATION CONFIGURATION FROM DATASTORE" "The joomla configuration file could not be obtained from the config datastore" "ERROR"
+			fi
+   		fi
 	fi
 fi
 
