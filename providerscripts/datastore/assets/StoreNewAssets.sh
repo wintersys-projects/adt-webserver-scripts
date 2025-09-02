5c6fa2aa399f90814d20c178e4fc32288e329c58
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-09-02 18:05:20 +0100

    Update StoreNewAssets.sh

--- providerscripts/datastore/assets/StoreNewAssets.sh
+++ providerscripts/datastore/assets/StoreNewAssets.sh
@@ -1,5 +1,27 @@
 #!/bin/sh
-
+####################################################################################
+# Description: This will store the assets files when you make a temporal backup
+# of your webrooot. The assets that will be persisted to the datastore will depend on
+# what PERSIST_ASSETS_TO_DATASTORE is set to as well as what DIRECTORIES_TO_MOUNT
+# is set to.
+# Author: Peter Winter
+# Date :  9/4/2016
+###################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+####################################################################################
+####################################################################################
+#set -x
 WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
 directories_to_sync="`${HOME}/utilities/config/ExtractConfigValues.sh 'DIRECTORIESTOMOUNT' 'stripped' | /bin/sed 's/:/ /g'`"
 
