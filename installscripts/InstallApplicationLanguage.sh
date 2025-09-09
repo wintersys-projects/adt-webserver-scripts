0bb7530610cc1502de466bd878e38d483a673ab0
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-09-09 08:46:02 +0100

    Update InstallApplicationLanguage.sh

--- installscripts/InstallApplicationLanguage.sh
+++ installscripts/InstallApplicationLanguage.sh
@@ -18,7 +18,7 @@
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 ###################################################################################
 ###################################################################################
-set -x
+#set -x
 
 APPLICATION_LANGUAGE="`${HOME}/utilities/config/ExtractConfigValue.sh 'APPLICATIONLANGUAGE'`"
 
