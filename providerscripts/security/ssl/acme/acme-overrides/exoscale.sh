320909daa3be016ee236ef5ced77d184894e4b0e
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-09-16 08:08:59 +0100

    Create exoscale.sh

---
+++ providerscripts/security/ssl/acme/acme-overrides/exoscale.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+# shellcheck disable=SC2034
+dns_exoscale_info='Exoscale.com
+Site: Exoscale.com
+Docs: github.com/acmesh-official/acme.sh/wiki/dnsapi#dns_exoscale
+'
+
+########  Public functions #####################
+
+# Usage: add  _acme-challenge.www.domain.com   "XKrxpRBosdIKFzxW_CT3KLZNf6q0HG9i01zxXp5CPBs"
+# Used to add txt record
+dns_exoscale_add() {
+        fulldomain=$1
+        txtvalue=$2
+        #if we are here presume we know we control the domain
+        return 0
+
+}
+
+# Usage: fulldomain txtvalue
+# Used to remove the txt record after validation
+dns_exoscale_rm() {
+        fulldomain=$1
+        txtvalue=$2
+        #if we are here presume we know we control the domain
+        return 0
+}
