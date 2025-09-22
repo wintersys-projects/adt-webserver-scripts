1abf410164867bd355a1dc54742a7d7d7294be59
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-09-22 13:04:58 +0100

    Create linode.sh

---
+++ providerscripts/security/ssl/acme/acme-overrides/linode.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+# shellcheck disable=SC2034
+dns_linode_info='Linode.com (Old)
+Deprecated. Use dns_linode_v4
+Site: Linode.com
+'
+
+
+########  Public functions #####################
+#
+#Usage: add  _acme-challenge.www.domain.com   "XKrxpRBosdIKFzxW_CT3KLZNf6q0HG9i01zxXp5CPBs"
+dns_linode_v4_add() {
+  return 0
+}
+
+#fulldomain txtvalue
+dns_linode_v4_rm() {
+  return 0
+}
