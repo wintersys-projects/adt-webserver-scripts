74067168f1a7b68736c96b1ed773fb9344effece
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-09-22 13:07:04 +0100

    Update vultr.sh

--- providerscripts/security/ssl/acme/acme-overrides/vultr.sh
+++ providerscripts/security/ssl/acme/acme-overrides/vultr.sh
@@ -12,10 +12,12 @@ Issues: github.com/acmesh-official/acme.sh/issues/2374
 #
 #Usage: add  _acme-challenge.www.domain.com   "XKrxpRBosdIKFzxW_CT3KLZNf6q0HG9i01zxXp5CPBs"
 dns_vultr_add() {
+  #if we are here presume we know we control the domain
   return 0
 }
 
 #fulldomain txtvalue
 dns_vultr_rm() {
+  #if we are here presume we know we control the domain
   return 0
 }
