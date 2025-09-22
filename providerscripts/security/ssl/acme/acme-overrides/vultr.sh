93c57eea4ed328744dabaa5ee48eda3d8ef15c86
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-09-22 13:05:44 +0100

    Create vultr.sh

---
+++ providerscripts/security/ssl/acme/acme-overrides/vultr.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+# shellcheck disable=SC2034
+dns_vultr_info='vultr.com
+Site: vultr.com
+Docs: github.com/acmesh-official/acme.sh/wiki/dnsapi2#dns_vultr
+Options:
+VULTR_API_KEY API Key
+Issues: github.com/acmesh-official/acme.sh/issues/2374
+'
+
+########  Public functions #####################
+#
+#Usage: add  _acme-challenge.www.domain.com   "XKrxpRBosdIKFzxW_CT3KLZNf6q0HG9i01zxXp5CPBs"
+dns_vultr_add() {
+  return 0
+}
+
+#fulldomain txtvalue
+dns_vultr_rm() {
+  return 0
+}
