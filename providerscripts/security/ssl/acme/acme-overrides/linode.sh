e87447a8cc265ebe895cc0aedcfc45d2d5b4f21d
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-09-22 13:06:49 +0100

    Update linode.sh

--- providerscripts/security/ssl/acme/acme-overrides/linode.sh
+++ providerscripts/security/ssl/acme/acme-overrides/linode.sh
@@ -10,10 +10,12 @@ Site: Linode.com
 #
 #Usage: add  _acme-challenge.www.domain.com   "XKrxpRBosdIKFzxW_CT3KLZNf6q0HG9i01zxXp5CPBs"
 dns_linode_v4_add() {
+  #if we are here presume we know we control the domain
   return 0
 }
 
 #fulldomain txtvalue
 dns_linode_v4_rm() {
+  #if we are here presume we know we control the domain
   return 0
 }
