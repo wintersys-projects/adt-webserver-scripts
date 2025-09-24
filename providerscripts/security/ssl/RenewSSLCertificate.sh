88b408082b9037a17d2c550559d1c7f8026aa731
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-09-24 10:12:00 +0100

    Update RenewSSLCertificate.sh

--- providerscripts/security/ssl/RenewSSLCertificate.sh
+++ providerscripts/security/ssl/RenewSSLCertificate.sh
@@ -1,1 +1,36 @@
+#!/bin/sh
+################################################################################
+# Author: Peter Winter
+# Date  : 07/07/2016
+# Description: This script tests the current SSL certificate. If it is out of date
+# or approaching its expiry date, a new certificate is generated and replaces it.
+# This check is also done on the build client, where a copy of the SSL certificate
+# but is necessary here, it is run daily from cron, in case the infrastructure is
+# left running for extended periods meaning there is no new builds and therefore no
+# opportunity to check for the validity of the certificate.
+##################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+###################################################################################
+###################################################################################
+#set -x
 
+WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
+DNS_CHOICE="`${HOME}/utilities/config/ExtractConfigValue.sh 'DNSCHOICE'`"
+SERVER_USER="`${HOME}/utilities/config/ExtractConfigValue.sh 'SERVERUSER'`"
+SSL_GENERATION_METHOD="`${HOME}/utilities/config/ExtractConfigValue.sh 'SSLGENERATIONMETHOD'`"
+SSL_GENERATION_SERVICE="`${HOME}/utilities/config/ExtractConfigValue.sh 'SSLGENERATIONSERVICE'`"
+
+
+
+ssl_bucket="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`-${DNS_CHOICE}-${ssl_service}-ssl"
