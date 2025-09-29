e70ef6a77e728eb31e3c2cb704b52b10725427b2
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-09-29 20:50:18 +0100

    Update ws.sh

--- ws.sh
+++ ws.sh
@@ -39,6 +39,7 @@ set fo-=or
 autocmd BufRead,BufWritePre *.sh normal gg=G
 ####ADT-ADDED####" >> /root/.vimrc
 /bin/echo 'export VISUAL=vim' >> ~/.bashrc
+/bin/echo 'export VISUAL=vim' >> /root/.bashrc
 fi
 
 #Set the intialial permissions for the build
