d416f33f9b36892d49fa596183970b917010659b
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-10-27 12:02:43 +0000

    Update InstallApacheConfigurationForReverseProxyFromRepo.sh

--- providerscripts/webserver/configuration/InstallApacheConfigurationForReverseProxyFromRepo.sh
+++ providerscripts/webserver/configuration/InstallApacheConfigurationForReverseProxyFromRepo.sh
@@ -18,6 +18,7 @@
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 #######################################################################################################
 #######################################################################################################
+set -x
 
 HOME="`/bin/cat /home/homedir.dat`"
 WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
