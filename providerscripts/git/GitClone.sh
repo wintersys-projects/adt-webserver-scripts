cc1f299d9590274b130a89a0ae7d1927769919b7
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-04 13:56:45 +0000

    Update GitClone.sh

--- providerscripts/git/GitClone.sh
+++ providerscripts/git/GitClone.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-###############################################################################################################################
+##################################################################################
 # Author : Peter Winter
-# Date   : 13/07/2016
-# Description : This script clones a git repository
-######################################################################################################################################################
+# Date   : 13/06/2016
+# Description : This script will clone a repository from git
+##################################################################################
 # License Agreement:
 # This file is part of The Agile Deployment Toolkit.
 # The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
@@ -16,8 +16,8 @@
 # GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
-#######################################################################################################
-#######################################################################################################
+######################################################################################
+######################################################################################
 #set -x
 
 repository_provider="${1}"
@@ -26,69 +26,68 @@ repository_ownername="${3}"
 repository_name="${4}"
 repository_password="${5}"
 
-clone_dir=""
-if ( [ "`/usr/bin/pwd`" = "/var/www/html" ] )
-then
-	clone_dir="/var/www/html"
-fi
-
-
-if ( [ "${repository_provider}" = "bitbucket" ] )
-then	
-	if ( [ "${repository_username}" = "" ] && [ "${repository_password}" = "" ] )
+count="0" 
+/bin/ls /tmp/test.$$
+while ( [ $? != "0" ] && [ "${count}" -lt "5" ] )
+do
+	count="`/usr/bin/expr ${count} + 1`"
+	if ( [ "${repository_provider}" = "bitbucket" ] )
 	then
-		/usr/bin/git clone https://bitbucket.org/${repository_ownername}/${repository_name}.git ${clone_dir}
-	else
-		if ( [ "${repository_password}" = "none" ] )
+		if ( [ "${repository_username}" = "" ] && [ "${repository_password}" = "" ] )
 		then
-			/usr/bin/git clone https://${repository_username}@bitbucket.org/${repository_ownername}/${repository_name}.git ${clone_dir}
+			/usr/bin/git clone https://bitbucket.org/${repository_ownername}/${repository_name}.git
 		else
-			if ( [ "`/bin/echo ${repository_password} | /bin/egrep -o '(ssh|ecdsa)'`" = "" ] )
+			if ( [ "${repository_password}" = "none" ] )
 			then
-				/usr/bin/git clone https://${repository_username}:${repository_password}@bitbucket.org/${repository_ownername}/${repository_name}.git ${clone_dir}
+				/usr/bin/git clone https://${repository_username}@bitbucket.org/${repository_ownername}/${repository_name}.git
 			else
-				/usr/bin/git clone git@bitbucket.org:${repository_ownername}/${repository_name}.git ${clone_dir}
-			fi	
+				if ( [ "`/bin/echo ${repository_password} | /bin/egrep -o '(ssh|ecdsa)'`" = "" ] )
+				then
+					/usr/bin/git clone https://${repository_username}:${repository_password}@bitbucket.org/${repository_ownername}/${repository_name}.git
+				else
+					/usr/bin/git clone git@bitbucket.org:${repository_ownername}/${repository_name}.git
+				fi		
+			fi
 		fi
 	fi
-fi
 
-if ( [ "${repository_provider}" = "github" ] )
-then
-	if ( [ "${repository_username}" = "" ] && [ "${repository_password}" = "" ] )
+	if ( [ "${repository_provider}" = "github" ] )
 	then
-		/usr/bin/git clone https://github.com/${repository_ownername}/${repository_name}.git ${clone_dir}
-	else
-		if ( [ "${repository_password}" = "none" ] )
+		if ( [ "${repository_username}" = "" ] && [ "${repository_password}" = "" ] )
 		then
-			/usr/bin/git clone https://${repository_username}@github.com/${repository_ownername}/${repository_name}.git ${clone_dir}
+			/usr/bin/git clone https://github.com/${repository_ownername}/${repository_name}.git
 		else
-			if ( [ "`/bin/echo ${repository_password} | /bin/egrep -o '(ssh|ecdsa)'`" = "" ] )
+			if ( [ "${repository_password}" = "none" ] )
 			then
-				/usr/bin/git clone https://${repository_username}:${repository_password}@github.com/${repository_ownername}/${repository_name}.git ${clone_dir}
+				/usr/bin/git clone https://${repository_username}@github.com/${repository_ownername}/${repository_name}.git
 			else
-				/usr/bin/git clone git@github.com:${repository_ownername}/${repository_name}.git ${clone_dir}
-			fi	
+				if ( [ "`/bin/echo ${repository_password} | /bin/egrep -o '(ssh|ecdsa)'`" = "" ] )
+				then
+					/usr/bin/git clone https://${repository_username}:${repository_password}@github.com/${repository_ownername}/${repository_name}.git
+				else
+					/usr/bin/git clone git@github.com:${repository_ownername}/${repository_name}.git
+				fi		
+			fi
 		fi
 	fi
-fi
 
-if ( [ "${repository_provider}" = "gitlab" ] )
-then
-	if ( [ "${repository_username}" = "" ] && [ "${repository_password}" = "" ] )
+	if ( [ "${repository_provider}" = "gitlab" ] )
 	then
-		/usr/bin/git clone https://gitlab.com/${repository_ownername}/${repository_name}.git ${clone_dir}
-	else
-		if ( [ "${repository_password}" = "none" ] )
+		if ( [ "${repository_username}" = "" ] && [ "${repository_password}" = "" ] )
 		then
-			/usr/bin/git clone https://${repository_username}@gitlab.com/${repository_ownername}/${repository_name}.git ${clone_dir}
+			/usr/bin/git clone https://@gitlab.com/${repository_ownername}/${repository_name}.git
 		else
-			if ( [ "`/bin/echo ${repository_password} | /bin/egrep -o '(ssh|ecdsa)'`" = "" ] )
+			if ( [ "${repository_password}" = "none" ] )
 			then
-				/usr/bin/git clone https://${repository_username}:${repository_password}@gitlab.com/${repository_ownername}/${repository_name}.git ${clone_dir}
+				/usr/bin/git clone https://${repository_username}@gitlab.com/${repository_ownername}/${repository_name}.git
 			else
-				/usr/bin/git clone git@gitlab.com:${repository_ownername}/${repository_name}.git ${clone_dir}
-			fi	
+				if ( [ "`/bin/echo ${repository_password} | /bin/egrep -o '(ssh|ecdsa)'`" = "" ] )
+				then
+					/usr/bin/git clone https://${repository_username}:${repository_password}@gitlab.com/${repository_ownername}/${repository_name}.git
+				else
+					/usr/bin/git clone git@gitlab.com:${repository_ownername}/${repository_name}.git
+				fi		
+			fi
 		fi
 	fi
-fi
+done
