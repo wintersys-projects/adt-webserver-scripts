b8bd3c521805839dd3109be102d71affea4e0b28
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-12 13:17:30 +0000

    Update ListFromDatastore.sh

--- providerscripts/datastore/ListFromDatastore.sh
+++ providerscripts/datastore/ListFromDatastore.sh
@@ -26,11 +26,11 @@ REGION="`${HOME}/utilities/config/ExtractConfigValue.sh 'REGION'`"
 
 if ( [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd'`" = "1" ] )
 then
-	config_file="`/bin/grep ${REGION} /root/.s3cfg-* | /usr/bin/awk -F':' '{print $1}'`"
+	config_file="`/bin/grep -H ${REGION} /root/.s3cfg-* | /usr/bin/awk -F':' '{print $1}'`"
 	datastore_tool="/usr/bin/s3cmd --config=${config_file} ls "
 elif ( [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s5cmd'`" = "1" ]  )
 then
-	config_file="`/bin/grep ${REGION} /root/.s5cfg-* | /usr/bin/awk -F':' '{print $1}'`"
+	config_file="`/bin/grep -H ${REGION} /root/.s5cfg-* | /usr/bin/awk -F':' '{print $1}'`"
 	host_base="`/bin/grep host_base ${config_file} | /bin/grep host_base | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
 	datastore_tool="/usr/bin/s5cmd --credentials-file ${config_file} --endpoint-url https://${host_base}  ls "
 fi
