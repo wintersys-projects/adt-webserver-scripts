910915029713fd78954771a845c67b1c7c2571b0
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-12 11:19:05 +0000

    Update PerformDatastoreMount.sh

--- providerscripts/datastore/new/PerformDatastoreMount.sh
+++ providerscripts/datastore/new/PerformDatastoreMount.sh
@@ -25,7 +25,7 @@ count="${2}"
 
 if ( [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd'`" = "1" ] )
 then
-        datastore_tool="/usr/bin/s3cmd "
+        datastore_tool="/usr/bin/s3cmd --config=/root/.s3cfg-${count} "
 elif ( [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s5cmd'`" = "1" ]  )
 then
         host_base="`/bin/grep host_base /root/.s5cfg-${count} | /bin/grep host_base | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
