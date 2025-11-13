9b6f6d0461def9e2628193765c9a74ebc276436c
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-13 14:30:34 +0000

    Update SyncWebrootsNew.sh

--- utilities/housekeeping/SyncWebrootsNew.sh
+++ utilities/housekeeping/SyncWebrootsNew.sh
@@ -6,4 +6,11 @@ then
 	/usr/bin/rsync -au "/var/www/html/" "/var/www/html1"
 fi
 
+#exclude the config files for each type of CMS
+#Get the directory to exclude from the PERSIST_ASSETS setup
+
 /usr/bin/diff -x '.*' --brief --exclude=images /var/www/html /var/www/html1 | /bin/grep -E "(Only in|differ$)"
+
+#Only in /var/www/html means added
+#Only in /var/www/html1 means deleted
+#differs means modified
