2cd2669bc23a520dd24b394f6602de600da0934f
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-18 20:00:14 +0000

    Update ListFromDatastore.sh

--- providerscripts/datastore/ListFromDatastore.sh
+++ providerscripts/datastore/ListFromDatastore.sh
@@ -51,7 +51,7 @@ then
 elif ( [ "${datastore_tool}" = "/usr/bin/s5cmd" ] )
 then
         config_file="`/bin/grep -H ${datastore_region} /root/.s5cfg-* | /usr/bin/awk -F':' '{print $1}'`"
-        host_base="`/bin/grep host_base ${config_file} | /bin/grep host_base | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
+        host_base="`/bin/grep host_base ${config_file} | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
         datastore_cmd="${datastore_tool} --credentials-file ${config_file} --endpoint-url https://${host_base}  ls s3://"
         if ( [ "${file_to_list}" = "" ] )
         then
