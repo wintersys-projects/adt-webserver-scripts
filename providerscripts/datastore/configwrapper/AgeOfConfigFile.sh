64e0ea496105ab477a9f91ca00125df6eeb3e770
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-18 12:53:37 +0000

    Update AgeOfConfigFile.sh

--- providerscripts/datastore/configwrapper/AgeOfConfigFile.sh
+++ providerscripts/datastore/configwrapper/AgeOfConfigFile.sh
@@ -30,22 +30,41 @@ fi
 
 SERVER_USER="`${HOME}/utilities/config/ExtractConfigValue.sh 'SERVERUSER'`"
 TOKEN="`/bin/echo ${SERVER_USER} | /usr/bin/fold -w 4 | /usr/bin/head -n 1 | /usr/bin/tr '[:upper:]' '[:lower:]'`"
-
 config_bucket="`/bin/echo "${WEBSITE_URL}"-config | /bin/sed 's/\./-/g'`-${TOKEN}"
-
 inspected_file="${config_bucket}/${1}"
+S3_HOST_BASE="`${HOME}/utilities/config/ExtractConfigValue.sh 'S3HOSTBASE'`"
+datastore_region="`/bin/echo "${S3_HOST_BASE}" | /bin/sed 's/|/ /g' | /usr/bin/awk '{print $1}' | /bin/sed -E 's/(.digitaloceanspaces.com|sos-|.exo.io|.linodeobjects.com|.vultrobjects.com)//g'`"
 
 if ( [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd'`" = "1" ] )
 then
-	time_file_written="`/usr/bin/s3cmd info s3://${inspected_file}| /bin/grep "Last mod" | /usr/bin/awk -F',' '{print $2}'`"
+        datastore_tool="/usr/bin/s3cmd"
 elif ( [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s5cmd'`" = "1" ]  )
 then
-	host_base="`/bin/grep host_base /root/.s5cfg | /bin/grep host_base | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
-	time_file_written="`/usr/bin/s5cmd --credentials-file /root/.s5cfg --endpoint-url https://${host_base} ls s3://${inspected_file} | /usr/bin/awk '{print $1,$2}'`"
+        datastore_tool="/usr/bin/s5cmd"
+elif ( [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:rclone'`" = "1" ]  )
+then
+        datastore_tool="/usr/bin/rclone"
+fi
+
+if ( [ "${datastore_tool}" = "/usr/bin/s3cmd" ] )
+then
+        config_file="`/bin/grep -H ${datastore_region} /root/.s3cfg-* | /usr/bin/awk -F':' '{print $1}'`"
+        datastore_cmd="${datastore_tool} --config=${config_file} info s3://${inspected_file}"
+        time_file_written="`${datastore_cmd} | /bin/grep "Last mod" | /usr/bin/awk -F',' '{print $2}'`"
+elif ( [ "${datastore_tool}" = "/usr/bin/s5cmd" ] )
+then
+        config_file="`/bin/grep -H ${datastore_region} /root/.s5cfg-* | /usr/bin/awk -F':' '{print $1}'`"
+        host_base="`/bin/grep host_base ${config_file}| /bin/grep host_base | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
+        datastore_cmd="${datastore_tool} --credentials-file  ${config_file} --endpoint-url https://${host_base} head s3://${inspected_file}"
+        time_file_written="`${datastore_cmd} | /usr/bin/jq -r '.last_modified'`"
+elif ( [ "${datastore_tool}" = "/usr/bin/rclone" ] )
+then
+        config_file="`/bin/grep -H ${datastore_region} /root/.config/rclone/rclone.conf-* | /usr/bin/awk -F':' '{print $1}'`"
+        datastore_cmd="${datastore_tool}--config ${config_file} lsl s3:${inspected_file}"
+        time_file_written="`${datastore_cmd} | /usr/bin/awk '{print $2}'`"
 fi
 
 time_file_written="`/usr/bin/date -d "${time_file_written}" +%s`"
-
 time_now="`/usr/bin/date +%s`"
 age_of_file_in_seconds="`/usr/bin/expr ${time_now} - ${time_file_written}`"
 /bin/echo ${age_of_file_in_seconds}
