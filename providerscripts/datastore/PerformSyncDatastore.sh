8c4deec2917720518dafeae7c45bd4ea21ed7e73
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-18 20:02:08 +0000

    Update PerformSyncDatastore.sh

--- providerscripts/datastore/PerformSyncDatastore.sh
+++ providerscripts/datastore/PerformSyncDatastore.sh
@@ -44,7 +44,7 @@ then
         bucket_prefix="s3://"
 elif ( [ "${datastore_tool}" = "/usr/bin/s5cmd" ] )
 then
-        host_base="`/bin/grep host_base /root/.s5cfg-${count} | /bin/grep host_base | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
+        host_base="`/bin/grep host_base /root/.s5cfg-${count} | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
         datastore_cmd="${datastore_tool} --credentials-file /root/.s5cfg-${count} --endpoint-url https://${host_base} sync "
         bucket_prefix="s3://"
 elif ( [ "${datastore_tool}" = "/usr/bin/rclone" ] )
