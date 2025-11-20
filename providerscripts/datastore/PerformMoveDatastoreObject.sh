0d5b9dd1f6b15b2be9870d65d379677f24a6a7bc
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-20 14:39:01 +0000

    Update PerformMoveDatastoreObject.sh

--- providerscripts/datastore/PerformMoveDatastoreObject.sh
+++ providerscripts/datastore/PerformMoveDatastoreObject.sh
@@ -51,7 +51,7 @@ then
         second_prefix="s3://"
 elif ( [ "${datastore_tool}" = "/usr/bin/rclone" ] )
 then
-        host_base="`/bin/grep ^endpoint /root/.config/rclone/rclone.conf-${count} | /bin/grep "^endpoint" | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
+        host_base="`/bin/grep ^endpoint /root/.config/rclone/rclone.conf-${count} | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
         datastore_cmd="${datastore_tool} --config /root/.config/rclone/rclone.conf-${count} --s3-endpoint ${host_base} ls s3:"
         datastore_cmd1="${datastore_tool} --config /root/.config/rclone/rclone.conf-${count} --s3-endpoint ${host_base} moveto s3:"
         second_prefix="s3:"
