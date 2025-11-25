9ba68317fe1da09bf420c828454d1d3da678bf33
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-25 17:22:20 +0000

    Update SetupAssetsStore.sh

--- providerscripts/datastore/assets/SetupAssetsStore.sh
+++ providerscripts/datastore/assets/SetupAssetsStore.sh
@@ -115,12 +115,10 @@ do
 		
 	if ( [ "`/bin/mount | /bin/grep "${asset_directory}"`" = "" ] )
 	then
-
 		${HOME}/providerscripts/datastore/MountDatastore.sh ${asset_bucket}
-
 		if ( [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:s3fs:repo'`" = "1" ] || [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:s3fs:source'`" = "1" ] )
 		then
-			/usr/bin/s3fs -o use_cache=${HOME}/s3mount_cache,allow_other,nonempty,kernel_cache,use_path_request_style,uid=${s3fs_uid},gid=${s3fs_gid},max_stat_cache_size=10000,stat_cache_expire=20,multireq_max=3 -ourl=https://${endpoint} ${asset_bucket} ${asset_directory}
+			/usr/bin/s3fs -o use_cache=${HOME}/s3mount_cache,allow_other,kernel_cache,use_path_request_style,uid=${s3fs_uid},gid=${s3fs_gid},max_stat_cache_size=10000,stat_cache_expire=20,multireq_max=3 -ourl=https://${endpoint} ${asset_bucket} ${asset_directory} &
 		elif ( [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:goof:binary'`" = "1" ] || [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:goof:source'`" = "1" ] )
 		then
 			/bin/mkdir ~/.aws
