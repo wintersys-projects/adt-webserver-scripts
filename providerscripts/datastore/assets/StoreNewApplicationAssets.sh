b4ae8cbcf136c5eedb93c46dab0f12e8dae7520e
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-25 13:33:43 +0000

    Update and rename StoreNewAssets.sh to StoreNewApplicationAssets.sh

--- providerscripts/datastore/assets/StoreNewApplicationAssets.sh
+++ providerscripts/datastore/assets/StoreNewApplicationAssets.sh
@@ -22,6 +22,17 @@
 ####################################################################################
 ####################################################################################
 #set -x
+
+if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh PERSISTASSETSTODATASTORE:0`" = "1" ] )
+then
+	exit
+fi
+
+if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`" = "1" ] || [ "`${HOME}/utilities/config/CheckConfigValue.sh BUILDARCHIVECHOICE:baseline`" = "1" ] )
+then
+	exit
+fi
+
 WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
 directories_to_sync="`${HOME}/utilities/config/ExtractConfigValues.sh 'DIRECTORIESTOMOUNT' 'stripped' | /bin/sed 's/:/ /g'`"
 
