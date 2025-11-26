adc31b3bf2dab289757e9b0f4be02c26805a16b1
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-26 15:18:10 +0000

    Update snippet.sh

--- providerscripts/datastore/assets/snippet.sh
+++ providerscripts/datastore/assets/snippet.sh
@@ -1,29 +1,42 @@
-dirs="merge=images3:merge=media2"
+dirs="images:merge=media3:merge=test4:hello"
 
 dirs="`/bin/echo ${dirs} | /bin/sed 's/:/ /g'`"
 
-newdirs=""
-livedirs=""
+single_dirs=""
+merge_dirs=""
 
 for setting in ${dirs}
 do
         if ( [ "`/bin/echo ${setting} | /bin/grep '^merge='`" != "" ] )
         then
                 no_dirs="`/bin/echo ${setting} | sed -E 's/.*(.)/\1/'`"
-                livedir="`/bin/echo ${setting} | /bin/sed 's/.$//g' | /bin/sed 's/merge=//g'`"
+                live_dir="`/bin/echo ${setting} | /bin/sed 's/.$//g' | /bin/sed 's/merge=//g'`"
                 count="1"
                 while ( [ "${count}" -le "${no_dirs}" ] )
                 do
-                        newdirs="${newdirs}${livedir}${count}:"
+                        merge_dirs="${merge_dirs}${live_dir}${count}:"
                         count="`/usr/bin/expr ${count} + 1`"
                 done
-                livedirs="${livedirs} ${livedir}"
         else
-                newdirs="${new_dirs}${setting}:"
+                single_dirs="${single_dirs}${setting}:"
         fi
+        merge_dirs="`/bin/echo ${merge_dirs} | /bin/sed 's/:$/ /g'`"
 done
 
-dirs="`/bin/echo ${newdirs} | /bin/sed 's/:$//g'`"
+#merge_dirs="`/bin/echo ${merge_dirs} | /bin/sed 's/:$//g'`"
+single_dirs="`/bin/echo ${single_dirs} | /bin/sed 's/:$//g'`"
 
-echo ${dirs}
-echo ${livedirs}
+directory_set=""
+
+for merge_dir in ${merge_dirs}
+do
+        bucket_dir="`/bin/echo ${merge_dir} | /bin/sed 's/:/ /g' | /usr/bin/awk '{print $1}' | /bin/sed 's/[0-9]$//g'`" 
+        directory_set="${directory_set}${bucket_dir} ${merge_dir}|"
+done
+
+directory_set="`/bin/echo ${directory_set} | /bin/sed 's/|$//g'`"
+
+/bin/echo "Merged directories"
+/bin/echo ${directory_set}
+/bin/echo "Single directories"
+echo ${single_dirs}
