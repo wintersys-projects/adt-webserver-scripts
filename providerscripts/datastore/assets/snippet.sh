740e8cf7b8edddf271f41cb5c8d7a2a427a1e278
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-26 22:29:57 +0000

    Update snippet.sh

--- providerscripts/datastore/assets/snippet.sh
+++ providerscripts/datastore/assets/snippet.sh
@@ -1,6 +1,7 @@
-dirs="images:merge=media3:merge=test4:hello"
+#dirs="`/bin/echo ${dirs} | /bin/sed 's/:/ /g'`"
+#application_asset_dirs="`${HOME}/utilities/config/ExtractConfigValues.sh 'DIRECTORIESTOMOUNT' 'stripped' | /bin/sed 's/:/ /g'`"
 
-dirs="`/bin/echo ${dirs} | /bin/sed 's/:/ /g'`"
+dirs="`${HOME}/utilities/config/ExtractConfigValues.sh 'DIRECTORIESTOMOUNT' 'stripped' | /bin/sed 's/:/ /g'`"
 
 single_dirs=""
 merge_dirs=""
@@ -24,17 +25,33 @@ do
 done
 
 single_dirs="`/bin/echo ${single_dirs} | /bin/sed 's/:$//g'`"
-directory_set=""
+merge_dirs_set=""
 for merge_dir in ${merge_dirs}
 do
         bucket_dir="`/bin/echo ${merge_dir} | /bin/sed 's/:/ /g' | /usr/bin/awk '{print $1}' | /bin/sed 's/[0-9]$//g'`" 
-        directory_set="${directory_set}${bucket_dir} ${merge_dir}|"
+        merge_dirs_set="${merge_dirs_set}${bucket_dir} ${merge_dir}|"
 done
 
-directory_set="`/bin/echo ${directory_set} | /bin/sed 's/|$//g'`"
+merge_dirs_set="`/bin/echo ${merge_dirs_set} | /bin/sed 's/|$//g'`"
 /bin/echo "Merged directories"
-/bin/echo ${directory_set}
+/bin/echo ${merge_dirs_set}
 /bin/echo "Single directories"
 echo ${single_dirs}
 
-#combine single_dirs and the bucket_dir into one list and mount them all
+dirs_to_mount_to="`/bin/echo ${merge_dirs_set} | /usr/bin/awk -F '|' '{for (i = 1; i <= NF; i++){print $i}}' | /usr/bin/awk '{print $NF}'`"
+dirs_to_merge_to="`/bin/echo ${merge_dirs_set} | /usr/bin/awk -F '|' '{for (i = 1; i <= NF; i++){print $i}}' | /usr/bin/awk '{print $1}'`"
+
+echo "XXXX"
+echo "${dirs_to_merge_to}" | /usr/bin/tr '\n' ' '
+echo
+echo "${single_dirs}:${dirs_to_mount_to}" | /bin/sed -e 's/^://g' -e 's/:$//g' | /usr/bin/tr '\n' ' ' | /bin/sed 's/:/ /g'
+
+# when I need to do the mergerfs part find the mount directories and the merged directory like this:
+
+exit
+
+test="images hello media1 media2 media3 test1 test2 test3 test4" 
+
+mount="media"
+
+echo ${test} |/bin/grep -wo "${mount}[0-9]"
