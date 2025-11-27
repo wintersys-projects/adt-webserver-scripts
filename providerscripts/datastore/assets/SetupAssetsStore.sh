f210d4d3f3d0004bc9d6dcf3a6565f535661d158
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-27 16:20:51 +0000

    Update SetupAssetsStore.sh

--- providerscripts/datastore/assets/SetupAssetsStore.sh
+++ providerscripts/datastore/assets/SetupAssetsStore.sh
@@ -200,6 +200,12 @@ do
                         done
                 fi
         fi
+        
+        while ( [ "`/bin/mount  | /bin/grep -P "${asset_directory}(?=\s|$)"`" = "" ] )
+        do
+                /bin/sleep 1
+        done
+        
         loop="`/usr/bin/expr ${loop} + 1`"
 done
 
