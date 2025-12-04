90a26fe863463ef97490e0a1c64e6995d55e7c28
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-04 18:13:26 +0000

    Update InitialiseDatastoreConfig.sh

--- providerscripts/datastore/InitialiseDatastoreConfig.sh
+++ providerscripts/datastore/InitialiseDatastoreConfig.sh
@@ -29,11 +29,17 @@ S3_HOST_BASE="`${HOME}/utilities/config/ExtractConfigValue.sh 'S3HOSTBASE'`"
 no_tokens="`/bin/echo "${S3_ACCESS_KEY}" | /usr/bin/fgrep -o '|' | /usr/bin/wc -l`"
 no_tokens="`/usr/bin/expr ${no_tokens} + 1`"
 
+multi_region_rclone=""
+if ( [ "${no_tokens}" -gt "1" ] )
+then
+        multi_region_rclone="multi_region_rclone"
+fi
+
 count="1"
 
 while ( [ "${count}" -le "${no_tokens}" ] )
 do
-        ${HOME}/providerscripts/datastore/PerformDatastoreConfig.sh "`/bin/echo "${S3_ACCESS_KEY}" | /usr/bin/cut -d "|" -f ${count}`" "`/bin/echo "${S3_SECRET_KEY}" | /usr/bin/cut -d "|" -f ${count}`" "`/bin/echo "${S3_LOCATION}" | /usr/bin/cut -d "|" -f ${count}`" "`/bin/echo "${S3_HOST_BASE}" | /usr/bin/cut -d "|" -f ${count}`" ${count}
+        ${HOME}/providerscripts/datastore/PerformDatastoreConfig.sh "`/bin/echo "${S3_ACCESS_KEY}" | /usr/bin/cut -d "|" -f ${count}`" "`/bin/echo "${S3_SECRET_KEY}" | /usr/bin/cut -d "|" -f ${count}`" "`/bin/echo "${S3_LOCATION}" | /usr/bin/cut -d "|" -f ${count}`" "`/bin/echo "${S3_HOST_BASE}" | /usr/bin/cut -d "|" -f ${count}`" ${count} ${multi_region_rclone}  
         count="`/usr/bin/expr ${count} + 1`"
 done
 
