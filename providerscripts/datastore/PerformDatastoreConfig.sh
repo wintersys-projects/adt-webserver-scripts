5a146b8a806bf96acd6a638ce4d92556cdb42392
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-05 09:09:49 +0000

    Update PerformDatastoreConfig.sh

--- providerscripts/datastore/PerformDatastoreConfig.sh
+++ providerscripts/datastore/PerformDatastoreConfig.sh
@@ -164,7 +164,6 @@ then
         fi
 fi
 
-S3_ACCESS_KEY="`${HOME}/utilities/config/ExtractConfigValue.sh 'S3ACCESSKEY'`"
 no_tokens="`/bin/echo "${S3_ACCESS_KEY}" | /usr/bin/fgrep -o '|' | /usr/bin/wc -l`"
 no_tokens="`/usr/bin/expr ${no_tokens} + 1`"
 
