41a1da98fc33bc786005db24e2994c883380ef11
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-08 10:56:53 +0000

    Update SetupFirewall.sh

--- security/SetupFirewall.sh
+++ security/SetupFirewall.sh
@@ -85,7 +85,7 @@ fi
 
 ssl_access_required="0"
 
-if ( [ "`/usr/bin/hostname | /bin/grep '\-rp-'`" != "" ] || ( [ "${NO_REVERSE_PROXY}" = "0" ] && [ "`/usr/bin/hostname | /bin/grep '^ws-'`" != "" ] ) || [ "`/usr/bin/hostname | /bin/grep '^auth-'`" != "" ] )
+if ( [ "`/usr/bin/hostname | /bin/grep '\-rp-'`" != "" ] || ( [ "${NO_REVERSE_PROXY}" = "0" ] && [ "`/usr/bin/hostname | /bin/grep '^ws-'`" != "" ] ) || [ "`/usr/bin/hostname | /bin/grep '\-auth-'`" != "" ] )
 then
         if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh AUTHENTICATIONSERVER:1`" = "0" ] )
         then
