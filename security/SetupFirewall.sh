0c10fcb47871c229f3d75744af065e6aafa78445
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-09 13:43:52 +0000

    Update SetupFirewall.sh

--- security/SetupFirewall.sh
+++ security/SetupFirewall.sh
@@ -269,13 +269,13 @@ then
                                                 updated="1"
                                         fi
                                 done
-                        elif ( [ "`/usr/sbin/iptables --list-rules | /bin/grep "${port}.*ACCEPT"`" = "" ] && [ "${delete}" != "yes" ] )
+                        elif ( [ "`/usr/sbin/iptables --list-rules | /bin/grep "${ip_address}.${port}.*ACCEPT"`" = "" ] && [ "${delete}" != "yes" ] )
                         then
                                 /usr/sbin/iptables -A INPUT -s ${ip_address} -p tcp --dport ${port} -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
                                 /usr/sbin/iptables -A OUTPUT -s ${ip_address} -p tcp --sport ${port} -m conntrack --ctstate ESTABLISHED -j ACCEPT
                                 updated="1"
                         else
-                                if ( [ "`/usr/sbin/iptables --list-rules | /bin/grep "${port}.*ACCEPT"`" != "" ] && [ "${delete}" = "yes" ] )
+                                if ( [ "`/usr/sbin/iptables --list-rules | /bin/grep "${ip_address}.${port}.*ACCEPT"`" != "" ] && [ "${delete}" = "yes" ] )
                                 then
                                         /usr/sbin/iptables -D INPUT -s ${ip_address} -p tcp --dport ${port} -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
                                         /usr/sbin/iptables -D OUTPUT -s ${ip_address} -p tcp --sport ${port} -m conntrack --ctstate ESTABLISHED -j ACCEPT
