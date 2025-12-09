cf9a29d9ef06c6056dc387dcc58af1a2ab5ade27
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-09 15:10:18 +0000

    Update SetupFirewall.sh

--- security/SetupFirewall.sh
+++ security/SetupFirewall.sh
@@ -272,16 +272,16 @@ then
                                 done
                         elif ( [ "`/usr/sbin/iptables --list-rules | /bin/grep "${ip_address}.${port}.*ACCEPT"`" = "" ] && [ "${delete}" != "yes" ] )
                         then
-								/usr/sbin/iptables -I INPUT -s ${ip_address} -p tcp -m tcp --dport ${port} -j ACCEPT
-                               # /usr/sbin/iptables -A INPUT -s ${ip_address} -p tcp --dport ${port} -m conntrack --ctstate NEW,RELATED,ESTABLISHED -j ACCEPT
-                               # /usr/sbin/iptables -A OUTPUT -s ${ip_address} -p tcp --sport ${port} -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+								#/usr/sbin/iptables -I INPUT -s ${ip_address} -p tcp -m tcp --dport ${port} -j ACCEPT
+                                /usr/sbin/iptables -A INPUT -s ${ip_address} -p tcp --dport ${port} -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+                                /usr/sbin/iptables -A OUTPUT -s ${ip_address} -p tcp --sport ${port} -m conntrack --ctstate ESTABLISHED -j ACCEPT
                                 updated="1"
                         else
                                 if ( [ "`/usr/sbin/iptables --list-rules | /bin/grep "${ip_address}.${port}.*ACCEPT"`" != "" ] && [ "${delete}" = "yes" ] )
                                 then
-										/usr/sbin/iptables -D INPUT -s ${ip_address} -p tcp -m tcp --dport ${port} -j ACCEPT
-                                      #  /usr/sbin/iptables -D INPUT -s ${ip_address} -p tcp --dport ${port} -m conntrack --ctstate NEW,RELATED,ESTABLISHED -j ACCEPT
-                                      #  /usr/sbin/iptables -D OUTPUT -s ${ip_address} -p tcp --sport ${port} -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+								#		/usr/sbin/iptables -D INPUT -s ${ip_address} -p tcp -m tcp --dport ${port} -j ACCEPT
+                                        /usr/sbin/iptables -D INPUT -s ${ip_address} -p tcp --dport ${port} -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+                                        /usr/sbin/iptables -D OUTPUT -s ${ip_address} -p tcp --sport ${port} -m conntrack --ctstate ESTABLISHED -j ACCEPT
                                         updated="1"
                                 fi
 
