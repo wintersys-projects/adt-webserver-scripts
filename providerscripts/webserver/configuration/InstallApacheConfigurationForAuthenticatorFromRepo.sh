6f9c158f64af857d53ec3ebc4b6c9b9443ecc6ea
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-10 17:31:59 +0000

    Update InstallApacheConfigurationForAuthenticatorFromRepo.sh

--- providerscripts/webserver/configuration/InstallApacheConfigurationForAuthenticatorFromRepo.sh
+++ providerscripts/webserver/configuration/InstallApacheConfigurationForAuthenticatorFromRepo.sh
@@ -122,7 +122,7 @@ then
 fi
 
 /bin/rm -r /var/www/html/*
-/bin/cp ${HOME}/providerscripts/webserver/configuration/authenticator/firewall/index.php /var/www/html/index.php
+/bin/cp ${HOME}/providerscripts/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/index.php /var/www/html/index.php
 /bin/chown www-data:www-data /var/www/html/index.php
 /bin/chmod 644 /var/www/html/index.php
 
