c81930e15385151398db239f15fe6d707d10bc3f
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-11 13:37:37 +0000

    Update submit.php

--- providerscripts/webserver/configuration/authenticator/firewall/submit.php
+++ providerscripts/webserver/configuration/authenticator/firewall/submit.php
@@ -1,8 +1,7 @@
 <?php
   $email = $_POST["email"];
-  $password = $_POST["password"];  
   $file="/tmp/authentication-emails.dat";
-  $data = "$email:$password\n";
+  $data = "$email\n";
   file_put_contents($file, $data, FILE_APPEND );
   echo "<div class='message'>Email Address Submitted, if needed you should shortly receive an authentication email at $Email</div>";
 ?>
