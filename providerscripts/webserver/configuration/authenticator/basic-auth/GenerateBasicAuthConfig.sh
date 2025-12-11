ef6ddd673c43284475a780c80ac7f3fc6e6facd5
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-11 18:52:34 +0000

    Update GenerateBasicAuthConfig.sh

--- providerscripts/webserver/configuration/authenticator/basic-auth/GenerateBasicAuthConfig.sh
+++ providerscripts/webserver/configuration/authenticator/basic-auth/GenerateBasicAuthConfig.sh
@@ -3,6 +3,18 @@
 MULTI_REGION="`${HOME}/utilities/config/ExtractConfigValue.sh 'MULTIREGION'`"
 WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURLORIGINAL'`"
 
+basic_auth_file=""
+if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh WEBSERVERCHOICE:APACHE`" = "1" ] )
+then
+	basic_auth_file="/etc/apache2/.htpasswd"
+elif ( [ "`${HOME}/utilities/config/CheckConfigValue.sh WEBSERVERCHOICE:NGINX`" = "1" ] )
+then
+	basic_auth_file="/etc/nginx/.htpasswd"
+elif ( [ "`${HOME}/utilities/config/CheckConfigValue.sh WEBSERVERCHOICE:LIGHTTPD`" = "1" ] )
+then
+	basic_auth_file="/etc/lighttpd/.htpasswd"
+fi
+
 if ( [ ! -d ${HOME}/runtime/authenticator ] )
 then
 	/bin/mkdir -p ${HOME}/runtime/authenticator 
@@ -20,11 +32,11 @@ do
 	username="`/bin/echo ${userdetails} | /usr/bin/awk -F':' '{print $1}'`"
 	password="`/bin/echo ${userdetails} | /usr/bin/awk -F':' '{print $2}'`"
 	
-	if ( [ ! -f /etc/apache2/.htpasswd ] )
+	if ( [ ! -f ${basic_auth_file} ] )
 	then
-		/usr/bin/htpasswd -b -c /etc/apache2/.htpasswd ${username} ${password}
+		/usr/bin/htpasswd -b -c ${basic_auth_file} ${username} ${password}
 	else
-		/usr/bin/htpasswd -b /etc/apache2/.htpasswd ${username} ${password}
+		/usr/bin/htpasswd -b ${basic_auth_file} ${username} ${password}
 	fi
 fi
 
