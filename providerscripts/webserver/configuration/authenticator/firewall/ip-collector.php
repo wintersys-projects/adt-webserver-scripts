3d3d8e180d752a8532c5703773066123451cac06
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-11 13:34:29 +0000

    Update ip-collector.php

--- providerscripts/webserver/configuration/authenticator/firewall/ip-collector.php
+++ providerscripts/webserver/configuration/authenticator/firewall/ip-collector.php
@@ -1,32 +1,23 @@
-<?php
-if(isset($_POST['submit'])){
-$IPAddress = "IP Address:".$_POST['ipaddress']."
-";
-$file=fopen("ipaddresses.dat", "a");
-fwrite($file, $IPAddress);
-fclose($file);
-echo "<div class='message'>Thanks if the IP Address is correct for your current browser, you be able to access the main webproperty at XXXXWEBSITEURLXXXX</div>";
-}
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="style.css">
-  <title>Enter your auth.nuocial.uk here and we will send you an authentication link if we know you</title>
+    <title>Enter the IP Address that you want to allow access to</title>
+    <link rel="stylesheet" href="style.css">
 </head>
-<body class="ipcollector-body">
-  <div class="ipcollector-wrapper">
-    <div class="ipcollector-heading">
-      Your IP Address (https://whatsmyip.com)
+
+<body>
+    <div class="main">
+        <h1>Laptop IP Address to allow access for</h1>
+        <h3>Please enter the IP Address of your laptop</h3>
+        <h3>If you don't know what it is you can go to</h3>
+        <a href="https://www.whatsmyip.com/">What's my laptop IP?</a>
+        <form action="submit1.php" method="post">
+          <label for="IPAddress">IPAddress:</label>
+            <input type="text" name="ipaddress" placeholder="IP Address" required autocomplete="off"> <br>                
+            <input type="submit" value="Submit">
+        </form>
     </div>
-    <form class="ipcollector-form" method="post">
-      <input type="text" name="ipaddress" placeholder="IP Address" required autocomplete="off"> <br>
-      <input type="submit" name="submit" value="SAVE" class="ipcollector-submit">
-    </form>
-  </div>
 </body>
+
 </html>
