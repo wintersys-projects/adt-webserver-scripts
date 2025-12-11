fb1a08f1963ca25afc1051e1ea1386b745dde5f1
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-11 14:17:46 +0000

    Update InstallLighttpdConfigurationForAuthenticatorFromRepo.sh

--- providerscripts/webserver/configuration/InstallLighttpdConfigurationForAuthenticatorFromRepo.sh
+++ providerscripts/webserver/configuration/InstallLighttpdConfigurationForAuthenticatorFromRepo.sh
@@ -102,11 +102,16 @@ fi
 /bin/cp ${HOME}/providerscripts/webserver/configuration/authenticator/lighttpd/online/repo/modules.conf /etc/lighttpd/modules.conf
 
 /bin/rm -r /var/www/html/*
-/bin/cp ${HOME}/providerscripts/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/index.php /var/www/html/index.php
-/bin/chown www-data:www-data /var/www/html/index.php
-/bin/chmod 644 /var/www/html/index.php
 
-/bin/sed -i "s/XXXXUSEREMAILDOMAINXXXX/${USER_EMAIL_DOMAIN}/g" /var/www/html/index.php
-/bin/sed -i "s/XXXXWEBSITEURLXXXX/${WEBSITE_URL}/g" /var/www/html/index.php
+if ( [ "${AUTHENTICATOR_TYPE}" = "firewall" ] )
+then
+	/bin/cp ${HOME}/providerscripts/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/index.html /var/www/html/index.html
+	/bin/cp ${HOME}/providerscripts/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/submit.php /var/www/html/submit.php
+	/bin/cp ${HOME}/providerscripts/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/submit1.php /var/www/html/submit1.php
+	/bin/chown www-data:www-data /var/www/html/*
+	/bin/chmod 644 /var/www/html*
+	/bin/sed -i "s/XXXXUSEREMAILDOMAINXXXX/${USER_EMAIL_DOMAIN}/g" /var/www/html/index.html
+	/bin/sed -i "s/XXXXWEBSITEURLXXXX/${WEBSITE_URL}/g" /var/www/html/index.html
+fi
 
 ${HOME}/providerscripts/email/SendEmail.sh "THE LIGHTTPD WEBSERVER HAS BEEN INSTALLED" "Lighttpd webserver is installed and primed" "INFO"
