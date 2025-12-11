cec99d73fd7d68d6f157de359fdfef3dc4bcdaf8
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-11 14:17:24 +0000

    Update InstallApacheConfigurationForAuthenticatorFromSource.sh

--- providerscripts/webserver/configuration/InstallApacheConfigurationForAuthenticatorFromSource.sh
+++ providerscripts/webserver/configuration/InstallApacheConfigurationForAuthenticatorFromSource.sh
@@ -120,12 +120,17 @@ do
 done
 
 /bin/rm -r /var/www/html/*
-/bin/cp ${HOME}/providerscripts/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/index.php /var/www/html/index.php
-/bin/chown www-data:www-data /var/www/html/index.php
-/bin/chmod 644 /var/www/html/index.php
 
-/bin/sed -i "s/XXXXUSEREMAILDOMAINXXXX/${USER_EMAIL_DOMAIN}/g" /var/www/html/index.php
-/bin/sed -i "s/XXXXWEBSITEURLXXXX/${WEBSITE_URL}/g" /var/www/html/index.php
+if ( [ "${AUTHENTICATOR_TYPE}" = "firewall" ] )
+then
+	/bin/cp ${HOME}/providerscripts/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/index.html /var/www/html/index.html
+	/bin/cp ${HOME}/providerscripts/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/submit.php /var/www/html/submit.php
+	/bin/cp ${HOME}/providerscripts/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/submit1.php /var/www/html/submit1.php
+	/bin/chown www-data:www-data /var/www/html/*
+	/bin/chmod 644 /var/www/html*
+	/bin/sed -i "s/XXXXUSEREMAILDOMAINXXXX/${USER_EMAIL_DOMAIN}/g" /var/www/html/index.html
+	/bin/sed -i "s/XXXXWEBSITEURLXXXX/${WEBSITE_URL}/g" /var/www/html/index.html
+fi
 
 /usr/bin/systemctl enable apache2.service
 /usr/bin/systemctl start apache2.service &
