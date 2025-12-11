916639859d8a0d2cab8659bcd5fbaa9d0aedae0c
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-11 17:14:53 +0000

    Update InstallLighttpdConfigurationForAuthenticatorFromSource.sh

--- providerscripts/webserver/configuration/InstallLighttpdConfigurationForAuthenticatorFromSource.sh
+++ providerscripts/webserver/configuration/InstallLighttpdConfigurationForAuthenticatorFromSource.sh
@@ -121,6 +121,7 @@ then
 	/bin/cp ${HOME}/providerscripts/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/submit.php /var/www/html/submit.php
 	/bin/chown www-data:www-data /var/www/html/*
 	/bin/chmod 644 /var/www/html/*
+	/bin/sed -i "s/XXXXUSEREMAILDOMAINXXXX/${USER_EMAIL_DOMAIN}/g" /var/www/html/index.html
 fi
 
 ${HOME}/providerscripts/email/SendEmail.sh "THE LIGHTTPD WEBSERVER HAS BEEN INSTALLED" "Lighttpd webserver is installed and primed" "INFO"
