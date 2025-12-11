f96c189a24c18129bf0f969e72554f92aa8cf668
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-11 14:18:25 +0000

    Update InstallNginxConfigurationForAuthenticatorFromRepo.sh

--- providerscripts/webserver/configuration/InstallNginxConfigurationForAuthenticatorFromRepo.sh
+++ providerscripts/webserver/configuration/InstallNginxConfigurationForAuthenticatorFromRepo.sh
@@ -91,8 +91,14 @@ fi
 /bin/echo "/etc/nginx/sites-available/${WEBSITE_NAME}" > ${HOME}/runtime/WEBSERVER_CONFIG_LOCATION.dat
 
 /bin/rm -r /var/www/html/*
-/bin/cp ${HOME}/providerscripts/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/index.php /var/www/html/index.php
-/bin/chown www-data:www-data /var/www/html/index.php
-/bin/chmod 644 /var/www/html/index.php
-/bin/sed -i "s/XXXXUSEREMAILDOMAINXXXX/${USER_EMAIL_DOMAIN}/g" /var/www/html/index.php
-/bin/sed -i "s/XXXXWEBSITEURLXXXX/${WEBSITE_URL}/g" /var/www/html/index.php
+
+if ( [ "${AUTHENTICATOR_TYPE}" = "firewall" ] )
+then
+	/bin/cp ${HOME}/providerscripts/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/index.html /var/www/html/index.html
+	/bin/cp ${HOME}/providerscripts/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/submit.php /var/www/html/submit.php
+	/bin/cp ${HOME}/providerscripts/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/submit1.php /var/www/html/submit1.php
+	/bin/chown www-data:www-data /var/www/html/*
+	/bin/chmod 644 /var/www/html*
+	/bin/sed -i "s/XXXXUSEREMAILDOMAINXXXX/${USER_EMAIL_DOMAIN}/g" /var/www/html/index.html
+	/bin/sed -i "s/XXXXWEBSITEURLXXXX/${WEBSITE_URL}/g" /var/www/html/index.html
+fi
