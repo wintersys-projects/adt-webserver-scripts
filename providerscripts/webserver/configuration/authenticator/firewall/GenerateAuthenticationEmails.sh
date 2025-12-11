9db1358aa90c9ebb79db158b8a68f909b187f60e
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-11 14:55:57 +0000

    Update GenerateAuthenticationEmails.sh

--- providerscripts/webserver/configuration/authenticator/firewall/GenerateAuthenticationEmails.sh
+++ providerscripts/webserver/configuration/authenticator/firewall/GenerateAuthenticationEmails.sh
@@ -29,8 +29,7 @@ fi
 
 if ( [ -f /tmp/authentication-emails.dat ] )
 then
-	/bin/cat /tmp/authentication-emails.dat >> ${HOME}/runtime/authenticator/authentication-emails.dat
-	/bin/rm /tmp/authentication-emails.dat
+	/bin/mv /tmp/authentication-emails.dat ${HOME}/runtime/authenticator/authentication-emails.dat
 fi
 
 email_list="`/bin/cat ${HOME}/runtime/authenticator/authentication-emails.dat | /usr/bin/awk -F':' '{print $NF}'`"
@@ -45,8 +44,8 @@ do
 	/bin/sed -i "s/XXXXWEBSITEURLXXXX/${WEBSITE_URL_ORIGINAL}/g" ${full_file_name}
 	/bin/chown www-data:www-data ${full_file_name}
 	/bin/chmod 644 ${full_file_name}
-	website_url="https://${WEBSITE_URL}/ip-address-${file_name}.php"
+	website_url="https://${WEBSITE_URL}/ip-address-${file_name}.html"
 	message="<!DOCTYPE html> <html> <body> <h1>IP address authorisation form for ${WEBSITE_URL_ORIGINAL}</h1> <p>From the SAME browser as you want to connect from (your phone broswer might have a different ip address to your laptop if one is on WIFI and one is on 5G go to www.whatsmyip.com and enter the IPV4 IP address in the form that appears when you click the link below. Cheers. This link will be valid for 5 minutes before being deleted. </p> <a href='"${website_url}"'>Enable Your IP Address</a> </body> </html>"
 	${HOME}/providerscripts/email/SendEmail.sh "Authenticated IP claim request for ${WEBSITE_URL_ORIGINAL}" "${message}" MANDATORY ${email_address} "HTML" "AUTHENTICATION"
-	/bin/sed -i "/:${email_address}$/d" ${HOME}/runtime/authenticator/authentication-emails.dat
+	/bin/sed -i "/${email_address}$/d" ${HOME}/runtime/authenticator/authentication-emails.dat
 done
