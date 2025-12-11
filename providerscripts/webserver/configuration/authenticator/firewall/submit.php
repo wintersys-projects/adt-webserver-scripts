9f343cc18f2630b73ac727ef947d873d2e03899a
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-11 13:58:37 +0000

    Update submit.php

--- providerscripts/webserver/configuration/authenticator/firewall/submit.php
+++ providerscripts/webserver/configuration/authenticator/firewall/submit.php
@@ -3,7 +3,7 @@
   $file="/tmp/authentication-emails.dat";
   $data = "$email\n";
   file_put_contents($file, $data, FILE_APPEND );
-  echo "<div class='message'>Email Address Submitted, if needed you should shortly receive an authentication email at $Email</div>";
+  echo "<div class='message'>Email Address Submitted, if needed you should shortly receive an authentication email at $email</div>";
   echo "<div class='message'>The Link in the email will be valid for 5 minutes</div>";
 
 ?>
