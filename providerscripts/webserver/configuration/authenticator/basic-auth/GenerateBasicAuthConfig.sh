f6ab33dae6301252574b816f530024aec261e112
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-11 18:49:22 +0000

    Update GenerateBasicAuthConfig.sh

--- providerscripts/webserver/configuration/authenticator/basic-auth/GenerateBasicAuthConfig.sh
+++ providerscripts/webserver/configuration/authenticator/basic-auth/GenerateBasicAuthConfig.sh
@@ -14,3 +14,17 @@ if ( [ -f /tmp/basic-auth.dat ] )
 then
 	/bin/mv /tmp/basic-auth.dat ${HOME}/runtime/authenticator/basic-auth.dat.$$
 fi
+
+for userdetails in `/bin/cat ${HOME}/runtime/authenticator/basic-auth.dat.$$`
+do
+	username="`/bin/echo ${userdetails} | /usr/bin/awk -F':' '{print $1}'`"
+	password="`/bin/echo ${userdetails} | /usr/bin/awk -F':' '{print $2}'`"
+	
+	if ( [ ! -f /etc/apache2/.htpasswd ] )
+	then
+		/usr/bin/htpasswd -b -c /etc/apache2/.htpasswd ${username} ${password}
+	else
+		/usr/bin/htpasswd -b /etc/apache2/.htpasswd ${username} ${password}
+	fi
+fi
+
