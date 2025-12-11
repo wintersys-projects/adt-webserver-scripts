dd9a0c4079dc95eb5603d1a2b56f16767c285008
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-11 12:17:22 +0000

    Update submit.php

--- providerscripts/webserver/configuration/authenticator/basic-auth/submit.php
+++ providerscripts/webserver/configuration/authenticator/basic-auth/submit.php
@@ -2,9 +2,7 @@
   $email = $_POST["email"];
 $password = $_POST["password"];
 $file="/tmp/basic-auth.dat";
-$data = "$email:$password";
-//fwrite($file, $data);
-file_put_contents($file, $data);
+$data = "$email:$password\n";
+file_put_contents($file, $data, FILE_APPEND );
 echo "<div class='message'>Email Address Submitted, if needed you should shortly receive an authentication email at $Email</div>";
-
 ?>
