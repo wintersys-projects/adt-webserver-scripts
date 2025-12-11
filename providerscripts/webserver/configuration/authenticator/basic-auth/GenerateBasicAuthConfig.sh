95ac3d09eb3885c83e74b5082fb4e5164e9fbef8
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-11 18:39:31 +0000

    Update GenerateBasicAuthConfig.sh

--- providerscripts/webserver/configuration/authenticator/basic-auth/GenerateBasicAuthConfig.sh
+++ providerscripts/webserver/configuration/authenticator/basic-auth/GenerateBasicAuthConfig.sh
@@ -1,7 +1,16 @@
+#!/bin/sh
 
+MULTI_REGION="`${HOME}/utilities/config/ExtractConfigValue.sh 'MULTIREGION'`"
+WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURLORIGINAL'`"
+
+if ( [ ! -d ${HOME}/runtime/authenticator ] )
+then
+	/bin/mkdir -p ${HOME}/runtime/authenticator 
+fi
+
+/bin/touch ${HOME}/runtime/authenticator/basic-auth.dat
 
 if ( [ -f /tmp/basic-auth.dat ] )
 then
-  /bin/mv /tmp/basic-auth.dat ${HOME}/rutnime/authenticator
-
+	/bin/mv /tmp/basic-auth.dat ${HOME}/runtime/authenticator/basic-auth.dat.$$
 fi
