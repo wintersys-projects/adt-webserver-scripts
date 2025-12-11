4b261d3ae55ce9b097bbaf76d7824247b0699940
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-11 13:35:49 +0000

    Create submit1.php

---
+++ providerscripts/webserver/configuration/authenticator/firewall/submit1.php
@@ -0,0 +1,8 @@
+<?php
+  $email = $_POST["email"];
+  $password = $_POST["password"];  
+  $file="/tmp/ipaddresses.dat";
+  $data = "$email:$password\n";
+  file_put_contents($file, $data, FILE_APPEND );
+  echo "<div class='message'>Email Address Submitted, if needed you should shortly receive an authentication email at $Email</div>";
+?>
