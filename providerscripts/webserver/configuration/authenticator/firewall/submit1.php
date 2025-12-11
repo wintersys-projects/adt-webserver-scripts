98743f5da9bce12e8c8365d99182b2d38a29bdcf
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-11 15:26:51 +0000

    Update submit1.php

--- providerscripts/webserver/configuration/authenticator/firewall/submit1.php
+++ providerscripts/webserver/configuration/authenticator/firewall/submit1.php
@@ -3,5 +3,5 @@
   $file="/tmp/ipaddresses.dat";
   $data = "$ipaddress\n";
   file_put_contents($file, $data, FILE_APPEND );
-  echo "<div class='message'>Access for your IP Address has been requested it might take up to a minute, any longer and there is a problem</div>";
+  echo "<div class='message'><h2>Access for your IP Address has been requested it might take up to a minute, any longer and there is a problem.</h2></div>";
 ?>
