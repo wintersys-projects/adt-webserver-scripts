fc886e3c5b037c864939453dd762d0368fb084be
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-15 16:22:01 +0000

    Update GenerateBasicAuthConfig.sh

--- providerscripts/webserver/configuration/authenticator/basic-auth/GenerateBasicAuthConfig.sh
+++ providerscripts/webserver/configuration/authenticator/basic-auth/GenerateBasicAuthConfig.sh
@@ -1,44 +1,30 @@
 #!/bin/sh
 
-MULTI_REGION="`${HOME}/utilities/config/ExtractConfigValue.sh 'MULTIREGION'`"
 WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURLORIGINAL'`"
-
-#basic_auth_file=""
-#if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh WEBSERVERCHOICE:APACHE`" = "1" ] )
-#then
-#	basic_auth_file="/etc/apache2/.htpasswd"
-#elif ( [ "`${HOME}/utilities/config/CheckConfigValue.sh WEBSERVERCHOICE:NGINX`" = "1" ] )
-#then
-#	basic_auth_file="/etc/nginx/.htpasswd"
-#elif ( [ "`${HOME}/utilities/config/CheckConfigValue.sh WEBSERVERCHOICE:LIGHTTPD`" = "1" ] )
-#then
-#	basic_auth_file="/etc/lighttpd/.htpasswd"
-#fi
 
 if ( [ ! -d ${HOME}/runtime/authenticator ] )
 then
-	/bin/mkdir -p ${HOME}/runtime/authenticator 
+        /bin/mkdir -p ${HOME}/runtime/authenticator 
 fi
 
 basic_auth_file="${HOME}/runtime/authenticator/basic-auth.dat"
 
 if ( [ -f /tmp/basic-auth.dat ] )
 then
-	/bin/mv /tmp/basic-auth.dat ${basic_auth_file}.$$
+        /bin/mv /tmp/basic-auth.dat ${basic_auth_file}.$$
 fi
-
 
 for userdetails in `/bin/cat ${basic_auth_file}.$$`
 do
-	username="`/bin/echo ${userdetails} | /usr/bin/awk -F':' '{print $1}'`"
-	password="`/bin/echo ${userdetails} | /usr/bin/awk -F':' '{print $2}'`"
-	
-	if ( [ ! -f ${basic_auth_file} ] )
-	then
-		/usr/bin/htpasswd -b -c ${basic_auth_file} ${username} ${password}
-	else
-		/usr/bin/htpasswd -b ${basic_auth_file} ${username} ${password}
-	fi
+        username="`/bin/echo ${userdetails} | /usr/bin/awk -F':' '{print $1}'`"
+        password="`/bin/echo ${userdetails} | /usr/bin/awk -F':' '{print $2}'`"
+
+        if ( [ ! -f ${basic_auth_file} ] )
+        then
+                /usr/bin/htpasswd -b -c ${basic_auth_file} ${username} ${password}
+        else
+                /usr/bin/htpasswd -b ${basic_auth_file} ${username} ${password}
+        fi
 fi
 
 /bin/rm  ${basic_auth_file}.$$
