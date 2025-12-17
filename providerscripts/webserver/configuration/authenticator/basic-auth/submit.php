c859db0fb64c646806878b49c63fe4392f073351
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-17 10:43:58 +0000

    Update submit.php

--- providerscripts/webserver/configuration/authenticator/basic-auth/submit.php
+++ providerscripts/webserver/configuration/authenticator/basic-auth/submit.php
@@ -1,10 +1,10 @@
 <?php
         $email = $_POST["email"];
-      //  $password = $_POST["password"];
+        $previous_password = $_POST["previous_password"];
         $file="/tmp/basic-auth.dat";
-       // $data = "$email:$password\n";
-       // file_put_contents($file, $data, FILE_APPEND );
-        file_put_contents($file, $email, FILE_APPEND );
+        $data = "$email:$previous_password\n";
+        file_put_contents($file, $data, FILE_APPEND );
+     //   file_put_contents($file, $email, FILE_APPEND );
         echo "<h2>Your basic auth password has been set.</h2>";
         echo "<h2>Please enter this email address and the password from the email I sent you when visiting our main website.</h2>";
 ?>
