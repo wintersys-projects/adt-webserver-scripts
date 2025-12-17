d4b001bf47a699d9e7c7cffdc48897d25cb27220
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-17 20:22:14 +0000

    Update InstallNginxConfigurationForWebserverFromRepo.sh

--- providerscripts/webserver/configuration/InstallNginxConfigurationForWebserverFromRepo.sh
+++ providerscripts/webserver/configuration/InstallNginxConfigurationForWebserverFromRepo.sh
@@ -88,7 +88,7 @@ else
 	fi
 fi
 
-if ( [ "${NO_AUTHENTICATORS}" != "0" ] && [ "${AUTHENTICATOR_TYPE}" = "basic-auth" ] && [ "${NO_REVERSE_PROXY}" != "0" ] )
+if ( [ "${NO_AUTHENTICATORS}" != "0" ] && [ "${AUTHENTICATOR_TYPE}" = "basic-auth" ] && [ "${NO_REVERSE_PROXY}" = "0" ] )
 then
 	/bin/sed -i -e "/#XXXXBASIC-AUTHXXXX/{r ${HOME}/providerscripts/webserver/configuration/${APPLICATION}/nginx/online/repo/basic-auth.conf" -e "d}" /etc/nginx/sites-available/${WEBSITE_NAME}
 	/bin/sed -i "s/XXXXWEBSITE_URLXXXXX/${WEBSITE_URL}/g" /etc/nginx/sites-available/${WEBSITE_NAME}
