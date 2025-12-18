0bbf26d87b3433ab7e29537ba439f70fc3a3424b
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-18 15:05:55 +0000

    Update InstallLighttpdConfigurationForWebserverFromRepo.sh

--- providerscripts/webserver/configuration/InstallLighttpdConfigurationForWebserverFromRepo.sh
+++ providerscripts/webserver/configuration/InstallLighttpdConfigurationForWebserverFromRepo.sh
@@ -88,7 +88,7 @@ then
 		fi
 	fi
 
-	if ( [ "${NO_AUTHENTICATORS}" != "0" ] && [ "${AUTHENTICATOR_TYPE}" = "basic-auth" ] && [ "${NO_REVERSE_PROXY}" != "0" ] )
+	if ( [ "${NO_AUTHENTICATORS}" != "0" ] && [ "${AUTHENTICATOR_TYPE}" = "basic-auth" ] && [ "${NO_REVERSE_PROXY}" = "0" ] )
 	then
 		/bin/sed -i -e "/#XXXXBASIC-AUTHXXXX/{r ${HOME}/providerscripts/webserver/configuration/${APPLICATION}/lighttpd/online/repo/basic-auth.conf" -e "d}" /etc/lighttpd/lighttpd.conf
 		/bin/sed -i "s;XXXXVPC_IP_RANGEXXXX;${VPC_IP_RANGE};g" /etc/lighttpd/lighttpd.conf
