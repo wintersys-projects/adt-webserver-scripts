0e84f026f8495241dceb46cb1ba905053a4801b0
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-19 11:30:53 +0000

    Update AuditSSHConnections.sh

--- utilities/security/AuditSSHConnections.sh
+++ utilities/security/AuditSSHConnections.sh
@@ -36,7 +36,7 @@ for ssh_client_ip in ${ssh_client_ips}
 do
         if ( [ "`/bin/grep ${ssh_client_ip} ${HOME}/runtime/ssh-audit/audit_trail`" = "" ] )
         then
-                /bin/echo ${ssh_client_ip} > ${HOME}/runtime/ssh-audit/audit_trail
+                /bin/echo "SSH connection first initiated at `/usr/bin/date` from IP address ${ssh_client_ip}" > ${HOME}/runtime/ssh-audit/audit_trail
                 ${HOME}/providerscripts/email/SendEmail.sh "SSH CONNECTION FROM A NEW IP ADDRESS" "There has been a new connection from an unknown IP ${ssh_client_ip} to machine `/usr/bin/hostname`" "INFO"
         fi
 done
