4446e4a3b94f78b0008a6955f348af12e79726b8
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-19 10:53:43 +0000

    Create AuditSSHConnections.sh

---
+++ utilities/security/AuditSSHConnections.sh
@@ -0,0 +1,12 @@
+ssh_client_ip="${SSH_CONNECTION}"
+if ( [ ! -d ${HOME}/runtime/ssh-audit ] )
+then
+	/bin/mkdir -p ${HOME}/runtime/ssh-audit
+fi
+
+/bin/touch ${HOME}/runtime/ssh-audit/audit_trail
+
+if ( [ "`/bin/grep ${ssh_client_ip} ${HOME}/runtime/ssh-audit/audit_trail`" = "" ] )
+then
+
+fi
