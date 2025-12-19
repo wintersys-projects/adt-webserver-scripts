26d9d320634a03c1bd5d3aeefb0252796302bbbf
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-19 11:29:23 +0000

    Update AuditSSHConnections.sh

--- utilities/security/AuditSSHConnections.sh
+++ utilities/security/AuditSSHConnections.sh
@@ -1,3 +1,28 @@
+#!/bin/sh
+######################################################################################
+# Author : Peter Winter
+# Date   : 11/08/2021
+# Description: This script will monitor for SSH connections from unknown IPs and send
+# an email if there are any connections from unknown IP addresses. 
+#######################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+########################################################################################
+########################################################################################
+#set -x
+
+HOME="`/bin/cat /home/homedir.dat`"
+
 ssh_client_ips="`/usr/bin/pinky | /usr/bin/awk '{print $NF}' | /usr/bin/tail -n +2`"
 
 if ( [ ! -d ${HOME}/runtime/ssh-audit ] )
