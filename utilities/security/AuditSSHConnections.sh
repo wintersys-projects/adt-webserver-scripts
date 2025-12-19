fe0197f98cd85d0e2634f395f70ed4a186d7e5d7
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-19 11:27:58 +0000

    Update AuditSSHConnections.sh

--- utilities/security/AuditSSHConnections.sh
+++ utilities/security/AuditSSHConnections.sh
@@ -1,12 +1,17 @@
-ssh_client_ip="${SSH_CONNECTION}"
+ssh_client_ips="`/usr/bin/pinky | /usr/bin/awk '{print $NF}' | /usr/bin/tail -n +2`"
+
 if ( [ ! -d ${HOME}/runtime/ssh-audit ] )
 then
-	/bin/mkdir -p ${HOME}/runtime/ssh-audit
+        /bin/mkdir -p ${HOME}/runtime/ssh-audit
 fi
 
 /bin/touch ${HOME}/runtime/ssh-audit/audit_trail
 
-if ( [ "`/bin/grep ${ssh_client_ip} ${HOME}/runtime/ssh-audit/audit_trail`" = "" ] )
-then
-	${HOME}/providerscripts/email/SendEmail.sh "SSH CONNECTION FROM A NEW IP ADDRESS" "There has been a new connection from an unknown IP ${ssh_client_ip} to machine `/usr/bin/hostname`" "INFO"
-fi
+for ssh_client_ip in ${ssh_client_ips}
+do
+        if ( [ "`/bin/grep ${ssh_client_ip} ${HOME}/runtime/ssh-audit/audit_trail`" = "" ] )
+        then
+                /bin/echo ${ssh_client_ip} > ${HOME}/runtime/ssh-audit/audit_trail
+                ${HOME}/providerscripts/email/SendEmail.sh "SSH CONNECTION FROM A NEW IP ADDRESS" "There has been a new connection from an unknown IP ${ssh_client_ip} to machine `/usr/bin/hostname`" "INFO"
+        fi
+done
