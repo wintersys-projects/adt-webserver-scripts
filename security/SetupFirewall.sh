03796dbbf57cd4da29a054e879d5d01844affe13
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-20 15:19:36 +0000

    Update SetupFirewall.sh

--- security/SetupFirewall.sh
+++ security/SetupFirewall.sh
@@ -265,16 +265,25 @@ then
                                 do
                                         if ( [ "`/usr/sbin/iptables --list-rules | /bin/grep "${ip}.*443.*ACCEPT"`" = "" ] )
                                         then
-                                                #/usr/sbin/iptables -A INPUT -s ${ip} -p tcp --dport 443 -m conntrack --ctstate NEW,RELATED,ESTABLISHED -j ACCEPT
-												/usr/sbin/iptables -I INPUT -s ${ip} -p tcp -m tcp --dport 443 -j ACCEPT
+												#original
+												#/usr/sbin/iptables -I INPUT -s ${ip} -p tcp -m tcp --dport 443 -j ACCEPT
+												/usr/sbin/iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+												/usr/sbin/iptables -A INPUT -s ${ip} -p tcp --dport ${port} -m conntrack --ctstate NEW -m limit --limit 20/min --limit-burst 30 -j ACCEPT
+												/usr/sbin/iptables -A INPUT -s ${ip} -p tcp --dport ${port} -m conntrack --ctstate NEW -j DROP
+												/usr/sbin/iptables -A OUTPUT -s ${ip} -p tcp --sport ${port} -m conntrack --ctstate NEW,ESTABLISHED,RELATED -j ACCEPT
                                                 updated="1"
                                         fi
                                 done
                         elif ( [ "`/usr/sbin/iptables --list-rules | /bin/grep "${ip_address}.${port}.*ACCEPT"`" = "" ] && [ "${delete}" != "yes" ] )
                         then
-								#/usr/sbin/iptables -I INPUT -s ${ip_address} -p tcp -m tcp --dport ${port} -j ACCEPT
-                                /usr/sbin/iptables -A INPUT -s ${ip_address} -p tcp --dport ${port} -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-                                /usr/sbin/iptables -A OUTPUT -s ${ip_address} -p tcp --sport ${port} -m conntrack --ctstate ESTABLISHED -j ACCEPT
+							/usr/sbin/iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+							/usr/sbin/iptables -A INPUT -s ${ip_address} -p tcp --dport ${port} -m conntrack --ctstate NEW -m limit --limit 20/min --limit-burst 30 -j ACCEPT
+							/usr/sbin/iptables -A INPUT -s ${ip_address} -p tcp --dport ${port} -m conntrack --ctstate NEW -j DROP
+							/usr/sbin/iptables -A OUTPUT -s ${ip_address} -p tcp --sport ${port} -m conntrack --ctstate NEW,ESTABLISHED,RELATED -j ACCEPT
+
+							#original
+                             #   /usr/sbin/iptables -A INPUT -s ${ip_address} -p tcp --dport ${port} -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+                             #   /usr/sbin/iptables -A OUTPUT -s ${ip_address} -p tcp --sport ${port} -m conntrack --ctstate ESTABLISHED -j ACCEPT
                                 updated="1"
                         else
                                 if ( [ "`/usr/sbin/iptables --list-rules | /bin/grep "${ip_address}.${port}.*ACCEPT"`" != "" ] && [ "${delete}" = "yes" ] )
