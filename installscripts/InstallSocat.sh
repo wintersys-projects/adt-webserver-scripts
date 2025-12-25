10f856c54e9756ed93d97a215e2d10fdfd50e657
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-25 22:38:33 +0000

    Update InstallSocat.sh

--- installscripts/InstallSocat.sh
+++ installscripts/InstallSocat.sh
@@ -61,7 +61,7 @@ do
 	count="`/usr/bin/expr ${count} + 1`"
 done
 
-if ( [ ! -f /usr/bin/socat ] && [ "${count}" = "5" ] )
+if ( [ ! -x /usr/bin/socat ] && [ "${count}" = "5" ] )
 then
 	${HOME}/providerscripts/email/SendEmail.sh "INSTALLATION ERROR SOCAT" "I believe that socat hasn't installed correctly, please investigate" "ERROR"
 else
