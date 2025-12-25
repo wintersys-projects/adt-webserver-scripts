7f5a010dfcbb948d885114d33d4531e0915beced
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-25 22:35:20 +0000

    Update InstallMergerFS.sh

--- installscripts/InstallMergerFS.sh
+++ installscripts/InstallMergerFS.sh
@@ -96,7 +96,7 @@ do
 	count="`/usr/bin/expr ${count} + 1`"
 done
 
-if ( [ ! -f /usr/bin/mergerfs ] && [ "${count}" = "5" ] )
+if ( [ ! -x /usr/bin/mergerfs ] && [ "${count}" = "5" ] )
 then
         ${HOME}/providerscripts/email/SendEmail.sh "INSTALLATION ERROR MERGERFS" "I believe that mergerfs hasn't installed correctly, please investigate" "ERROR"
 else
