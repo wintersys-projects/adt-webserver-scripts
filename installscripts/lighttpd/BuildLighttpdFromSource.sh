f329a3e931f6b86859a53b56a12a540f9e374ef3
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-27 18:27:12 +0000

    Update BuildLighttpdFromSource.sh

--- installscripts/lighttpd/BuildLighttpdFromSource.sh
+++ installscripts/lighttpd/BuildLighttpdFromSource.sh
@@ -37,11 +37,8 @@ cd /usr/local/src/
 minor_version="`/usr/bin/curl -L https://api.github.com/repos/lighttpd/lighttpd1.4/tags | /usr/bin/jq -r '.[] | .name' | /usr/bin/awk -F'-' '{print $2}' | /usr/bin/head -1`"
 major_version="`/bin/echo ${minor_version} | /usr/bin/awk -F'.' -v OFS="." '{print $1,$2}'`"
 
-#This one
-#/usr/bin/wget https://github.com/lighttpd/lighttpd${major_version}/archive/refs/tags/lighttpd-${minor_version}.tar.gz
-# or this one
 #/usr/bin/wget https://download.lighttpd.net/lighttpd/releases-${major_version}.x/lighttpd-${minor_version}.tar.gz
-#
+#https://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-1.4.82.sha256sum
 #/bin/tar xvfz lighttpd-${minor_version}.tar.gz
 #cd lighttpd${major_version}-lighttpd-${minor_version}
 
