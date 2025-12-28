c9f80b5921cb474474af28e2826ede2c56e80c62
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-28 19:07:51 +0000

    Update InstallApacheConfigurationForWebserverFromSource.sh

--- providerscripts/webserver/configuration/InstallApacheConfigurationForWebserverFromSource.sh
+++ providerscripts/webserver/configuration/InstallApacheConfigurationForWebserverFromSource.sh
@@ -40,7 +40,7 @@ AUTH_SERVER_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'AUTHSERVERURL'
 /bin/cp ${HOME}/providerscripts/webserver/configuration/${APPLICATION}/apache/online/source/httpd.conf /etc/apache2/httpd.conf
 /bin/cp ${HOME}/providerscripts/webserver/configuration/${APPLICATION}/apache/online/source/envvars.conf /etc/apache2/envvars
 /bin/cp ${HOME}/providerscripts/webserver/configuration/${APPLICATION}/apache/online/source/ports.conf /etc/apache2/ports.conf
-/bin/cp ${HOME}/providerscripts/webserver/configuration/${APPLICATION}/apache/online/source/init.d.conf /etc/init.d/apache2
+#/bin/cp ${HOME}/providerscripts/webserver/configuration/${APPLICATION}/apache/online/source/init.d.conf /etc/init.d/apache2
 
 /bin/sed -i "s/^#ServerRoot.*/ServerRoot \"\/etc\/apache2\"/g" /etc/apache2/httpd.conf
 
