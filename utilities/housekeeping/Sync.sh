bb959151ba6ab8a76bf2e6d3e9d93ca514aec1ac
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-29 20:22:39 +0000

    Update Sync.sh

--- utilities/housekeeping/Sync.sh
+++ utilities/housekeeping/Sync.sh
@@ -1,4 +1,5 @@
 config_file="`${HOME}/application/configuration/GetApplicationConfigFilename.sh`"
+machine_ip="`${HOME}/utilities/processing/GetIP.sh`"
 
 command_body=""
 if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh PERSISTASSETSTODATASTORE:1`" = "1" ] )
@@ -17,7 +18,13 @@ then
         /usr/bin/rsync -av ${command_body} /var/www/html/ /var/www/html1
 else
         echo "added"
-        /usr/bin/rsync -rv --checksum --ignore-times ${command_body} /var/www/html/ /var/www/html1 | /usr/bin/tail -n +2 | /usr/bin/head -n -2 | /bin/sed '/^$/d'
+        for file in `/usr/bin/rsync -rv --checksum --ignore-times ${command_body} /var/www/html/ /var/www/html1 | /usr/bin/tail -n +2 | /usr/bin/head -n -2 | /bin/sed '/^$/d'`
+        do
+                /usr/bin/tar frp ./updates.${machine_ip}.$$.tar.gz  /var/www/html/${file} --owner=www-data --group=www-data
+        done
         echo "removed"
-        /usr/bin/rsync -rv --checksum --ignore-times ${command_body} /var/www/html1/ /var/www/html | /usr/bin/tail -n +2 | /usr/bin/head -n -2 | /bin/sed '/^$/d'
+        for file in `/usr/bin/rsync -rv --checksum --ignore-times ${command_body} /var/www/html1/ /var/www/html | /usr/bin/tail -n +2 | /usr/bin/head -n -2 | /bin/sed '/^$/d'`
+        do
+                /usr/bin/tar frp ./deletes.${machine_ip}.$$.tar.gz  /var/www/html1/${file} --owner=www-data --group=www-data
+        done
 fi
