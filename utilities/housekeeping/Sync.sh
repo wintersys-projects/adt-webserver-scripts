94eaa6b77abd1d4e084773ca9f7579bff35381f1
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-29 22:18:59 +0000

    Update Sync.sh

--- utilities/housekeeping/Sync.sh
+++ utilities/housekeeping/Sync.sh
@@ -12,6 +12,15 @@ fi
 
 command_body="${command_body} --exclude '"${config_file}"'" 
 
+if ( [ ! -d ${HOME}/runtime/webroot_sync/outgoing ] )
+then
+        /bin/mkdir -p ${HOME}/runtime/webroot_sync/outgoing
+fi
+
+if ( [ ! -d ${HOME}/runtime/webroot_sync/incoming ] )
+then
+        /bin/mkdir -p ${HOME}/runtime/webroot_sync/incoming
+fi
 
 if ( [ ! -d /var/www/html1 ] )
 then
@@ -20,11 +29,19 @@ else
         echo "added"
         for file in `/usr/bin/rsync -rv --checksum --ignore-times ${command_body} /var/www/html/ /var/www/html1 | /usr/bin/tail -n +2 | /usr/bin/head -n -2 | /bin/sed '/^$/d'`
         do
-                /usr/bin/tar frp ./updates.${machine_ip}.$$.tar.gz  /var/www/html/${file} --owner=www-data --group=www-data
+                /usr/bin/tar frp ${HOME}/runtime/webroot_sync/outgoing/additions.${machine_ip}.$$.tar.gz  /var/www/html/${file} --owner=www-data --group=www-data
         done
         echo "removed"
         for file in `/usr/bin/rsync -rv --checksum --ignore-times ${command_body} /var/www/html1/ /var/www/html | /usr/bin/tail -n +2 | /usr/bin/head -n -2 | /bin/sed '/^$/d'`
         do
-                /usr/bin/tar frp ./deletes.${machine_ip}.$$.tar.gz  /var/www/html1/${file} --owner=www-data --group=www-data
+                /usr/bin/tar frp ${HOME}/runtime/webroot_sync/outgoing/deletes.${machine_ip}.$$.tar.gz  /var/www/html1/${file} --owner=www-data --group=www-data
+                /bin/rm /var/www/html1/${file}
         done
 fi
+
+#  Create Bucket if it doesn't exist multiregion/webrootsync if MULTI_REGION=1 or configbucket/webrootsync if MULTI_REGION=0
+#  PutToDatastore addition file and delete file to multiregion/webrootsync/additions and deletes to multiregion/webrootsync/deletes
+# SyncFromDatastore down all additions and deletes from multiregion/webrootsync/additions and multiregion/webrootsync/deletes
+# Apply the additions and the deletes if they are not already processed  to the local machine and copy the touch a file in the processed 
+# directory with the same name as the synced file
+# write script that runs daily puts a block on syncing and cleans out local directories and the datastore buckets call it "ResetWebrootSync"
