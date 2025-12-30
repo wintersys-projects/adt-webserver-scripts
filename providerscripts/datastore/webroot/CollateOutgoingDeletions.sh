c43447eb0d1ff0954093336d3523a050d881eec5
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-30 20:12:05 +0000

    Update CollateOutgoingDeletions.sh

--- providerscripts/datastore/webroot/CollateOutgoingDeletions.sh
+++ providerscripts/datastore/webroot/CollateOutgoingDeletions.sh
@@ -18,7 +18,7 @@ command_body="${command_body} --exclude '"${config_file}"'"
         
 for file in `/usr/bin/rsync -rv --checksum --ignore-times ${command_body} /var/www/html1/ /var/www/html | /usr/bin/tail -n +2 | /usr/bin/head -n -2 | /bin/sed '/^$/d'`
 do
-        /usr/bin/tar frp ${HOME}/runtime/webroot_sync/outgoing/deletions/deletions.${machine_ip}.$$.tar.gz  /var/www/html1/${file} --owner=www-data --group=www-data
+        /usr/bin/tar frp ${HOME}/runtime/webroot_sync/outgoing/deletions/deletions.${machine_ip}.$$.tar  /var/www/html1/${file} --owner=www-data --group=www-data
         if ( [ -f /var/www/html1/${file} ] )
         then
                 /bin/rm /var/www/html1/${file}
@@ -30,15 +30,15 @@ done
 
 if ( [ "${MULTI_REGION}" != "1" ] )
 then
-        if ( [ -f ${HOME}/runtime/webroot_sync/outgoing/deletions/deletions.${machine_ip}.$$.tar.gz ] )
+        if ( [ -f ${HOME}/runtime/webroot_sync/outgoing/deletions/deletions.${machine_ip}.$$.tar ] )
         then
-                ${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh  ${HOME}/runtime/webroot_sync/outgoing/deletions/deletions.${machine_ip}.$$.tar.gz webrootsync/deletions "yes"
+                ${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh  ${HOME}/runtime/webroot_sync/outgoing/deletions/deletions.${machine_ip}.$$.tar webrootsync/deletions "yes"
         fi
 else
         multi_region_bucket="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`-multi-region"
 
-        if ( [ -f ${HOME}/runtime/webroot_sync/outgoing/deletions/deletions.${machine_ip}.$$.tar.gz ] )
+        if ( [ -f ${HOME}/runtime/webroot_sync/outgoing/deletions/deletions.${machine_ip}.$$.tar ] )
         then
-                ${HOME}/providerscripts/datastore/configwrapper/PutToDatastore.sh  ${HOME}/runtime/webroot_sync/outgoing/deletions/deletions.${machine_ip}.$$.tar.gz ${multi_region_bucket}/webrootsync/deletions "yes"
+                ${HOME}/providerscripts/datastore/configwrapper/PutToDatastore.sh  ${HOME}/runtime/webroot_sync/outgoing/deletions/deletions.${machine_ip}.$$.tar ${multi_region_bucket}/webrootsync/deletions "yes"
         fi
 fi
