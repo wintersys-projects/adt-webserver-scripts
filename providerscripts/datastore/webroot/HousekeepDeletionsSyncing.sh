c943d9c08a6a86bc8d3a904436ad97ca40892f06
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-30 19:45:22 +0000

    Update HousekeepDeletionsSyncing.sh

--- providerscripts/datastore/webroot/HousekeepDeletionsSyncing.sh
+++ providerscripts/datastore/webroot/HousekeepDeletionsSyncing.sh
@@ -1,13 +1,13 @@
 MULTI_REGION="`${HOME}/utilities/config/ExtractConfigValue.sh 'MULTIREGION'`"
 WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
 
-for expired_addition_archive in `/usr/bin/find ${HOME}/runtime/webroot_sync/processed -type f -mmin +1 | /bin/grep 'additions'`
+for expired_deletion_archive in `/usr/bin/find ${HOME}/runtime/webroot_sync/processed -type f -mmin +1 | /bin/grep 'deletions'`
 do
         if ( [ "${MULTI_REGION}" != "1" ] )
         then
-                ${HOME}/providerscripts/datastore/configwrapper/DeleteFromConfigDatastore.sh webrootsync/additions/${expired_addition_archive}
+                ${HOME}/providerscripts/datastore/configwrapper/DeleteFromConfigDatastore.sh webrootsync/deletions/${expired_deletion_archive}
         else
                 multi_region_bucket="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`-multi-region"
-                ${HOME}/providerscripts/datastore/DeleteFromDatastore.sh ${multi_region_bucket}/webrootsync/additions/${expired_addition_archive}
+                ${HOME}/providerscripts/datastore/DeleteFromDatastore.sh ${multi_region_bucket}/webrootsync/deletions/${expired_deletion_archive}
         fi
 done
