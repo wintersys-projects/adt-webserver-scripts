6390316c3acc772016228f6c2e25e7e31bf413e3
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-30 23:08:35 +0000

    Update WebrootSyncingController.sh

--- providerscripts/datastore/webroot-sync/WebrootSyncingController.sh
+++ providerscripts/datastore/webroot-sync/WebrootSyncingController.sh
@@ -1,7 +1,7 @@
 
 if ( [ ! -f ${HOME}/runtime/webroot_sync/SYNCING_INITIALISED ] )
 then
-  ${HOME}/providerscripts/datastore/webroot/InitialiseWebrootSyncing.sh
+  ${HOME}/providerscripts/datastore/webroot-sync/InitialiseWebrootSyncing.sh
   /bin/touch ${HOME}/runtime/webroot_sync/SYNCING_INITIALISED
 fi
 
