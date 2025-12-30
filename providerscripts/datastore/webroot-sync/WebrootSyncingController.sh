d652f296c7cc4a1f77c3584cf48b07dc3c225a9e
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-30 22:39:48 +0000

    Update WebrootSyncingController.sh

--- providerscripts/datastore/webroot-sync/WebrootSyncingController.sh
+++ providerscripts/datastore/webroot-sync/WebrootSyncingController.sh
@@ -6,11 +6,11 @@ then
 fi
 
 
-${HOME}/providerscripts/datastore/webroot/CollateIncomingAdditions.sh #Incoming additions are files that have been added to the webroots of other servers
-${HOME}/providerscripts/datastore/webroot/CollateIncomingDeletions.sh #Incoming deletions are files that have been removed to the webroots of other servers
-${HOME}/providerscripts/datastore/webroot/CollateOutgoingAdditions.sh #Outgoing additions are files that have been added from this server's webroot
-${HOME}/providerscripts/datastore/webroot/CollateOutgoingDeletions.sh #Outgoing deletions are files that have been removed from this server's webroot
+${HOME}/providerscripts/datastore/webroot-sync/CollateIncomingAdditions.sh #Incoming additions are files that have been added to the webroots of other servers
+${HOME}/providerscripts/datastore/webroot-sync/CollateIncomingDeletions.sh #Incoming deletions are files that have been removed to the webroots of other servers
+${HOME}/providerscripts/datastore/webroot-sync/CollateOutgoingAdditions.sh #Outgoing additions are files that have been added from this server's webroot
+${HOME}/providerscripts/datastore/webroot-sync/CollateOutgoingDeletions.sh #Outgoing deletions are files that have been removed from this server's webroot
 
 
-${HOME}/providerscripts/datastore/webroot/HousekeepAdditionsSyncing.sh
-${HOME}/providerscripts/datastore/webroot/HousekeepDeletionsSyncing.sh
+${HOME}/providerscripts/datastore/webroot-sync/HousekeepAdditionsSyncing.sh
+${HOME}/providerscripts/datastore/webroot-sync/HousekeepDeletionsSyncing.sh
