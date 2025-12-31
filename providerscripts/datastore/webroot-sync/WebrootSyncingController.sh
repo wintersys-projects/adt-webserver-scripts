a439e683582ff510cf6088e982718f6ef65aa8d2
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-31 23:02:45 +0000

    Update WebrootSyncingController.sh

--- providerscripts/datastore/webroot-sync/WebrootSyncingController.sh
+++ providerscripts/datastore/webroot-sync/WebrootSyncingController.sh
@@ -1,9 +1,36 @@
 
-if ( [ ! -f ${HOME}/runtime/webroot_sync/SYNCING_INITIALISED ] )
+if ( [ ! -d ${HOME}/runtime/webroot_sync/outgoing/additions ] )
 then
-  ${HOME}/providerscripts/datastore/webroot-sync/InitialiseWebrootSyncing.sh
-  /bin/touch ${HOME}/runtime/webroot_sync/SYNCING_INITIALISED
+        /bin/mkdir -p ${HOME}/runtime/webroot_sync/outgoing/additions
 fi
+
+if ( [ ! -d ${HOME}/runtime/webroot_sync/outgoing/deletions ] )
+then
+        /bin/mkdir -p ${HOME}/runtime/webroot_sync/outgoing/deletions
+fi
+
+if ( [ ! -d ${HOME}/runtime/webroot_sync/incoming/additions ] )
+then
+        /bin/mkdir -p ${HOME}/runtime/webroot_sync/incoming/additions
+fi
+
+if ( [ ! -d ${HOME}/runtime/webroot_sync/incoming/deletions ] )
+then
+        /bin/mkdir -p ${HOME}/runtime/webroot_sync/incoming/deletions
+fi
+
+if ( [ ! -d ${HOME}/runtime/webroot_sync/processed ] )
+then
+        /bin/mkdir -p ${HOME}/runtime/webroot_sync/processed
+fi
+
+
+
+#if ( [ ! -f ${HOME}/runtime/webroot_sync/SYNCING_INITIALISED ] )
+#then
+#  ${HOME}/providerscripts/datastore/webroot-sync/InitialiseWebrootSyncing.sh
+#  /bin/touch ${HOME}/runtime/webroot_sync/SYNCING_INITIALISED
+#fi
 
 
 #${HOME}/providerscripts/datastore/webroot-sync/CollateIncomingAdditions.sh #Incoming additions are files that have been added to the webroots of other servers
