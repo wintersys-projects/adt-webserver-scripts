cad1bce21c9728a6fae64dee5458861ce2cafea8
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-31 20:25:53 +0000

    Create webrootsync-new.sh

---
+++ providerscripts/datastore/webroot-sync/webrootsync-new.sh
@@ -0,0 +1,32 @@
+set -x
+exclude_list=`${HOME}/application/configuration/GetApplicationConfigFilename.sh`
+
+if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh PERSISTASSETSTODATASTORE:1`" = "1" ] )
+then
+        for dir in `/usr/bin/mount | /bin/grep -Eo "/var/www/html.* " | /usr/bin/awk '{print $1}' | /usr/bin/tr '\n' ' ' | /bin/sed 's;/var/www/html/;;g'`
+        do
+                exclude_list="${exclude_list}$|${dir}"
+        done
+fi
+
+
+
+
+additions=`cd /var/www/html ; /usr/bin/find . -depth -type f | /bin/grep -Ev "(${exclude_list})" | /usr/bin/cpio -pdmv /var/www/html1 2>&1 | /bin/grep -v "not created: newer or same age version exists"`
+#echo ${additions}
+
+config_file="`${HOME}/application/configuration/GetApplicationConfigFilename.sh`"
+
+deletes=`/usr/bin/rsync --dry-run -vr ${command_body} --delete /var/www/html1/ /var/www/html | /usr/bin/head -n +3 | /usr/bin/tail -n +2 | /bin/sed '/^$/d' | /bin/grep -Ev "(${exclude_list})"`
+
+full_path_deletes=""
+for file in ${deletes}
+do
+        full_path_deletes="${full_path_deletes} /var/www/html/${file}"
+done
+
+echo ${full_path_deletes}
+
+#add deletes to tar archive
+
+#delete deletes from /var/www/html1
