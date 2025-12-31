acee49583e0ca6fd3f9732bec6b9855e965c9b91
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-31 23:00:15 +0000

    Update CollateOutgoingWebrootUpdates.sh

--- providerscripts/datastore/webroot-sync/CollateOutgoingWebrootUpdates.sh
+++ providerscripts/datastore/webroot-sync/CollateOutgoingWebrootUpdates.sh
@@ -13,12 +13,11 @@ additions=`cd /var/www/html ; /usr/bin/find . -depth -type f | /bin/grep -Ev "($
 
 for file in ${additions}
 do
-        /usr/bin/tar    ${HOME}/runtime/webroot_sync/outgoing/additions/additions.${machine_ip}.$$.tar.gz
+        /usr/bin/tar ufp ${HOME}/runtime/webroot_sync/outgoing/additions/additions.${machine_ip}.$$.tar.gz  /var/www/html/${file} --owner=www-data --group=www-data
+
 done 
-#tar additions to a tar archive additions.${machine_ip}.tar.gz
 
 config_file="`${HOME}/application/configuration/GetApplicationConfigFilename.sh`"
-
 deletes=`/usr/bin/rsync --dry-run -vr ${command_body} --delete /var/www/html1/ /var/www/html | /usr/bin/head -n +3 | /usr/bin/tail -n +2 | /bin/sed '/^$/d' | /bin/grep -Ev "(${exclude_list})"`
 
 full_path_deletes=""
@@ -35,12 +34,11 @@ do
         /bin/echo "${sync_file}" >> ${HOME}/runtime/webroot_sync/outgoing/deletions/deletions.${machine_ip}.$$.log
 done
 
-
 if ( [ "${MULTI_REGION}" != "1" ] )
 then
         if ( [ -f ${HOME}/runtime/webroot_sync/outgoing/additions/additions.${machine_ip}.$$.tar ] )
         then
-                ${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh  ${HOME}/runtime/webroot_sync/outgoing/additions/additions.${machine_ip}.$$.tar.gz webrootsync/additions "yes"
+                ${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh  ${HOME}/runtime/webroot_sync/outgoing/additions/additions.${machine_ip}.$$.tar webrootsync/additions "yes"
         fi
         if ( [ -f ${HOME}/runtime/webroot_sync/outgoing/deletions/deletions.${machine_ip}.$$.log ] )
         then
@@ -52,19 +50,9 @@ else
         then
                 ${HOME}/providerscripts/datastore/configwrapper/PutToDatastore.sh  ${HOME}/runtime/webroot_sync/outgoing/additions/additions.${machine_ip}.$$.tar ${multi_region_bucket}/webrootsync/additions "yes"
         fi
-fi
-
-if ( [ "${MULTI_REGION}" != "1" ] )
-then
-        if ( [ -f ${HOME}/runtime/webroot_sync/outgoing/deletions/deletions.${machine_ip}.$$.tar.gz ] )
-        then
-                ${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh  ${HOME}/runtime/webroot_sync/outgoing/deletions/deletions.${machine_ip}.$$.tar.gz webrootsync/deletions "yes"
-        fi
-else
-        multi_region_bucket="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`-multi-region"
-
         if ( [ -f ${HOME}/runtime/webroot_sync/outgoing/deletions/deletions.${machine_ip}.$$.log ] )
         then
                 ${HOME}/providerscripts/datastore/configwrapper/PutToDatastore.sh  ${HOME}/runtime/webroot_sync/outgoing/deletions/deletions.${machine_ip}.$$.log ${multi_region_bucket}/webrootsync/deletions "yes"
         fi
 fi
+
