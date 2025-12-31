00c47a580f9da2737d282b99a8102d86cbf28c70
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-31 22:49:38 +0000

    Update CollateIncomingWebrootUpdates.sh

--- providerscripts/datastore/webroot-sync/CollateIncomingWebrootUpdates.sh
+++ providerscripts/datastore/webroot-sync/CollateIncomingWebrootUpdates.sh
@@ -29,3 +29,40 @@ do
                 /bin/touch ${HOME}/runtime/webroot_sync/processed/${archive}
         fi
 done
+
+for archive in `/bin/ls ${HOME}/runtime/webroot_sync/incoming/deletions`
+do
+        if ( [ "`/bin/echo ${archive} | /bin/grep "${machine_ip}"`" = "" ] && [ ! -f ${HOME}/runtime/webroot_sync/processed/${archive} ] )
+        then
+                deletions="`/bin/tar tvf ${HOME}/runtime/webroot_sync/incoming/deletions/${archive} -C / --keep-newer-files | /usr/bin/awk '{print $NF}'`"
+                directories=""
+                for file in ${deletions}
+                do
+                        source_file="/${file}"
+                        sync_file="`/bin/echo ${source_file} | /bin/sed 's;/html/;/html1/;'`"
+                        if ( [ -f ${source_file} ] )
+                        then
+                                /bin/rm ${source_file}
+                        fi
+                        if ( [ -f ${sync_file} ] )
+                        then
+                                /bin/rm ${sync_file}
+                        fi
+                        if ( [ -d ${source_file} ] )
+                        then
+                                if ( [ "`/usr/bin/find ${source_file} -maxdepth 0 -empty -exec echo {} is empty. \; | /bin/grep 'is empty'`" != "" ] )
+                                then
+                                        /bin/rm -r ${source_file}
+                                fi
+                        fi
+                        if ( [ -d ${sync_file} ] )
+                        then
+                                if ( [ "`/usr/bin/find ${sync_file} -maxdepth 0 -empty -exec echo {} is empty. \; | /bin/grep 'is empty'`" != "" ] )
+                                then
+                                        /bin/rm -r ${sync_file}
+                                fi
+                        fi
+                done
+                /bin/touch ${HOME}/runtime/webroot_sync/processed/${archive}
+        fi
+done
