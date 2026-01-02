a3d959e83d68b256706878be0952eb575ece3efe
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-02 19:46:09 +0000

    Update WebrootSyncingController.sh

--- providerscripts/datastore/webroot-sync/WebrootSyncingController.sh
+++ providerscripts/datastore/webroot-sync/WebrootSyncingController.sh
@@ -34,24 +34,9 @@ then
         /bin/mkdir -p ${HOME}/runtime/webroot_sync/incoming/historical/deletions
 fi
 
-
-
-
-#if ( [ ! -f ${HOME}/runtime/webroot_sync/SYNCING_INITIALISED ] )
-#then
-#  ${HOME}/providerscripts/datastore/webroot-sync/InitialiseWebrootSyncing.sh
-#  /bin/touch ${HOME}/runtime/webroot_sync/SYNCING_INITIALISED
-#fi
-
-
-#${HOME}/providerscripts/datastore/webroot-sync/CollateIncomingAdditions.sh #Incoming additions are files that have been added to the webroots of other servers
-#${HOME}/providerscripts/datastore/webroot-sync/CollateIncomingDeletions.sh #Incoming deletions are files that have been removed to the webroots of other servers
-#${HOME}/providerscripts/datastore/webroot-sync/CollateOutgoingAdditions.sh #Outgoing additions are files that have been added from this server's webroot
-#${HOME}/providerscripts/datastore/webroot-sync/CollateOutgoingDeletions.sh #Outgoing deletions are files that have been removed from this server's webroot
-
 if ( [ ! -d /var/www/html1 ] )
 then
-        ${HOME}/providerscripts/datastore/webroot-sync/ProcessHistoricalWebrootUpdates.sh
+        ${HOME}/providerscripts/datastore/webroot-sync/ProcessIncomingHistoricalWebrootUpdates.sh
 fi
 
 ${HOME}/providerscripts/datastore/webroot-sync/ProcessOutgoingWebrootUpdates.sh
