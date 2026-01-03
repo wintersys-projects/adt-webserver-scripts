6785335e34de53fe1813efaac21c552354e4c902
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-03 17:13:58 +0000

    Update WebrootSyncingController.sh

--- providerscripts/datastore/webroot-sync/WebrootSyncingController.sh
+++ providerscripts/datastore/webroot-sync/WebrootSyncingController.sh
@@ -1,8 +1,8 @@
 
 
 running="`/bin/ps -ef | /bin/grep WebrootSyncingController.sh | /bin/grep -v grep | /usr/bin/wc -l`"
+running="`/usr/bin/expr ${running} / 2`"
 expected_running="`/usr/bin/crontab -l | /bin/grep WebrootSyncingController.sh | /usr/bin/wc -l`"
-expected_running="`/usr/bin/expr ${expected_running} / 2`"
 
 if ( [ "${running}" != "${expected_running}" ] )
 then
