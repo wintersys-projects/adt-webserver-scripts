bfad1b307bc109029562fa889d03f6c641f8272b
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-04 15:22:34 +0000

    Update HousekeepAdditionsSyncing.sh

--- providerscripts/datastore/webroot-sync/HousekeepAdditionsSyncing.sh
+++ providerscripts/datastore/webroot-sync/HousekeepAdditionsSyncing.sh
@@ -5,7 +5,7 @@ additions="`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatast
 
 for addition in ${additions}
 do
-        if ( [ "`/bin/echo ${addition} | /bin/sed 's/\./ /g' | /usr/bin/wc -w`" = "7" ] )
+        if ( [ "`/bin/echo ${addition} | /bin/sed 's/\./ /g' | /usr/bin/wc -w`" = "8" ] )
         then
                 if ( [ "`${HOME}/providerscripts/datastore/configwrapper/AgeOfConfigFile.sh webrootsync/additions/${addition}`" -gt "300" ] )
                 then
