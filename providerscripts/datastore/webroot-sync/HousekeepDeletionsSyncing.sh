ee6247eebc2a880e956f1d1f8315eac74f6f8b78
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-04 15:02:27 +0000

    Update HousekeepDeletionsSyncing.sh

--- providerscripts/datastore/webroot-sync/HousekeepDeletionsSyncing.sh
+++ providerscripts/datastore/webroot-sync/HousekeepDeletionsSyncing.sh
@@ -1,18 +1,22 @@
+set -x
 MULTI_REGION="`${HOME}/utilities/config/ExtractConfigValue.sh 'MULTIREGION'`"
 WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
 
-deletions="`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh webrootsync/deletions/deletions*.tar.gz 2>/dev/null`"
+deletions="`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh deletions`"
 
 for deletion in ${deletions}
 do
-        if ( [ "`${HOME}/providerscripts/datastore/configwrapper/AgeOfConfigFile.sh webrootsync/deletions/${deletion}`" -gt "300" ] )
+        if ( [ "`/bin/echo ${deletion} | /bin/sed 's/\./ /g' | /usr/bin/wc -w`" = "7" ] )
         then
-                if ( [ "${MULTI_REGION}" != "1" ] )
+                if ( [ "`${HOME}/providerscripts/datastore/configwrapper/AgeOfConfigFile.sh webrootsync/deletions/${deletion}`" -gt "300" ] )
                 then
-                        ${HOME}/providerscripts/datastore/configwrapper/DeleteFromConfigDatastore.sh webrootsync/deletions/${deletion}
-                else
-                        multi_region_bucket="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`-multi-region"
-                        ${HOME}/providerscripts/datastore/DeleteFromDatastore.sh ${multi_region_bucket}/webrootsync/deletions/${deletion}
+                        if ( [ "${MULTI_REGION}" != "1" ] )
+                        then
+                                ${HOME}/providerscripts/datastore/configwrapper/DeleteFromConfigDatastore.sh webrootsync/deletions/${deletion}
+                        else
+                                multi_region_bucket="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`-multi-region"
+                                ${HOME}/providerscripts/datastore/DeleteFromDatastore.sh ${multi_region_bucket}/webrootsync/deletions/${deletion}
+                        fi
                 fi
         fi
 done
