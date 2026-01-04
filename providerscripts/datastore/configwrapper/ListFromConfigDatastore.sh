96a228e9e473cdecfe446c91cb99cc7b7b099d36
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-04 14:58:49 +0000

    Update ListFromConfigDatastore.sh

--- providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh
+++ providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh
@@ -53,7 +53,7 @@ then
 elif ( [ "${datastore_tool}" = "/usr/bin/rclone" ] )
 then
         host_base="`/bin/grep ^endpoint /root/.config/rclone/rclone.conf-1 | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
-
+        file_to_list="`/bin/echo ${file_to_list} | /usr/bin/awk -F'/' '{print $NF}'`"
         include=""
         if ( [ "${file_to_list}" != "" ] )
         then
