40fd95b40449e49ee41e364094d29e0e601c44f8
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-05 22:05:19 +0000

    Update DeleteHistoricalAdditions.sh

--- providerscripts/datastore/webroot-sync/DeleteHistoricalAdditions.sh
+++ providerscripts/datastore/webroot-sync/DeleteHistoricalAdditions.sh
@@ -25,14 +25,14 @@
 ####################################################################################
 #set -x
 
-historical_additions="`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh webrootsync/historical/additions/additions*.tar.gz`"
+historical_additions="`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh webrootsync/historical/additions/additions*.tar.gz 2>/dev/null`"
 
 for addition in ${historical_additions}
 do
         ${HOME}/providerscripts/datastore/configwrapper/DeleteFromConfigDatastore.sh webrootsync/historical/additions/${addition}
 done
 
-historical_deletions="`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh webrootsync/historical/deletions/deletions*.log`"
+historical_deletions="`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh webrootsync/historical/deletions/deletions*.log 2>/dev/null`"
 
 for deletion in ${historical_deletions}
 do
