a7f070a87dc23275769a21faea35b196ee94e7e4
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-05 17:45:01 +0000

    Update HousekeepAdditionsSyncing.sh

--- providerscripts/datastore/webroot-sync/HousekeepAdditionsSyncing.sh
+++ providerscripts/datastore/webroot-sync/HousekeepAdditionsSyncing.sh
@@ -1,7 +1,29 @@
+#!/bin/sh
+#####################################################################################
+# Author: Peter Winter
+# Date :  9/4/2016
+# Description: Keep addition archives around for 5 minutes (300 seconds)
+# and once these archives are more than 5 minutes old they can be deleted and the 
+# historical copy will then become the authoritative archive.
+#####################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+####################################################################################
+####################################################################################
+#set -x
+
 MULTI_REGION="`${HOME}/utilities/config/ExtractConfigValue.sh 'MULTIREGION'`"
 WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
-
-#additions="`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh additions yes`"
 
 additions="`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh webrootsync/additions/additions*.tar.gz`"
 
