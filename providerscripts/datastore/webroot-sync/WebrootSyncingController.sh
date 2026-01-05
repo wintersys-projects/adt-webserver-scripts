f659b52772ccb8fae90f0222d074de7e36c07f5c
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-05 09:15:25 +0000

    Update WebrootSyncingController.sh

--- providerscripts/datastore/webroot-sync/WebrootSyncingController.sh
+++ providerscripts/datastore/webroot-sync/WebrootSyncingController.sh
@@ -75,6 +75,11 @@ then
         /bin/mkdir -p ${HOME}/runtime/webroot_sync/processed
 fi
 
+if ( [ ! -d ${HOME}/runtime/webroot_sync/processed/historical ] )
+then
+        /bin/mkdir -p ${HOME}/runtime/webroot_sync/processed/historical
+fi
+
 if ( [ ! -d ${HOME}/runtime/webroot_sync/historical/incoming/additions ] )
 then
         /bin/mkdir -p ${HOME}/runtime/webroot_sync/historical/incoming/additions
