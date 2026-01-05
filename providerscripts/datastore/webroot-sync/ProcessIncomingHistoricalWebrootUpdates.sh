5aa8309c60736ed099c82fea6a8230f0b4a58f6d
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-05 17:47:32 +0000

    Update ProcessIncomingHistoricalWebrootUpdates.sh

--- providerscripts/datastore/webroot-sync/ProcessIncomingHistoricalWebrootUpdates.sh
+++ providerscripts/datastore/webroot-sync/ProcessIncomingHistoricalWebrootUpdates.sh
@@ -1,3 +1,27 @@
+#!/bin/sh
+#####################################################################################
+# Author: Peter Winter
+# Date :  9/4/2016
+# Description: When a new machine is deployed (as the result of an autoscaling event)
+# then the existing machines might well have updated webroots and so when a new machine
+# is built (or a machine has been offline for a period of time for some reason, a reboot
+# maybe) then the current set of historical archives need to be applied to bring the 
+# machines up to date with the other webservers in the fleet.
+#####################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+####################################################################################
+####################################################################################
 #set -x
 
 MULTI_REGION="`${HOME}/utilities/config/ExtractConfigValue.sh 'MULTIREGION'`"
