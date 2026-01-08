9625a01909d6cbad6f400675f4ac186efdbaf558
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-08 15:12:56 +0000

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/joomla/InitialiseApplicationConfiguration.sh
+++ application/configuration/joomla/InitialiseApplicationConfiguration.sh
@@ -26,28 +26,46 @@ then
 	exit
 fi
 
-${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh joomla_configuration.php ${HOME}/runtime
+while ( [ ! -f /var/lib/adt-config/joomla_configuration.php ] )
+do
+	/bin/sleep 1
+done
 
-if ( [ ! -f ${HOME}/runtime/joomla_configuration.php ] )
-then
-	${HOME}/providerscripts/email/SendEmail.sh "CONFIGURATION FILE ABSENT" "Unable to obtain the joomla configuration from the datastore during application initiation" "ERROR"
-fi
+/bin/cp /var/lib/adt-config/joomla_configuration.php /var/www/html/configuration.php
+/bin/chmod 600 /var/www/html/configuration.php
+/bin/chown www-data:www-data /var/www/html/configuration.php
 
-/usr/bin/php -ln ${HOME}/runtime/joomla_configuration.php
+/usr/bin/php -ln /var/www/html/configuration.php
 
 if ( [ "$?" = "0" ] )
 then
-	/bin/cp ${HOME}/runtime/joomla_configuration.php /var/www/html/configuration.php
-	/bin/chmod 600 /var/www/html/configuration.php
-	/bin/chown www-data:www-data /var/www/html/configuration.php
-
-	if ( [ ! -f /var/www/html/configuration.php ] )
-	then
-		${HOME}/providerscripts/email/SendEmail.sh "CONFIGURATION FILE ABSENT" "Failed to copy joomla configuration file to the live location during application initiation" "ERROR"
-	else
-		/bin/touch ${HOME}/runtime/INITIAL_CONFIG_SET
-	fi
-
+	/bin/touch ${HOME}/runtime/INITIAL_CONFIG_SET
 else
-	${HOME}/providerscripts/email/SendEmail.sh "CONFIGURATION FILE MALFORMED" "The joomla configuration file appears to be malformed during application initiation" "ERROR"
-fi 
+	${HOME}/providerscripts/email/SendEmail.sh "CONFIGURATION FILE ABSENT" "Failed to copy joomla configuration file to the live location during application initiation" "ERROR"	
+fi
+
+#${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh joomla_configuration.php ${HOME}/runtime
+
+#if ( [ ! -f ${HOME}/runtime/joomla_configuration.php ] )
+#then
+#	${HOME}/providerscripts/email/SendEmail.sh "CONFIGURATION FILE ABSENT" "Unable to obtain the joomla configuration from the datastore during application initiation" "ERROR"
+#fi
+
+#/usr/bin/php -ln ${HOME}/runtime/joomla_configuration.php
+
+#if ( [ "$?" = "0" ] )
+#then
+#	/bin/cp ${HOME}/runtime/joomla_configuration.php /var/www/html/configuration.php
+#	/bin/chmod 600 /var/www/html/configuration.php
+#	/bin/chown www-data:www-data /var/www/html/configuration.php
+#
+#	if ( [ ! -f /var/www/html/configuration.php ] )
+#	then
+#		${HOME}/providerscripts/email/SendEmail.sh "CONFIGURATION FILE ABSENT" "Failed to copy joomla configuration file to the live location during application initiation" "ERROR"
+#	else
+#		/bin/touch ${HOME}/runtime/INITIAL_CONFIG_SET
+#	fi
+#
+#else
+#	${HOME}/providerscripts/email/SendEmail.sh "CONFIGURATION FILE MALFORMED" "The joomla configuration file appears to be malformed during application initiation" "ERROR"
+#fi 
