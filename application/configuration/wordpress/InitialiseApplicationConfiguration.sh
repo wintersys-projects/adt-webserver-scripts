e6965e926f6999b7644ad5abe340736331c7748b
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-08 20:47:59 +0000

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/wordpress/InitialiseApplicationConfiguration.sh
+++ application/configuration/wordpress/InitialiseApplicationConfiguration.sh
@@ -25,28 +25,53 @@ then
 	exit
 fi
 
-${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh wordpress_config.php ${HOME}/runtime
+${HOME}/providerscripts/datastore/configwrapper/PerformSyncFromConfigDatastore.sh
 
-if ( [ ! -f ${HOME}/runtime/wordpress_config.php ] )
+if ( [ ! -f /var/lib/adt-config/wordpress_config.php ] )
 then
-	${HOME}/providerscripts/email/SendEmail.sh "CONFIGURATION FILE ABSENT" "Unable to obtain the wordpress configuration from the datastore during application initiation" "ERROR"
+	${HOME}/providerscripts/email/SendEmail.sh "CONFIGURATION FILE ABSENT" "Failed to copy joomla configuration file to the live location during application initiation" "ERROR"	
 fi
 
-/usr/bin/php -ln ${HOME}/runtime/wordpress_config.php
+/bin/cp /var/lib/adt-config/wordpress_config.php /var/www/html/wp-config.php
+/bin/chmod 600 /var/www/html/wp-config.php
+/bin/chown www-data:www-data /var/www/html/wp-config.php
+
+/usr/bin/php -ln /var/www/html/wp-config.php
 
 if ( [ "$?" = "0" ] )
 then
-	/bin/cp ${HOME}/runtime/wordpress_config.php /var/www/html/wp-config.php
-	/bin/chmod 600 /var/www/html/wp-config.php
-	/bin/chown www-data:www-data /var/www/html/wp-config.php
-
-	if ( [ ! -f /var/www/html/wp-config.php ] )
-	then
-		${HOME}/providerscripts/email/SendEmail.sh "CONFIGURATION FILE ABSENT" "Failed to copy wordpress configuration file to the live location during application initiation" "ERROR"
-	else
-		/bin/touch ${HOME}/runtime/INITIAL_CONFIG_SET
-	fi
-
+	/bin/touch ${HOME}/runtime/INITIAL_CONFIG_SET
 else
-	${HOME}/providerscripts/email/SendEmail.sh "CONFIGURATION FILE MALFORMED" "The wordpress configuration file appears to be malformed during application initiation" "ERROR"
-fi 
+	${HOME}/providerscripts/email/SendEmail.sh "CONFIGURATION FILE ABSENT" "Failed to copy joomla configuration file to the live location during application initiation" "ERROR"	
+fi
+
+#if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`" = "1" ] )
+#then
+#	exit
+#fi
+
+#${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh wordpress_config.php ${HOME}/runtime
+
+#if ( [ ! -f ${HOME}/runtime/wordpress_config.php ] )
+#then
+#	${HOME}/providerscripts/email/SendEmail.sh "CONFIGURATION FILE ABSENT" "Unable to obtain the wordpress configuration from the datastore during application initiation" "ERROR"
+#fi
+
+#/usr/bin/php -ln ${HOME}/runtime/wordpress_config.php
+
+#if ( [ "$?" = "0" ] )
+#then
+#	/bin/cp ${HOME}/runtime/wordpress_config.php /var/www/html/wp-config.php
+#	/bin/chmod 600 /var/www/html/wp-config.php
+#	/bin/chown www-data:www-data /var/www/html/wp-config.php
+#
+#	if ( [ ! -f /var/www/html/wp-config.php ] )
+#	then
+#		${HOME}/providerscripts/email/SendEmail.sh "CONFIGURATION FILE ABSENT" "Failed to copy wordpress configuration file to the live location during application initiation" "ERROR"
+#	else
+#		/bin/touch ${HOME}/runtime/INITIAL_CONFIG_SET
+#	fi
+#
+#else#
+#	${HOME}/providerscripts/email/SendEmail.sh "CONFIGURATION FILE MALFORMED" "The wordpress configuration file appears to be malformed during application initiation" "ERROR"
+#fi 
