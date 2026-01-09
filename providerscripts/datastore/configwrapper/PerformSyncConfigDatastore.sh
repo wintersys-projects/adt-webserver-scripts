9ee98169c09f08211abac910a05d06ee34799848
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-09 22:33:00 +0000

    Update PerformSyncConfigDatastore.sh

--- providerscripts/datastore/configwrapper/PerformSyncConfigDatastore.sh
+++ providerscripts/datastore/configwrapper/PerformSyncConfigDatastore.sh
@@ -1,4 +1,10 @@
-set -x
+#set -x
+
+if ( [ ! -d /var/lib/adt-config-1 ] )
+then
+        /bin/mkdir /var/lib/adt-config-1
+        /bin/cp -r /var/lib/adt-config/* /var/lib/adt-config-1
+fi
 
 deletes_command='/usr/bin/rsync --dry-run --ignore-existing -vr /var/lib/adt-config-1/ /var/lib/adt-config/ 2>&1 | /bin/sed -e "/^$/d" -e  "/.*\/$/d" | /usr/bin/tail -n +2 | /usr/bin/head -n -2 | /usr/bin/tr " "
 "\\n" '
@@ -16,13 +22,14 @@ do
         fi
 done
 
+additions="`/usr/bin/find /var/lib/adt-config/ -mmin -1`"
+
+for file in ${additions}
+do
+        /bin/cp ${file} `/bin/echo ${file} | /bin/sed 's:adt-config:adt-config-1:'`
+done
+
 ${HOME}/providerscripts/datastore/configwrapper/SyncToConfigDatastore.sh /var/lib/adt-config ""
 ${HOME}/providerscripts/datastore/configwrapper/SyncFromConfigDatastore.sh "" /var/lib/adt-config
-
-if ( [ ! -d /var/lib/adt-config-1 ] )
-then
-        /bin/mkdir /var/lib/adt-config-1
-        /bin/cp -r /var/lib/adt-config/* /var/lib/adt-config-1
-fi
 
 
