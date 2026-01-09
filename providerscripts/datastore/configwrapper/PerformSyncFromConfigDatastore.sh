e009dc8597eec7a5cb4c24d10fbecbbc5a5411cf
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-09 19:49:39 +0000

    Update PerformSyncFromConfigDatastore.sh

--- providerscripts/datastore/configwrapper/PerformSyncFromConfigDatastore.sh
+++ providerscripts/datastore/configwrapper/PerformSyncFromConfigDatastore.sh
@@ -1,14 +1,16 @@
 set -x
 
-${HOME}/providerscripts/datastore/configwrapper/SyncFromConfigDatastore.sh "" /var/lib/adt-config.$$
+${HOME}/providerscripts/datastore/configwrapper/SyncFromConfigDatastore.sh "" /var/lib/adt-config
 
-if ( [ -d /var/lib/adt-config ] )
-then
-        if ( [ -d /var/lib/adt-config.old ] )
-        then
-                /bin/rm -r /var/lib/adt-config.old
-        fi
-        /bin/mv /var/lib/adt-config /var/lib/adt-config.old
-fi
+#${HOME}/providerscripts/datastore/configwrapper/SyncFromConfigDatastore.sh "" /var/lib/adt-config.$$
 
-/bin/mv /var/lib/adt-config.$$ /var/lib/adt-config
+#if ( [ -d /var/lib/adt-config ] )
+#then
+ #       if ( [ -d /var/lib/adt-config.old ] )
+  #      then
+   #             /bin/rm -r /var/lib/adt-config.old
+    #    fi
+     #   /bin/mv /var/lib/adt-config /var/lib/adt-config.old
+#fi
+
+#/bin/mv /var/lib/adt-config.$$ /var/lib/adt-config
