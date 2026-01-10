857d65848966e7174fd1879c20611f9801f9ba8a
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-10 21:34:02 +0000

    Update PerformSyncConfigDatastore.sh

--- providerscripts/datastore/configwrapper/PerformSyncConfigDatastore.sh
+++ providerscripts/datastore/configwrapper/PerformSyncConfigDatastore.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 #set -x
 
+if ( [ "`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh INSTALLED_SUCCESSFULLY`" = "" ] )
+then
+        exit
+fi
+
 if ( [ ! -d /var/lib/adt-config1 ] )
 then
         /bin/mkdir -p /var/lib/adt-config1
