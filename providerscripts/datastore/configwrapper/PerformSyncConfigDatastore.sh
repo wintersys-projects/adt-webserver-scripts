365039f483a86787e3d5c330247d10c600c28b15
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-10 23:35:57 +0000

    Update PerformSyncConfigDatastore.sh

--- providerscripts/datastore/configwrapper/PerformSyncConfigDatastore.sh
+++ providerscripts/datastore/configwrapper/PerformSyncConfigDatastore.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#set -x
+set -x
 
 if ( [ "`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh INSTALLED_SUCCESSFULLY`" = "" ] )
 then
@@ -8,12 +8,15 @@ fi
 
 if ( [ ! -d /var/lib/adt-config1 ] )
 then
+        ${HOME}/providerscripts/datastore/configwrapper/SyncFromConfigDatastore.sh "" /var/lib/adt-config
+        /usr/bin/rsync -ru /var/lib/adt-config /var/lib/adt-config1
         /bin/mkdir -p /var/lib/adt-config1
         /bin/cp -r /var/lib/adt-config/* /var/lib/adt-config1/
 fi
 
-deletes_command='/usr/bin/rsync --dry-run -vr --ignore-existing --exclude 'additions' --exclude 'deletions' --exclude 'webrootsync' /var/lib/adt-config1/ /var/lib/adt-config 2>&1 | /bin/sed -e "/^$/d" -e  "/.*\/$/d" | /usr/bin/tail -n +2 | /usr/bin/head -n -2 | /usr/bin/tr " " "\\n" '
+deletes_command='/usr/bin/rsync -acnv --dry-run --exclude 'additions' --exclude 'deletions' --exclude 'webrootsync' /var/lib/adt-config1/ /var/lib/adt-config 2>&1 | /bin/sed -e "/^$/d" -e  "/.*\/$/d" | /usr/bin/tail -n +2 | /usr/bin/head -n -2 | /usr/bin/tr " " "\\n"'
 deletes=`eval ${deletes_command}`
+
 if ( [ "${deletes}" != "" ] )
 then
         for delete in ${deletes}
@@ -21,25 +24,21 @@ then
                 place_to_put=""
                 if ( [ "`/bin/echo ${delete} | /bin/grep '/'`" != "" ] )
                 then
-                        place_to_put="`/bin/echo ${delete} | /bin/sed 's:/[^/]*$::'`"
+                        place_to_put="/`/bin/echo ${delete} | /bin/sed 's:/[^/]*$::'`"
                 fi
-                ${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh /var/lib/adt-config/${delete} deletions/${place_to_put} "no"
+                ${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh /var/lib/adt-config1/${delete} deletions${place_to_put} "no"
         done
 fi
 
-additions_command='cd /var/lib/adt-config ; /usr/bin/rsync -ri --dry-run --ignore-existing --exclude 'additions' --exclude 'deletions' --exclude 'webrootsync' /var/lib/adt-config/ /var/lib/adt-config1/ | /usr/bin/cut -d" " -f2 | /bin/sed -e "/.*\/$/d" | /usr/bin/cpio -pdmvu /var/lib/adt-config1 2>&1 | /bin/grep "^/var" | /bin/sed "s;/var/lib/adt-config1/;;g" | /usr/bin/tr " " "\\n"'
-modifieds_command='cd /var/lib/adt-config ; /usr/bin/rsync -ri --dry-run --checksum --exclude 'additions' --exclude 'deletions' --exclude 'webrootsync'  /var/lib/adt-config/ /var/lib/adt-config1/ | /usr/bin/cut -d" " -f2 | /bin/sed -e  "/.*\/$/d" | /usr/bin/cpio -pdmvu /var/lib/adt-config1 2>&1 | /bin/grep "^/var" | /bin/sed "s;/var/lib/adt-config1/;;g" | /usr/bin/tr " " "\\n"'
-additions=""
+additions_command='/usr/bin/rsync -acnv --dry-run --checksum --exclude 'additions' --exclude 'deletions' --exclude 'webrootsync' /var/lib/adt-config/ /var/lib/adt-config1 2>&1 | /bin/sed -e "/^$/d" -e  "/.*\/$/d" | /usr/bin/tail -n +2 | /usr/bin/head -n -2 | /usr/bin/tr " " "\\n"'
+
 additions=`eval ${additions_command}`
-modifieds=`eval ${modifieds_command}`
-additions="${additions} ${modifieds}"
 
 if ( [ "${additions}" != "" ] )
 then
         for addition in ${additions}
         do
-             #   /bin/cp -r /var/lib/adt-config/additions/* /var/lib/config1/
-               # /usr/bin/rsync -ru /var/lib/adt-config/additions/ /var/lib/config1
+                #               /usr/bin/rsync -a /var/lib/adt-config/additions/ /var/lib/config1
                 place_to_put=""
                 if ( [ "`/bin/echo ${addition} | /bin/grep '/'`" != "" ] )
                 then
@@ -47,12 +46,13 @@ then
                 fi
                 ${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh /var/lib/adt-config/${addition} additions/${place_to_put} "no"
         done
-fi       
+fi    
+
+/bin/sleep 5
 
 ${HOME}/providerscripts/datastore/configwrapper/SyncFromConfigDatastore.sh "" /var/lib/adt-config.$$
 /usr/bin/rsync -ru /var/lib/adt-config.$$/additions/ /var/lib/adt-config.$$
 /usr/bin/rsync -ru /var/lib/adt-config.$$/additions/ /var/lib/adt-config1
-##/bin/cp -r /var/lib/adt-config.$$/additions/* /var/lib/adt-config.$$/
 for deletion in `/bin/ls /var/lib/adt-config.$$/deletions`
 do
         if ( [ -f /var/lib/adt-config.$$/${deletion} ] )
