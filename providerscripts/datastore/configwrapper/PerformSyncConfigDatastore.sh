eb1288818a5580299c56fbf9108a6966dd6efdd0
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-10 17:23:58 +0000

    Update PerformSyncConfigDatastore.sh

--- providerscripts/datastore/configwrapper/PerformSyncConfigDatastore.sh
+++ providerscripts/datastore/configwrapper/PerformSyncConfigDatastore.sh
@@ -5,6 +5,14 @@ then
         deletes_command='/usr/bin/rsync --dry-run -vr --ignore-existing /var/lib/adt-config1/ /var/lib/adt-config 2>&1 | /bin/sed -e "/^$/d" -e  "/.*\/$/d" | /usr/bin/tail -n +2 | /usr/bin/head -n -2 | /usr/bin/tr " " "\\n" '
         deletes=`eval ${deletes_command}`
         ${HOME}/providerscripts/datastore/configwrapper/SyncToConfigDatastore.sh /var/lib/adt-config/
+
+        additions_command='cd /var/www/html ; /usr/bin/rsync -ri --dry-run --ignore-existing '${exclude_command}' /var/lib/adt-config/ /var/www/adt-config1/ | /usr/bin/cut -d" " -f2 | /bin/sed -e "s;^;\./;g" -e "/.*\/$/d" | /usr/bin/cpio -pdmvu /var/lib/adt-config1 2>&1 | /bin/grep "^/var" | /bin/sed "s;/var/www/html1/;;g" | /usr/bin/tr " " "\\n"'
+        modifieds_command='cd /var/www/html ; /usr/bin/rsync -ri --dry-run --checksum '${exclude_command}' /var/lib/adt-config/ /var/lib/adt-config1/ | /usr/bin/cut -d" " -f2 | /bin/sed -e "s;^;\./;g" -e  "/.*\/$/d" | /usr/bin/cpio -pdmvu /var/lib/adt-config1 2>&1 | /bin/grep "^/var" | /bin/sed "s;/var/www/html1/;;g" | /usr/bin/tr " " "\\n"'
+        additions=""
+        additions=`eval ${additions_command}`
+        modifieds=`eval ${modifieds_command}`
+        additions="${additions} ${modifieds}"
+        
 fi
 
 /bin/sleep 5
@@ -22,6 +30,9 @@ then
 fi
 
 ${HOME}/providerscripts/datastore/configwrapper/SyncFromConfigDatastore.sh "" /var/lib/adt-config.$$
+
+additions_command='cd /var/www/html ; /usr/bin/rsync -ri --dry-run --checksum '${exclude_command}' /var/lib/adt-config1/ /var/www/adt-config1.$$/'
+eval `additions_command`
 
 if ( [ -d /var/lib/adt-config ] )
 then
