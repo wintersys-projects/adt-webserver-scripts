17f8c717008b32a7511b335a2a98e6e46cbaeee0
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-10 04:55:57 +0000

    Update PerformSyncConfigDatastore.sh

--- providerscripts/datastore/configwrapper/PerformSyncConfigDatastore.sh
+++ providerscripts/datastore/configwrapper/PerformSyncConfigDatastore.sh
@@ -6,6 +6,21 @@ then
         /bin/cp -r /var/lib/adt-config/* /var/lib/adt-config-1
 fi
 
+deletions="`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh deletions`"
+for file in ${deletions}
+do
+        if ( [ -f /var/lib/adt-config/${file} ] )
+        then
+                /bin/rm /var/lib/adt-config/${file} 
+        fi
+        if ( [ -f /var/lib/adt-config1/${file} ] )
+        then
+                /bin/rm /var/lib/adt-config1/${file} 
+        fi
+fi
+
+${HOME}/providerscripts/datastore/configwrapper/SyncFromConfigDatastore.sh "additions" /var/lib/adt-config
+        
 deletes_command='/usr/bin/rsync --dry-run --ignore-existing -vr /var/lib/adt-config-1/ /var/lib/adt-config/ 2>&1 | /bin/sed -e "/^$/d" -e  "/.*\/$/d" | /usr/bin/tail -n +2 | /usr/bin/head -n -2 | /usr/bin/tr " "
 "\\n" '
 
@@ -17,6 +32,7 @@ do
         then
                 file="`/bin/echo ${file} | /bin/sed 's:/var/lib/adt-config/::'`"
                 ${HOME}/providerscripts/datastore/configwrapper/DeleteFromConfigDatastore.sh ${file}
+                ${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${file} deletions/${place_to_put}
                 /bin/rm /var/lib/adt-config-1/${file}
         else
                 /bin/cp /var/lib/adt-config/${file} /var/lib/adt-config-1/${file}
@@ -29,10 +45,12 @@ for file in ${additions}
 do
         place_to_put="`/bin/echo ${file} | /bin/sed 's:/var/lib/adt-config/::' | sed 's:/[^/]*$::'`"
         ${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${file} ${place_to_put}
+        ${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${file} additions/${place_to_put}
+
         /bin/cp ${file} `/bin/echo ${file} | /bin/sed 's:adt-config:adt-config-1:'`
 done
 
-${HOME}/providerscripts/datastore/configwrapper/SyncToConfigDatastore.sh /var/lib/adt-config ""
-${HOME}/providerscripts/datastore/configwrapper/SyncFromConfigDatastore.sh "" /var/lib/adt-config
+#${HOME}/providerscripts/datastore/configwrapper/SyncToConfigDatastore.sh /var/lib/adt-config ""
+#${HOME}/providerscripts/datastore/configwrapper/SyncFromConfigDatastore.sh "" /var/lib/adt-config
 
 
