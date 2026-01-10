8a25f7057261ca1c95290cffe1a465a7def3b279
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-10 21:37:52 +0000

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/moodle/InitialiseApplicationConfiguration.sh
+++ application/configuration/moodle/InitialiseApplicationConfiguration.sh
@@ -25,14 +25,16 @@ then
 	exit
 fi
 
-${HOME}/providerscripts/datastore/configwrapper/PerformSyncConfigDatastore.sh
+#${HOME}/providerscripts/datastore/configwrapper/PerformSyncConfigDatastore.sh
 
-if ( [ ! -f /var/lib/adt-config/moodle_config.php ] )
-then
-	${HOME}/providerscripts/email/SendEmail.sh "CONFIGURATION FILE ABSENT" "Failed to copy joomla configuration file to the live location during application initiation" "ERROR"	
-fi
+#if ( [ ! -f /var/lib/adt-config/moodle_config.php ] )
+#then#
+#	${HOME}/providerscripts/email/SendEmail.sh "CONFIGURATION FILE ABSENT" "Failed to copy joomla configuration file to the live location during application initiation" "ERROR"	
+#fi
 
-/bin/cp /var/lib/adt-config/moodle_config.php /var/www/html/config.php
+#/bin/cp /var/lib/adt-config/moodle_config.php /var/www/html/config.php
+
+${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh moodle_config.php /var/www/html/config.php
 /bin/chmod 600 /var/www/html/config.php
 /bin/chown www-data:www-data /var/www/html/config.php
 
