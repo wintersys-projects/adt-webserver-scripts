e6fe820e357f1a3d656759d6963d864b8933a79a
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-11 15:46:40 +0000

    Update PerformSyncConfigDatastore.sh

--- providerscripts/datastore/configwrapper/PerformSyncConfigDatastore.sh
+++ providerscripts/datastore/configwrapper/PerformSyncConfigDatastore.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -x
+#set -x
 
 machine_ip="`${HOME}/utilities/processing/GetIP.sh`"
 
@@ -19,8 +19,8 @@ then
         /bin/mkdir -p /var/lib/adt-config/deletions
 fi
 
-/usr/bin/diff -qr /var/lib/adt-config /var/lib/adt-config1 | /bin/grep "^Only in /var/lib/adt-config1" | /bin/sed -e 's;: ;/;' -e 's:/var/lib/adt-config1/::' | /usr/bin/awk '{print $NF}' > /var/lib/adt-config/de
-letions/deletes-${machine_ip}.log
+/usr/bin/diff -qr /var/lib/adt-config /var/lib/adt-config1 | /bin/grep "^Only in /var/lib/adt-config1" | /bin/sed -e 's;: ;/;' -e 's:/var/lib/adt-config1/::' | /usr/bin/awk '{print $NF}' > /var/lib/adt-config/deletions/deletes-${machine_ip}.log
+
 
 ${HOME}/providerscripts/datastore/configwrapper/SyncToConfigDatastore.sh "/var/lib/adt-config" "root"
 
@@ -35,9 +35,17 @@ ${HOME}/providerscripts/datastore/configwrapper/SyncFromConfigDatastore.sh "root
 
 if ( [ "`/usr/bin/find /var/lib/adt-config.$$/deletions  -maxdepth 0 -type d -empty 2>/dev/null`" = "" ] )
 then
-        for file in `/usr/bin/find /var/lib/adt-config.$$/deletions`
+        for file in `/usr/bin/find /var/lib/adt-config.$$/deletions | /bin/grep '.log$' | /bin/grep -v ${machine_ip}`
         do
-                /bin/echo ${file}
+                deletes="`/bin/cat ${file}`"
+                for delete in ${deletes}
+                do
+                        if ( [ -f /var/lib/adt-config.$$/${delete} ] )
+                        then
+                                /bin/rm /var/lib/adt-config.$$/${delete}
+                        fi
+                done
+                /bin/rm ${file}
         done
 fi
 
