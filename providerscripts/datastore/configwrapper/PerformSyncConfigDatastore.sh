37dde298fe42e2e40873924148c4e0095e151cb8
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-11 14:19:57 +0000

    Update PerformSyncConfigDatastore.sh

--- providerscripts/datastore/configwrapper/PerformSyncConfigDatastore.sh
+++ providerscripts/datastore/configwrapper/PerformSyncConfigDatastore.sh
@@ -6,4 +6,10 @@ then
         exit
 fi
 
+if ( [ ! -d /var/lib/adt-config1 ] )
+then
+        ${HOME}/providerscripts/datastore/configwrapper/SyncFromConfigDatastore.sh "root" /var/lib/adt-config
+        /usr/bin/rsync -ru /var/lib/adt-config/ /var/lib/adt-config1
+fi
+
 
