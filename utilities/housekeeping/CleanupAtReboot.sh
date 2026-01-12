2e498cf57eabfbd823841edeb4c7fa2f26a4835b
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-12 10:12:16 +0000

    Update CleanupAtReboot.sh

--- utilities/housekeeping/CleanupAtReboot.sh
+++ utilities/housekeeping/CleanupAtReboot.sh
@@ -20,8 +20,8 @@
 #######################################################################################
 #######################################################################################
 
-${HOME}/providerscripts/datastore/configwrapper/DeleteFromConfigDatastore.sh "webserverpublicips/*" "no" "no"
-${HOME}/providerscripts/datastore/configwrapper/DeleteFromConfigDatastore.sh "webserverips/*" "no" "no"
+${HOME}/providerscripts/datastore/configwrapper/DeleteFromConfigDatastore.sh "webserverpublicips/*" "yes" "no"
+${HOME}/providerscripts/datastore/configwrapper/DeleteFromConfigDatastore.sh "webserverips/*" "yes" "no"
 
 /bin/touch ${HOME}/runtime/REBOOT_EVENT
 
