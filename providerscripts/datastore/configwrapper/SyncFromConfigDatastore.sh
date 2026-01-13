5f88ed36282543c155664205913865449d1895f9
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-13 13:27:16 +0000

    Update SyncFromConfigDatastore.sh

--- providerscripts/datastore/configwrapper/SyncFromConfigDatastore.sh
+++ providerscripts/datastore/configwrapper/SyncFromConfigDatastore.sh
@@ -60,14 +60,14 @@ fi
 if ( [ "${datastore_tool}" = "/usr/bin/s3cmd" ] )
 then
         host_base="`/bin/grep ^host_base /root/.s3cfg-1 | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
-        datastore_cmd="${datastore_tool} --config=/root/.s3cfg-1 --host=https://${host_base} --dry-run sync --exclude-from  ${HOME}/runtime/datastore_workarea/config_datastore_sync_exclude.dat s3://${config_bucket}/"
+        datastore_cmd="${datastore_tool} --config=/root/.s3cfg-1 --host=https://${host_base} --dry-run sync --exclude-from  ${HOME}/runtime/datastore_workarea/config_datastore_sync_exclude.dat --delete-removed sync s3://${config_bucket}/"
      #   place_to_sync="`/bin/echo ${place_to_sync} | /bin/sed 's/\*.*//g'`/"
         /bin/echo "*webrootsync*" > ${HOME}/runtime/datastore_workarea/config_datastore_sync_exclude.dat
       #  slasher="/"
 elif ( [ "${datastore_tool}" = "/usr/bin/s5cmd" ] )
 then
         host_base="`/bin/grep ^host_base /root/.s5cfg-1 | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
-        datastore_cmd="${datastore_tool} --credentials-file /root/.s5cfg-1 --dry-run --endpoint-url https://${host_base} sync --exclude '*webrootsync*' s3://${config_bucket}/"
+        datastore_cmd="${datastore_tool} --credentials-file /root/.s5cfg-1 --dry-run --endpoint-url https://${host_base} --exclude '*webrootsync*' sync s3://${config_bucket}/"
       #  if ( [ "${place_to_sync}" = "" ] )
       #  then
       #          place_to_sync="*"
