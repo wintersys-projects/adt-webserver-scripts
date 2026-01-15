86cd68b98f21723624899ccfae77cf4fdc4ba358
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-15 13:53:52 +0000

    Update ActivateConfigDatastore.sh

--- providerscripts/datastore/config/ActivateConfigDatastore.sh
+++ providerscripts/datastore/config/ActivateConfigDatastore.sh
@@ -145,10 +145,17 @@ file_removed() {
         live_dir="${1}"
         deleted_file="${2}"
 
-        new_deletes_index="`/bin/cat ${HOME}/runtime/datastore_workarea/config/new_deletes_index.dat`"
         new_creates_index="`/bin/cat ${HOME}/runtime/datastore_workarea/config/new_creates_index.dat`"
+        new_deletes_index="`/bin/cat ${HOME}/runtime/datastore_workarea/config/new_deletes_index.dat`"
 
-        /bin/echo "${live_dir}${deleted_file}" >> ${HOME}/runtime/datastore_workarea/config/newdeletes-${new_deletes_index}.log
+        if ( [ "${new_deletes_index}" = "1" ] )
+        then
+                redirection=">"
+        else
+                redirection=">>"
+        fi
+
+        /bin/echo "${live_dir}${deleted_file}" ${redirection} ${HOME}/runtime/datastore_workarea/config/newdeletes-${new_deletes_index}.log
         /bin/sed -i "\:${live_dir}${deleted_file}:d" ${HOME}/runtime/datastore_workarea/config/newcreates-${new_creates_index}.log
 
         check_dir="`/bin/echo ${live_dir} | /bin/sed 's/adt-config/adt-config1/g'`"
@@ -175,11 +182,18 @@ file_modified() {
         place_to_put="`/bin/echo ${live_dir} | /bin/sed 's:/var/lib/adt-config/::' | /bin/sed 's:/$::g'`"
         new_creates_index="`/bin/cat ${HOME}/runtime/datastore_workarea/config/new_creates_index.dat`"
 
+        if ( [ "${new_creates_index}" = "1" ] )
+        then
+                redirection=">"
+        else
+                redirection=">>"
+        fi
+
         if ( [ "`/bin/echo ${modified_file} | /bin/grep '^\.'`" = "" ] )
         then
                 if ( [ ! -d ${live_dir}${modified_file} ] )
                 then
-                        /bin/echo "${live_dir}${modified_file}" >> ${HOME}/runtime/datastore_workarea/config/newcreates-${new_creates_index}.log
+                        /bin/echo "${live_dir}${modified_file}" ${redirection} ${HOME}/runtime/datastore_workarea/config/newcreates-${new_creates_index}.log
                         check_dir="`/bin/echo ${live_dir} | /bin/sed 's/adt-config/adt-config1/g'`"
 
                         if ( [ ! -f ${check_dir}/${modified_file} ] ||  [ "`/usr/bin/diff ${live_dir}/${modified_file} ${check_dir}/${modified_file}`" != "" ] )
@@ -204,12 +218,18 @@ file_created() {
         place_to_put="`/bin/echo ${live_dir} | /bin/sed 's:/var/lib/adt-config/::' | /bin/sed 's:/$::g'`"
         new_creates_index="`/bin/cat ${HOME}/runtime/datastore_workarea/config/new_creates_index.dat`"
 
+        if ( [ "${new_creates_index}" = "1" ] )
+        then
+                redirection=">"
+        else
+                redirection=">>"
+        fi
 
         if ( [ "`/bin/echo ${created_file} | /bin/grep '^\.'`" = "" ] )
         then
                 if ( [ ! -d ${live_dir}${created_file} ] )
                 then
-                        /bin/echo "${live_dir}${created_file}" >> ${HOME}/runtime/datastore_workarea/config/newcreates-${new_creates_log}.log
+                        /bin/echo "${live_dir}${created_file}" ${redirection} ${HOME}/runtime/datastore_workarea/config/newcreates-${new_creates_log}.log
                         check_dir="`/bin/echo ${live_dir} | /bin/sed 's/adt-config/adt-config1/g'`"
 
                         if ( [ ! -f ${check_dir}/${created_file} ] ||  [ "`/usr/bin/diff ${live_dir}/${created_file} ${check_dir}/${created_file}`" != "" ] )
