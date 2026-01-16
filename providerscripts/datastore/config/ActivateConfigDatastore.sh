1150774e4f8292a3d6cda3d408e956aa141b0b9e
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-16 18:09:51 +0000

    Update ActivateConfigDatastore.sh

--- providerscripts/datastore/config/ActivateConfigDatastore.sh
+++ providerscripts/datastore/config/ActivateConfigDatastore.sh
@@ -18,11 +18,13 @@ monitor_for_datastore_changes() {
         while ( [ 1 ] )
         do
                 /bin/sleep 5
-                while ( [ -f ${HOME}/runtime/DATASTORE_EVENT_ACTIVE ] )
+                while ( [ -f ${HOME}/runtime/DATASTORE_FILE_EVENT_ACTIVE ] )
                 do
                         /bin/sleep 1
                 done
+                /bin/touch ${HOME}/runtime/DATASTORE_SYNC_EVENT_ACTIVE
                 ${HOME}/providerscripts/datastore/config/tooling/SyncFromConfigDatastoreWithDelete.sh "root" "/var/lib/adt-config"
+                /bin/rm ${HOME}/runtime/DATASTORE_SYNC_EVENT_ACTIVE
         done
 }
 
@@ -96,7 +98,22 @@ file_created() {
 
 /usr/bin/inotifywait -q -m -r -e modify,delete,create /var/lib/adt-config | while read DIRECTORY EVENT FILE 
 do
-        /bin/touch ${HOME}/runtime/DATASTORE_EVENT_ACTIVE
+        if ( [ -f ${HOME}/runtime/DATASTORE_SYNC_EVENT_ACTIVE ] )
+        then
+                if ( [ -f ${DIRECTORY}${FILE} ] )
+                then
+                        /bin/chmod 444 ${DIRECTORY}${FILE}
+                fi
+                while ( [ -f ${HOME}/runtime/DATASTORE_SYNC_EVENT_ACTIVE ] )
+                do
+                        /bin/sleep 1
+                done
+                if ( [ -f ${DIRECTORY}${FILE} ] )
+                then
+                        /bin/chmod 644 ${DIRECTORY}${FILE}
+                fi
+        fi
+        /bin/touch ${HOME}/runtime/DATASTORE_FILE_EVENT_ACTIVE
         case ${EVENT} in
                 MODIFY*)
                         file_modified "${DIRECTORY}" "${FILE}"
@@ -123,6 +140,6 @@ do
                         file_removed "${DIRECTORY}" "${FILE}"
                         ;;
         esac
-        /bin/rm ${HOME}/runtime/DATASTORE_EVENT_ACTIVE
+        /bin/rm ${HOME}/runtime/DATASTORE_FILE_EVENT_ACTIVE
 
 done
