fde9f28151ce65dc1a152cb5b660d2c9552fa6b5
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-19 15:57:48 +0000

    Update ProcessIncomingHistoricalWebrootUpdates.sh

--- providerscripts/datastore/webroot-sync/ProcessIncomingHistoricalWebrootUpdates.sh
+++ providerscripts/datastore/webroot-sync/ProcessIncomingHistoricalWebrootUpdates.sh
@@ -32,55 +32,55 @@ deletions_present="0"
 
 if ( [ "${MULTI_REGION}" != "1" ] )
 then
-        if ( [ "`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh webrootsync/historical/additions/additions*.tar.gz`" != "" ] )
+        if ( [ "`${HOME}/providerscripts/datastore/config/toolkit/ListFromConfigDatastore.sh webrootsync/historical/additions/additions*.tar.gz`" != "" ] )
         then
                 additions_present="1"
         fi
 
-        if ( [ "`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh webrootsync/historical/deletions/deletions*.log`" != "" ] )
+        if ( [ "`${HOME}/providerscripts/datastore/config/toolkit/ListFromConfigDatastore.sh webrootsync/historical/deletions/deletions*.log`" != "" ] )
         then
                 deletions_present="1"
         fi
         if ( [ "${additions_present}" = "1" ] )
         then
-                additions="`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh webrootsync/historical/additions/additions*.tar.gz`"
+                additions="`${HOME}/providerscripts/datastore/config/toolkit/ListFromConfigDatastore.sh webrootsync/historical/additions/additions*.tar.gz`"
                 for addition in ${additions}
                 do
-                        ${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh webrootsync/historical/additions/${addition} ${HOME}/runtime/webroot_sync/historical/incoming/additions
+                        ${HOME}/providerscripts/datastore/config/toolkit/GetFromConfigDatastore.sh webrootsync/historical/additions/${addition} ${HOME}/runtime/webroot_sync/historical/incoming/additions
                 done
         fi
         if ( [ "${deletions_present}" = "1" ] )
         then
-                deletions="`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh webrootsync/historical/deletions/deletions*.log`"
+                deletions="`${HOME}/providerscripts/datastore/config/toolkit/ListFromConfigDatastore.sh webrootsync/historical/deletions/deletions*.log`"
                 for deletion in ${deletions}
                 do
-                        ${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh webrootsync/historical/deletions/${deletion} ${HOME}/runtime/webroot_sync/historical/incoming/deletions
+                        ${HOME}/providerscripts/datastore/config/toolkit/GetFromConfigDatastore.sh webrootsync/historical/deletions/${deletion} ${HOME}/runtime/webroot_sync/historical/incoming/deletions
                 done
         fi
 else
         multi_region_bucket="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`-multi-region"
-        if ( [ "`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh ${multi_region_bucket}/webrootsync/historical/additions/additions*.tar.gz`" != "" ] )
+        if ( [ "`${HOME}/providerscripts/datastore/config/toolkit/ListFromConfigDatastore.sh ${multi_region_bucket}/webrootsync/historical/additions/additions*.tar.gz`" != "" ] )
         then
                 additions_present="1"
         fi
-        if ( [ "`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh ${multi_region_bucket}/webrootsync/historical/deletions/deletions*.log`" != "" ] )
+        if ( [ "`${HOME}/providerscripts/datastore/config/toolkit/ListFromConfigDatastore.sh ${multi_region_bucket}/webrootsync/historical/deletions/deletions*.log`" != "" ] )
         then
                 deletions_present="1"
         fi
         if ( [ "${additions_present}" = "1" ] )
         then
-                additions="`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh ${multi_region_bucket}/webrootsync/historical/additions/additions*.tar.gz`"
+                additions="`${HOME}/providerscripts/datastore/config/toolkit/ListFromConfigDatastore.sh ${multi_region_bucket}/webrootsync/historical/additions/additions*.tar.gz`"
                 for addition in ${additions}
                 do
-                        ${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh ${multi_region_bucket}/webrootsync/historical/additions/${addition} ${HOME}/runtime/webroot_sync/historical/incoming/additions
+                        ${HOME}/providerscripts/datastore/config/toolkit/GetFromConfigDatastore.sh ${multi_region_bucket}/webrootsync/historical/additions/${addition} ${HOME}/runtime/webroot_sync/historical/incoming/additions
                 done
         fi
         if ( [ "${deletions_present}" = "1" ] )
         then
-                deletions="`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh ${multi_region_bucket}/webrootsync/historical/deletions/deletions*.log`"
+                deletions="`${HOME}/providerscripts/datastore/config/toolkit/ListFromConfigDatastore.sh ${multi_region_bucket}/webrootsync/historical/deletions/deletions*.log`"
                 for deletion in ${deletions}
                 do
-                        ${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh ${multi_region_bucket}/webrootsync/historical/deletions/${deletion} ${HOME}/runtime/webroot_sync/historical/incoming/deletions
+                        ${HOME}/providerscripts/datastore/config/toolkit/GetFromConfigDatastore.sh ${multi_region_bucket}/webrootsync/historical/deletions/${deletion} ${HOME}/runtime/webroot_sync/historical/incoming/deletions
                 done
         fi
 fi
