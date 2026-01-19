42c9aceb4e26764ff890bad9b398516f624ca316
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-19 18:22:05 +0000

    Update HousekeepDeletionsSyncing.sh

--- providerscripts/datastore/webroot/HousekeepDeletionsSyncing.sh
+++ providerscripts/datastore/webroot/HousekeepDeletionsSyncing.sh
@@ -36,7 +36,7 @@ do
                         ${HOME}/providerscripts/datastore/config/toolkit/DeleteFromConfigDatastore.sh webrootsync/deletions/${deletion} "no" "no"
                 else
                         multi_region_bucket="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`-multi-region"
-                        ${HOME}/providerscripts/datastore/DeleteFromDatastore.sh ${multi_region_bucket}/webrootsync/deletions/${deletion}
+                        ${HOME}/providerscripts/datastore/dedicated/DeleteFromDatastore.sh ${multi_region_bucket}/webrootsync/deletions/${deletion}
                 fi
         fi
 done
