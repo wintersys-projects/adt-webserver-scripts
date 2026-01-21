4664bd2fd47e42a04bdcad45292b2b14803f7a49
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-21 10:51:15 +0000

    Update SyncFromConfigDatastoreWithDelete.sh

--- providerscripts/datastore/config/wrapper/SyncFromConfigDatastoreWithDelete.sh
+++ providerscripts/datastore/config/wrapper/SyncFromConfigDatastoreWithDelete.sh
@@ -1,1 +1,12 @@
 ${HOME}/providerscripts/datastore/configwrapper/SyncFromConfigDatastore.sh webrootsync/additions ${HOME}/runtime/webroot_sync/incoming
+
+if ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "toolkit" ] )
+then
+
+elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "lightweight" ] )
+then
+
+elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "heavyweight" ] )
+then
+
+fi
