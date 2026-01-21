ab333cd1206ee7434d90e37a2916e549b4696991
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-21 10:52:46 +0000

    Update AgeOfConfigFile.sh

--- providerscripts/datastore/config/wrapper/AgeOfConfigFile.sh
+++ providerscripts/datastore/config/wrapper/AgeOfConfigFile.sh
@@ -1,9 +1,10 @@
- "`${HOME}/providerscripts/datastore/config/toolkit/AgeOfConfigFile.sh wordpress_config.php`"
+
+file_to_interrogate="${1}"
 
 
 if ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "toolkit" ] )
 then
-
+    ${HOME}/providerscripts/datastore/config/toolkit/AgeOfConfigFile.sh "${file_to_interrogate}"
 elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "lightweight" ] )
 then
 
