d9f12d9ecc41e703a4379fe2ec23ed7ecf38fb78
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-21 10:50:48 +0000

    Update ListFromConfigDatastore.sh

--- providerscripts/datastore/config/wrapper/ListFromConfigDatastore.sh
+++ providerscripts/datastore/config/wrapper/ListFromConfigDatastore.sh
@@ -1,1 +1,12 @@
 ${HOME}/providerscripts/datastore/config/toolkit/ListFromConfigDatastore.sh reverseproxyips/*`"
+
+if ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "toolkit" ] )
+then
+
+elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "lightweight" ] )
+then
+
+elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "heavyweight" ] )
+then
+
+fi
