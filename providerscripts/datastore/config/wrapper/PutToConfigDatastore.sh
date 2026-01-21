4304262291b72e4c945115b8a816f03f732e32fa
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-21 10:51:03 +0000

    Update PutToConfigDatastore.sh

--- providerscripts/datastore/config/wrapper/PutToConfigDatastore.sh
+++ providerscripts/datastore/config/wrapper/PutToConfigDatastore.sh
@@ -1,1 +1,12 @@
  ${HOME}/providerscripts/datastore/config/toolkit/PutToConfigDatastore.sh ${public_ip} authenticatorpublicip "no"
+
+ if ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "toolkit" ] )
+then
+
+elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "lightweight" ] )
+then
+
+elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "heavyweight" ] )
+then
+
+fi
