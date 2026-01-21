cc16c9b230ffa8f997f217fe2c37b25c5c4e37f6
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-21 13:25:46 +0000

    Update PutToConfigDatastore.sh

--- providerscripts/datastore/config/wrapper/PutToConfigDatastore.sh
+++ providerscripts/datastore/config/wrapper/PutToConfigDatastore.sh
@@ -1,14 +1,20 @@
- file_to_put="${1}"
- place_to_put="${2}"
- delete="${3}"
+file_to_put="${1}"
+place_to_put="${2}"
+delete="${3}"
 
 if ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "toolkit" ] )
 then
-	${HOME}/providerscripts/datastore/config/toolkit/PutToConfigDatastore.sh "${file_to_put}" "${place_to_put}" "${delete}"
+        ${HOME}/providerscripts/datastore/config/toolkit/PutToConfigDatastore.sh "${file_to_put}" "${place_to_put}" "${delete}"
 elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "lightweight" ] ||  [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "heavyweight" ] )
 then
-	if ( [ -f ${file_to_put} ] )
-	then
-		/bin/cp ${file_to_put} /var/lib/adt-config/${place_to_put}
-	fi
+        if ( [ -f ${file_to_put} ] )
+        then
+                if ( [ ! -d /var/lib/adt-config/${place_to_put} ] )
+                then
+                        /bin/mkdir -p /var/lib/adt-config/${place_to_put}
+                fi
+
+                /bin/cp ${file_to_put} /var/lib/adt-config/${place_to_put}
+
+        fi
 fi
