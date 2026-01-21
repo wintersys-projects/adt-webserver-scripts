4348405d8317630e519646cf08279dda0b8deb60
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-21 10:56:22 +0000

    Update GetFromConfigDatastore.sh

--- providerscripts/datastore/config/wrapper/GetFromConfigDatastore.sh
+++ providerscripts/datastore/config/wrapper/GetFromConfigDatastore.sh
@@ -1,8 +1,9 @@
-${HOME}/providerscripts/datastore/config/toolkit/GetFromConfigDatastore.sh joomla_configuration.php ${HOME}/runtime
+file_to_get="${1}"
+place_to_put="${2}"
 
 if ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "toolkit" ] )
 then
-
+  ${HOME}/providerscripts/datastore/config/toolkit/GetFromConfigDatastore.sh "${file_to_get}" "${place_to_put}"
 elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "lightweight" ] )
 then
 
