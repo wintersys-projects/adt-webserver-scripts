bc89d8b79ba99bbccbd9a4cd6f3b435aaec68d07
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-21 12:56:36 +0000

    Update ListFromConfigDatastore.sh

--- providerscripts/datastore/config/wrapper/ListFromConfigDatastore.sh
+++ providerscripts/datastore/config/wrapper/ListFromConfigDatastore.sh
@@ -1,15 +1,14 @@
+#set -x
+
 file_to_list="${1}"
 
 if ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "toolkit" ] )
 then
-	if ( [ "`${HOME}/providerscripts/datastore/config/toolkit/ListFromConfigDatastore.sh "${file_to_list}"`" != "" ] )
-	then
-		/bin/echo "found"
-	fi
+        ${HOME}/providerscripts/datastore/config/toolkit/ListFromConfigDatastore.sh "${file_to_list}"
 elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "lightweight" ] || [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "heavyweight" ] )
 then
-	if ( [ -f ${file_to_list} ] )
-	then
-		/bin/echo "found"
-	fi
+        if ( [ -f /var/lib/adt-config/${file_to_list} ] )
+        then
+                /bin/ls /var/lib/adt-config/${file_to_list} | /usr/bin/awk -F'/' '{print $NF}'
+        fi
 fi
