f1567c2dd1bdbb9ccbfeaf4a08c4dbd76be3b2f4
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-21 10:58:58 +0000

    Update PutToConfigDatastore.sh

--- providerscripts/datastore/config/wrapper/PutToConfigDatastore.sh
+++ providerscripts/datastore/config/wrapper/PutToConfigDatastore.sh
@@ -1,8 +1,10 @@
- ${HOME}/providerscripts/datastore/config/toolkit/PutToConfigDatastore.sh ${public_ip} authenticatorpublicip "no"
+ file_to_put="${1}"
+ place_to_put="${2}"
+ delete="${3}"
 
- if ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "toolkit" ] )
+if ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "toolkit" ] )
 then
-
+	${HOME}/providerscripts/datastore/config/toolkit/PutToConfigDatastore.sh "${file_to_put}" "${place_to_put}" "${delete}"
 elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "lightweight" ] )
 then
 
