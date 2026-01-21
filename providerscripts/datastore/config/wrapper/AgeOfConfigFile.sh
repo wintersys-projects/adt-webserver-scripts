99d16ab4b325e1f87f52697c8c05989d133c4e71
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-21 11:37:43 +0000

    Update AgeOfConfigFile.sh

--- providerscripts/datastore/config/wrapper/AgeOfConfigFile.sh
+++ providerscripts/datastore/config/wrapper/AgeOfConfigFile.sh
@@ -7,8 +7,8 @@ then
     ${HOME}/providerscripts/datastore/config/toolkit/AgeOfConfigFile.sh "${file_to_interrogate}"
 elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "lightweight" ] )
 then
-
+:
 elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "heavyweight" ] )
 then
-
+:
 fi
