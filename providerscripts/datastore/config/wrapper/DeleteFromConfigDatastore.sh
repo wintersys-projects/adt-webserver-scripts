ae6bcd0da2901eebb826a9f5d79dd337387b0b54
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-21 11:54:55 +0000

    Update DeleteFromConfigDatastore.sh

--- providerscripts/datastore/config/wrapper/DeleteFromConfigDatastore.sh
+++ providerscripts/datastore/config/wrapper/DeleteFromConfigDatastore.sh
@@ -6,10 +6,19 @@ recursive="${3}"
 if ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "toolkit" ] )
 then
 	${HOME}/providerscripts/datastore/config/toolkit/DeleteFromConfigDatastore.sh "${file_to_delete}" "${local}" "${recursive}"
-elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "lightweight" ] )
+elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "lightweight" ] ||  [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "heavyweight" ] )
 then
-:
-elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "heavyweight" ] )
-then
-:
+	if ( [ -f /var/lib/adt-config/${file_to_delete} ] )
+	then
+		/bin/rm /var/lib/adt-config/${file_to_delete}
+	fi
+	if ( [ -d /var/lib/adt-config/${file_to_delete} ] )
+	then
+		if ( [ "${recursive}" = "yes" ] )
+		then
+			/bin/rm -r /var/lib/adt-config/${file_to_delete}
+		else
+			/bin/rmdir /var/lib/adt-config/${file_to_delete}
+		fi
+	fi
 fi
