f72d9eb8951674238a55b75fb5aea5668aaa205b
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-21 10:50:22 +0000

    Update DeleteFromConfigDatastore.sh

--- providerscripts/datastore/config/wrapper/DeleteFromConfigDatastore.sh
+++ providerscripts/datastore/config/wrapper/DeleteFromConfigDatastore.sh
@@ -1,1 +1,12 @@
 ${HOME}/providerscripts/datastore/config/tooling/DeleteFromConfigDatastore.sh "${file_to_delete}" "no" "no"
+
+if ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "toolkit" ] )
+then
+
+elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "lightweight" ] )
+then
+
+elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "heavyweight" ] )
+then
+
+fi
