3cafdd58f756002519dd053fdafeb47c00bca875
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-21 11:56:30 +0000

    Update GetFromConfigDatastore.sh

--- providerscripts/datastore/config/wrapper/GetFromConfigDatastore.sh
+++ providerscripts/datastore/config/wrapper/GetFromConfigDatastore.sh
@@ -4,12 +4,10 @@ place_to_put="${2}"
 if ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "toolkit" ] )
 then
   ${HOME}/providerscripts/datastore/config/toolkit/GetFromConfigDatastore.sh "${file_to_get}" "${place_to_put}"
-elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "lightweight" ] )
+elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "lightweight" ] || [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "heavyweight" ] )
 then
-:
-
-elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "heavyweight" ] )
-then
-:
-
+  if ( [ -f /var/lib/adt-config/${file_to_get} ] )
+  then
+    /bin/cp /var/lib/adt-config/${file_to_get} /var/lib/adt-config/${place_to_put}
+  fi
 fi
