ea4fc91f6d960c9dad54a4aaea3ff1611e7a19ea
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-21 11:58:08 +0000

    Update ListFromConfigDatastore.sh

--- providerscripts/datastore/config/wrapper/ListFromConfigDatastore.sh
+++ providerscripts/datastore/config/wrapper/ListFromConfigDatastore.sh
@@ -2,11 +2,14 @@ file_to_list="${1}"
 
 if ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "toolkit" ] )
 then
-	${HOME}/providerscripts/datastore/config/toolkit/ListFromConfigDatastore.sh "${file_to_list}"
-elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "lightweight" ] )
+	if ( [ "`${HOME}/providerscripts/datastore/config/toolkit/ListFromConfigDatastore.sh "${file_to_list}"`" != "" ] )
+	then
+		/bin/echo "found"
+	fi
+elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "lightweight" ] || [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "heavyweight" ] )
 then
-:
-elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "heavyweight" ] )
-then
-:
+	if ( [ -f ${file_to_list} ] )
+	then
+		/bin/echo "found"
+	fi
 fi
