f078fb8743766b371ab066e593240a06dc1f9ec0
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-22 11:53:47 +0000

    Update ActivateConfigDatastoreLightweight.sh

--- providerscripts/datastore/config/ActivateConfigDatastoreLightweight.sh
+++ providerscripts/datastore/config/ActivateConfigDatastoreLightweight.sh
@@ -7,22 +7,37 @@ exec 2>/tmp/err
 if ( [ ! -d /var/lib/adt-config ] )
 then
         /bin/mkdir /var/lib/adt-config
-		${HOME}/providerscripts/datastore/config/toolkit/SyncFromConfigDatastoreWithDelete.sh "root" "/var/lib/adt-config"
+        ${HOME}/providerscripts/datastore/config/toolkit/SyncFromConfigDatastoreWithDelete.sh "root" "/var/lib/adt-config"
 fi
 
-if ( [ ! -d /var/lib/adt-config-processing ] )
-then
-	/bin/mkdir /var/lib/adt-config-processing
-fi
 
 monitor_for_datastore_changes() {
         while ( [ 1 ] )
         do
                 /bin/sleep 30
                 ${HOME}/providerscripts/datastore/config/toolkit/SyncFromConfigDatastoreWithDelete.sh "root" "/var/lib/adt-config"
-				if ( [ -d /var/lib/adt-config ] )
+
+                for file_to_delete_marker in `/usr/bin/find /var/lib/adt-config | grep 'delete_me$'`
+                do
+                        if ( [ -f ${file_to_delete_marker} ] )
+                        then
+                                /bin/rm ${file_to_delete_marker}
+                        fi
+
+                        if ( [ -f `/bin/echo ${file_to_delete_marker} | /bin/sed 's:\.delete_me::g'` ] )
+                        then
+                                /bin/rm `/bin/echo ${file_to_delete_marker} | /bin/sed 's:\.delete_me::g'`
+                        fi
+
+                        file_to_delete_marker="`/bin/echo ${file_to_delete_marker} | /bin/sed -e 's:/var/lib/adt-config/::g'`"
+                        file_to_delete_real="`/bin/echo ${file_to_delete_marker} | /bin/sed -e 's:/var/lib/adt-config/::g' -e 's/\.delete_me//g'`"
+                        ${HOME}/providerscripts/datastore/config/toolkit/DeleteFromConfigDatastore.sh "${file_to_delete_marker}" 
+                        ${HOME}/providerscripts/datastore/config/toolkit/DeleteFromConfigDatastore.sh "${file_to_delete_real}" 
+                done
+
+                if ( [ -d /var/lib/adt-config ] )
                 then
-                	/usr/bin/find /var/lib/adt-config -type d -empty -delete
+                        /usr/bin/find /var/lib/adt-config -type d -empty -delete
                 fi
         done
 }
@@ -31,59 +46,121 @@ monitor_for_datastore_changes &
 
 /usr/bin/inotifywait -q -m -r -e modify,delete,create /var/lib/adt-config | while read DIRECTORY EVENT FILE 
 do
-	if ( [ -f ${DIRECTORY}${FILE} ] && ( [ "`/bin/echo ${FILE} | /bin/grep "^\."`" = "" ] && [ "`/bin/echo ${FILE} | /bin/grep '\~$'`" = "" ] && [ "`/bin/echo ${FILE} | /bin/grep  -E '\.[a-z0-9]{8,}\.partial'`" = "" ] ) || [ "${EVENT}" = "DELETE" ]  )
-	then
-		case ${EVENT} in
-			MODIFY*)
-				file_for_processing="`/bin/echo ${DIRECTORY}${FILE} | /bin/sed 's:/var/lib/adt-config/:/var/lib/adt-config-processing/:'`"
+        if ( [ -f ${DIRECTORY}${FILE} ] && ( [ "`/bin/echo ${FILE} | /bin/grep "^\."`" = "" ] && [ "`/bin/echo ${FILE} | /bin/grep '\~$'`" = "" ] && [ "`/bin/echo ${FILE} | /bin/grep  -E '\.[a-z0-9]{8,}\.partial'`" = "" ] ) || [ "${EVENT}" = "DELETE" ]  )
+        then
+                case ${EVENT} in
+                        MODIFY*)
+                                file_for_processing="`/bin/echo ${DIRECTORY}${FILE}`"
+                                if ( [ "`/bin/echo ${file_for_processing} | /bin/sed 's:/: :g' | /usr/bin/wc -w`" -gt "4" ] )
+                                then
+                                        place_to_put="`/bin/echo ${file_for_processing} | /bin/sed 's:/[^/]*$::' | /bin/sed 's:/var/lib/adt-config/::g'`"
+                                else
+                                        place_to_put="root"
+                                fi
+                                ${HOME}/providerscripts/datastore/config/toolkit/PutToConfigDatastore.sh ${file_for_processing} ${place_to_put} "yes" 
+                                ;;
+                        CREATE*)
+                                file_for_processing="`/bin/echo ${DIRECTORY}${FILE}`"
+                                if ( [ "`/bin/echo ${file_for_processing} | /bin/sed 's:/: :g' | /usr/bin/wc -w`" -gt "4" ] )
+                                then
+                                        place_to_put="`/bin/echo ${file_for_processing} | /bin/sed 's:/[^/]*$::' | /bin/sed 's:/var/lib/adt-config/::g'`"
+                                else
+                                        place_to_put="root"
+                                fi
+                                ${HOME}/providerscripts/datastore/config/toolkit/PutToConfigDatastore.sh ${file_for_processing} ${place_to_put} "yes" 
+                                ;;
+                        DELETE*)
+                                file_for_processing="${DIRECTORY}${FILE}"
+                                if ( [ "`/bin/echo ${file_for_processing} | /bin/sed 's:/: :g' | /usr/bin/wc -w`" -gt "4" ] )
+                                then
+                                        place_to_put="`/bin/echo ${file_for_processing} | /bin/sed 's:/[^/]*$::' | /bin/sed 's:/var/lib/adt-config/::g'`"
+                                else
+                                        place_to_put="root"
+                                fi
+                                /bin/touch ${file_for_processing}.delete_me
+                                ${HOME}/providerscripts/datastore/config/toolkit/PutToConfigDatastore.sh ${file_for_processing}.delete_me ${place_to_put} "yes" 
+                                ;;
+                esac
+        fi
+done
+#!/bin/sh
+set -x
+
+exec 1>/tmp/out
+exec 2>/tmp/err
+
+if ( [ ! -d /var/lib/adt-config ] )
+then
+        /bin/mkdir /var/lib/adt-config
+                ${HOME}/providerscripts/datastore/config/toolkit/SyncFromConfigDatastoreWithDelete.sh "root" "/var/lib/adt-config"
+fi
+
+if ( [ ! -d /var/lib/adt-config-processing ] )
+then
+        /bin/mkdir /var/lib/adt-config-processing
+fi
+
+monitor_for_datastore_changes() {
+        while ( [ 1 ] )
+        do
+                /bin/sleep 30
+                ${HOME}/providerscripts/datastore/config/toolkit/SyncFromConfigDatastoreWithDelete.sh "root" "/var/lib/adt-config"
+                                if ( [ -d /var/lib/adt-config ] )
+                then
+                        /usr/bin/find /var/lib/adt-config -type d -empty -delete
+                fi
+        done
+}
+
+monitor_for_datastore_changes &
+
+/usr/bin/inotifywait -q -m -r -e modify,delete,create /var/lib/adt-config | while read DIRECTORY EVENT FILE 
+do
+        if ( [ -f ${DIRECTORY}${FILE} ] && ( [ "`/bin/echo ${FILE} | /bin/grep "^\."`" = "" ] && [ "`/bin/echo ${FILE} | /bin/grep '\~$'`" = "" ] && [ "`/bin/echo ${FILE} | /bin/grep  -E '\.[a-z0-9]{8,}\.partial'`" = "" ] ) || [ "${EVENT}" = "DELETE" ]  )
+        then
+                case ${EVENT} in
+                        MODIFY*)
+                                file_for_processing="`/bin/echo ${DIRECTORY}${FILE} | /bin/sed 's:/var/lib/adt-config/:/var/lib/adt-config-processing/:'`"
                 directory_to_put="`/bin/echo ${file_for_processing} | /bin/sed 's:/[^/]*$::'`"
                 
-				if ( [ ! -d ${directory_to_put} ] )
+                                if ( [ ! -d ${directory_to_put} ] )
                 then
-                	/bin/mkdir -p ${directory_to_put}
+                        /bin/mkdir -p ${directory_to_put}
                 fi
                 
-				/bin/cp ${DIRECTORY}${FILE} ${directory_to_put}
-				
-				if ( [ "`/bin/echo ${DIRECTORY}${FILE} | /bin/sed 's:/: :g' | /usr/bin/wc -w`" -gt "4" ] )
-				then
-					place_to_put="`/bin/echo ${DIRECTORY}${FILE} | /bin/sed 's:/[^/]*$::' | /bin/sed 's:/var/lib/adt-config/::g'`"
-				else
-					place_to_put="root"
-				fi
-				${HOME}/providerscripts/datastore/config/toolkit/PutToConfigDatastore.sh ${file_for_processing} ${place_to_put} "yes" &
-				;;
-			CREATE*)
-				file_for_processing="`/bin/echo ${DIRECTORY}${FILE} | /bin/sed 's:/var/lib/adt-config/:/var/lib/adt-config-processing/:'`"
+                                /bin/cp ${DIRECTORY}${FILE} ${directory_to_put}
+
+                                if ( [ "`/bin/echo ${DIRECTORY}${FILE} | /bin/sed 's:/: :g' | /usr/bin/wc -w`" -gt "4" ] )
+                                then
+                                        place_to_put="`/bin/echo ${DIRECTORY}${FILE} | /bin/sed 's:/[^/]*$::' | /bin/sed 's:/var/lib/adt-config/::g'`"
+                                else
+                                        place_to_put="root"
+                                fi
+                                ${HOME}/providerscripts/datastore/config/toolkit/PutToConfigDatastore.sh ${file_for_processing} ${place_to_put} "yes" &
+                                ;;
+                        CREATE*)
+                                file_for_processing="`/bin/echo ${DIRECTORY}${FILE} | /bin/sed 's:/var/lib/adt-config/:/var/lib/adt-config-processing/:'`"
                 directory_to_put="`/bin/echo ${file_for_processing} | /bin/sed 's:/[^/]*$::'`"
                 
-				if ( [ ! -d ${directory_to_put} ] )
+                                if ( [ ! -d ${directory_to_put} ] )
                 then
-                	/bin/mkdir -p ${directory_to_put}
+                        /bin/mkdir -p ${directory_to_put}
                 fi
                 
-				/bin/cp ${DIRECTORY}${FILE} ${directory_to_put}
-				
-				if ( [ "`/bin/echo ${DIRECTORY}${FILE} | /bin/sed 's:/: :g' | /usr/bin/wc -w`" -gt "4" ] )
-				then
-					place_to_put="`/bin/echo ${DIRECTORY}${FILE} | /bin/sed 's:/[^/]*$::' | /bin/sed 's:/var/lib/adt-config/::g'`"
-				else
-					place_to_put="root"
-				fi
-				${HOME}/providerscripts/datastore/config/toolkit/PutToConfigDatastore.sh ${file_for_processing} ${place_to_put} "yes" &
+                                /bin/cp ${DIRECTORY}${FILE} ${directory_to_put}
+
+                                if ( [ "`/bin/echo ${DIRECTORY}${FILE} | /bin/sed 's:/: :g' | /usr/bin/wc -w`" -gt "4" ] )
+                                then
+                                        place_to_put="`/bin/echo ${DIRECTORY}${FILE} | /bin/sed 's:/[^/]*$::' | /bin/sed 's:/var/lib/adt-config/::g'`"
+                                else
+                                        place_to_put="root"
+                                fi
+                                ${HOME}/providerscripts/datastore/config/toolkit/PutToConfigDatastore.sh ${file_for_processing} ${place_to_put} "yes" &
                 ;;
-			DELETE*)
-				file_for_processing="`/bin/echo ${DIRECTORY}${FILE} | /bin/sed 's:/var/lib/adt-config/:/var/lib/adt-config-processing/:'`"
-				
-				if ( [ "`/bin/echo ${DIRECTORY}${FILE} | /bin/sed 's:/: :g' | /usr/bin/wc -w`" -gt "4" ] )
-				then
-					place_to_put="`/bin/echo ${DIRECTORY}${FILE} | /bin/sed 's:/[^/]*$::' | /bin/sed 's:/var/lib/adt-config/::g'`"
-				else
-					place_to_put="root"
-				fi
-				/bin/touch ${file_for_processing}.delete_me
-				${HOME}/providerscripts/datastore/config/toolkit/PutToConfigDatastore.sh ${file_for_processing}.delete_me ${place_to_put} "yes" &
-                ;;
-		esac
-	fi
+                        DELETE*)
+                file_to_delete="`/bin/echo ${DIRECTORY}${FILE} | /bin/sed -e 's:/var/lib/adt-config/::' -e 's://:/:'`"
+                ${HOME}/providerscripts/datastore/config/toolkit/DeleteFromConfigDatastore.sh "${file_to_delete}" "no" "no"
+                                ;;
+                esac
+        fi
 done
