eaa82f620576530a416a7b5c0b02617c4ea009cb
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-24 13:09:04 +0000

    Update ProcessOutgoingWebrootUpdates.sh

--- providerscripts/datastore/filesystems-sync/ProcessOutgoingWebrootUpdates.sh
+++ providerscripts/datastore/filesystems-sync/ProcessOutgoingWebrootUpdates.sh
@@ -102,7 +102,7 @@ fi
 
 if ( [ -f ${HOME}/runtime/filesystem_sync/outgoing/deletions/deletions.${machine_ip}.$$.log ] )
 then
-        ${HOME}/providerscripts/datastore/dedicated/PutToDatastore.sh  ${HOME}/runtime/filesystem_sync/outgoing/deletions/deletions.${machine_ip}.$$.log ${sync_bucket}/filesystem-sync/deletions "no" "distributed"
+        ${HOME}/providerscripts/datastore/dedicated/PutToDatastore.sh  "sync" "${HOME}/runtime/filesystem_sync/outgoing/deletions/deletions.${machine_ip}.$$.log" "filesystem-sync/deletions" "distributed" "no"
         /bin/mv ${HOME}/runtime/filesystem_sync/outgoing/deletions/deletions.${machine_ip}.$$.log ${HOME}/runtime/filesystem_sync/outgoing/deletions/deletions.${machine_ip}.$$.${rnd}.log 
-        ${HOME}/providerscripts/datastore/dedicated/PutToDatastore.sh  ${HOME}/runtime/filesystem_sync/outgoing/deletions/deletions.${machine_ip}.$$.${rnd}.log ${sync_bucket}/filesystem-sync/historical/deletions "no" "distributed"
+        ${HOME}/providerscripts/datastore/dedicated/PutToDatastore.sh  "sync" "${HOME}/runtime/filesystem_sync/outgoing/deletions/deletions.${machine_ip}.$$.${rnd}.log" "filesystem-sync/historical/deletions" "distributed" "no"
 fi
