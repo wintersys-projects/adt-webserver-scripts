a4ae775ff83bbc5a924341df957918e6e989e233
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-24 14:29:28 +0000

    Update ProcessIncomingHistoricalWebrootUpdates.sh

--- providerscripts/datastore/filesystems-sync/ProcessIncomingHistoricalWebrootUpdates.sh
+++ providerscripts/datastore/filesystems-sync/ProcessIncomingHistoricalWebrootUpdates.sh
@@ -32,31 +32,31 @@ additions_present="0"
 deletions_present="0"
 #sync_bucket="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`-sync-tunnel`/bin/echo ${target_directory} | /bin/sed 's:/:-:g'`"
 
-if ( [ "`${HOME}/providerscripts/datastore/dedicated/ListFromDatastore.sh "sync" "filesystem-sync/historical/additions/additions*.tar.gz"`" != "" ] )
+if ( [ "`${HOME}/providerscripts/datastore/dedicated/ListFromDatastore.sh "sync" "filesystem-sync/historical/additions/additions*.tar.gz" "${target_directory}"`" != "" ] )
 then
         additions_present="1"
 fi
 
-if ( [ "`${HOME}/providerscripts/datastore/dedicated/ListFromDatastore.sh "sync" "filesystem-sync/historical/deletions/deletions*.log"`" != "" ] )
+if ( [ "`${HOME}/providerscripts/datastore/dedicated/ListFromDatastore.sh "sync" "filesystem-sync/historical/deletions/deletions*.log" "${target_directory}"`" != "" ] )
 then
         deletions_present="1"
 fi
 
 if ( [ "${additions_present}" = "1" ] )
 then
-        additions="`${HOME}/providerscripts/datastore/dedicated/ListFromDatastore.sh "sync" "filesystem-sync/historical/additions/additions*.tar.gz"`"
+        additions="`${HOME}/providerscripts/datastore/dedicated/ListFromDatastore.sh "sync" "filesystem-sync/historical/additions/additions*.tar.gz" "${target_directory}"`"
         for addition in ${additions}
         do
-                ${HOME}/providerscripts/datastore/dedicated/GetFromDatastore.sh "sync" "filesystem-sync/historical/additions/${addition}" "${HOME}/runtime/filesystem_sync/historical/incoming/additions"
+                ${HOME}/providerscripts/datastore/dedicated/GetFromDatastore.sh "sync" "filesystem-sync/historical/additions/${addition}" "${HOME}/runtime/filesystem_sync/historical/incoming/additions"  "${target_directory}"
         done
 fi
 
 if ( [ "${deletions_present}" = "1" ] )
 then
-        deletions="`${HOME}/providerscripts/datastore/dedicated/ListFromDatastore.sh "sync" "filesystem-sync/historical/deletions/deletions*.log"`"
+        deletions="`${HOME}/providerscripts/datastore/dedicated/ListFromDatastore.sh "sync" "filesystem-sync/historical/deletions/deletions*.log" "${target_directory}"`"
         for deletion in ${deletions}
         do
-                ${HOME}/providerscripts/datastore/dedicated/GetFromDatastore.sh "sync" "filesystem-sync/historical/deletions/${deletion}" "${HOME}/runtime/filesystem_sync/historical/incoming/deletions"
+                ${HOME}/providerscripts/datastore/dedicated/GetFromDatastore.sh "sync" "filesystem-sync/historical/deletions/${deletion}" "${HOME}/runtime/filesystem_sync/historical/incoming/deletions" "${target_directory}"
         done
 fi
 
