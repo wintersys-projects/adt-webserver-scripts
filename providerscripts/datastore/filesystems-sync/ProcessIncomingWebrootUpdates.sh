67043ff560fe4c2632da42fad0b15aa8ca1d8c61
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-24 14:30:16 +0000

    Update ProcessIncomingWebrootUpdates.sh

--- providerscripts/datastore/filesystems-sync/ProcessIncomingWebrootUpdates.sh
+++ providerscripts/datastore/filesystems-sync/ProcessIncomingWebrootUpdates.sh
@@ -32,31 +32,31 @@ deletions_present="0"
 
 #sync_bucket="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`-sync-tunnel`/bin/echo ${target_directory} | /bin/sed 's:/:-:g'`"
 
-if ( [ "`${HOME}/providerscripts/datastore/dedicated/ListFromDatastore.sh "sync" "filesystem-sync/additions/additions*.tar.gz"`" != "" ] )
+if ( [ "`${HOME}/providerscripts/datastore/dedicated/ListFromDatastore.sh "sync" "filesystem-sync/additions/additions*.tar.gz" "${target_directory}"`" != "" ] )
 then
         additions_present="1"
 fi
 
-if ( [ "`${HOME}/providerscripts/datastore/dedicated/ListFromDatastore.sh "sync" "filesystem-sync/deletions/deletions*.log"`" != "" ] )
+if ( [ "`${HOME}/providerscripts/datastore/dedicated/ListFromDatastore.sh "sync" "filesystem-sync/deletions/deletions*.log" "${target_directory}"`" != "" ] )
 then
         deletions_present="1"
 fi
 
 if ( [ "${additions_present}" = "1" ] )
 then
-        additions="`${HOME}/providerscripts/datastore/dedicated/ListFromDatastore.sh "sync" "filesystem-sync/additions/additions*.tar.gz"`"
+        additions="`${HOME}/providerscripts/datastore/dedicated/ListFromDatastore.sh "sync" "filesystem-sync/additions/additions*.tar.gz" "${target_directory}"`"
         for addition in ${additions}
         do
-                ${HOME}/providerscripts/datastore/dedicated/GetFromDatastore.sh "sync" "filesystem-sync/additions/${addition}" "${HOME}/runtime/filesystem_sync/incoming/additions"
+                ${HOME}/providerscripts/datastore/dedicated/GetFromDatastore.sh "sync" "filesystem-sync/additions/${addition}" "${HOME}/runtime/filesystem_sync/incoming/additions" "${target_directory}"
         done
 fi
 
 if ( [ "${deletions_present}" = "1" ] )
 then
-        deletions="`${HOME}/providerscripts/datastore/dedicated/ListFromDatastore.sh "sync" "filesystem-sync/deletions/deletions*.log"`"
+        deletions="`${HOME}/providerscripts/datastore/dedicated/ListFromDatastore.sh "sync" "filesystem-sync/deletions/deletions*.log" "${target_directory}"`"
         for deletion in ${deletions}
         do
-                ${HOME}/providerscripts/datastore/dedicated/GetFromDatastore.sh "sync" "filesystem-sync/deletions/${deletion} ${HOME}/runtime/filesystem_sync/incoming/deletions"
+                ${HOME}/providerscripts/datastore/dedicated/GetFromDatastore.sh "sync" "filesystem-sync/deletions/${deletion} ${HOME}/runtime/filesystem_sync/incoming/deletions" "${target_directory}"
         done
 fi
 
