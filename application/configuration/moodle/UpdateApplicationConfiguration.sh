8289ba153a3a0e5036ef96e62d709038b0f50fe2
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-25 21:00:06 +0000

    Update UpdateApplicationConfiguration.sh

--- application/configuration/moodle/UpdateApplicationConfiguration.sh
+++ application/configuration/moodle/UpdateApplicationConfiguration.sh
@@ -60,18 +60,18 @@ then
 			/usr/bin/php -ln ${HOME}/runtime/moodle_config.php
 			if ( [ "$?" = "0" ] )
 			then
-				${HOME}/providerscripts/datastore/toolkit/PutToDatastore.sh "config" "${HOME}/runtime/moodle_config.php" "root" "local" "yes"
+				${HOME}/providerscripts/datastore/operations/PutToDatastore.sh "config" "${HOME}/runtime/moodle_config.php" "root" "local" "yes"
 			fi
 		fi
-	elif ( [ "`${HOME}/providerscripts/datastore/toolkit/ListFromDatastore.sh "config" "moodle_config.php"`" != "" ] && [ "`/usr/bin/find ${HOME}/runtime/moodle_config.php -cmin -1`" = "" ] )
+	elif ( [ "`${HOME}/providerscripts/datastore/operations/ListFromDatastore.sh "config" "moodle_config.php"`" != "" ] && [ "`/usr/bin/find ${HOME}/runtime/moodle_config.php -cmin -1`" = "" ] )
  	then
-		if ( [ "`${HOME}/providerscripts/datastore/toolkit/AgeOfDatastoreFile.sh "config" "moodle_config.php"`" -lt "130" ] && [ "`/usr/bin/find /var/www/html/config.php -cmin -1`" = "" ] )
+		if ( [ "`${HOME}/providerscripts/datastore/operations/AgeOfDatastoreFile.sh "config" "moodle_config.php"`" -lt "130" ] && [ "`/usr/bin/find /var/www/html/config.php -cmin -1`" = "" ] )
 		then
 			if ( [ -f ${HOME}/runtime/moodle_config.php ] )
 			then
 				/bin/mv ${HOME}/runtime/moodle_config.php ${HOME}/runtime/moodle_config.php-archive-$$
 			fi
-			${HOME}/providerscripts/datastore/toolkit/GetFromDatastore.sh "config" "moodle_config.php" "${HOME}/runtime"			
+			${HOME}/providerscripts/datastore/operations/GetFromDatastore.sh "config" "moodle_config.php" "${HOME}/runtime"			
 
 			if ( [ -f ${HOME}/runtime/moodle_config.php ] )
 			then
