7a8b0a816b43464f680b9d04dce8eb64b193daa5
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-25 08:44:30 +0000

    Update UpdateApplicationConfiguration.sh

--- application/configuration/wordpress/UpdateApplicationConfiguration.sh
+++ application/configuration/wordpress/UpdateApplicationConfiguration.sh
@@ -61,7 +61,7 @@ then
 				/usr/bin/php -ln ${HOME}/runtime/wordpress_config.php
 				if ( [ "$?" = "0" ] )
 				then
-					${HOME}/providerscripts/datastore/config/wrapper/PutToConfigDatastore.sh ${HOME}/runtime/wordpress_config.php "root" "no"
+					${HOME}/providerscripts/datastore/dedicated/PutToDatastore.sh "config" "${HOME}/runtime/wordpress_config.php" "root" "local" "yes"
 				fi
 			fi
 	elif ( [ "`${HOME}/providerscripts/datastore/config/wrapper/ListFromConfigDatastore.sh wordpress_config.php`" != "" ] && [ "`/usr/bin/find ${HOME}/runtime/wordpress_config.php -cmin -1`" = "" ] )
