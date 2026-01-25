9bb1f62729d9ece70287303d37ad33dbb9bffb1f
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-25 20:38:32 +0000

    Update ConnectToWebserverVPS.sh

--- utilities/remote/ConnectToWebserverVPS.sh
+++ utilities/remote/ConnectToWebserverVPS.sh
@@ -32,7 +32,7 @@ SSH_PORT="`${HOME}/utilities/config/ExtractConfigValue.sh 'SSHPORT'`"
 ALGORITHM="`${HOME}/utilities/config/ExtractConfigValue.sh 'ALGORITHM'`"
 BUILD_IDENTIFIER="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILDIDENTIFIER'`"
 
-ips="`${HOME}/providerscripts/datastore/toolkit/ListFromDatastore.sh "config" "webserverips/*"`"
+ips="`${HOME}/providerscripts/datastore/operations/ListFromDatastore.sh "config" "webserverips/*"`"
 
 if ( [ "${ips}" != "" ] )
 then
