2ef7e5afc5e00aa68a0ebb8a6b8f6d0a0a46ecc5
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-25 09:03:48 +0000

    Update ConnectToRemoteMySQL.sh

--- utilities/remote/ConnectToRemoteMySQL.sh
+++ utilities/remote/ConnectToRemoteMySQL.sh
@@ -67,8 +67,8 @@ if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:D
 then
         HOST="`${HOME}/utilities/config/ExtractConfigValue.sh 'DBIDENTIFIER'`"
 else
-        HOST="`${HOME}/providerscripts/datastore/config/wrapper/ListFromConfigDatastore.sh databaseip/*`"
-        HOST2="`${HOME}/providerscripts/datastore/config/wrapper/ListFromConfigDatastore.sh databasepublicip/*`"
+        HOST="`${HOME}/providerscripts/datastore/dedicated/ListFromDatastore.sh "config" "databaseip/*"`"
+        HOST2="`${HOME}/providerscripts/datastore/dedicated/ListFromDatastore.sh "config" "databasepublicip/*"`"
 fi
 
 DB_PORT="`${HOME}/utilities/config/ExtractConfigValue.sh 'DBPORT'`"
