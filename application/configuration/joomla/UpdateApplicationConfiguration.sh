e76a3dac58f85c68d896b48b766c6772e998a33b
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-25 08:54:25 +0000

    Update UpdateApplicationConfiguration.sh

--- application/configuration/joomla/UpdateApplicationConfiguration.sh
+++ application/configuration/joomla/UpdateApplicationConfiguration.sh
@@ -75,7 +75,7 @@ then
 				/bin/mv ${HOME}/runtime/joomla_configuration.php ${HOME}/runtime/joomla_configuration.php-archive-$$
 			fi
 		
-			${HOME}/providerscripts/datastore/dedicated/GetFromDatastore.sh "config" "joomla_configuration.php" "${HOME}/runtime/joomla_configuration.php"			
+			${HOME}/providerscripts/datastore/dedicated/GetFromDatastore.sh "config" "joomla_configuration.php" "${HOME}/runtime"			
 
 			if ( [ -f ${HOME}/runtime/joomla_configuration.php ] )
 			then
