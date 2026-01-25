f364f5a0fc7800648a1383c62c69600f1fbca7c6
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-25 18:37:14 +0000

    Update UpdateApplicationConfiguration.sh

--- application/configuration/wordpress/UpdateApplicationConfiguration.sh
+++ application/configuration/wordpress/UpdateApplicationConfiguration.sh
@@ -61,19 +61,19 @@ then
 				/usr/bin/php -ln ${HOME}/runtime/wordpress_config.php
 				if ( [ "$?" = "0" ] )
 				then
-					${HOME}/providerscripts/datastore/dedicated/PutToDatastore.sh "config" "${HOME}/runtime/wordpress_config.php" "root" "local" "yes"
+					${HOME}/providerscripts/datastore/toolkit/PutToDatastore.sh "config" "${HOME}/runtime/wordpress_config.php" "root" "local" "yes"
 				fi
 			fi
-	elif ( [ "`${HOME}/providerscripts/datastore/dedicated/ListFromDatastore.sh "config" "wordpress_config.php"`" != "" ] && [ "`/usr/bin/find ${HOME}/runtime/wordpress_config.php -cmin -1`" = "" ] )
+	elif ( [ "`${HOME}/providerscripts/datastore/toolkit/ListFromDatastore.sh "config" "wordpress_config.php"`" != "" ] && [ "`/usr/bin/find ${HOME}/runtime/wordpress_config.php -cmin -1`" = "" ] )
 	then
-		if ( [ "`${HOME}/providerscripts/datastore/dedicated/AgeOfDatastoreFile.sh "config" "wordpress_config.php"`" -lt "130" ] && [ "`/usr/bin/find /var/www/html/wp-config.php -cmin -1`" = "" ] )
+		if ( [ "`${HOME}/providerscripts/datastore/toolkit/AgeOfDatastoreFile.sh "config" "wordpress_config.php"`" -lt "130" ] && [ "`/usr/bin/find /var/www/html/wp-config.php -cmin -1`" = "" ] )
 		then
 			if ( [ -f ${HOME}/runtime/wordpress_config.php ] )
 			then
 				/bin/mv ${HOME}/runtime/wordpress_config.php ${HOME}/runtime/wordpress_config.php-archive-$$
 			fi
 			
-			${HOME}/providerscripts/datastore/dedicated/GetFromDatastore.sh "config" "wordpress_config.php" "${HOME}/runtime"			
+			${HOME}/providerscripts/datastore/toolkit/GetFromDatastore.sh "config" "wordpress_config.php" "${HOME}/runtime"			
 			
 			if ( [ -f ${HOME}/runtime/wordpress_config.php ] )
 			then
