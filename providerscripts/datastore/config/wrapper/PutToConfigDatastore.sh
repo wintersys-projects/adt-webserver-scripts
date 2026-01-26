48d3ee7bf9ff739bd10c6a05d932eec3f94d9c1f
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-26 10:26:31 +0000

    Update PutToConfigDatastore.sh

--- providerscripts/datastore/config/wrapper/PutToConfigDatastore.sh
+++ providerscripts/datastore/config/wrapper/PutToConfigDatastore.sh
@@ -1,10 +1,15 @@
-file_to_put="${1}"
-place_to_put="${2}"
-delete="${3}"
+#!/bin/sh
+
+bucket_type="${1}"
+file_to_put="${2}"
+place_to_put="${3}"
+mode="${4}"
+delete="${5}"
+additional_specifier="${6}"
 
 if ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "tool" ] )
 then
-        ${HOME}/providerscripts/datastore/config/toolkit/PutToConfigDatastore.sh "${file_to_put}" "${place_to_put}" "${delete}"
+        ${HOME}/providerscripts/datastore/config/toolkit/PutToConfigDatastore.sh "${bucket_type}" "${file_to_put}" "${place_to_put}" "${mode}" "${delete}" "${additional_specifier}"
 elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "lightweight" ] ||  [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "heavyweight" ] )
 then
         if ( [ -f ${file_to_put} ] )
