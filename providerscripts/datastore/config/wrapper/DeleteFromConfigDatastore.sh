b3ea1dc95ee9e41d7cf8df92d4d4a3fdbc785423
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-26 10:22:47 +0000

    Update DeleteFromConfigDatastore.sh

--- providerscripts/datastore/config/wrapper/DeleteFromConfigDatastore.sh
+++ providerscripts/datastore/config/wrapper/DeleteFromConfigDatastore.sh
@@ -3,7 +3,7 @@ file_to_delete="${1}"
 local="${2}"   #are these two needed?
 recursive="${3}"
 
-if ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "toolkit" ] )
+if ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "tool" ] )
 then
 	${HOME}/providerscripts/datastore/config/toolkit/DeleteFromConfigDatastore.sh "${file_to_delete}" "${local}" "${recursive}"
 elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "lightweight" ] ||  [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "heavyweight" ] )
