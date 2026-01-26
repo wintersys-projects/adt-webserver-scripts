08831701420256e599549ed7d180d8d11ba08009
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-26 10:35:30 +0000

    Update AgeOfConfigFile.sh

--- providerscripts/datastore/config/wrapper/AgeOfConfigFile.sh
+++ providerscripts/datastore/config/wrapper/AgeOfConfigFile.sh
@@ -1,10 +1,10 @@
 
-file_to_interrogate="${1}"
-
+bucket_type="${1}"
+file_to_interrogate="${2}"
 
 if ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "tool" ] )
 then
-    ${HOME}/providerscripts/datastore/operations/AgeOfDatastoreFile.sh "${file_to_interrogate}"
+    ${HOME}/providerscripts/datastore/operations/AgeOfDatastoreFile.sh "${bucket_type}" "${file_to_interrogate}"
 elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "lightweight" ] )
 then
 :
