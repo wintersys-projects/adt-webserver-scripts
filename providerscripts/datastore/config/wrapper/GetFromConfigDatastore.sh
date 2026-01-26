e6c36c9836430f3d751a1043d4400a7dcc6f8e11
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-26 10:31:36 +0000

    Update GetFromConfigDatastore.sh

--- providerscripts/datastore/config/wrapper/GetFromConfigDatastore.sh
+++ providerscripts/datastore/config/wrapper/GetFromConfigDatastore.sh
@@ -1,9 +1,13 @@
-file_to_get="${1}"
-place_to_put="${2}"
+#!/bin/sh
+
+bucket_type="${1}"
+file_to_get="${2}"
+destination="${3}"
+additional_specifier="${4}"
 
 if ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "tool" ] )
 then
-  ${HOME}/providerscripts/datastore/config/toolkit/GetFromConfigDatastore.sh "${file_to_get}" "${place_to_put}"
+  ${HOME}/providerscripts/datastore/config/toolkit/GetFromConfigDatastore.sh "${bucket_type}" "${file_to_get}" "${destination}" "${additional_specifier}"
 elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "lightweight" ] || [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "heavyweight" ] )
 then
   if ( [ -f /var/lib/adt-config/${file_to_get} ] )
