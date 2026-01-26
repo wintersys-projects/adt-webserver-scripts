318bacb2da9d841b5445cd380fa6fee56ff7e137
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-26 11:01:33 +0000

    Update Backup.sh

--- application/backupscripts/Backup.sh
+++ application/backupscripts/Backup.sh
@@ -31,7 +31,7 @@ then
         exit
 fi
 
-if ( [ "`${HOME}/providerscripts/datastore/operations/ListFromDatastore.sh "config" "INSTALLED_SUCCESSFULLY"`" = "" ] )
+if ( [ "`${HOME}/providerscripts/datastore/config/wrapper/ListFromDatastore.sh "config" "INSTALLED_SUCCESSFULLY"`" = "" ] )
 then
         exit
 fi
