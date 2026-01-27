a8aef35cdd6a279fd38dadb3ab78d813c944ba72
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-27 09:59:33 +0000

    Update ActivateConfigDatastoreHeavyweight.sh

--- providerscripts/datastore/config/ActivateConfigDatastoreHeavyweight.sh
+++ providerscripts/datastore/config/ActivateConfigDatastoreHeavyweight.sh
@@ -2,7 +2,7 @@
 while ( [ 1 ] )
 do
   /bin/sleep 2 && ${HOME}/providerscripts/datastore/filesystems-sync/FileSystemsSyncingController.sh '2' '/var/lib/adt-config' &
-  /bin/sleep 15 && ${HOME}/providerscripts/datastore/filesystems-sync/FileSystemsSyncingController.sh '15' '/var/lib/adt-config' &
-  /bin/sleep 30 && ${HOME}/providerscripts/datastore/filesystems-sync/FileSystemsSyncingController.sh '30' '/var/lib/adt-config' &
-  /bin/sleep 45 && ${HOME}/providerscripts/datastore/filesystems-sync/FileSystemsSyncingController.sh '45' '/var/lib/adt-config' &
+ # /bin/sleep 15 && ${HOME}/providerscripts/datastore/filesystems-sync/FileSystemsSyncingController.sh '15' '/var/lib/adt-config' &
+ # /bin/sleep 30 && ${HOME}/providerscripts/datastore/filesystems-sync/FileSystemsSyncingController.sh '30' '/var/lib/adt-config' &
+ # /bin/sleep 45 && ${HOME}/providerscripts/datastore/filesystems-sync/FileSystemsSyncingController.sh '45' '/var/lib/adt-config' &
 done
