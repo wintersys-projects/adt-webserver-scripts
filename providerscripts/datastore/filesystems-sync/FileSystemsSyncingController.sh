94617dbb4c07475b4d9f4d6922173d2f3ca55d2e
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-27 10:22:12 +0000

    Update FileSystemsSyncingController.sh

--- providerscripts/datastore/filesystems-sync/FileSystemsSyncingController.sh
+++ providerscripts/datastore/filesystems-sync/FileSystemsSyncingController.sh
@@ -148,15 +148,15 @@ then
         then
                 ${HOME}/providerscripts/datastore/operations/DeleteFromDatastore.sh "sync" "*" "distributed"
         fi
-        ${HOME}/providerscripts/datastore/filesystems-sync/ProcessIncomingHistoricalWebrootUpdates.sh "${target_directory}"
+        ${HOME}/providerscripts/datastore/filesystems-sync/heavyweight/ProcessIncomingHistoricalWebrootUpdates.sh "${target_directory}"
 else
-        ${HOME}/providerscripts/datastore/filesystems-sync/ProcessOutgoingWebrootUpdates.sh "${target_directory}"
-        ${HOME}/providerscripts/datastore/filesystems-sync/ProcessIncomingWebrootUpdates.sh "${target_directory}"
+        ${HOME}/providerscripts/datastore/filesystems-sync/heavyweight/ProcessOutgoingWebrootUpdates.sh "${target_directory}"
+        ${HOME}/providerscripts/datastore/filesystems-sync/heavyweight/ProcessIncomingWebrootUpdates.sh "${target_directory}"
 fi
 
 
-${HOME}/providerscripts/datastore/filesystems-sync/HousekeepAdditionsSyncing.sh "${target_directory}"
-${HOME}/providerscripts/datastore/filesystems-sync/HousekeepDeletionsSyncing.sh "${target_directory}"
+${HOME}/providerscripts/datastore/filesystems-sync/heavyweight/HousekeepAdditionsSyncing.sh "${target_directory}"
+${HOME}/providerscripts/datastore/filesystems-sync/heavyweight/HousekeepDeletionsSyncing.sh "${target_directory}"
 
 if ( [ -f ${HOME}/runtime/filesystem_sync/DISABLE_EXECUTION:${execution_order} ] )
 then
