e7c49917591948e6d57bc811d09404e1287c83de
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-28 16:07:18 +0000

    Update PutToDatastore.sh

--- providerscripts/datastore/operations/PutToDatastore.sh
+++ providerscripts/datastore/operations/PutToDatastore.sh
@@ -2,7 +2,9 @@
 #####################################################################################
 # Author: Peter Winter
 # Date :  9/4/2016
-# Description: Coordinating putting a file into a bucket in the datastore
+# Description: Put a file to a bucket in your datastore(s) This can be called in 
+# local or distributed mode. Local mode is when your servers are operating in single
+# region mode and distributed mode is what is used if you are operating in multi-region mode. 
 #####################################################################################
 # License Agreement:
 # This file is part of The Agile Deployment Toolkit.
