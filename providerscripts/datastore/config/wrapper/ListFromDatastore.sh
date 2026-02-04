fe31a88d0232ead07a54ec88eb45c18837cbe92a
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-02-04 11:45:38 +0000

    Update ListFromDatastore.sh

--- providerscripts/datastore/config/wrapper/ListFromDatastore.sh
+++ providerscripts/datastore/config/wrapper/ListFromDatastore.sh
@@ -30,7 +30,7 @@ then
         ${HOME}/providerscripts/datastore/operations/ListFromDatastore.sh "${bucket_type}" "${file_to_list}" "${additional_specifier}"
 elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "lightweight" ] || [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "DATASTORECONFIGSTYLE" | /usr/bin/awk -F':' '{print $NF}'`" = "heavyweight" ] )
 then
-        if ( [ -f /var/lib/adt-config/${file_to_list} ] )
+        if ( [ -f /var/lib/adt-config/${file_to_list} ] || [ -d /var/lib/adt-config/${file_to_list} ] )
         then
                 /bin/ls /var/lib/adt-config/${file_to_list} | /usr/bin/awk -F'/' '{print $NF}'
         fi
