cfe1103cde45a59f08086c72f8ddc684ef6d1477
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-02-12 08:56:47 +0000

    Update InstallNginxConfigurationForWebserverFromRepo.sh

--- providerscripts/webserver/configuration/InstallNginxConfigurationForWebserverFromRepo.sh
+++ providerscripts/webserver/configuration/InstallNginxConfigurationForWebserverFromRepo.sh
@@ -37,30 +37,15 @@ VPC_IP_RANGE="`${HOME}/utilities/config/ExtractConfigValue.sh 'VPCIPRANGE'`"
 BUILD_MACHINE_IP="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILDMACHINEIP'`"
 AUTH_SERVER_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'AUTHSERVERURL'`"
 
+if ( [ "`/usr/bin/find /etc/nginx/sites-available -prune -empty 2>/dev/null`" = "" ] )
+then
+	/bin/rm /etc/nginx/sites-available/*
+fi
 
-#/bin/mkdir /etc/nginx/cache 2>/dev/null
-
-#if ( [ -f /etc/nginx/sites-available/${WEBSITE_NAME} ] )
-#then
-#	/bin/rm /etc/nginx/sites-available/${WEBSITE_NAME}
-#fi
-
-#if ( [ -h /etc/nginx/sites-enabled/${WEBSITE_NAME} ] )
-#then
-#	/usr/bin/unlink /etc/nginx/sites-enabled/${WEBSITE_NAME}
-#fi
-
-#if ( [ -h /etc/nginx/sites-enabled/default ] )
-#then
-#	/usr/bin/unlink /etc/nginx/sites-enabled/default
-#fi
-
-#if ( [ ! -d /etc/nginx/sites-available ] )
-#then
-#	/bin/mkdir -p /etc/nginx/sites-available
-#fi
-
-
+if ( [ "`/usr/bin/find /etc/nginx/sites-enable -prune -empty 2>/dev/null`" = "" ] )
+then
+	/bin/rm /etc/nginx/sites-enable/*
+fi
 
 /usr/bin/openssl dhparam -dsaparam -out /etc/ssl/certs/dhparam.pem 4096
 
@@ -86,11 +71,8 @@ if ( [ "`/bin/echo ${port} | /bin/grep -o "^[0-9]*$"`" != "" ] )
 then
 	/bin/sed -i "/#XXXXPHPSOCKETXXXX/d" ${HOME}/providerscripts/webserver/configuration/${APPLICATION}/nginx/online/repo/site-available.conf
 	/bin/sed -i "s/#XXXXPHPPORTXXXX//g" ${HOME}/providerscripts/webserver/configuration/${APPLICATION}/nginx/online/repo/site-available.conf
-	#/bin/sed -i "s/#XXXXPORTMODEONXXXX//g" ${HOME}/providerscripts/webserver/configuration/${APPLICATION}/nginx/online/repo/site-available.conf
 else
-	#/bin/sed -i "/#XXXXPORTMODEONXXXX/d" ${HOME}/providerscripts/webserver/configuration/${APPLICATION}/nginx/online/repo/site-available.conf
 	/bin/sed -i "/#XXXXPHPPORTXXXX/d" ${HOME}/providerscripts/webserver/configuration/${APPLICATION}/nginx/online/repo/site-available.conf
-	#/bin/sed -i "s/#XXXXPHPVERSIONXXXX/${PHP_VERSION}/g" ${HOME}/providerscripts/webserver/configuration/${APPLICATION}/nginx/online/repo/site-available.conf
 	/bin/sed -i "s/#XXXXPHPSOCKETXXXX//g" ${HOME}/providerscripts/webserver/configuration/${APPLICATION}/nginx/online/repo/site-available.conf	
 fi
 
@@ -111,24 +93,8 @@ then
 		/bin/sed -i "s/XXXXCLOUDFLAREXXXX//g" ${HOME}/providerscripts/webserver/configuration/${APPLICATION}/nginx/online/repo/nginx.conf
 	fi
 	/bin/cp ${HOME}/providerscripts/webserver/configuration/${APPLICATION}/nginx/online/repo/nginx.conf /etc/nginx/nginx.conf
-fi
-
-if ( [ -f /etc/nginx/nginx.conf ] )
-then
 	/bin/chmod 600  /etc/nginx/nginx.conf
 	/bin/chown root:root  /etc/nginx/nginx.conf
-fi
-
-
-#Activate it
-if ( [ -f /etc/nginx/sites-available/default ] )
-then
-	/bin/rm /etc/nginx/sites-available/default
-fi
-
-if ( [ ! -d /etc/nginx/sites-enabled ] )
-then
-	/bin/mkdir -p /etc/nginx/sites-enabled
 fi
 
 if ( [ -f /etc/nginx/sites-available/${WEBSITE_NAME} ] )
