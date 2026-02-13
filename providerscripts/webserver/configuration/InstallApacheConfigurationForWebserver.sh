dd2a9ad00cf2781b05e353e0d542912c2aa93bc2
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-02-13 09:54:44 +0000

    Update InstallApacheConfigurationForWebserver.sh

--- providerscripts/webserver/configuration/InstallApacheConfigurationForWebserver.sh
+++ providerscripts/webserver/configuration/InstallApacheConfigurationForWebserver.sh
@@ -36,7 +36,7 @@ VPC_IP_RANGE="`${HOME}/utilities/config/ExtractConfigValue.sh 'VPCIPRANGE'`"
 BUILD_MACHINE_IP="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILDMACHINEIP'`"
 APPLICATION="`${HOME}/utilities/config/ExtractConfigValue.sh 'APPLICATION'`"
 AUTH_SERVER_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'AUTHSERVERURL'`"
-
+port="`${HOME}/utilities/config/ExtractBuildStyleValues.sh "PHP" "stripped" | /usr/bin/awk -F'|' '{print $NF}'`"
 
 #You need to provide a mpm module to use in the buildsytles file even if it is mpm_prefork
 /usr/sbin/a2dismod mpm_prefork
@@ -58,69 +58,57 @@ then
 	/usr/sbin/a2enconf php${PHP_VERSION}-fpm
 fi
 
-if ( [ -d /etc/nginx/sites-available ] && [ "`/usr/bin/find /etc/nginx/sites-available -prune -empty 2>/dev/null`" = "" ] )
+if ( [ -d /etc/apache2/sites-available ] && [ "`/usr/bin/find /etc/nginx/sites-available -prune -empty 2>/dev/null`" = "" ] )
 then
-	/bin/rm /etc/nginx/sites-available/*
+	/bin/rm /etc/apache2/sites-available/*
 else
-	/bin/mkdir -p /etc/nginx/sites-available
+	/bin/mkdir -p /etc/apache2/sites-available
 fi
 
-if ( [ -d /etc/nginx/sites-enabled ] && [ "`/usr/bin/find /etc/nginx/sites-enabled -prune -empty 2>/dev/null`" = "" ] )
+if ( [ -d /etc/apache2/sites-enabled ] && [ "`/usr/bin/find /etc/nginx/sites-enabled -prune -empty 2>/dev/null`" = "" ] )
 then
-	/bin/rm /etc/nginx/sites-enabled/*
+	/bin/rm /etc/apache2/sites-enabled/*
 else
-	/bin/mkdir -p /etc/nginx/sites-enabled
+	/bin/mkdir -p /etc/apache2/sites-enabled
 fi
 
 /usr/bin/openssl dhparam -dsaparam -out /etc/ssl/certs/dhparam.pem 4096
-
-#if ( [ -f ${HOME}/providerscripts/webserver/configuration/${APPLICATION}/apache/online/repo/site-available.conf ] )
-#then
-#	/bin/cp ${HOME}/providerscripts/webserver/configuration/${APPLICATION}/apache/online/repo/site-available.conf /etc/apache2/sites-available/${WEBSITE_NAME}.conf
-#	/bin/sed -i "s/XXXXWEBSITEURLXXXX/${WEBSITE_URL}/g" /etc/apache2/sites-available/${WEBSITE_NAME}.conf
-#	export HOME="`/bin/cat /home/homedir.dat`"
-#	/bin/sed -i "s,XXXXHOMEXXXX,${HOME},g" /etc/apache2/sites-available/${WEBSITE_NAME}.conf
-#	/bin/sed -i "s/XXXXROOTDOMAINXXXX/${ROOT_DOMAIN}/g" /etc/apache2/sites-available/${WEBSITE_NAME}.conf
-	/bin/chmod 600 /etc/apache2/sites-available/${WEBSITE_NAME}.conf
-	/bin/chown root:root /etc/apache2/sites-available/${WEBSITE_NAME}.conf
-	/usr/sbin/a2ensite ${WEBSITE_NAME}
-#	/bin/echo "/etc/apache2/sites-available/${WEBSITE_NAME}.conf" > ${HOME}/runtime/WEBSERVER_CONFIG_LOCATION.dat
-#fi
 
 export HOME="`/bin/cat /home/homedir.dat`"
 /bin/sed -i "s/XXXXWEBSITEURLXXXX/${WEBSITE_URL}/g" ${HOME}/providerscripts/webserver/configuration/application/apache/online/repo/site-available.conf
 /bin/sed -i "s,XXXXHOMEXXXX,${HOME},g" ${HOME}/providerscripts/webserver/configuration/application/apache/online/repo/site-available.conf
 /bin/sed -i "s/XXXXROOTDOMAINXXXX/${ROOT_DOMAIN}/g" ${HOME}/providerscripts/webserver/configuration/application/apache/online/repo/site-available.conf
 
-
-
-
 if ( [ "${NO_AUTHENTICATORS}" != "0" ] && [ "${AUTHENTICATOR_TYPE}" = "basic-auth" ] && [ "${NO_REVERSE_PROXY}" = "0" ] )
 then
-	/bin/sed -i -e "/#XXXXBASIC-AUTHXXXX/{r ${HOME}/providerscripts/webserver/configuration/${APPLICATION}/apache/online/repo/basic-auth.conf" -e "d}" /etc/apache2/sites-available/${WEBSITE_NAME}.conf
-	/bin/sed -i "s/Require all granted/#Require all granted/g" /etc/apache2/sites-available/${WEBSITE_NAME}.conf
-	/bin/sed -i "s;XXXXVPC_IP_RANGEXXXX;127.0.0.1 ${VPC_IP_RANGE};g" /etc/apache2/sites-available/${WEBSITE_NAME}.conf
-	/bin/sed -i "s/XXXXBUILD_MACHINE_IPXXXX/${BUILD_MACHINE_IP}/g" /etc/apache2/sites-available/${WEBSITE_NAME}.conf
-	/bin/sed -i "s/XXXXWEBSITE_URLXXXXX/${AUTH_SERVER_URL}/g" /etc/apache2/sites-available/${WEBSITE_NAME}.conf
+	/bin/sed -i "s/Require all granted/#Require all granted/g" ${HOME}/providerscripts/webserver/configuration/application/apache/online/repo/site-available.conf
+	/bin/sed -i "s;XXXXVPC_IP_RANGEXXXX;127.0.0.1 ${VPC_IP_RANGE};g" ${HOME}/providerscripts/webserver/configuration/application/apache/online/repo/site-available.conf
+	/bin/sed -i "s/XXXXBUILD_MACHINE_IPXXXX/${BUILD_MACHINE_IP}/g" ${HOME}/providerscripts/webserver/configuration/application/apache/online/repo/site-available.conf
+	/bin/sed -i "s/XXXXWEBSITE_URLXXXXX/${AUTH_SERVER_URL}/g" ${HOME}/providerscripts/webserver/configuration/application/apache/online/repo/site-available.conf
+	/bin/sed -i "s/XXXXPORTXXXX/${port}/" ${HOME}/providerscripts/webserver/configuration/application/apache/online/repo/site-available.conf
+	/bin/sed -i "s/XXXXPHPVERSIONXXXX/${PHP_VERSION}/" ${HOME}/providerscripts/webserver/configuration/application/apache/online/repo/site-available.conf
 	/bin/touch /etc/apache2/.htpasswd
 fi
 
-
 if ( [ "`/bin/echo ${port} | /bin/grep -o "^[0-9]*$"`" != "" ] )
 then
-	/bin/sed -i "s/#XXXXPHPPORTXXXX//g" ${HOME}/providerscripts/webserver/configuration/application/nginx/site-available.conf
+	/bin/sed -i "s/#XXXXFASTCGIPORTXXXX//g" ${HOME}/providerscripts/webserver/configuration/application/nginx/site-available.conf
 else
-	/bin/sed -i "s/#XXXXPHPSOCKETXXXX//g" ${HOME}/providerscripts/webserver/configuration/application/nginx/site-available.conf	
+	/bin/sed -i "s/#XXXXFASTCGISOCKETXXXX//g" ${HOME}/providerscripts/webserver/configuration/application/nginx/site-available.conf	
 fi
 
+/bin/cat ${HOME}/providerscripts/webserver/configuration/application/apache/online/repo/site-available.conf > /etc/apache2/sites-available/${WEBSITE_NAME}.conf
+/bin/chmod 600 /etc/apache2/sites-available/${WEBSITE_NAME}.conf
+/bin/chown root:root /etc/apache2/sites-available/${WEBSITE_NAME}.conf
+/usr/sbin/a2ensite ${WEBSITE_NAME}
 
-if ( [ -f ${HOME}/providerscripts/webserver/configuration/${APPLICATION}/apache/online/repo/htaccess.conf ] )
+if ( [ -f ${HOME}/providerscripts/webserver/configuration/application/apache/online/repo/htaccess.conf ] )
 then
 	if ( [ ! -d ${HOME}/runtime/overridehtaccess ] )
 	then
 		/bin/mkdir -p ${HOME}/runtime/overridehtaccess
 	fi
-	/bin/cp ${HOME}/providerscripts/webserver/configuration/${APPLICATION}/apache/online/repo/htaccess.conf ${HOME}/runtime/overridehtaccess/htaccess.conf
+	/bin/cp ${HOME}/providerscripts/webserver/configuration/application/apache/htaccess-main.conf ${HOME}/runtime/overridehtaccess/htaccess.conf
 fi
 
 if ( [ "${MOD_SECURITY}" = "1" ] && [ "${NO_REVERSE_PROXY}" = "0" ] )
@@ -128,19 +116,6 @@ then
 	/bin/sed -i -e "/#XXXXMODSECURITYXXXX/{r ${HOME}/providerscripts/webserver/configuration/${APPLICATION}/apache/online/repo/modsecurity.conf" -e "d}" /etc/apache2/sites-available/${WEBSITE_NAME}.conf
 fi
 
-port="`${HOME}/utilities/config/ExtractBuildStyleValues.sh "PHP" "stripped" | /usr/bin/awk -F'|' '{print $NF}'`"
-
-if ( [ "`/bin/echo ${port} | /bin/grep -o "^[0-9]*$"`" = "" ] )
-then
-	if ( [ -f ${HOME}/providerscripts/webserver/configuration/${APPLICATION}/apache/online/repo/fastcgi.conf ] )
-	then
-		/bin/sed -i -e "/XXXXFASTCGIXXXX/{r ${HOME}/providerscripts/webserver/configuration/${APPLICATION}/apache/online/repo/fastcgi.conf" -e "d}" /etc/apache2/sites-available/${WEBSITE_NAME}.conf
-		/bin/sed -i "s/XXXXPHPVERSIONXXXX/${PHP_VERSION}/" /etc/apache2/sites-available/${WEBSITE_NAME}.conf
-	fi
-else
-	/bin/sed -i -e "/XXXXFASTCGIXXXX/{r ${HOME}/providerscripts/webserver/configuration/${APPLICATION}/apache/online/repo/fastcgi-port.conf" -e "d}" /etc/apache2/sites-available/${WEBSITE_NAME}.conf
-	/bin/sed -i "s/XXXXPORTXXXX/${port}/" /etc/apache2/sites-available/${WEBSITE_NAME}.conf
-fi
 
 config_settings="`${HOME}/utilities/config/ExtractBuildStyleValues.sh "APACHE:settings" "stripped" | /bin/sed 's/|.*//g' | /bin/sed 's/:/ /g'`"
 
