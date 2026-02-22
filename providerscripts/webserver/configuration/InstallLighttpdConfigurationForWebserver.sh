e07ec005d5b38bd0abac4ee7f9423ee977fcdaba
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-02-22 00:26:35 +0000

    Update InstallLighttpdConfigurationForWebserver.sh

--- providerscripts/webserver/configuration/InstallLighttpdConfigurationForWebserver.sh
+++ providerscripts/webserver/configuration/InstallLighttpdConfigurationForWebserver.sh
@@ -92,14 +92,17 @@ fi
 /bin/chmod 600 /etc/lighttpd/modules.conf
 /bin/echo "/etc/lighttpd/lighttpd.conf" > ${HOME}/runtime/WEBSERVER_CONFIG_LOCATION.dat
 
-if ( [ -f ${HOME}/providerscripts/webserver/configuration/application/lighttpd/rc.local ] )
+if ( [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'LIGHTTPD:source'`" = "1" ] )
 then
-	/bin/cp ${HOME}/providerscripts/webserver/configuration/application/lighttpd/rc.local /etc/rc.local
-fi
+	if ( [ -f ${HOME}/providerscripts/webserver/configuration/application/lighttpd/rc.local ] )
+	then
+		/bin/cp ${HOME}/providerscripts/webserver/configuration/application/lighttpd/rc.local /etc/rc.local
+	fi
 
-if ( [ -f ${HOME}/providerscripts/webserver/configuration/application/lighttpd/lighttpd-service.conf ] )
-then
-	/bin/cp ${HOME}/providerscripts/webserver/configuration/application/lighttpd/lighttpd-service.conf  /etc/systemd/system/rc-local.service		
+	if ( [ -f ${HOME}/providerscripts/webserver/configuration/application/lighttpd/lighttpd-service.conf ] )
+	then
+		/bin/cp ${HOME}/providerscripts/webserver/configuration/application/lighttpd/lighttpd-service.conf  /etc/systemd/system/rc-local.service		
+	fi
 fi
 
 config_settings="`${HOME}/utilities/config/ExtractBuildStyleValues.sh "LIGHTTPD:settings" "stripped" | /bin/sed 's/|.*//g' | /bin/sed 's/:/ /g'`"
