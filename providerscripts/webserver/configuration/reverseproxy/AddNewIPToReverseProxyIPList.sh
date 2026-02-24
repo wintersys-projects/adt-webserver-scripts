b18e2281508eb77d63cc8e9441d79b5d2e370c76
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-02-24 00:05:33 +0000

    Update AddNewIPToReverseProxyIPList.sh

--- providerscripts/webserver/configuration/reverseproxy/AddNewIPToReverseProxyIPList.sh
+++ providerscripts/webserver/configuration/reverseproxy/AddNewIPToReverseProxyIPList.sh
@@ -76,12 +76,6 @@ do
         fi
 done
 
-if ( [ -f /etc/lighttpd/lighttpd.conf ] )
-then
-        /bin/grep "*host*port*" /etc/lighttpd/lighttpd.conf | /usr/bin/tac  | /bin/sed -s '1s/.$//' | /usr/bin/tac > /etc/lighttpd/lighttpd.conf.$$
-        /bin/mv /etc/lighttpd/lighttpd.conf.$$ /etc/lighttpd/lighttpd.conf
-fi
-
 if ( [ "${updated}" = "1" ] )
 then
         ${HOME}/providerscripts/webserver/ReloadWebserver.sh
