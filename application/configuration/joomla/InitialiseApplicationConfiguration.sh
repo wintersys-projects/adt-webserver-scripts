e1e01896092c0e4fe55691392e24a4c0a0ea2f1f
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-03-04 13:26:20 +0000

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/joomla/InitialiseApplicationConfiguration.sh
+++ application/configuration/joomla/InitialiseApplicationConfiguration.sh
@@ -102,8 +102,8 @@ ${HOME}/utilities/remote/ConnectToRemoteMySQL.sh < /var/www/html/installation/sq
 
 extension_id="`${HOME}/utilities/remote/ConnectToRemoteMySQL.sh `/bin/echo "select extension_id,name from ${dbprefix}extensions where name='files_joomla';"` | /bin/grep 'files_joomla' | /usr/bin/awk '{print $1}'`"
 version_id="`/bin/ls /var/www/html/administrator/components/com_admin/sql/updates/mysql | /usr/bin/tail -n +1`"
-
-/bin/echo "INSERT INTO \`${dbprefix}schemas\` (\`extension_id\`, \`version_id\`) VALUES (${extension_id}, '"${version_id}"');"
+/bin/echo "INSERT INTO \`${dbprefix}schemas\` (\`extension_id\`, \`version_id\`) VALUES (${extension_id}, '"${version_id}"');" > /var/www/html/installation/sql/mysql/noninteractive_fudge_with_dbprefix.sql
+${HOME}/utilities/remote/ConnectToRemoteMySQL.sh < /var/www/html/installation/sql/mysql/noninteractive_fudge_with_dbprefix.sql
 
 if ( [ -d /var/www/html/installation ] )
 then
