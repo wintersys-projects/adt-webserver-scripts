fb9efc853d9dc46751cb0b0c25d502f98062a33e
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-03-04 18:02:07 +0000

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/joomla/InitialiseApplicationConfiguration.sh
+++ application/configuration/joomla/InitialiseApplicationConfiguration.sh
@@ -21,7 +21,7 @@ then
 
                 if ( [ "${dbprefix}" = "jos_" ] )
                 then
-                        dbprefix="`/usr/bin/tr -dc A-Za-z0-9 </dev/urandom | /usr/bin/head -c 5; /bin/echo`_"
+                        dbprefix="`/usr/bin/tr -dc a-z0-9 </dev/urandom | /usr/bin/head -c 5; /bin/echo`_"
                 fi
                 /bin/echo ${dbprefix} > /var/www/html/dbp.dat
                 /bin/chown www-data:www-data /var/www/html/dbp.dat
