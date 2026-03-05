d7343971eaff727248c3b2298244748d96742283
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-03-05 19:07:49 +0000

    Update InstallVirginDeployment.sh

--- application/configuration/joomla/InstallVirginDeployment.sh
+++ application/configuration/joomla/InstallVirginDeployment.sh
@@ -23,41 +23,39 @@
 
 if ( [ ! -d ${HOME}/runtime/downloads_work_area ] )
 then
-  /bin/mkdir -p ${HOME}/runtime/downloads_work_area
+        /bin/mkdir -p ${HOME}/runtime/downloads_work_area
 fi
 
 cd ${HOME}/runtime/downloads_work_area
-
-#cd /var/www/html
 SOURCECODE_URL="`/bin/grep "^SOURCECODE_URL" ${HOME}/runtime/application.dat | /bin/sed 's/SOURCECODE_URL://g' | /bin/sed 's/:/ /g'`"
-SOURCECODE_MD5="`/bin/grep "^SOURCECODE_MD5" ${HOME}/runtime/application.dat | /bin/sed 's/SOURCECODE_URL://g' | /bin/sed 's/:/ /g'`"
-SOURCECODE_SHA1="`/bin/grep "^SOURCECODE_SHA1" ${HOME}/runtime/application.dat | /bin/sed 's/SOURCECODE_URL://g' | /bin/sed 's/:/ /g'`"
+SOURCECODE_MD5="`/bin/grep "^SOURCECODE_MD5" ${HOME}/runtime/application.dat | /bin/sed 's/SOURCECODE_MD5://g' | /bin/sed 's/:/ /g'`"
+SOURCECODE_SHA1="`/bin/grep "^SOURCECODE_SHA1" ${HOME}/runtime/application.dat | /bin/sed 's/SOURCECODE_SHA1://g' | /bin/sed 's/:/ /g'`"
 
 /usr/bin/wget https://${SOURCECODE_URL}
 /bin/echo "${0} `/bin/date`: Downloaded joomla from ${SOURCECODE_URL}" 
 
 verified_archive_type=""
-if ( [ "`/bin/echo ${SOURCECODE_URL} | /bin/grep '\.zip$`" != "" ] && ( [ "`/usr/bin/md5sum Joomla_*.zip | /usr/bin/awk '{print $1}'`" = "${SOURCECODE_MD5}" ] || [ "`/usr/bin/sha1sum Joomla_*.zip | /usr/bin/awk '{print $1}'`" = "${SOURCECODE_MD5}" ] ) )
+if ( [ "`/bin/echo ${SOURCECODE_URL} | /bin/grep '\.zip$'`" != "" ] && ( [ "`/usr/bin/md5sum Joomla_*.zip | /usr/bin/awk '{print $1}'`" = "${SOURCECODE_MD5}" ] || [ "`/usr/bin/sha1sum Joomla_*.zip | /usr/bin/awk '{print $1}'`" = "${SOURCECODE_SHA1}" ] ) )
 then
-  good_to_go="1"
-  verified_archive_type=".zip"
-elif ( [ "`/bin/echo ${SOURCECODE_URL} | /bin/grep '\.tar.gz$`" != "" ] && ( [ "`/usr/bin/md5sum Joomla_*.tar.gz | /usr/bin/awk '{print $1}'`" = "${SOURCECODE_MD5}" ] || [ "`/usr/bin/sha1sum Joomla_*.zip | /usr/bin/awk '{print $1}'`" = "${SOURCECODE_MD5}" ] ) )
+        good_to_go="1"
+        verified_archive_type="zip"
+elif ( [ "`/bin/echo ${SOURCECODE_URL} | /bin/grep '\.tar.gz$'`" != "" ] && ( [ "`/usr/bin/md5sum Joomla_*.tar.gz | /usr/bin/awk '{print $1}'`" = "${SOURCECODE_MD5}" ] || [ "`/usr/bin/sha1sum Joomla_*.zip | /usr/bin/awk '{print $1}'`" = "${SOURCECODE_SHA1}" ] ) )
 then
-  good_to_go="1"
-  verified_archive_type=".tar.gz"
+        good_to_go="1"
+        verified_archive_type="tar.gz"
 fi
 
 if ( [ "${verified_archive_type}" != "" ] )
 then
-  if ( [ "${verified_archive_type}" = ".zip" ] )
-  then
-    /usr/bin/python3 -m zipfile -e Joomla_*.${verified_archive_type} /var/www/html/ 
-  elif ( [ "${verified_archive_type}" = ".tar.gz" ] )
-  then
-    /bin/tar xvfz Joomla_*${verified_archive_type} -C /var/www/html/
-  fi
-  /bin/rm Joomla_*${verified_archive_type}
-  /bin/chown -R www-data:www-data /var/www/html/*
-  cd ${HOME}
-  /bin/echo "success"
+        if ( [ "${verified_archive_type}" = "zip" ] )
+        then
+                /usr/bin/python3 -m zipfile -e Joomla_*.${verified_archive_type} /var/www/html/ 
+        elif ( [ "${verified_archive_type}" = "tar.gz" ] )
+        then
+                /bin/tar xvfz Joomla_*${verified_archive_type} -C /var/www/html/
+        fi
+        /bin/rm Joomla_*${verified_archive_type}
+        /bin/chown -R www-data:www-data /var/www/html/*
+        cd ${HOME}
+        /bin/echo "success"
 fi
