8ffb8c034148689e3111a92322cbc16c1ebd5df5
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-03-05 10:03:59 +0000

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/joomla/InitialiseApplicationConfiguration.sh
+++ application/configuration/joomla/InitialiseApplicationConfiguration.sh
@@ -1,6 +1,6 @@
 set -x
 
-if ( [ "`/bin/grep "^INTERACTIVE_APPLICATION_INSTALL" ${HOME}/runtime/application.dat | /bin/sed 's/INTERACTIVE_APPLICATION_INSTALL://g' | /bin/sed 's/:/ /g'`" = "yes" ] )
+if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`" = "1" ] && [ "`/bin/grep "^INTERACTIVE_APPLICATION_INSTALL" ${HOME}/runtime/application.dat | /bin/sed 's/INTERACTIVE_APPLICATION_INSTALL://g' | /bin/sed 's/:/ /g'`" = "yes" ] )
 then
         exit
 fi
