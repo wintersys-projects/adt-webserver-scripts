2ae8771fc4ab064cb9c1cbed6f8159d0fdb1789c
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-03-08 17:18:22 +0000

    Update CheckIfApplicationIsInstalled.sh

--- application/configuration/wordpress/CheckIfApplicationIsInstalled.sh
+++ application/configuration/wordpress/CheckIfApplicationIsInstalled.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ###########################################################################################################
-# Description: Check if wordpress is installed 
+# Description:Check if a joomla application has been installed
 # Author : Peter Winter
 # Date: 17/05/2017
 ######################################################################################################
@@ -18,16 +18,28 @@
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 #######################################################################################################
 #######################################################################################################
-#set -x
+#set -x #do not set this during a live deployment the application will fail to install
 
 if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh APPLICATION:wordpress`" = "1" ] )
 then
-	if ( [ -f /var/www/html/wp-login.php ] && [ -d /var/www/html/wp-content ] && [ -f /var/www/html/wp-cron.php ] && [ -d /var/www/html/wp-admin ] && [ -d /var/www/html/wp-includes ] && [ -f /var/www/html/wp-settings.php ] )
-	then
-		if ( [ "`/usr/bin/find /var/www/html -type d -maxdepth 1 | /usr/bin/wc -l`" -gt "5" ] && [ "`/usr/bin/find /var/www/html -type f | /usr/bin/wc -l`" -gt "5" ] )
+	installed="1"
+	
+	directories="`/bin/grep "^APPLICATION_INTEGRITY_DIRECTORIES" ${HOME}/runtime/application.dat | /bin/sed 's/APPLICATION_INTEGRITY_DIRECTORIES://g' | /bin/sed 's/:/ /g'`"
+	for directory in ${directories}
+	do
+		if ( [ ! -d /var/www/html/${directory} ] )
 		then
-			installed="1"
+			installed="0"
 		fi
-	fi
+	done
+	
+	files="`/bin/grep "^APPLICATION_INTEGRITY_FILES" ${HOME}/runtime/application.dat | /bin/sed 's/APPLICATION_INTEGRITY_FILES://g' | /bin/sed 's/:/ /g'`"
+	for file in ${files}
+	do
+		if ( [ ! -f /var/www/html/${file} ] )
+		then
+			installed="0"
+		fi
+	done
 fi
 
