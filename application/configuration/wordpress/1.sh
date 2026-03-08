4d88aab2cda9564d9c73a56a3570028121f1c76e
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-03-08 17:49:41 +0000

    Update 1.sh

--- application/configuration/wordpress/1.sh
+++ application/configuration/wordpress/1.sh
@@ -1,19 +1,17 @@
 
-/var/www/html/wp-config-sample.php
+
 
 if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`" = "1" ] && [ "`/bin/grep "^INTERACTIVE_APPLICATION_INSTALL" ${HOME}/runtime/application.dat | /bin/sed 's/INTERACTIVE_APPLICATION_INSTALL://g' | /bin/sed 's/:/ /g'`" = "yes" ] )
 then
         exit
 fi
 
-if ( [ -f /var/www/html/configuration.php ] )
+if ( [ -f /var/www/html/wp-config.php ] )
 then
-        /bin/rm /var/www/html/configuration.php
+        /bin/rm /var/www/html/wp-config.php
 fi
 
-if ( [ -f /var/www/html/installation/configuration.php-dist ] )
+if ( [ -f /var/www/html/wp-config-sample.php ] )
 then
-        /bin/cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+        /bin/cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php.default
 fi
-
-/bin/cp /var/www/html/configuration.php.default ${HOME}/runtime/configuration.php
