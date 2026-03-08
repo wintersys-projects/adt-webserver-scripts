50f10b29dfdfb729544991091083a737d3a762dc
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-03-08 17:53:42 +0000

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/wordpress/InitialiseApplicationConfiguration.sh
+++ application/configuration/wordpress/InitialiseApplicationConfiguration.sh
@@ -15,3 +15,23 @@ if ( [ -f /var/www/html/wp-config-sample.php ] )
 then
         /bin/cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php.default
 fi
+
+if ( [ -f ${HOME}/runtime/application.dat ] )
+then
+        # We need our database prefix because that will be what is used in the database dump
+        while ( [ ! -f /var/www/html/dbp.dat ] || [ "`/bin/cat  ${HOME}/runtime/wp-config.php`" = "" ] )
+        do
+                dbprefix="`/bin/grep "dbprefix"  ${HOME}/runtime/wp-config.php | /usr/bin/awk -F"'" '{print $2}'`"
+
+                if ( [ "${dbprefix}" = "wp_" ] )
+                then
+                        dbprefix="`/usr/bin/tr -dc a-z0-9 </dev/urandom | /usr/bin/head -c 5; /bin/echo`_"
+                fi
+                /bin/echo ${dbprefix} > /var/www/html/dbp.dat
+                /bin/chown www-data:www-data /var/www/html/dbp.dat
+                /bin/chmod 600 /var/www/html/dbp.dat
+        done
+
+        dbprefix="`/bin/cat /var/www/html/dbp.dat`"
+        salt="`/usr/bin/curl https://api.wordpress.org/secret-key/1.1/salt`"
+
