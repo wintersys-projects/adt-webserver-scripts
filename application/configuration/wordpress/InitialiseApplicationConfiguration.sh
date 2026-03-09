c8a9574cd079d58976a5c5472a206201f49d1b8d
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-03-09 09:56:39 +0000

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/wordpress/InitialiseApplicationConfiguration.sh
+++ application/configuration/wordpress/InitialiseApplicationConfiguration.sh
@@ -1,7 +1,4 @@
-if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`" = "1" ] && [ "`/bin/grep "^INTERACTIVE_APPLICATION_INSTALL" ${HOME}/runtime/application.dat | /bin/sed 's/INTERACTIVE_APPLICATION_INSTALL://g' | /bin/sed 's/:/ /g'`" = "yes" ] )
-then
-        exit
-fi
+#set -x
 
 if ( [ -f /var/www/html/wp-config.php ] )
 then
