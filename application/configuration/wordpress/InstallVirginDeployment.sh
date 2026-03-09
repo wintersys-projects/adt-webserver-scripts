a677afa767ee04b96336caff33140a023d295021
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-03-09 10:06:18 +0000

    Update InstallVirginDeployment.sh

--- application/configuration/wordpress/InstallVirginDeployment.sh
+++ application/configuration/wordpress/InstallVirginDeployment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-#################################################################################
-# Description: This script will obtain and extract the sourcecode for wordpress into 
+#####################################################################################
+# Description: This script will obtain and extract the sourcecode for joomla into 
 # the webroot directory
 # Author: Peter Winter
 # Date: 04/01/2017
-##################################################################################
+######################################################################################
 # License Agreement:
 # This file is part of The Agile Deployment Toolkit.
 # The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
@@ -17,24 +17,47 @@
 # GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
-##############################################################################
-##############################################################################
+######################################################################################
+######################################################################################
 #set -x
 
-BUILDOS="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
-${HOME}/installscripts/InstallWPCLI.sh ${BUILDOS}
-
-cd /var/www/html
-/usr/bin/wget http://wordpress.org/latest.tar.gz
-/bin/tar xvfz latest.tar.gz
-/bin/rm latest.tar.gz
-if ( [ -d /var/www/html/wp-content ] )
+if ( [ ! -d ${HOME}/runtime/downloads_work_area ] )
 then
-	/bin/mv /var/www/html/wordpress/wp-content/* /var/www/html/wp-content
- 	/bin/rmdir /var/www/html/wordpress/wp-content
+        /bin/mkdir -p ${HOME}/runtime/downloads_work_area
 fi
-/bin/mv /var/www/html/wordpress/* /var/www/html
-/bin/rmdir /var/www/html/wordpress
-/bin/chown -R www-data:www-data /var/www/html/*
-cd /home/${SERVER_USER}
-/bin/echo "success"
+
+cd ${HOME}/runtime/downloads_work_area
+SOURCECODE_URL="`/bin/grep "^SOURCECODE_URL" ${HOME}/runtime/application.dat | /bin/sed 's/SOURCECODE_URL://g' | /bin/sed 's/:/ /g'`"
+SOURCECODE_MD5="`/bin/grep "^SOURCECODE_MD5" ${HOME}/runtime/application.dat | /bin/sed 's/SOURCECODE_MD5://g' | /bin/sed 's/:/ /g'`"
+SOURCECODE_SHA1="`/bin/grep "^SOURCECODE_SHA1" ${HOME}/runtime/application.dat | /bin/sed 's/SOURCECODE_SHA1://g' | /bin/sed 's/:/ /g'`"
+
+/usr/bin/wget https://${SOURCECODE_URL}
+/bin/echo "${0} `/bin/date`: Downloaded wordpress from ${SOURCECODE_URL}" 
+
+verified_archive_type=""
+if ( [ "`/bin/echo ${SOURCECODE_URL} | /bin/grep '\.zip$'`" != "" ] && ( [ "`/usr/bin/md5sum wordpress-*.zip | /usr/bin/awk '{print $1}'`" = "${SOURCECODE_MD5}" ] || [ "`/usr/bin/sha1sum wordpress-*.zip | /usr/bin/awk '{print $1}'`" = "${SOURCECODE_SHA1}" ] ) )
+then
+        good_to_go="1"
+        verified_archive_type="zip"
+elif ( [ "`/bin/echo ${SOURCECODE_URL} | /bin/grep '\.tar.gz$'`" != "" ] && ( [ "`/usr/bin/md5sum wordpress-*.tar.gz | /usr/bin/awk '{print $1}'`" = "${SOURCECODE_MD5}" ] || [ "`/usr/bin/sha1sum wordpress-*.zip | /usr/bin/awk '{print $1}'`" = "${SOURCECODE_SHA1}" ] ) )
+then
+        good_to_go="1"
+        verified_archive_type="tar.gz"
+fi
+
+if ( [ "${verified_archive_type}" != "" ] )
+then
+        if ( [ "${verified_archive_type}" = "zip" ] )
+        then
+                /usr/bin/python3 -m zipfile -e wordpress-*.${verified_archive_type} /var/www/html/ 
+        elif ( [ "${verified_archive_type}" = "tar.gz" ] )
+        then
+                /bin/tar xvfz wordpress-*.${verified_archive_type} -C /var/www/html/
+        fi
+        /bin/mv /var/www/html/wordpress/* /var/www/html
+        /bin/rm -r /var/www/html/wordpress
+        /bin/rm wordpress-*.${verified_archive_type}
+        /bin/chown -R www-data:www-data /var/www/html/*
+        cd ${HOME}
+        /bin/echo "success"
+fi
