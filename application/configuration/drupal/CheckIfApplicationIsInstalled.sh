1e8a2e66e92a104f5185ce38565beeb93d4c6248
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-03-10 09:11:58 +0000

    Update CheckIfApplicationIsInstalled.sh

--- application/configuration/drupal/CheckIfApplicationIsInstalled.sh
+++ application/configuration/drupal/CheckIfApplicationIsInstalled.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ###########################################################################################################
-# Description: Check if a drupal application has been installed
+# Description:Check if a drupal application has been installed
 # Author : Peter Winter
 # Date: 17/05/2017
 ######################################################################################################
@@ -18,15 +18,27 @@
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 #######################################################################################################
 #######################################################################################################
-#set -x
+#set -x #do not set this during a live deployment the application will fail to install
 
 if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh APPLICATION:drupal`" = "1" ] )
 then
-	if ( [ -f /var/www/html/core/misc/drupal.js ] && [ -d /var/www/html/themes ] && [ -d /var/www/html/modules ] && [ -d /var/www/html//profiles ] )
-	then
-		if ( [ "`/usr/bin/find /var/www/html -maxdepth 1 -type d | /usr/bin/wc -l`" -gt "5" ] && [ "`/usr/bin/find /var/www/html -type f | /usr/bin/wc -l`" -gt "5" ] )
+	installed="1"
+	
+	directories="`/bin/grep "^APPLICATION_INTEGRITY_DIRECTORIES" ${HOME}/runtime/application.dat | /bin/sed 's/APPLICATION_INTEGRITY_DIRECTORIES://g' | /bin/sed 's/:/ /g'`"
+	for directory in ${directories}
+	do
+		if ( [ ! -d /var/www/html/${directory} ] )
 		then
-			installed="1"
+			installed="0"
 		fi
-	fi
+	done
+	
+	files="`/bin/grep "^APPLICATION_INTEGRITY_FILES" ${HOME}/runtime/application.dat | /bin/sed 's/APPLICATION_INTEGRITY_FILES://g' | /bin/sed 's/:/ /g'`"
+	for file in ${files}
+	do
+		if ( [ ! -f /var/www/html/${file} ] )
+		then
+			installed="0"
+		fi
+	done
 fi
