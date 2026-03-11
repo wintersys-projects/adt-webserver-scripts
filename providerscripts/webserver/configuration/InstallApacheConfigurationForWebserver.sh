2ecc55760bcee690d98dae06b981393616ddf30b
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-03-11 13:23:28 +0000

    Update InstallApacheConfigurationForWebserver.sh

--- providerscripts/webserver/configuration/InstallApacheConfigurationForWebserver.sh
+++ providerscripts/webserver/configuration/InstallApacheConfigurationForWebserver.sh
@@ -78,11 +78,6 @@ else
 	/bin/sed -i "s/#XXXXFASTCGISOCKETXXXX//g" ${HOME}/providerscripts/webserver/configuration/application/apache/site-available.conf
 fi
 
-if ( [ "${APPLICATION}" = "moodle" ] )
-then
-        /bin/sed 's;DocumentRoot /var/www/html;DocumentRoot /var/www/html/public;' ${HOME}/providerscripts/webserver/configuration/application/apache/site-available.conf
-fi
-
 /bin/sed -i "s/#XXXX${APPLICATION}XXXX//g" ${HOME}/providerscripts/webserver/configuration/application/apache/site-available.conf
 /bin/sed -i '/#XXXX.*/d' ${HOME}/providerscripts/webserver/configuration/application/apache/site-available.conf
 /bin/cat -s ${HOME}/providerscripts/webserver/configuration/application/apache/site-available.conf > /etc/apache2/sites-available/${WEBSITE_NAME}
