fa77fd8ae427557d495b73c137f4bb9f9f3e0de2
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-03-12 13:04:24 +0000

    Update InstallVirginDeployment.sh

--- application/configuration/joomla/InstallVirginDeployment.sh
+++ application/configuration/joomla/InstallVirginDeployment.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 #####################################################################################
-# Description: This script will obtain and extract the sourcecode for joomla into 
-# the webroot directory
+# Description: This script will download and unpack joomla. The source url for which
+# version of joomla to use is set in  
+# ${BUILD_HOME}/application/descriptors/joomla.dat
+# And this can be set to any valid URL of your choosing which includes alpha, beta and
+# release candidate archives of joomla.
+# Tar achives and zip archives are supported and which is used depends on the setting in
+# ${BUILD_HOME}/application/descriptors/joomla.dat. 
+# The archives have checksum verifications applied so you have to supply the expected
+# and valid checksum(s) for your archive in 
+# ${BUILD_HOME}/application/descriptors/joomla.dat.
 # Author: Peter Winter
 # Date: 04/01/2017
 ######################################################################################
