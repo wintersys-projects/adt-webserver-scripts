5a963e745060aeed5add28b69862b8a7e7b2915d
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-03-12 09:21:41 +0000

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/joomla/InitialiseApplicationConfiguration.sh
+++ application/configuration/joomla/InitialiseApplicationConfiguration.sh
@@ -1,4 +1,4 @@
-set -x
+#set -x
 
 if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`" = "1" ] && [ "`/bin/grep "^INTERACTIVE_APPLICATION_INSTALL" ${HOME}/runtime/application.dat | /bin/sed 's/INTERACTIVE_APPLICATION_INSTALL://g' | /bin/sed 's/:/ /g'`" = "yes" ] )
 then
@@ -171,60 +171,7 @@ then
                         fi
                 fi
         
-            #    if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:Maria`" = "1" ] || [ "`${HOME}/utilities/config/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:MySQL`" = "1" ] )
-            #    then
-            #            /bin/cat /var/www/html/installation/sql/mysql/base.sql | /bin/sed "s/#__/${dbprefix}/g" > /var/www/html/installation/sql/mysql/base_with_dbprefix.sql
-            #            /bin/cat /var/www/html/installation/sql/mysql/extensions.sql | /bin/sed "s/#__/${dbprefix}/g" > /var/www/html/installation/sql/mysql/extensions_with_dbprefix.sql
-            #            /bin/cat /var/www/html/installation/sql/mysql/supports.sql | /bin/sed "s/#__/${dbprefix}/g" > /var/www/html/installation/sql/mysql/supports_with_dbprefix.sql
-#
- #                       ${HOME}/utilities/remote/ConnectToRemoteMySQL.sh < /var/www/html/installation/sql/mysql/base_with_dbprefix.sql 
-  #                      ${HOME}/utilities/remote/ConnectToRemoteMySQL.sh < /var/www/html/installation/sql/mysql/extensions_with_dbprefix.sql 
-   #                     ${HOME}/utilities/remote/ConnectToRemoteMySQL.sh < /var/www/html/installation/sql/mysql/supports_with_dbprefix.sql 
-#
-#
- #                       username="`/bin/grep "^APPLICATION_USERNAME" ${HOME}/runtime/application.dat | /bin/sed 's/APPLICATION_USERNAME://g' | /bin/sed 's/:/ /g'`"
-  #                      password="`/bin/grep "^APPLICATION_PASSWORD_HASH" ${HOME}/runtime/application.dat | /bin/sed 's/APPLICATION_PASSWORD_HASH://g' | /bin/sed 's/:/ /g'`"
-   #                     descriptive_name="`/bin/grep "^APPLICATION_DESCRIPTIVE_USERNAME" ${HOME}/runtime/application.dat | /bin/sed 's/APPLICATION_DESCRIPTIVE_USERNAME://g' | /bin/sed 's/:/ /g'`"
-#
- #                       /bin/echo "INSERT INTO \`${dbprefix}users\` (\`name\`, \`username\`, \`password\`, \`params\`, \`registerDate\`, \`lastvisitDate\`, \`lastResetTime\`) VALUES ('"${descriptive_name}"', '"${username}"','"${password}"', '', NOW(), NOW(), NOW());" > /var/www/html/installation/sql/mysql/user_with_dbprefix.sql 
-  #                      /bin/echo "INSERT INTO \`${dbprefix}user_usergroup_map\` (\`user_id\`,\`group_id\`) VALUES (LAST_INSERT_ID(),'8');" >> /var/www/html/installation/sql/mysql/user_with_dbprefix.sql 
-#
- #                       ${HOME}/utilities/remote/ConnectToRemoteMySQL.sh < /var/www/html/installation/sql/mysql/user_with_dbprefix.sql  
-#
- #                       extension_id="`${HOME}/utilities/remote/ConnectToRemoteMySQL.sh "select extension_id,name from ${dbprefix}extensions where name='files_joomla';" | /bin/grep 'files_joomla' | /usr/bin/awk '{print $1}'`"
-  #                      version_id="`/bin/ls /var/www/html/administrator/components/com_admin/sql/updates/mysql | /usr/bin/tail -n -1`"
-   #                     /bin/echo "INSERT INTO \`${dbprefix}schemas\` (\`extension_id\`, \`version_id\`) VALUES (${extension_id}, '"${version_id}"');" > /var/www/html/installation/sql/mysql/noninteractive_fudge_with_dbprefix.sql
-    #                    ${HOME}/utilities/remote/ConnectToRemoteMySQL.sh < /var/www/html/installation/sql/mysql/noninteractive_fudge_with_dbprefix.sql
-     #           elif ( [ "`${HOME}/utilities/config/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:Postgres`" = "1" ] )
-      #          then
-       #                 /bin/cat /var/www/html/installation/sql/postgresql/base.sql | /bin/sed "s/#__/${dbprefix}/g" > /var/www/html/installation/sql/postgresql/base_with_dbprefix.psql
-        #                /bin/cat /var/www/html/installation/sql/postgresql/extensions.sql | /bin/sed "s/#__/${dbprefix}/g" > /var/www/html/installation/sql/postgresql/extensions_with_dbprefix.psql
-         #               /bin/cat /var/www/html/installation/sql/postgresql/supports.sql | /bin/sed "s/#__/${dbprefix}/g" > /var/www/html/installation/sql/postgresql/supports_with_dbprefix.psql
-#
- #                       ${HOME}/utilities/remote/ConnectToRemotePostgres.sh < /var/www/html/installation/sql/postgresql/base_with_dbprefix.psql 
-  #                      ${HOME}/utilities/remote/ConnectToRemotePostgres.sh < /var/www/html/installation/sql/postgresql/extensions_with_dbprefix.psql 
-   #                     ${HOME}/utilities/remote/ConnectToRemotePostgres.sh < /var/www/html/installation/sql/postgresql/supports_with_dbprefix.psql 
-#
- #                       username="`/bin/grep "^APPLICATION_USERNAME" ${HOME}/runtime/application.dat | /bin/sed 's/APPLICATION_USERNAME://g' | /bin/sed 's/:/ /g'`"
-  #                      password="`/bin/grep "^APPLICATION_PASSWORD_HASH" ${HOME}/runtime/application.dat | /bin/sed 's/APPLICATION_PASSWORD_HASH://g' | /bin/sed 's/:/ /g'`"
-   #                     descriptive_name="`/bin/grep "^APPLICATION_DESCRIPTIVE_USERNAME" ${HOME}/runtime/application.dat | /bin/sed 's/APPLICATION_DESCRIPTIVE_USERNAME://g' | /bin/sed 's/:/ /g'`"
-#
- #                       /bin/echo 'INSERT INTO '${dbprefix}'users (name, username,  password, params, "registerDate", "lastvisitDate", "lastResetTime") VALUES ( '\'${descriptive_name}\'', '\'${username}\'', '\'${password}\'', '"''"', now(), now(), now()) RETURNING id;' > /var/www/html/installation/sql/postgresql/user_with_dbprefix.psql 
-#                        returning_id="`${HOME}/utilities/remote/ConnectToRemotePostgres.sh < /var/www/html/installation/sql/postgresql/user_with_dbprefix.psql | /bin/grep -oE '[0-9]+' | /bin/sed -n '1p' | /bin/sed 's/ //g'`"
-#
- #                       /bin/echo "INSERT INTO ${dbprefix}user_usergroup_map (user_id, group_id) VALUES (${returning_id},'8');" > /var/www/html/installation/sql/postgresql/user_with_dbprefix.psql 
-#
- #                       ${HOME}/utilities/remote/ConnectToRemotePostgres.sh < /var/www/html/installation/sql/postgresql/user_with_dbprefix.psql
-#
- #                       extension_id="`${HOME}/utilities/remote/ConnectToRemotePostgres.sh "select extension_id,name from ${dbprefix}extensions where name='files_joomla';" | /bin/grep 'files_joomla' | /usr/bin/awk '{print $1}' | /usr/bin/tail -n -1`"
-  #                      version_id="`/bin/ls /var/www/html/administrator/components/com_admin/sql/updates/postgresql | /usr/bin/tail -n -1`"
-   #                     /bin/echo "INSERT INTO ${dbprefix}schemas (extension_id, version_id) VALUES (${extension_id}, '"${version_id}"');" > /var/www/html/installation/sql/postgresql/noninteractive_fudge_with_dbprefix.psql
-    #                    ${HOME}/utilities/remote/ConnectToRemotePostgres.sh < /var/www/html/installation/sql/postgresql/noninteractive_fudge_with_dbprefix.psql
-#
- #               fi
-
                 cd /var/www/html
-
                 website_name="`/bin/grep "^WEBSITE_NAME:" ${HOME}/runtime/application.dat | /usr/bin/awk -F':' '{print $NF}'`"
                 website_username="`/bin/grep "^WEBSITE_USERNAME:" ${HOME}/runtime/application.dat | /usr/bin/awk -F':' '{print $NF}'`"
                 website_password="`/bin/grep "^WEBSITE_PASSWORD:" ${HOME}/runtime/application.dat | /usr/bin/awk -F':' '{print $NF}'`"
@@ -251,12 +198,6 @@ then
                         /bin/chown www-data:www-data /var/www/html/configuration.php
                         /bin/touch ${HOME}/runtime/INITIAL_CONFIG_SET
                 fi
-
-
-#               if ( [ -d /var/www/html/installation ] )
- #              then
-  #                     /bin/rm -r /var/www/html/installation
-   #            fi
         fi
 fi
 
