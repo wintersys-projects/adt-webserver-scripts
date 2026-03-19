51c9a72c9de5cfd4899524f8cde44482eec67137
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-03-19 17:45:14 +0000

    Update InstallLighttpdConfigurationForWebserver.sh

--- providerscripts/webserver/configuration/InstallLighttpdConfigurationForWebserver.sh
+++ providerscripts/webserver/configuration/InstallLighttpdConfigurationForWebserver.sh
@@ -18,7 +18,7 @@
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 ####################################################################################
 ####################################################################################
-#set -x
+set -x
 
 HOME="`/bin/cat /home/homedir.dat`"
 PHP_VERSION="`${HOME}/utilities/config/ExtractConfigValue.sh 'PHPVERSION'`"
