fc6981022ca7a31ab89195bc01aba8583a95176d
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-03-19 12:29:52 +0000

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/wordpress/InitialiseApplicationConfiguration.sh
+++ application/configuration/wordpress/InitialiseApplicationConfiguration.sh
@@ -38,7 +38,7 @@ if ( !defined('ABSPATH') )
 /** Location of your WordPress configuration. */
 require_once(ABSPATH . '../wp-config.php');" > /var/www/html/wordpress/wp-config.php
 /bin/chown www-data:www-data /var/www/html/wordpress/wp-config.php
-/bin/chown 600 /var/www/html/wordpress/wp-config.php
+/bin/chown 440 /var/www/html/wordpress/wp-config.php
 
 if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`" = "1" ] && [ "`/bin/grep "^INTERACTIVE_APPLICATION_INSTALL" ${HOME}/runtime/application.dat | /bin/sed 's/INTERACTIVE_APPLICATION_INSTALL://g' | /bin/sed 's/:/ /g'`" = "yes" ] )
 then
