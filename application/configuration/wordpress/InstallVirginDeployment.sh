0e7da3b63b81a84cbbc7c5b8d9bb2ccc2bdd28a6
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-03-20 18:40:47 +0000

    Update InstallVirginDeployment.sh

--- application/configuration/wordpress/InstallVirginDeployment.sh
+++ application/configuration/wordpress/InstallVirginDeployment.sh
@@ -67,8 +67,16 @@ then
         then
                 /bin/tar xvfz wordpress.${verified_archive_type} -C /var/www/html/
         fi
-      #  /bin/mv /var/www/html/wordpress/* /var/www/html
-      #  /bin/rm -r /var/www/html/wordpress
+        webroot_directory="`/bin/grep "^WEBROOT_DIRECTORY:" ${HOME}/runtime/application.dat | /usr/bin/awk -F':' '{print $NF}'`"
+
+        if ( [ "${webroot_directory}" != "" ] )
+        then
+                /bin/mkdir -p ${webroot_directory}
+                /bin/chown www-data:www-data ${webroot_directory}
+                /bin/chmod 755 ${webroot_directory}
+                /bin/mv /var/www/html/wordpress/* /${webroot_directory}
+                /bin/rm -r /var/www/html/wordpress
+        fi
         /bin/rm wordpress.${verified_archive_type}
         /bin/chown -R www-data:www-data /var/www/html/*
         cd ${HOME}
