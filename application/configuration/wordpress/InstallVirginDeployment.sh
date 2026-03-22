4c4d7f53530465ba07badac75e2fcad18db99e33
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-03-22 20:14:20 +0000

    Update InstallVirginDeployment.sh

--- application/configuration/wordpress/InstallVirginDeployment.sh
+++ application/configuration/wordpress/InstallVirginDeployment.sh
@@ -19,14 +19,8 @@
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 ######################################################################################
 ######################################################################################
-#set -x
+set -x
 
-if ( [ ! -d ${HOME}/runtime/downloads_work_area ] )
-then
-        /bin/mkdir -p ${HOME}/runtime/downloads_work_area
-fi
-
-cd ${HOME}/runtime/downloads_work_area
 SOURCECODE_URL="`/bin/grep "^SOURCECODE_URL" ${HOME}/runtime/application.dat | /bin/sed 's/SOURCECODE_URL://g' | /bin/sed 's/:/ /g'`"
 SOURCECODE_MD5="`/bin/grep "^SOURCECODE_MD5" ${HOME}/runtime/application.dat | /bin/sed 's/SOURCECODE_MD5://g' | /bin/sed 's/:/ /g'`"
 SOURCECODE_SHA1="`/bin/grep "^SOURCECODE_SHA1" ${HOME}/runtime/application.dat | /bin/sed 's/SOURCECODE_SHA1://g' | /bin/sed 's/:/ /g'`"
