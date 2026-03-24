8a3d1d5aeb98d86a35050a92024ed9c9c5566289
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-03-24 09:52:12 +0000

    Update InstallWPCLI.sh

--- installscripts/InstallWPCLI.sh
+++ installscripts/InstallWPCLI.sh
@@ -1,14 +1,90 @@
-
+#!/bin/sh
+######################################################################################################
+# Description: This script will install wpcli
+# Author: Peter Winter
+# Date: 17/01/2017
+#######################################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+#######################################################################################################
+#######################################################################################################
+#set -x
 
 HOME="`/bin/cat /home/homedir.dat`"
-BUILDOS="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
-${HOME}/installscripts/InstallGnuPG.sh
-/usr/bin/curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-/usr/bin/curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar.asc
-/usr/bin/curl -L https://raw.githubusercontent.com/wp-cli/builds/gh-pages/wp-cli.pgp | gpg --import
-/usr/bin/gpg --verify wp-cli.phar.asc wp-cli.phar
-/bin/chmod +x wp-cli.phar
-/bin/mv wp-cli.phar /usr/local/bin/wp
 
-# Verify installation
-/usr/local/bin/wp --info
+if ( [ "${1}" != "" ] )
+then
+	buildos="${1}"
+fi
+
+if ( [ "${buildos}" = "" ] )
+then
+	BUILDOS="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
+else 
+	BUILDOS="${buildos}"
+fi
+
+
+apt=""
+if ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt" ] )
+then
+	apt="/usr/bin/apt"
+elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt-get" ] )
+then
+	apt="/usr/bin/apt-get"
+fi
+
+export DEBIAN_FRONTEND=noninteractive
+install_command="${apt} -o DPkg::Lock::Timeout=-1 -o Dpkg::Use-Pty=0 -qq -y install " 
+${HOME}/installscripts/InstallGnuPG.sh
+
+count="0"
+while ( [ ! -x /usr/local/bin/wp ] && [ "${count}" -lt "5" ] )
+do
+	if ( [ "${apt}" != "" ] )
+	then
+		if ( [ "${BUILDOS}" = "ubuntu" ] )
+		then
+			/usr/bin/curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+			/usr/bin/curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar.asc
+			/usr/bin/curl -L https://raw.githubusercontent.com/wp-cli/builds/gh-pages/wp-cli.pgp | gpg --import
+			/usr/bin/gpg --verify wp-cli.phar.asc wp-cli.phar
+			/bin/chmod +x wp-cli.phar	
+			/bin/mv wp-cli.phar /usr/local/bin/wp
+	
+			# Verify installation
+			/usr/local/bin/wp --info
+		fi
+
+		if ( [ "${BUILDOS}" = "debian" ] )
+		then    
+			/usr/bin/curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+			/usr/bin/curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar.asc
+			/usr/bin/curl -L https://raw.githubusercontent.com/wp-cli/builds/gh-pages/wp-cli.pgp | gpg --import
+			/usr/bin/gpg --verify wp-cli.phar.asc wp-cli.phar
+			/bin/chmod +x wp-cli.phar	
+			/bin/mv wp-cli.phar /usr/local/bin/wp
+	
+			# Verify installation
+			/usr/local/bin/wp --info
+		fi
+	fi
+	count="`/usr/bin/expr ${count} + 1`"
+done
+
+if ( [ ! -x /usr/local/bin/wp ] && [ "${count}" = "5" ] )
+then
+	${HOME}/providerscripts/email/SendEmail.sh "INSTALLATION ERROR wp-cli" "I believe that wp-cli hasn't installed correctly, please investigate" "ERROR"
+else
+	/bin/touch ${HOME}/runtime/installedsoftware/InstallWPCLI.sh				
+fi
