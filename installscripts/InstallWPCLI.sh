4a385d12862eecb48ea994617cb5b8b3c7f17cd5
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-03-24 09:43:23 +0000

    Create InstallWPCLI.sh

---
+++ installscripts/InstallWPCLI.sh
@@ -0,0 +1,10 @@
+
+/usr/bin/curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+/usr/bin/curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar.asc
+/usr/bin/curl -L https://raw.githubusercontent.com/wp-cli/builds/gh-pages/wp-cli.pgp | gpg --import
+/usr/bin/gpg --verify wp-cli.phar.asc wp-cli.phar
+/bin/chmod +x wp-cli.phar
+/bin/mv wp-cli.phar /usr/local/bin/wp
+
+# Verify installation
+/usr/local/bin/wp --info
