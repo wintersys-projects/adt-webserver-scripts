d1a7d1c69b4adcf250d3a69c9b4b5aed1d0e3b07
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-03-24 09:47:23 +0000

    Update InstallWPCLI.sh

--- installscripts/InstallWPCLI.sh
+++ installscripts/InstallWPCLI.sh
@@ -1,4 +1,6 @@
 
+
+${HOME}/installscripts/InstallGnuPG.sh
 /usr/bin/curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 /usr/bin/curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar.asc
 /usr/bin/curl -L https://raw.githubusercontent.com/wp-cli/builds/gh-pages/wp-cli.pgp | gpg --import
