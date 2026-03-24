d33f9a03f6cdd96c59131d45481918e26f78d2b3
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-03-24 10:39:11 +0000

    Update InstallVirginDeployment.sh

--- application/configuration/wordpress/InstallVirginDeployment.sh
+++ application/configuration/wordpress/InstallVirginDeployment.sh
@@ -21,6 +21,9 @@
 ######################################################################################
 #set -x
 
+BUILDOS="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
+${HOME}/installscripts/InstallWPCLI.sh ${BUILDOS}
+
 webroot_directory="`/bin/grep "^WEBROOT_DIRECTORY:" ${HOME}/runtime/application.dat | /usr/bin/awk -F':' '{print $NF}'`"
 
 if ( [ "${webroot_directory}" = "" ] )
