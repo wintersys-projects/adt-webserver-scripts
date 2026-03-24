3a33b6af3f655e800b97bbcd087712376f7dc530
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-03-24 10:07:25 +0000

    Update InstallVirginDeployment.sh

--- application/configuration/wordpress/InstallVirginDeployment.sh
+++ application/configuration/wordpress/InstallVirginDeployment.sh
@@ -1,1 +1,24 @@
-sudo -u www-data /usr/local/bin/wp core download --version=latest --path=/tmp --force
+#!/bin/sh
+#####################################################################################
+# Description: This script will obtain and extract the sourcecode for joomla into 
+# the webroot directory
+# Author: Peter Winter
+# Date: 04/01/2017
+######################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+######################################################################################
+######################################################################################
+#set -x
+
+/usr/bin/sudo -u www-data /usr/local/bin/wp core download --version=latest --path=/tmp --force
