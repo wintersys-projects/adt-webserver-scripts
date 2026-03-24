91559d3560642f3b95461cc24c11222cb3fe7ed5
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-03-24 09:58:53 +0000

    Update InstallWPCLI.sh

--- installscripts/InstallWPCLI.sh
+++ installscripts/InstallWPCLI.sh
@@ -57,7 +57,7 @@ do
 		then
 			/usr/bin/curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 			/usr/bin/curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar.asc
-			/usr/bin/curl -L https://raw.githubusercontent.com/wp-cli/builds/gh-pages/wp-cli.pgp | gpg --import
+			/usr/bin/curl -L https://raw.githubusercontent.com/wp-cli/builds/gh-pages/wp-cli.pgp | /usr/bin/gpg --import
 			/usr/bin/gpg --verify wp-cli.phar.asc wp-cli.phar
 			/bin/chmod +x wp-cli.phar	
 			/bin/mv wp-cli.phar /usr/local/bin/wp
@@ -70,7 +70,7 @@ do
 		then    
 			/usr/bin/curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 			/usr/bin/curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar.asc
-			/usr/bin/curl -L https://raw.githubusercontent.com/wp-cli/builds/gh-pages/wp-cli.pgp | gpg --import
+			/usr/bin/curl -L https://raw.githubusercontent.com/wp-cli/builds/gh-pages/wp-cli.pgp | /usr/bin/gpg --import
 			/usr/bin/gpg --verify wp-cli.phar.asc wp-cli.phar
 			/bin/chmod +x wp-cli.phar	
 			/bin/mv wp-cli.phar /usr/local/bin/wp
