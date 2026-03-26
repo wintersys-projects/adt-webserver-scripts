a9a5a8ac2ce40c5c0efcda2f1f0ab5cc5a79ea48
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-03-26 12:05:25 +0000

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/drupal/InitialiseApplicationConfiguration.sh
+++ application/configuration/drupal/InitialiseApplicationConfiguration.sh
@@ -50,7 +50,7 @@ config_file="`/bin/grep "^CONFIG_FILE:" ${HOME}/runtime/application.dat | /usr/b
 
 if ( [ "${config_file}" = "" ] )
 then
-        config_file="/var/www/html/drupal/settings.php"
+        config_file="/var/www/html/settings.php"
 fi
 
 if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`" = "1" ] && [ "`/bin/grep "^INTERACTIVE_APPLICATION_INSTALL" ${HOME}/runtime/application.dat | /bin/sed 's/INTERACTIVE_APPLICATION_INSTALL://g' | /bin/sed 's/:/ /g'`" = "yes" ] )
