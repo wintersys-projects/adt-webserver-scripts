5f3ffda47cee5e0ae631a097891ad9bc35207b4b
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-03-29 09:38:54 +0100

    Refactor RemoveApplicationBranding.sh for clarity
    Updated the script to clarify its purpose and improve variable handling for website branding.

--- application/branding/RemoveApplicationBranding.sh
+++ application/branding/RemoveApplicationBranding.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 ###########################################################################################################
-# Description: This script will remove application specific branding from an application's webroot
-# sourcecode
-# Author: Peter Winter
-# Date: 05/02/2017
-###########################################################################################################
+# Description: When we baseline an appplication we have developed we don't want to be stuck with the
+# branding that we gave it during development so this script removes all the application's branding and 
+# replaces it with generic placeholders which can then be substituted for when the baseline is deployed
+# by a 3rd party
+# Author : Peter Winter
+# Date: 17/05/2017
+######################################################################################################
 # License Agreement:
 # This file is part of The Agile Deployment Toolkit.
 # The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
@@ -19,16 +21,18 @@
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 #######################################################################################################
 #######################################################################################################
-#set -x
+
 HOME="`/bin/cat /home/homedir.dat`"
 
-for applicationdir in `/bin/ls -d ${HOME}/application/branding/*/`
-do
-	applicationname="`/bin/echo ${applicationdir} | /bin/sed 's/\/$//' | /usr/bin/awk -F'/' '{print $NF}'`"
-	if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh APPLICATION:${applicationname}`" = "1" ] )
-	then
-		${applicationdir}RemoveApplicationBranding.sh
-	fi
-done
+WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
+ROOT_DOMAIN="`/bin/echo ${WEBSITE_URL} | /usr/bin/awk -F'.' '{$1=""}1' | /bin/sed 's/^ //g' | /bin/sed 's/ /./g'`"
+WEBSITE_DISPLAY_NAME="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEDISPLAYNAME'`"
+WEBSITE_DISPLAY_NAME_UPPER="`/bin/echo ${WEBSITE_DISPLAY_NAME} | /usr/bin/tr '[:lower:]' '[:upper:]'`"
+WEBSITE_DISPLAY_NAME_LOWER="`/bin/echo ${WEBSITE_DISPLAY_NAME} | /usr/bin/tr '[:upper:]' '[:lower:]'`"
+WEBSITE_DISPLAY_NAME_FIRST="`/bin/echo ${WEBSITE_DISLAY_NAME_LOWER} | /bin/sed -e 's/\b\(.\)/\u\1/g'`"
+domainspecifier="`/bin/echo ${WEBSITE_URL} | /usr/bin/awk -F'.' '{ for(i = 1; i <= NF; i++) { print $i; } }' | /usr/bin/cut -c1-3 | /usr/bin/tr '\n' '-' | /bin/sed 's/-//g'`"
+
+/usr/bin/find . -type f -exec sed -i -e "s/${domainspecifier}/ApplicationDomainSpec/g" -e "s/${WEBSITE_URL}/applicationdomainwww.tld/g" -e "s/${ROOT_DOMAIN}/applicationrootdomain.tld/g" -e "s/${WEBSITE_DISPLAY_NAME}/The GreatApplication/g" -e "s/${WEBSITE_DISPLAY_NAME_UPPER}/THE GREATAPPLICATION/g" -e "s/${WEBSITE_DISPLAY_NAME}/GreatApplication/g" -e "s/${WEBSITE_DISPLAY_NAME_UPPER}/GREATAPPLICATION/g" -e "s/${WEBSITE_DISPLAY_NAME_LOWER}/greatapplication/g" -e "s/${WEBSITE_DISPLAY_NAME_FIRST}/Greatapplication/g" {} \;
+
 
 
