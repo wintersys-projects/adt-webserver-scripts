346c6e9539e29d98bfc23f644399be706a3542fc
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-03-29 09:38:14 +0100

    Update ApplyApplicationBranding.sh

--- application/branding/ApplyApplicationBranding.sh
+++ application/branding/ApplyApplicationBranding.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 ###########################################################################################################
-# Description: This is the generic script through which application specfic branding is routed. 
-# You will need to provide an application specific directory for any application you want to support
-# as per the examples given.
-# Author : Peter Winter
-# Date: 17/05/2017
-######################################################################################################
+# Description: When you make a baseline of an application, deployment specific values are replaced with generic
+# placeholder values throughout the files in your webroot. This is "removing the application branding". When you
+# deploy from the baseline again the generic placeholders are replaced with deployment specific values again and 
+# that is what this script does. This makes it possible for the same baseline to be deployed with different
+# application branding
+# Author: Peter Winter
+# Date : 09/12/2017
+########################################################################################################### 
 # License Agreement:
 # This file is part of The Agile Deployment Toolkit.
 # The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
@@ -20,15 +22,19 @@
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 #######################################################################################################
 #######################################################################################################
+#set -x
 
 HOME="`/bin/cat /home/homedir.dat`"
+WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
+ROOT_DOMAIN="`/bin/echo ${WEBSITE_URL} | /usr/bin/awk -F'.' '{$1=""}1' | /bin/sed 's/^ //g' | /bin/sed 's/ /./g'`"
+WEBSITE_NAME="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEDISPLAYNAME' | /bin/sed 's/_/ /g'`"
+WEBSITE_NAME_UPPER="`/bin/echo ${WEBSITE_NAME} | /usr/bin/tr '[:lower:]' '[:upper:]'`"
+WEBSITE_NAME_LOWER="`/bin/echo ${WEBSITE_NAME} | /usr/bin/tr '[:upper:]' '[:lower:]'`"
+WEBSITE_NAME_FIRST="`/bin/echo ${WEBSITE_NAME_LOWER} | /bin/sed -e 's/\b\(.\)/\u\1/g'`"
+domainspecifier="`/bin/echo ${WEBSITE_URL} | /usr/bin/awk -F'.' '{ for(i = 1; i <= NF; i++) { print $i; } }' | /usr/bin/cut -c1-3 | /usr/bin/tr '\n' '-' | /bin/sed 's/-//g'`"
 
-for applicationdir in `/bin/ls -d ${HOME}/application/branding/*/`
-do
-	applicationname="`/bin/echo ${applicationdir} | /bin/sed 's/\/$//' | /usr/bin/awk -F'/' '{print $NF}'`"
-	if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh APPLICATION:${applicationname}`" = "1" ] )
-	then
-		${applicationdir}ApplyApplicationBranding.sh
-	fi
-done
+/bin/echo "Applyiing  application branding. This may take a bit of time...."
+/usr/bin/find /var/www/html -type f -exec sed -i -e "s/ApplicationDomainSpec/${domainspecifier}/g" -e "s/applicationdomainwww.tld/${WEBSITE_URL}/g" -e "s/applicationrootdomain.tld/${ROOT_DOMAIN}/g" -e "s/The GreatApplication/${WEBSITE_NAME}/g" -e "s/THE GREATAPPLICATION/${WEBSITE_NAME_UPPER}/g" -e "s/GREATAPPLICATION/${WEBSITE_NAME_UPPER}/g" -e  "s/GreatApplication/${WEBSITE_NAME}/g" -e "s/greatapplication/${WEBSITE_NAME_LOWER}/g" -e "s/Greatapplication/${WEBSITE_NAME_FIRST}/g" {} \;
+
+
 
